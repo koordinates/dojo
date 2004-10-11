8bdcc7d54461fbd979a19b990004cfb7cbfc9004
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2004-10-11 05:08:24 +0000

    more style fixes
    git-svn-id: http://svn.dojotoolkit.org/src@35 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/bootstrap1.js
+++ src/bootstrap1.js
@@ -39,29 +39,28 @@
  */
 //:GLVAR Object djConfig
 var djConfig;
-if (typeof djConfig == 'undefined') djConfig = {};
-
+if(typeof djConfig == 'undefined'){ djConfig = {}; }
 
 /**
  * dojo is the root variable of (almost all) our public symbols.
  */
 //=java public class dojo {
 var dojo;
-if (typeof dojo == 'undefined') dojo = {};
+if(typeof dojo == 'undefined'){ dojo = {}; }
 
 //=java public static HostEnv hostenv;
 dojo.hostenv = {
-    is_debug_ : (typeof djConfig['isDebug'] == 'undefined' ? false : djConfig['isDebug']),
-    base_script_uri_ : (typeof djConfig['baseScriptUri'] == 'undefined' ? undefined : djConfig['baseScriptUri']),
-    base_relative_path_ : (typeof djConfig['baseRelativePath'] == 'undefined' ? '' : djConfig['baseRelativePath']),
-    library_script_uri_ : (typeof djConfig['libraryScriptUri'] == 'undefined' ? '' : djConfig['libraryScriptUri']),
+	is_debug_ : ((typeof djConfig['isDebug'] == 'undefined') ? false : djConfig['isDebug']),
+	base_script_uri_ : ((typeof djConfig['baseScriptUri'] == 'undefined') ? undefined : djConfig['baseScriptUri']),
+	base_relative_path_ : ((typeof djConfig['baseRelativePath'] == 'undefined') ? '' : djConfig['baseRelativePath']),
+	library_script_uri_ : ((typeof djConfig['libraryScriptUri'] == 'undefined') ? '' : djConfig['libraryScriptUri']),
 
-    // for recursion protection
-    loading_modules_ : {},
+	// for recursion protection
+	loading_modules_ : {},
 
-    // lookup cache for modules.
-    // NOTE: this is partially redundant a private variable in the jsdown implementation, but we don't want to couple the two.
-    modules_ : {}
+	// lookup cache for modules.
+	// NOTE: this is partially redundant a private variable in the jsdown implementation, but we don't want to couple the two.
+	modules_ : {}
 };
 
 //=java } /* dojo */
@@ -89,22 +88,26 @@ var dj_global = this; //typeof window == 'undefined' ? this : window;
 /*@if (@_jscript_version >= 7)
 function dj_debug(... args : Object[]) {
 @else @*/
-function dj_debug() {
-    var args = arguments;
+function dj_debug(){
+	var args = arguments;
 /*@end @*/
-    if (typeof dojo.hostenv.println != 'function') dj_throw("attempt to call dj_debug when there is no dojo.hostenv println implementation (yet?)");
-    if (!dojo.hostenv.is_debug_) return;
-    var s = 'DEBUG: ';
-    for(var i=0;i<args.length;++i) s += args[i];
-    dojo.hostenv.println(s);
+	if(typeof dojo.hostenv.println != 'function'){
+		dj_throw("attempt to call dj_debug when there is no dojo.hostenv println implementation (yet?)");
+	}
+	if(!dojo.hostenv.is_debug_){ return; }
+	var s = 'DEBUG: ';
+	for(var i=0;i<args.length;++i){ s += args[i]; }
+	dojo.hostenv.println(s);
 }
 
 /**
 * Throws an Error object given the string err. For now, will also do a println to the user first.
 */
-function dj_throw(message) {
-    if (typeof dojo.hostenv != 'undefined' && typeof dojo.hostenv.println != 'undefined') dojo.hostenv.println("fatal error: " + message);
-    throw Error(message);
+function dj_throw(message){
+	if((typeof dojo.hostenv != 'undefined')&&(typeof dojo.hostenv.println != 'undefined')){ 
+		dojo.hostenv.println("fatal error: " + message);
+	}
+	throw Error(message);
 }
 
 /*
@@ -112,8 +115,8 @@ function dj_throw(message) {
  * TODO: overriding Error.prototype.toString won't accomplish this?
  * ... since natively generated Error objects do not always reflect such things?
  */
-function dj_error_to_string(excep) {
-    return typeof excep.message !== 'undefined' ? excep.message : 
+function dj_error_to_string(excep){
+	return (typeof excep.message !== 'undefined' ? excep.message): 
 	(typeof excep.description !== 'undefined' ? excep.description : excep);
 }
 
@@ -121,9 +124,9 @@ function dj_error_to_string(excep) {
 /**
  * Rethrows the provided Error object excep, with the additional message given by message.
  */
-function dj_rethrow(message, excep) {
-    var emess = dj_error_to_string(excep);
-    dj_throw(message + ": " + emess);
+function dj_rethrow(message, excep){
+	var emess = dj_error_to_string(excep);
+	dj_throw(message + ": " + emess);
 }
 
 /**
@@ -134,28 +137,30 @@ function dj_rethrow(message, excep) {
  * - JSC eval() takes an optional second argument which can be 'unsafe'.
  * - Mozilla/SpiderMonkey eval() takes an optional second argument which is the scope object for new symbols.
 */
-function dj_eval(s) {return eval(s);}
+function dj_eval(s){ return eval(s); }
 
 
 /**
  * Convenience for throwing an exception because some function is not implemented.
  */
-function dj_unimplemented(funcname, extra) {
-    var mess = "No implementation of function '" + funcname + "'";
-    if (typeof extra != 'undefined' && extra) mess += " " + extra;
-    mess += " (host environment '" + dojo.hostenv.getName() + "')";
-    dj_throw(mess);
+function dj_unimplemented(funcname, extra){
+	var mess = "No implementation of function '" + funcname + "'";
+	if((typeof extra != 'undefined')&&(extra)){ mess += " " + extra; }
+	mess += " (host environment '" + dojo.hostenv.getName() + "')";
+	dj_throw(mess);
 }
 
 /**
  * Does inheritance
  */
-function dj_inherits(subclass, superclass) {
- if (typeof superclass != 'function') { dj_throw("eek: superclass not a function: " + superclass +"\nsubclass is: " + subclass);}
-  subclass.prototype = new superclass();
-  subclass.prototype.constructor = subclass;
-  // TODO: subclass.super = superclass gives JScript error?
-  subclass['super'] = superclass;
+function dj_inherits(subclass, superclass){
+	if(typeof superclass != 'function'){ 
+		dj_throw("eek: superclass not a function: " + superclass + "\nsubclass is: " + subclass);
+	}
+	subclass.prototype = new superclass();
+	subclass.prototype.constructor = subclass;
+	// TODO: subclass.super = superclass gives JScript error?
+	subclass['super'] = superclass;
 }
 
 
@@ -180,13 +185,13 @@ dojo.hostenv.version_ = '(unset)';
  * Return the name of the hostenv.
  */
 //=java public abstract String getName() {}
-dojo.hostenv.getName = function() {return this.name_;}
+dojo.hostenv.getName = function(){ return this.name_; }
 
 /**
 * Return the version of the hostenv.
 */
 //=java public abstract String getVersion() {}
-dojo.hostenv.getVersion = function() {return this.version_;}
+dojo.hostenv.getVersion = function(){ return this.version_; }
 
 /**
  * Display a line of text to the user.
@@ -201,8 +206,8 @@ dojo.hostenv.getVersion = function() {return this.version_;}
  * with an implementation that doesn't rely on it.
  */
 //=java protected abstract String getText(String uri);
-dojo.hostenv.getText = function(uri) {
-    dj_unimplemented('dojo.hostenv.getText', "uri=" + uri);
+dojo.hostenv.getText = function(uri){
+	dj_unimplemented('dojo.hostenv.getText', "uri=" + uri);
 }
 
 /**
@@ -210,8 +215,9 @@ dojo.hostenv.getText = function(uri) {
  * private method that must be implemented by the hostenv.
  */
 //=java protected abstract String getLibraryScriptUri();
-dojo.hostenv.getLibraryScriptUri = function() {
-    dj_unimplemented('dojo.hostenv.getLibraryScriptUri','');
+dojo.hostenv.getLibraryScriptUri = function(){
+	// FIXME: need to implement!!!
+	dj_unimplemented('dojo.hostenv.getLibraryScriptUri','');
 }
 
 // ****************************************************************
@@ -223,16 +229,20 @@ dojo.hostenv.getLibraryScriptUri = function() {
  * It is either the empty string, or a non-empty string ending in '/'.
  */
 //=java public String getBaseScriptUri();
-dojo.hostenv.getBaseScriptUri = function() {
-    if (typeof this.base_script_uri_ != 'undefined') return this.base_script_uri_;
-    var uri = this.library_script_uri_;
-    if (!uri) {
-	uri = this.library_script_uri_ = this.getLibraryScriptUri();
-	if (!uri) dj_throw("Nothing returned by getLibraryScriptUri(): " + uri);
-    }
-    var lastslash = uri.lastIndexOf('/');
-    var base = this.base_relative_path_ + (lastslash == -1 ? '' : uri.substring(0,lastslash + 1)); // inclusive of slash
-    return this.base_script_uri_ = base;
+dojo.hostenv.getBaseScriptUri = function(){
+	if(typeof this.base_script_uri_ != 'undefined'){ return this.base_script_uri_; }
+	var uri = this.library_script_uri_;
+	if(!uri){
+		uri = this.library_script_uri_ = this.getLibraryScriptUri();
+		if(!uri){
+			dj_throw("Nothing returned by getLibraryScriptUri(): " + uri);
+		}
+	}
+
+	var lastslash = uri.lastIndexOf('/');
+	// inclusive of slash
+	var base = this.base_relative_path_ + (lastslash == -1 ? '' : uri.substring(0,lastslash + 1));
+	return this.base_script_uri_ = base;
 }
 
 /**
@@ -240,7 +250,7 @@ dojo.hostenv.getBaseScriptUri = function() {
 */
 //=java public void setBaseScriptUri(String uri);
 // In JScript .NET, see interface System._AppDomain implemented by System.AppDomain.CurrentDomain. Members include AppendPrivatePath, RelativeSearchPath, BaseDirectory.
-dojo.hostenv.setBaseScriptUri = function(uri) {this.base_script_uri_ = uri};
+dojo.hostenv.setBaseScriptUri = function(uri){ this.base_script_uri_ = uri }
 
 /**
  * Loads and interprets the script located at relpath, which is relative to the script root directory.
@@ -254,14 +264,18 @@ dojo.hostenv.setBaseScriptUri = function(uri) {this.base_script_uri_ = uri};
  * @param relpath A relative path to a script (no leading '/', and typically ending in '.js').
  */
 //=java public Boolean loadPath(String relpath);
-dojo.hostenv.loadPath = function(relpath) {
-    if (!relpath) dj_throw("Missing relpath argument");
-    if (relpath.charAt(0) == '/' || relpath.match(/^\w+:/)) dj_throw("Illegal argument '" + relpath + "'; must be relative path");
-    var base = this.getBaseScriptUri();
-    var uri = base + relpath;
-    //this.println("base=" + base + " relpath=" + relpath);
-    var ok = this.loadUri(uri);
-    return ok;
+dojo.hostenv.loadPath = function(relpath){
+	if(!relpath){
+		dj_throw("Missing relpath argument");
+	}
+	if((relpath.charAt(0) == '/')||(relpath.match(/^\w+:/))){
+		dj_throw("Illegal argument '" + relpath + "'; must be relative path");
+	}
+	var base = this.getBaseScriptUri();
+	var uri = base + relpath;
+	//this.println("base=" + base + " relpath=" + relpath);
+	var ok = this.loadUri(uri);
+	return ok;
 }
 
 /**
@@ -269,14 +283,12 @@ dojo.hostenv.loadPath = function(relpath) {
  * Returns true if it succeeded. Returns false if the URI reading failed. Throws if the evaluation throws.
  * The result of the eval is not available to the caller.
  */
-dojo.hostenv.loadUri = function(uri) {
-    var contents = this.getText(uri, null, true);
-    if (contents == null) return 0;
-    var value = dj_eval(contents);
-    return 1;
+dojo.hostenv.loadUri = function(uri){
+	var contents = this.getText(uri, null, true);
+	if(contents == null){ return 0; }
+	var value = dj_eval(contents);
+	return 1;
 }
-
-
 
 /**
 * loadModule("A.B") first checks to see if symbol A.B is defined. 
@@ -301,62 +313,70 @@ dojo.hostenv.loadUri = function(uri) {
 *
 * dj_load is an alias for dojo.hostenv.loadModule
 */
-dojo.hostenv.loadModule = function(modulename, exact_only) {
-    var module = this.findModule(modulename, 0);
-    if (module) {
-      return module;
-    }
-
-    // protect against infinite recursion from mutual dependencies
-    if (typeof this.loading_modules_[modulename] !== 'undefined') dj_throw("recursive attempt to load module '" + modulename + "'");
-    this.loading_modules_[modulename] = 1;
-
-    // convert periods to slashes
-    var relpath = modulename.replace(/\./g, '/') + '.js';
- 
-    var ok = this.loadPath(relpath);
-    if (!ok && !exact_only) {
-	var syms = modulename.split(/\./);
-	syms.pop();
-	while(syms.length) {
-	    relpath = syms.join('/') + '.js';
-	    ok = this.loadPath(relpath);
-	    if (ok) break;
-	    syms.pop();
+dojo.hostenv.loadModule = function(modulename, exact_only){
+	var module = this.findModule(modulename, 0);
+	if(module){
+		return module;
 	}
-    }
 
-    if (!ok) {
-	dj_throw("Could not find module '" + modulename + "'; last tried path '" + relpath + "'");
-    }
+	// protect against infinite recursion from mutual dependencies
+	if (typeof this.loading_modules_[modulename] !== 'undefined'){
+		dj_throw("recursive attempt to load module '" + modulename + "'");
+	}
+	this.loading_modules_[modulename] = 1;
 
-    // check that the symbol was defined
-    module = this.findModule(modulename, false); // pass in false so we can give better error
-    if (!module) dj_throw("Module symbol '" + modulename + "' is not defined after loading '" + relpath + "'"); 
+	// convert periods to slashes
+	var relpath = modulename.replace(/\./g, '/') + '.js';
 
-    return module;
+	var ok = this.loadPath(relpath);
+	if((!ok)&&(!exact_only)){
+		var syms = modulename.split(/\./);
+		syms.pop();
+		while(syms.length){
+			relpath = syms.join('/') + '.js';
+			ok = this.loadPath(relpath);
+			if(ok){ break; }
+			syms.pop();
+		}
+	}
+
+	if(!ok){
+		dj_throw("Could not find module '" + modulename + "'; last tried path '" + relpath + "'");
+	}
+
+	// check that the symbol was defined
+	module = this.findModule(modulename, false); // pass in false so we can give better error
+	if(!module){
+		dj_throw("Module symbol '" + modulename + "' is not defined after loading '" + relpath + "'"); 
+	}
+
+	return module;
 }
 
-function dj_load(modulename, exact_only) {return dojo.hostenv.loadModule(modulename, exact_only);}
+function dj_load(modulename, exact_only){
+	return dojo.hostenv.loadModule(modulename, exact_only); 
+}
 
 
 /*
  * private utility to  evaluate a string like "A.B" without using eval.
  */
-function dj_eval_object_path(objpath) {
-    // fast path for no periods
-    if (objpath.indexOf('.') == -1) {
-	dj_debug("typeof this[",objpath,"]=",typeof(this[objpath]), " and typeof dj_global[]=", typeof(dj_global[objpath])); 
-	return typeof dj_global[objpath] == 'undefined' ? undefined : dj_global[objpath];
-    }
+function dj_eval_object_path(objpath){
+	// fast path for no periods
+	if(objpath.indexOf('.') == -1){
+		dj_debug("typeof this[",objpath,"]=",typeof(this[objpath]), " and typeof dj_global[]=", typeof(dj_global[objpath])); 
+		return (typeof dj_global[objpath] == 'undefined') ? undefined : dj_global[objpath];
+	}
 
-    var syms = objpath.split(/\./);
-    var obj = dj_global;
-    for(var i=0;i<syms.length;++i) {
-	obj = obj[syms[i]];
-	if (typeof obj == 'undefined' || !obj) return obj;
-    }
-    return obj;
+	var syms = objpath.split(/\./);
+	var obj = dj_global;
+	for(var i=0;i<syms.length;++i){
+		obj = obj[syms[i]];
+		if((typeof obj == 'undefined')||(!obj)){
+			return obj;
+		}
+	}
+	return obj;
 }
 
 /**
@@ -364,16 +384,16 @@ function dj_eval_object_path(objpath) {
 * or uses what already exists. It returns the result.
 * dj_package(packname) is an alias for dojo.hostenv.startPackage(packname).
 */
-function dj_package(packname) {
-    var syms = packname.split(/\./);
-    var obj = dj_global;
-    for(var i=0;i<syms.length;++i) {
-	var childobj = obj[syms[i]];
-	if (typeof childobj == 'undefined' || !childobj) {
-	  obj = (obj[syms[i]] = {});
+function dj_package(packname){
+	var syms = packname.split(/\./);
+	var obj = dj_global;
+	for(var i=0;i<syms.length;++i){
+		var childobj = obj[syms[i]];
+		if((typeof childobj == 'undefined')||(!childobj)){
+			obj = (obj[syms[i]] = {});
+		}
 	}
-    }
-    return obj;
+	return obj;
 }
 
 dojo.hostenv.startPackage = dj_package;
@@ -386,15 +406,21 @@ dojo.hostenv.startPackage = dj_package;
  */
 //=java public Object findModule(String modulename, boolean must_exist);
 dojo.hostenv.findModule = function(modulename, must_exist) {
-    // check cache
-    if (typeof this.modules_[modulename] != 'undefined') return this.modules_[modulename];
+	// check cache
+	if(typeof this.modules_[modulename] != 'undefined'){
+		return this.modules_[modulename];
+	}
 
-    // see if symbol is defined anyway
-    var module = dj_eval_object_path(modulename);
-    if (typeof module !== 'undefined' && module) return this.modules_[modulename] = module;
+	// see if symbol is defined anyway
+	var module = dj_eval_object_path(modulename);
+	if((typeof module !== 'undefined')&&(module)){
+		return this.modules_[modulename] = module;
+	}
 
-    if (must_exist) dj_throw("no loaded module named '" + modulename + "'");
-    return null;
+	if(must_exist){
+		dj_throw("no loaded module named '" + modulename + "'");
+	}
+	return null;
 }
 
 //=java } /* class HostEnv */
