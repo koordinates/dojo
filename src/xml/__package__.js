986a6c49bb63f074d2b223f27972abeacbf1ef1b
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2004-12-02 22:49:04 +0000

    weird
    git-svn-id: http://svn.dojotoolkit.org/src@135 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/xml/__package__.js
+++ src/xml/__package__.js
@@ -1,2 +1,6 @@
 dojo.hostenv.loadModule("dojo.xml.Parse");
-dojo.hostenv.loadModule("dojo.xml.DomUtil");
+dojo.hostenv.loadModule("dojo.xml.domUtil");
+dojo.hostenv.conditionalLoadModule({
+    browser: 	["dojo.xml.htmlUtil"],
+    svg: 		["dojo.xml.svgUtil"]
+});
