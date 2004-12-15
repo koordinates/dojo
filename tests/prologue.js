e8f5e4d5a28e6a915c2f052a6429238f309e3834
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2004-12-15 02:27:36 +0000

    fixing prologue to allow loading to happen correctly. Need to investigate further why it seems to append "undefined" instead of "" when it's not there, though.
    git-svn-id: http://svn.dojotoolkit.org/src@165 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/prologue.js
+++ tests/prologue.js
@@ -1,20 +1,11 @@
-/*
 djConfig = { 
-	isDebug: true,
-	baseRelativePath: "../"
+	baseRelativePath: "../",
 };
 
-load([djConfig.baseRelativePath+"src/bootstrap1.js"]);
-// FIXME: need a better way to determine which hostenv to load here!!!
-load([djConfig.baseRelativePath+"src/hostenv_rhino.js"]);
-load([djConfig.baseRelativePath+"src/bootstrap2.js"]);
-*/
 load("/Users/alex/projects/dojo/src/bootstrap1.js");
+// FIXME: need a way to determine which hostenv to load here!!!
 load("/Users/alex/projects/dojo/src/hostenv_rhino.js");
 load("/Users/alex/projects/dojo/src/bootstrap2.js");
-// FIXME: need a better way to determine which hostenv to load here!!!
-// load("/Users/alex/projects/dojo/src/hostenv_rhino.js");
-// load("/Users/alex/projects/dojo/src/bootstrap2.js");
 
 // compat fixes for BUFakeDom.js and the JUM implementation:
 var bu_alert = (typeof this.alert != 'undefined') ? this.alert : (this.load && this.print ? this.print : function() {});
