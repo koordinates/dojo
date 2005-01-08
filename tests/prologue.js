9013ebad44ca727d2d3b040f35f0d0bbb7863a8a
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-01-08 01:01:30 +0000

    Fixed Alex's lame-ass path issues.
    git-svn-id: http://svn.dojotoolkit.org/src@193 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/prologue.js
+++ tests/prologue.js
@@ -3,10 +3,10 @@ djConfig = {
 	isDebug: true
 };
 
-load("/Users/alex/projects/dojo/src/bootstrap1.js");
+load("../src/bootstrap1.js");
 // FIXME: need a way to determine which hostenv to load here!!!
-load("/Users/alex/projects/dojo/src/hostenv_rhino.js");
-load("/Users/alex/projects/dojo/src/bootstrap2.js");
+load("../src/hostenv_rhino.js");
+load("../src/bootstrap2.js");
 
 // compat fixes for BUFakeDom.js and the JUM implementation:
 var bu_alert = (typeof this.alert != 'undefined') ? this.alert : (this.load && this.print ? this.print : function() {});
