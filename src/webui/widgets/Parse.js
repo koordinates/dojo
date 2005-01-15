f47d5e1ddc10efcf68b8a985b509c32fe095c7e8
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-01-15 01:09:15 +0000

    cleanup of parsing code... not exhaustively tested, but seems to 'work' ok with test_button.html
    git-svn-id: http://svn.dojotoolkit.org/src@221 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/webui/widgets/Parse.js
+++ src/webui/widgets/Parse.js
@@ -1,123 +1,122 @@
 dojo.hostenv.startPackage("dojo.webui.widgets.parse");
 
-dojo.webui.widgets.parse.ParseFragment = function(fragment) {
+dojo.webui.widgets.parse = function(fragment) {
 	this.propertySetsList = [];
 	this.fragment = fragment;
-}
+	/*	createComponents recurses over a raw JavaScript object structure,
+			and calls the corresponding handler for its normalized tagName if it exists
+	*/
 
-/*	createComponents recurses over a raw JavaScript object structure,
-		and calls the corresponding handler for its normalized tagName if it exists
-*/
-
-dojo.webui.widgets.parse.ParseFragment.prototype.createComponents = function(fragment) {
-	for(var item in fragment){
-		if(	(fragment[item].tagName)&&(fragment[item] != fragment.nodeRef)&&
-			(dojo.webui.widgets.tags[fragment[item].tagName.toLowerCase()]) ){
-			dojo.webui.widgets.tags[fragment[item].tagName.toLowerCase()](fragment[item], this);
-		}
-		if((typeof fragment[item] == "object")&&(fragment[item] != fragment.nodeRef)&&(fragment[item] != fragment.tagName)){
-			this.createComponents(fragment[item]);
-		}
-	}
-}
-
-/*  parsePropertySets checks the top level of a raw JavaScript object
-		structure for any propertySets.  It stores an array of references to 
-		propertySets that it finds.
-*/
-
-dojo.webui.widgets.parse.ParseFragment.prototype.parsePropertySets = function(fragment) {
-	this.propertySets = [];
-	for(var item in fragment){
-		if(	(fragment[item].tagName == "dojo:propertyset") ) {
-			propertySets.push(fragment[item]);
-		}
-	}
-	// FIXME: should we store these propertySets somewhere for later retrieval
-	this.propertySetsList.push(propertySets);
-	return propertySets;
-}
-
-/*  parseProperties checks a raw JavaScript object structure for
-		properties, and returns an array of properties that it finds.
-*/
-
-
-dojo.webui.widgets.parse.ParseFragment.prototype.parseProperties = function(fragment) {
-	var properties = {};
-	for (var item in fragment) {
-		// case: its a tagName or nodeRef
-		if((fragment[item] == fragment.tagName) || (fragment[item] != fragment.nodeRef)) {
-			// do nothing
-		} else if(fragment[item]) {
-			if(fragment[item].tagName && dojo.webui.widgets.tags[fragment[item].tagName.toLowerCase()]) {
-				// TODO: it isn't a property or property set, it's a fragment, so do something else
-				// TODO: handle xlink:href for external property sets
+	this.createComponents = function(fragment) {
+		for(var item in fragment){
+			if(	(fragment[item].tagName)&&(fragment[item] != fragment.nodeRef)&&
+				(dojo.webui.widgets.tags[fragment[item].tagName.toLowerCase()]) ){
+				dojo.webui.widgets.tags[fragment[item].tagName.toLowerCase()](fragment[item], this);
 			}
-			if(fragment[item][0] && fragment[item][0].value) {
-					properties[fragment[item][0]] = fragment[item][0].value;
+			if((typeof fragment[item] == "object")&&(fragment[item] != fragment.nodeRef)&&(fragment[item] != fragment.tagName)){
+				this.createComponents(fragment[item]);
 			}
-			if(typeof fragment[item] == "object") {
-					// check for nested properties
-					this.parseProperties(fragment[item]);
-			}			
 		}
 	}
-	return properties;
-}
 
-/* getPropertySetById returns the propertySet that matches the provided id
-*/
-
-dojo.webui.widgets.parse.ParseFragment.prototype.getPropertySetById = function(propertySetId){
-	for (propertySet in this.propertySetsList) {
-		if(propertySetId == this.propertySetsList[propertySet]["id"][0].value) {
-			return this.propertySetsList[propertySet]
+	/*  parsePropertySets checks the top level of a raw JavaScript object
+			structure for any propertySets.  It stores an array of references to 
+			propertySets that it finds.
+	*/
+	
+	this.parsePropertySets = function(fragment) {
+		this.propertySets = [];
+		for(var item in fragment){
+			if(	(fragment[item].tagName == "dojo:propertyset") ) {
+				propertySets.push(fragment[item]);
+			}
 		}
+		// FIXME: should we store these propertySets somewhere for later retrieval
+		this.propertySetsList.push(propertySets);
+		return propertySets;
 	}
-	return "";
-}
-
-/* getPropertySetsByClass returns the propertySet(s) that match(es) the provided componentClass
-*/
-
-dojo.webui.widgets.parse.ParseFragment.prototype.getPropertySetsByClass = function(propertySetClass){
-	var propertySets = [];
-	for (propertySet in this.propertySetsList) {
-		if(this.propertySetsList[propertySet]["componentClass"] && (propertySetId == this.propertySetsList[propertySet]["componentClass"][0].value)) {
-			propertySets.push(this.propertySetsList[propertySet]);
-		}
-	}
-	return propertySets;
-}
-
-/* getPropertySets returns the propertySet for a given component fragment
-*/
-
-
-dojo.webui.widgets.parse.ParseFragment.prototype.getPropertySets = function(fragment) {
-	if(fragment["dojo:propertyproviderlist"]) { 
-		var propertyProviderIds = fragment["dojo:propertyproviderlist"].value.split(" ") || fragment["dojo:propertyproviderlist"].value;
-		var propertySetClass = fragment.tagName;
-		var propertySets = [];
-		// FIXME: should the propertyProviderList attribute contain # syntax for reference to ids or not?
-		// FIXME: need a better test to see if this is local or external
-		// FIXME: doesn't handle nested propertySets, or propertySets that just contain information about css documents, etc.
-		for (propertySetId in propertyProviderIds) {
-			if(propertySetId.indexOf("..")==-1 && propertySetId.indexOf("://")==-1) {
-				// get a reference to a propertySet within the current parsed structure
-				var propertySet = this.getPropertySetById(propertySetId);
-				if(propertySet != "") {
-					propertySets.push(propertySet);
+	
+	/*  parseProperties checks a raw JavaScript object structure for
+			properties, and returns an array of properties that it finds.
+	*/
+	
+	
+	this.parseProperties = function(fragment) {
+		var properties = {};
+		for (var item in fragment) {
+			// case: its a tagName or nodeRef
+			if((fragment[item] == fragment.tagName) || (fragment[item] != fragment.nodeRef)) {
+				// do nothing
+			} else if(fragment[item]) {
+				if(fragment[item].tagName && dojo.webui.widgets.tags[fragment[item].tagName.toLowerCase()]) {
+					// TODO: it isn't a property or property set, it's a fragment, so do something else
+					// TODO: handle xlink:href for external property sets
 				}
-			} else {
-				// FIXME: add code to parse and return a propertySet from another document
+				if(fragment[item][0] && fragment[item][0].value) {
+						properties[fragment[item][0]] = fragment[item][0].value;
+				}
+				if(typeof fragment[item] == "object") {
+						// check for nested properties
+						this.parseProperties(fragment[item]);
+				}			
 			}
 		}
-		propertySets.push(this.getPropertySetsByClass(propertySetClass));
-		return propertySets
-	} else {
+		return properties;
+	}
+
+	/* getPropertySetById returns the propertySet that matches the provided id
+	*/
+	
+	this.getPropertySetById = function(propertySetId){
+		for (propertySet in this.propertySetsList) {
+			if(propertySetId == this.propertySetsList[propertySet]["id"][0].value) {
+				return this.propertySetsList[propertySet]
+			}
+		}
 		return "";
+	}
+	
+	/* getPropertySetsByClass returns the propertySet(s) that match(es) the provided componentClass
+	*/
+	
+	this.getPropertySetsByClass = function(propertySetClass){
+		var propertySets = [];
+		for (propertySet in this.propertySetsList) {
+			if(this.propertySetsList[propertySet]["componentClass"] && (propertySetId == this.propertySetsList[propertySet]["componentClass"][0].value)) {
+				propertySets.push(this.propertySetsList[propertySet]);
+			}
+		}
+		return propertySets;
+	}
+	
+	/* getPropertySets returns the propertySet for a given component fragment
+	*/
+	
+	
+	this.getPropertySets = function(fragment) {
+		if(fragment["dojo:propertyproviderlist"]) { 
+			var propertyProviderIds = fragment["dojo:propertyproviderlist"].value.split(" ") || fragment["dojo:propertyproviderlist"].value;
+			var propertySetClass = fragment.tagName;
+			var propertySets = [];
+			// FIXME: should the propertyProviderList attribute contain # syntax for reference to ids or not?
+			// FIXME: need a better test to see if this is local or external
+			// FIXME: doesn't handle nested propertySets, or propertySets that just contain information about css documents, etc.
+			for (propertySetId in propertyProviderIds) {
+				if(propertySetId.indexOf("..")==-1 && propertySetId.indexOf("://")==-1) {
+					// get a reference to a propertySet within the current parsed structure
+					var propertySet = this.getPropertySetById(propertySetId);
+					if(propertySet != "") {
+						propertySets.push(propertySet);
+					}
+				} else {
+					// FIXME: add code to parse and return a propertySet from another document
+				}
+			}
+			propertySets.push(this.getPropertySetsByClass(propertySetClass));
+			return propertySets
+		} else {
+			return "";
+		}
 	}
 }
 
--- src/xml/Parse.js
+++ src/xml/Parse.js
@@ -1,4 +1,4 @@
-dojo.hostenv.startPackage("dojo.xml.Parse");
+dojo.hostenv.startPackage("dojo.xml.parse");
 
 dojo.hostenv.loadModule("dojo.xml.domUtil");
 
@@ -23,119 +23,114 @@ dojo.???.foo.baz.xyzzy = {}
 dojo.???.foo.baz.xyzzy.value = "xyzzy"
 
 */
-
- // TODO: resolve call stack for all of this within namespaced context
-
- 
 // using documentFragment nomenclature to generalize in case we don't want to require passing a collection of nodes with a single parent
-dojo.xml.Parse.ParseDocumentFragment = function(){}
-//	this.domUtil = new dojo.xml.DomUtil();
-
-dojo.xml.Parse.ParseDocumentFragment.prototype.parseFragment = function(documentFragment) {
-	// handle parent element
-	var parsedFragment = {};
-	// var tagName = dojo.xml.domUtil.getTagName(node);
-	var tagName = dojo.xml.domUtil.getTagName(documentFragment);
-	// TODO: What if document fragment is just text... need to check for nodeType perhaps?
-	parsedFragment[tagName] = new Array(documentFragment.tagName);
-	var attributeSet = this.parseAttributes(documentFragment);
-	for(var attr in attributeSet){
-		if(!parsedFragment[attr]){
-			parsedFragment[attr] = [];
+dojo.xml.parse = function(){
+	this.parseFragment = function(documentFragment) {
+		// handle parent element
+		var parsedFragment = {};
+		// var tagName = dojo.xml.domUtil.getTagName(node);
+		var tagName = dojo.xml.domUtil.getTagName(documentFragment);
+		// TODO: What if document fragment is just text... need to check for nodeType perhaps?
+		parsedFragment[tagName] = new Array(documentFragment.tagName);
+		var attributeSet = this.parseAttributes(documentFragment);
+		for(var attr in attributeSet){
+			if(!parsedFragment[attr]){
+				parsedFragment[attr] = [];
+			}
+			parsedFragment[attr][parsedFragment[attr].length] = attributeSet[attr];
 		}
-		parsedFragment[attr][parsedFragment[attr].length] = attributeSet[attr];
-	}
-
-	for (var childNode in documentFragment.childNodes){
-		switch(documentFragment.childNodes[childNode].nodeType){
-			case  dojo.xml.domUtil.nodeTypes.ELEMENT_NODE: // element nodes, call this function recursively
-				parsedFragment[tagName].push(this.parseElement(documentFragment.childNodes[childNode]));
-				break;
-			case  dojo.xml.domUtil.nodeTypes.TEXT_NODE: // if a single text node is the child, treat it as an attribute
-				if(documentFragment.childNodes.length == 1){
-					if(!parsedFragment[documentFragment.tagName]){
-						parsedFragment[tagName] = [];
+	
+		for (var childNode in documentFragment.childNodes){
+			switch(documentFragment.childNodes[childNode].nodeType){
+				case  dojo.xml.domUtil.nodeTypes.ELEMENT_NODE: // element nodes, call this function recursively
+					parsedFragment[tagName].push(this.parseElement(documentFragment.childNodes[childNode]));
+					break;
+				case  dojo.xml.domUtil.nodeTypes.TEXT_NODE: // if a single text node is the child, treat it as an attribute
+					if(documentFragment.childNodes.length == 1){
+						if(!parsedFragment[documentFragment.tagName]){
+							parsedFragment[tagName] = [];
+						}
+						parsedFragment[tagName].push({ value: documentFragment.childNodes[0].nodeValue });
 					}
-					parsedFragment[tagName].push({ value: documentFragment.childNodes[0].nodeValue });
-				}
-				break;
+					break;
+			}
 		}
+		
+		return parsedFragment;
 	}
+
+	this.parseElement = function(node,hasParentNodeSet){
+		// TODO: make this namespace aware
+		var parsedNodeSet = {};
+		var tagName = dojo.xml.domUtil.getTagName(node);
+		parsedNodeSet[tagName] = [];
+		var attributeSet = this.parseAttributes(node);
+		for(var attr in attributeSet){
+			if(!parsedNodeSet[tagName][attr]){
+				parsedNodeSet[tagName][attr] = [];
+			}
+			parsedNodeSet[tagName][attr].push(attributeSet[attr]);
+		}
 	
-	return parsedFragment;
-}
-
-dojo.xml.Parse.ParseDocumentFragment.prototype.parseElement = function(node,hasParentNodeSet){
-	// TODO: make this namespace aware
-	var parsedNodeSet = {};
-	var tagName = dojo.xml.domUtil.getTagName(node);
-	parsedNodeSet[tagName] = [];
-	var attributeSet = this.parseAttributes(node);
-	for(var attr in attributeSet){
-		if(!parsedNodeSet[tagName][attr]){
-			parsedNodeSet[tagName][attr] = [];
-		}
-		parsedNodeSet[tagName][attr].push(attributeSet[attr]);
-	}
-
-	// FIXME: we might want to make this optional or provide cloning instead of
-	// referencing, but for now, we include a node reference to allow
-	// instantiated components to figure out their "roots"
-	parsedNodeSet[tagName].nodeRef = node;
-	parsedNodeSet.tagName = tagName;
-
-	var ntypes = dojo.xml.domUtil.nodeTypes;
-
-	for(var i=0; i<node.childNodes.length; i++){
-		switch(node.childNodes[i].nodeType){
-			case  ntypes.ELEMENT_NODE: // element nodes, call this function recursively
- 				parsedNodeSet[tagName].push(this.parseElement(node.childNodes[i],true));
-				break;
-			case  ntypes.ATTRIBUTE_NODE: // attribute node... not meaningful here
-				break;
-			case  ntypes.TEXT_NODE: // if a single text node is the child, treat it as an attribute
-				if(node.childNodes.length == 1) {
-					parsedNodeSet[tagName].push({ value: node.childNodes[0].nodeValue });
-				}
-				break;
-			case  ntypes.CDATA_SECTION_NODE: // cdata section... not sure if this would ever be meaningful... might be...
-				break;
-			case  ntypes.ENTITY_REFERENCE_NODE: // entity reference node... not meaningful here
-				break;
-			case  ntypes.ENTITY_NODE: // entity node... not sure if this would ever be meaningful
-				break;
-			case  ntypes.PROCESSING_INSTRUCTION_NODE: // processing instruction node... not meaningful here
-				break;
-			case  ntypes.COMMENT_NODE: // comment node... not not sure if this would ever be meaningful 
-				break;
-			case  ntypes.DOCUMENT_NODE: // document node... not sure if this would ever be meaningful
-				break;
-			case  ntypes.DOCUMENT_TYPE_NODE: // document type node... not meaningful here
-				break;
-			case  ntypes.DOCUMENT_FRAGMENT_NODE: // document fragment node... not meaningful here
-				break;
-			case  ntypes.NOTATION_NODE:// notation node... not meaningful here
-				break;
-		}
-	}
-	//return (hasParentNodeSet) ? parsedNodeSet[node.tagName] : parsedNodeSet;
-	return parsedNodeSet;
-}
-
-/* parses a set of attributes on a node into an object tree */
-dojo.xml.Parse.ParseDocumentFragment.prototype.parseAttributes = function(node) {
-	// TODO: make this namespace aware
-	var parsedAttributeSet = {};
-	// TODO: should we allow for duplicate attributes at this point... would any of the relevant dom implementations even allow this?
-	/*for(var i=0; i<node.attributes.length; i++) {
-		if(!parsedAttributeSet[node.attributes[i].nodeName]) {
-			parsedAttributeSet[node.attributes[i].nodeName] = [];
-		}
-		parsedAttributeSet[node.attributes[i].nodeName][parsedAttributeSet[node.attributes[i].nodeName].length] = { value: node.attributes[i].nodeValue };
-	}*/
+		// FIXME: we might want to make this optional or provide cloning instead of
+		// referencing, but for now, we include a node reference to allow
+		// instantiated components to figure out their "roots"
+		parsedNodeSet[tagName].nodeRef = node;
+		parsedNodeSet.tagName = tagName;
 	
-	for(var i=0; i<node.attributes.length; i++) {
-		parsedAttributeSet[node.attributes[i].nodeName] = { value: node.attributes[i].nodeValue };
+		var ntypes = dojo.xml.domUtil.nodeTypes;
+	
+		for(var i=0; i<node.childNodes.length; i++){
+			switch(node.childNodes[i].nodeType){
+				case  ntypes.ELEMENT_NODE: // element nodes, call this function recursively
+					parsedNodeSet[tagName].push(this.parseElement(node.childNodes[i],true));
+					break;
+				case  ntypes.ATTRIBUTE_NODE: // attribute node... not meaningful here
+					break;
+				case  ntypes.TEXT_NODE: // if a single text node is the child, treat it as an attribute
+					if(node.childNodes.length == 1) {
+						parsedNodeSet[tagName].push({ value: node.childNodes[0].nodeValue });
+					}
+					break;
+				case  ntypes.CDATA_SECTION_NODE: // cdata section... not sure if this would ever be meaningful... might be...
+					break;
+				case  ntypes.ENTITY_REFERENCE_NODE: // entity reference node... not meaningful here
+					break;
+				case  ntypes.ENTITY_NODE: // entity node... not sure if this would ever be meaningful
+					break;
+				case  ntypes.PROCESSING_INSTRUCTION_NODE: // processing instruction node... not meaningful here
+					break;
+				case  ntypes.COMMENT_NODE: // comment node... not not sure if this would ever be meaningful 
+					break;
+				case  ntypes.DOCUMENT_NODE: // document node... not sure if this would ever be meaningful
+					break;
+				case  ntypes.DOCUMENT_TYPE_NODE: // document type node... not meaningful here
+					break;
+				case  ntypes.DOCUMENT_FRAGMENT_NODE: // document fragment node... not meaningful here
+					break;
+				case  ntypes.NOTATION_NODE:// notation node... not meaningful here
+					break;
+			}
+		}
+		//return (hasParentNodeSet) ? parsedNodeSet[node.tagName] : parsedNodeSet;
+		return parsedNodeSet;
 	}
-	return parsedAttributeSet;
+
+	/* parses a set of attributes on a node into an object tree */
+	this.parseAttributes = function(node) {
+		// TODO: make this namespace aware
+		var parsedAttributeSet = {};
+		// TODO: should we allow for duplicate attributes at this point... would any of the relevant dom implementations even allow this?
+		/*for(var i=0; i<node.attributes.length; i++) {
+			if(!parsedAttributeSet[node.attributes[i].nodeName]) {
+				parsedAttributeSet[node.attributes[i].nodeName] = [];
+			}
+			parsedAttributeSet[node.attributes[i].nodeName][parsedAttributeSet[node.attributes[i].nodeName].length] = { value: node.attributes[i].nodeValue };
+		}*/
+		
+		for(var i=0; i<node.attributes.length; i++) {
+			parsedAttributeSet[node.attributes[i].nodeName] = { value: node.attributes[i].nodeValue };
+		}
+		return parsedAttributeSet;
+	}
 }
