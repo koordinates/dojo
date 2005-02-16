2c20cdfb9eeb58ec077148fe39e8fbc849326217
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-02-16 04:01:56 +0000

    adding connect-once test
    git-svn-id: http://svn.dojotoolkit.org/src@295 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/event/test_Event.js
+++ tests/event/test_Event.js
@@ -1,10 +1,12 @@
 dojo.hostenv.loadModule("dojo.event.Event");
 
 function testObjectClass(){
+	this.funcCallCount = 0;
 	this.lastReturn =  null;
 	this.secondLastReturn = null;
 
 	this.func1 = function(arg1, arg2){
+		this.funcCallCount++;
 		this.secondLastReturn = this.lastReturn;
 		this.lastReturn = "func1, arg1: "+arg1+", arg2: "+arg2;
 		jum.debug(this.lastReturn);
@@ -12,6 +14,7 @@ function testObjectClass(){
 	}
 
 	this.func2 = function(arg1, arg2){
+		this.funcCallCount++;
 		this.secondLastReturn = this.lastReturn;
 		this.lastReturn = "func2, arg1: "+arg1+", arg2: "+arg2;
 		jum.debug(this.lastReturn);
@@ -80,3 +83,40 @@ function test_event_around(){
 	jum.assertEquals("test14", obj1.lastReturn, "func1, arg1: 2, arg2: 1");
 	jum.assertEquals("test15", obj1.secondLastReturn, null);
 }
+
+function test_event_kwConnect(){
+	var obj1 = new testObjectClass();
+
+	// test to see if "after" gets set as the default type
+	dojo.event.kwConnect({
+		srcObj: obj1, 
+		srcFunc: "func1", 
+		adviceObj: obj1, 
+		adviceFunc: "func2"
+	});
+
+	jum.assertTrue("test16", obj1.func1("1", "2")=="func1, arg1: 1, arg2: 2");
+	jum.assertEquals("test17", obj1.secondLastReturn, "func1, arg1: 1, arg2: 2");
+	jum.assertEquals("test18", obj1.lastReturn, "func2, arg1: 1, arg2: 2");
+}
+
+function test_event_connectOnce(){
+	var obj1 = new testObjectClass();
+
+	// connect once via kwConnect()
+	dojo.event.kwConnect({
+		once: true,
+		type: "after",
+		srcObj: obj1, 
+		srcFunc: "func1", 
+		adviceObj: obj1, 
+		adviceFunc: "func2"
+	});
+
+	// and then through connect()
+	dojo.event.connect("after", obj1, "func1", obj1, "func2", null, null, true);
+
+	jum.assertTrue("test19", obj1.func1("1", "2")=="func1, arg1: 1, arg2: 2");
+	jum.assertEquals("test20", obj1.funcCallCount, 2);
+}
+
