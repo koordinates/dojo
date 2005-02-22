cc174833c41e9ff2df8ed745fc0c8d13b5e9ca7d
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-02-22 05:39:40 +0000

    first shot at an XMLHTTP transit. Still needs work and testing. POST doesn't work right yet.
    git-svn-id: http://svn.dojotoolkit.org/src@319 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/io/BrowserIO.js
+++ src/io/BrowserIO.js
@@ -1,1 +1,155 @@
 dojo.hostenv.startPackage("dojo.io.BrowserIO");
+
+dojo.io.formHasFile = function(formNode){
+	function checkChildrenForFile(node){
+		var hasTrue = false;
+		for(var x=0; x<node.childNodes.length; x++){
+			if(node.nodeType==1){
+				if(node.nodeName.toLowerCase() == "input"){
+					if(node.getAttribute("type")=="file"){
+						return true;
+					}
+				}
+				if(node.childNodes.length){
+					if(checkChildrenForFile(node)){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	return checkChildrenForFile(formNode);
+}
+
+dojo.io.buildFormGetString = function(sNode){
+	//the argument is a DOM Node corresponding to a form element.
+	var tvar = "";
+	var ctyp = sNode.nodeName ? sNode.nodeName.toLowerCase() : "";
+	var etyp = sNode.type ? sNode.type.toLowerCase() : "";
+	if(( (ctyp=="input") && (etyp!="radio") && (etyp!="checkbox")) || (ctyp=="select") || (ctyp=="textarea")){
+		tvar = sNode.getAttribute("name") + "=" + sNode.value  + "&";
+	}else if(ctyp=="input"){
+		if(sNode.checked){
+			tvar = sNode.getAttribute("name") + "=" + sNode.value  + "&";
+		}
+	}
+	if(sNode.hasChildNodes()){
+		for(var temp_count=(sNode.childNodes.length-1); temp_count >= 0; temp_count=temp_count-1){
+			tvar += dojo.io.buildFormGetString(sNode.childNodes.item(temp_count));
+		}
+	}
+	return tvar;
+}
+
+dojo.io.XMLHTTPTransport= new function(){
+	this.canHandle = function(kwArgs){
+		// canHandle just tells dojo.io.bind() if this is a good transport to
+		// use for the particular type of request.
+		if(	
+			(
+				(kwArgs["mimetype"] == "text/plain") ||
+				(kwArgs["mimetype"] == "text/html") ||
+				(kwArgs["mimetype"] == "text/javascript")
+			)&&(
+				(kwArgs["method"] == "get") ||
+				( 
+					(kwArgs["method"] == "post") && 
+					( (!kwArgs["formNode"])||(this.formHasFile(kwArgs["formNode"])) ) 
+				) 
+			)
+		){
+			return true;
+		}
+
+		return false;
+	}
+
+	this.bind = function(kwArgs){
+		var hdlrObj = {};
+
+		// set up a handler object
+		for(var x=0; x<dojo.io.hdlrFuncNames.length; x++){
+			var fn = dojo.io.hdlrFuncNames[x];
+			if(typeof kwArgs.handler == "object"){
+				if(typeof kwArgs.handler[fn] == "function"){
+					hdlrObj[fn] = kwArgs.handler[fn]||kwArgs.handler["handle"];
+				}
+			}else if(typeof kwArgs.handler == "function"){
+				hdlrObj[fn] = kwArgs.handler;
+			}
+		}
+
+		// much of this is from getText, but reproduced here because we need
+		// more flexibility
+		var http = null;
+		var last_e = null;
+		var received = false;
+		try{ 
+			http = new XMLHttpRequest(); 
+		}catch(e){}
+		if(!http){
+		for(var i=0; i<3; ++i){
+			var progid = DJ_XMLHTTP_PROGIDS[i];
+			try{
+				http = new ActiveXObject(progid);
+			}catch(e){
+				last_e = e;
+			}
+
+			if(http){
+				if(DJ_XMLHTTP_PROGIDS.length != 1){
+					DJ_XMLHTTP_PROGIDS = [progid];  // optimize for next time
+				}
+				break;
+			}
+		}
+
+		if((last_e)&&(!http)){
+			dj_rethrow("Could not create a new ActiveXObject using any of the progids " + DJ_XMLHTTP_PROGIDS.join(', '), last_e);
+		}else if(!http){
+			return dj_throw("No XMLHTTP implementation available");
+		}
+
+
+		// build a handler function that calls back to the handler obj
+		http.onreadystatechange = function(){
+			if((4==http.readyState)&&(http["status"])){
+				if(received){ return; } // Opera 7.6 is foo-bar'd
+				received = true;
+				if(http.status==200){
+					hdlrObj.load("load", http.responseText, http);
+				}else{
+					var errObj = new dojo.io.Error("sampleTransport Error: "+evt.msg);
+					hdlrObj.error("error", errObj);
+				}
+			}
+		}
+
+		var url = kwArgs.url+"?";
+		if(kwArgs["formNode"]){
+			// FIXME: need to fix this for POST!!
+			url += this.buildFormGetString(kwArgs.formNode);
+		}
+
+		if(kwArgs["content"]){
+			url += "?"+this.argsFromMap(kwArgs.content);
+		}
+
+		/*
+		if(kwArgs.method == "post"){
+			// FIXME: !!!!
+			// http.open("POST", uri, true);
+			// http.send(postContent);
+		}else{
+		}
+		*/
+		http.open("GET", url, true);
+		http.send(null);
+		return;
+	}
+
+	dojo.io.transports.add("XMLHTTPTranport");
+}
+
