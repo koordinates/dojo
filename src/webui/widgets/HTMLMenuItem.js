3341a520fac97d30320311fb779cfd264600ccaf
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-03-28 23:08:39 +0000

    add missing context menu files
    git-svn-id: http://svn.dojotoolkit.org/src@422 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/webui/widgets/HTMLMenuItem.js
@@ -0,0 +1,43 @@
+dojo.hostenv.startPackage("dojo.webui.widgets.HTMLMenuItem");
+
+dojo.hostenv.loadModule("dojo.webui.widgets.MenuItem");
+
+dojo.webui.widgets.HTMLMenuItem = function(){
+	// if DOMButton turns into a mixin, we should subclass Button instead and
+	// just mix in the DOMButton properties.
+	dojo.webui.widgets.DomMenuItem.call(this);
+	dojo.webui.HTMLWidget.call(this);
+
+	this.templatePath = "src/webui/widgets/templates/HTMLMenuItemTemplate.html";
+	this.templateCSSPath = "src/webui/widgets/templates/HTMLMenuItemTemplate.css";
+
+	this.label = "huzzah!";
+	this.labelNode = null;
+
+	this.setLabel = function(){
+		this.labelNode.innerHTML = this.label;
+		// this.domNode.label = this.label;
+	}
+
+	this.fillInTemplate = function(){
+		this.setLabel();
+	}
+
+	this.onHide = function(){
+		// FIXME: use whatever we use to do more general style setting?
+		this.domNode.style.display = "none";
+	}
+	
+	this.onFoo = function(){}
+}
+
+/*
+new function(){ // namespace protection closure
+	var hbp = dojo.webui.widgets.HTMLMenuItem.prototype;
+	hbp.templateString = ["<button />"].join("");
+}; // FIXME: why isnt the (function(){})(); syntax working here??
+*/
+
+dj_inherits(dojo.webui.widgets.HTMLMenuItem, dojo.webui.widgets.DomMenuItem);
+
+// dojo.webui.widgets.HTMLMenuItem.prototype.templateString = "<button class='dojoButton' dojoAttachEvent='onClick; onMouseMove: onFoo;' dojoAttachPoint='labelNode'></button>";
---
+++ src/webui/widgets/MenuItem.js
@@ -0,0 +1,24 @@
+dojo.hostenv.startPackage("dojo.webui.widgets.MenuItem");
+
+dojo.hostenv.loadModule("dojo.webui.DomWidget");
+
+dojo.webui.widgets.MenuItem = function(){
+	dojo.webui.Widget.call(this);
+	this.widgetType = "MenuItem";
+	
+	this.isContainer = false;
+}
+dj_inherits(dojo.webui.widgets.MenuItem, dojo.webui.Widget);
+
+
+// FIXME: own file? Mixin instead?
+dojo.webui.widgets.DomMenuItem = function(){
+	dojo.webui.widgets.MenuItem.call(this);
+//	dojo.webui.widgets.Menu.call(this);
+	dojo.webui.DomWidget.call(this, true);
+}
+dj_inherits(dojo.webui.widgets.DomMenuItem, dojo.webui.widgets.MenuItem);
+
+dojo.webui.widgets.tags["dojo:menuitem"] = function(fragment, widgetParser){
+	dojo.webui.widgets.buildWidgetFromParseTree("dojo:menuitem", fragment, widgetParser);
+}
