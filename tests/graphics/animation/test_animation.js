2c11324151c7e8ab7042f96e44632b5cee45aba6
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-04-27 20:44:20 +0000

    created simple animation test
    git-svn-id: http://svn.dojotoolkit.org/src@500 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/graphics/animation/test_animation.js
+++
@@ -1,9 +0,0 @@
-dojo.hostenv.loadModule("dojo.graphics.animation.Animation");
-
-
-function test_animation_packagefile(){
-	jum.assertEquals("test1", typeof dojo.graphics, "object");
-	jum.assertEquals("test2", typeof dojo.graphics.animation, "object");
-	jum.assertEquals("test3", typeof dojo.graphics.animation.Animation, "function");
-	jum.assertEquals("test5", typeof dojo.graphics.animation.AnimationEvent, "function");
-}
