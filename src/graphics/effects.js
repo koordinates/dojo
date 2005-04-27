c7591bb71ea89a817596895b6122cc8a21a3f2af
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-04-27 22:43:32 +0000

    adding stub for namespace unaware effects
    git-svn-id: http://svn.dojotoolkit.org/src@507 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/graphics/effects.js
@@ -0,0 +1,17 @@
+dojo.hostenv.startPackage("dojo.graphics.effects");
+
+dojo.hostenv.loadModule("dojo.animation.*");
+
+dojo.graphics.effects = new function() {
+	this.fadeOut = function(node, duration, callback) {
+		dj_unimplemented('dojo.graphics.effects.fadeOut');
+	}
+
+	this.fadeIn = function(node, duration, callback) {
+		dj_unimplemented('dojo.graphics.effects.fadeIn');
+	}
+
+	this.fade = function(node, duration, startOpac, endOpac, callback) {
+		dj_unimplemented('dojo.graphics.effects.fade');
+	}
+}
