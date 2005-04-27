7dc452d00d59f25feb57fa80496caa4e85f11a53
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-04-27 19:41:37 +0000

    tests should now pass
    git-svn-id: http://svn.dojotoolkit.org/src@497 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/graphics/animation/test_animation.js
+++ tests/graphics/animation/test_animation.js
@@ -2,8 +2,8 @@ dojo.hostenv.loadModule("dojo.graphics.animation.Animation");
 
 
 function test_animation_packagefile(){
-	jum.assertTrue("test1", typeof dojo.graphics=="object");
-	jum.assertTrue("test1", typeof dojo.graphics.animation=="object");
-	jum.assertTrue("test1", typeof dojo.graphics.animation.Animation=="object");
-	jum.assertTrue("test1", typeof dojo.graphics.animation.AnimationEvent=="object");
+	jum.assertEquals("test1", typeof dojo.graphics, "object");
+	jum.assertEquals("test2", typeof dojo.graphics.animation, "object");
+	jum.assertEquals("test3", typeof dojo.graphics.animation.Animation, "function");
+	jum.assertEquals("test5", typeof dojo.graphics.animation.AnimationEvent, "function");
 }
