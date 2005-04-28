f5bd5324f6dc64033312da9beb1b98697812527a
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-04-28 16:33:51 +0000

    updating w/ better tests for the search feature. still need to test searches other that startstring
    git-svn-id: http://svn.dojotoolkit.org/src@523 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/webui/widgets/test_ComboBox.js
+++ tests/webui/widgets/test_ComboBox.js
@@ -1,5 +1,69 @@
 dojo.hostenv.loadModule("dojo.webui.widgets.ComboBox");
 
+var comboData = [
+	["Alabama","AL"],
+	["Alaska","AK"],
+	["American Samoa","AS"],
+	["Arizona","AZ"],
+	["Arkansas","AR"],
+	["Armed Forces Europe","AE"],
+	["Armed Forces Pacific","AP"],
+	["Armed Forces the Americas","AA"],
+	["California","CA"],
+	["Colorado","CO"],
+	["Connecticut","CT"],
+	["Delaware","DE"],
+	["District of Columbia","DC"],
+	["Federated States of Micronesia","FM"],
+	["Florida","FL"],
+	["Georgia","GA"],
+	["Guam","GU"],
+	["Hawaii","HI"],
+	["Idaho","ID"],
+	["Illinois","IL"],
+	["Indiana","IN"],
+	["Iowa","IA"],
+	["Kansas","KS"],
+	["Kentucky","KY"],
+	["Louisiana","LA"],
+	["Maine","ME"],
+	["Marshall Islands","MH"],
+	["Maryland","MD"],
+	["Massachusetts","MA"],
+	["Michigan","MI"],
+	["Minnesota","MN"],
+	["Mississippi","MS"],
+	["Missouri","MO"],
+	["Montana","MT"],
+	["Nebraska","NE"],
+	["Nevada","NV"],
+	["New Hampshire","NH"],
+	["New Jersey","NJ"],
+	["New Mexico","NM"],
+	["New York","NY"],
+	["North Carolina","NC"],
+	["North Dakota","ND"],
+	["Northern Mariana Islands","MP"],
+	["Ohio","OH"],
+	["Oklahoma","OK"],
+	["Oregon","OR"],
+	["Pennsylvania","PA"],
+	["Puerto Rico","PR"],
+	["Rhode Island","RI"],
+	["South Carolina","SC"],
+	["South Dakota","SD"],
+	["Tennessee","TN"],
+	["Texas","TX"],
+	["Utah","UT"],
+	["Vermont","VT"],
+	["Virgin Islands, U.S.","VI"],
+	["Virginia","VA"],
+	["Washington","WA"],
+	["West Virginia","WV"],
+	["Wisconsin","WI"],
+	["Wyoming","WY"]
+];
+
 function test_combobox_ctor(){
 	// jum.debug(dojo.webui.widgets.ComboBox);
 	// jum.debug(typeof dojo.webui.widgets.ComboBox);
@@ -16,87 +80,29 @@ function test_combobox_dataprovider(){
 
 	jum.assertEquals("test30", typeof dojo.webui.widgets.ComboBoxDataProvider, "function");
 
-	/*
-	for(var x in dojo.webui.widgets){
-		print(x);
-	}
-	print(dojo.webui.widgets.ComboBoxDataProvider);
-	*/
 	var provider = new dojo.webui.widgets.ComboBoxDataProvider();
-	print(provider);
-	provider.setData([
-		["Alabama","AL"],
-		["Alaska","AK"],
-		["American Samoa","AS"],
-		["Arizona","AZ"],
-		["Arkansas","AR"],
-		["Armed Forces Europe","AE"],
-		["Armed Forces Pacific","AP"],
-		["Armed Forces the Americas","AA"],
-		["California","CA"],
-		["Colorado","CO"],
-		["Connecticut","CT"],
-		["Delaware","DE"],
-		["District of Columbia","DC"],
-		["Federated States of Micronesia","FM"],
-		["Florida","FL"],
-		["Georgia","GA"],
-		["Guam","GU"],
-		["Hawaii","HI"],
-		["Idaho","ID"],
-		["Illinois","IL"],
-		["Indiana","IN"],
-		["Iowa","IA"],
-		["Kansas","KS"],
-		["Kentucky","KY"],
-		["Louisiana","LA"],
-		["Maine","ME"],
-		["Marshall Islands","MH"],
-		["Maryland","MD"],
-		["Massachusetts","MA"],
-		["Michigan","MI"],
-		["Minnesota","MN"],
-		["Mississippi","MS"],
-		["Missouri","MO"],
-		["Montana","MT"],
-		["Nebraska","NE"],
-		["Nevada","NV"],
-		["New Hampshire","NH"],
-		["New Jersey","NJ"],
-		["New Mexico","NM"],
-		["New York","NY"],
-		["North Carolina","NC"],
-		["North Dakota","ND"],
-		["Northern Mariana Islands","MP"],
-		["Ohio","OH"],
-		["Oklahoma","OK"],
-		["Oregon","OR"],
-		["Pennsylvania","PA"],
-		["Puerto Rico","PR"],
-		["Rhode Island","RI"],
-		["South Carolina","SC"],
-		["South Dakota","SD"],
-		["Tennessee","TN"],
-		["Texas","TX"],
-		["Utah","UT"],
-		["Vermont","VT"],
-		["Virgin Islands, U.S.","VI"],
-		["Virginia","VA"],
-		["Washington","WA"],
-		["West Virginia","WV"],
-		["Wisconsin","WI"],
-		["Wyoming","WY"]
-	]);
+	provider.setData(comboData);
 
 	// test the results of our search
 	var searchTester = function(data){
-		jum.debug(data.length);
+		var expectedReturns = [
+			["Washington","WA"],
+			["West Virginia","WV"],
+			["Wisconsin","WI"],
+			["Wyoming","WY"]
+		];
+
+		var expectedLabels = [];
+		for(var x=0; x<expectedReturns.length; x++){
+			expectedLabels.push(expectedReturns[x][0]);
+		}
+		jum.assertEquals("test40", data.length, 4);
 		for(var x=0; x<data.length; x++){
-			jum.debug(data[x][0]);
+			// jum.debug(data[x][0]);
+			jum.assertTrue("testfoo", dojo.alg.inArr(expectedLabels, data[x][0]));
 		}
 	}
 
-	print(searchTester);
 	dojo.event.connect(provider, "provideSearchResults", searchTester);
 	provider.startSearch("W");
 }
