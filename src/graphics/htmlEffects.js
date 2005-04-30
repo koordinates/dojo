c1a81338e7729c4c85667a30b8ad8cb4561f0dbb
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-04-30 09:05:33 +0000

    added color fading functions (highlighting)
    git-svn-id: http://svn.dojotoolkit.org/src@536 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/graphics/htmlEffects.js
+++ src/graphics/htmlEffects.js
@@ -1,7 +1,7 @@
 dojo.hostenv.startPackage("dojo.graphics.htmlEffects");
 
 dojo.hostenv.loadModule("dojo.animation.*");
-dojo.hostenv.loadModule("dojo.xml.htmlUtil");
+dojo.hostenv.loadModule("dojo.xml.*");
 
 dojo.graphics.htmlEffects = new function() {
 	var _this = this;
@@ -57,5 +57,93 @@ dojo.graphics.htmlEffects = new function() {
 		}
 		anim.play(true);
 		return anim;
+	}
+
+	// Fade from startRGB to the node's background color
+	this.colorFadeIn = function(node, startRGB, duration, delay, callback) {
+		var color, nd = node, wasTransparent = false;
+		do {
+			color = dojo.xml.domUtil.getStyle(nd, "background-color");
+			if(nd == document.body) { nd = null; break; }
+			if(nd == node && color == "transparent") { wasTransparent = true; }
+			nd = nd.parentNode;
+		} while(nd && color == "transparent");
+
+		if( color == "transparent" ) {
+			color = [255, 255, 255];
+		} else {
+			color = dojo.xml.domUtil.extractRGB(color);
+		}
+
+		var anim = _this.colorFade(node, startRGB, color, duration, function() {
+			if( wasTransparent ) {
+				node.style.backgroundColor = "transparent";
+			}
+			if( typeof callback == "function" ) {
+				callback(node);
+			}
+		}, delay > 0);
+		anim.onAnimate({x:startRGB[0], y:startRGB[1], z:startRGB[2]});
+		if( delay > 0 ) {
+			setTimeout(function(){anim.play(true)}, delay);
+		}
+		return anim;
+	}
+
+	// alias for (probably?) common use/terminology
+	this.highlight = this.colorFadeIn;
+
+	// Fade from node's background color to endRGB
+	this.colorFadeOut = function(node, endRGB, duration, delay, callback) {
+		var color, nd = node;
+		do {
+			color = dojo.xml.domUtil.getStyle(nd, "background-color");
+			if(nd == document.body) { nd = null; break; }
+			nd = nd.parentNode;
+		} while(nd && color == "transparent");
+
+		if( color == "transparent" ) {
+			color = [255, 255, 255];
+		} else {
+			color = dojo.xml.domUtil.extractRGB(color);
+		}
+
+		var anim = _this.colorFade(node, color, endRGB, duration, callback, delay > 0);
+		anim.onAnimate({x:color[0], y:color[1], z:color[2]});
+		if( delay > 0 ) {
+			setTimeout(function(){anim.play(true)}, delay);
+		}
+		return anim;
+	}
+
+	// FIXME: not sure which name is better. an alias here may be bad.
+	this.colorFadeTo = this.colorFadeOut;
+
+	// Fade node background from startRGB to endRGB
+	this.colorFade = function(node, startRGB, endRGB, duration, callback, dontPlay) {
+		var anim = new dojo.animation.Animation(
+			new dojo.math.curves.Line(startRGB, endRGB),
+			duration, 0);
+		anim.onAnimate = function(e) {
+			var rgb = [Math.round(e.x), Math.round(e.y), Math.round(e.z)];
+			node.style.backgroundColor = "rgb(" + rgb.join(",") + ")";
+		}
+		if( typeof callback == "function" ) {
+			anim.onEnd = function(e) {
+				callback(node);
+			}
+		}
+		if( !dontPlay ) { anim.play(true); }
+		return anim;
+	}
+
+	this.zigTo = function(node, points, duration, callback) {
+		points.splice(0, 1, [node.offsetLeft, node.offsetTop]);
+		return _this.zig(node, points, duration, callback);
+	}
+
+	this.zig = function(node, points, duration, callback) {
+		// FIXME: waiting for math.curves.OpenPolygon
+		dj_unimplemented("dojo.graphics.htmlEffects.zig");
 	}
 }
