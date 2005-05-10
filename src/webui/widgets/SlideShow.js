d2cf788ee93d54948a636ee1a3b8229b19e1c471
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-10 18:42:28 +0000

    updates from the process of writing the article
    git-svn-id: http://svn.dojotoolkit.org/src@581 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/webui/widgets/SlideShow.js
+++ src/webui/widgets/SlideShow.js
@@ -16,6 +16,7 @@ dojo.webui.widgets.HTMLSlideShow = function(){
 	dojo.webui.DomWidget.call(this, true);
 	dojo.webui.HTMLWidget.call(this);
 	this.templatePath = "src/webui/widgets/templates/HTMLSlideShow.html";
+	this.templateCSSPath = "src/webui/widgets/templates/HTMLSlideShow.css";
 
 	this.widgetType = "SlideShow";
 	this.imgUrls = [];
@@ -28,7 +29,6 @@ dojo.webui.widgets.HTMLSlideShow = function(){
 	this.background = "img2";
 	this.foreground = "img1";
 	this.stopped = false;
-	this.zIndex = 0;
 
 	// our DOM nodes:
 	this.imagesContainer = null;
@@ -38,10 +38,25 @@ dojo.webui.widgets.HTMLSlideShow = function(){
 	this.img1 = null;
 	this.img2 = null;
 
+	this.fillInTemplate = function(){
+		dojo.xml.htmlUtil.setOpacity(this.img1, 0.9999);
+		dojo.xml.htmlUtil.setOpacity(this.img2, 0.9999);
+		with(this.imagesContainer.style){
+			width = this.imgWidth+"px";
+			height = this.imgHeight+"px";
+		}
+		if(this.imgUrls.length>1){
+			this.img2.src = this.imgUrls[this.urlsIdx++];
+			this.endTransition();
+		}else{
+			this.img1.src = this.imgUrls[this.urlsIdx++];
+		}
+	}
+
 	this.toggleStopped = function(){
 		if(this.stopped){
 			this.stopped = false;
-			this.fadeEnded();
+			this.endTransition();
 			this.startStopButton.value= "stop";
 		}else{
 			this.stopped = true;
@@ -64,12 +79,12 @@ dojo.webui.widgets.HTMLSlideShow = function(){
 		dojo.graphics.htmlEffects.fadeOut(this[this.foreground], 
 			this.transitionInterval, 
 			function(){ 
-				_this.fadeEnded(); 
+				_this.endTransition(); 
 			}
 		);
 	}
 
-	this.fadeEnded = function(){
+	this.endTransition = function(){
 		// move the foreground image to the background 
 		with(this[this.background].style){ zIndex = parseInt(zIndex)+1; }
 		with(this[this.foreground].style){ zIndex = parseInt(zIndex)-1; }
@@ -79,6 +94,10 @@ dojo.webui.widgets.HTMLSlideShow = function(){
 		this.foreground = this.background;
 		this.background = tmp;
 
+		this.loadNextImage();
+	}
+
+	this.loadNextImage = function(){
 		// load a new image in that container, and make sure it informs
 		// us when it finishes loading
 		dojo.event.kwConnect({
@@ -94,22 +113,6 @@ dojo.webui.widgets.HTMLSlideShow = function(){
 			this.urlsIdx = 0;
 		}
 	}
-
-	this.fillInTemplate = function(){
-		dojo.xml.htmlUtil.setOpacity(this.img1, 1.0);
-		dojo.xml.htmlUtil.setOpacity(this.img2, 1.0);
-		with(this.imagesContainer.style){
-			width = this.imgWidth+"px";
-			height = this.imgHeight+"px";
-		}
-		if(this.imgUrls.length>1){
-			this.img2.src = this.imgUrls[this.urlsIdx++];
-			this.fadeEnded();
-		}else{
-			this.img1.src = this.imgUrls[this.urlsIdx++];
-		}
-	}
-
 }
 dj_inherits(dojo.webui.widgets.HTMLSlideShow, dojo.webui.DomWidget);
 dojo.webui.widgets.tags.addParseTreeHandler("dojo:slideshow");
