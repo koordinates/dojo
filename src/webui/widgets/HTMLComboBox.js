5584c8db095f0a5b790887e591d2627cda2fad9d
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-10 16:17:28 +0000

    make the combo box actually show search results
    git-svn-id: http://svn.dojotoolkit.org/src@579 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/webui/widgets/HTMLComboBox.js
+++ src/webui/widgets/HTMLComboBox.js
@@ -1,6 +1,7 @@
 dojo.hostenv.startPackage("dojo.webui.widgets.HTMLComboBox");
 
 dojo.hostenv.loadModule("dojo.webui.widgets.ComboBox");
+dojo.hostenv.loadModule("dojo.io.*");
 
 dojo.webui.widgets.HTMLComboBox = function(){
 	dojo.webui.widgets.DomComboBox.call(this);
@@ -12,18 +13,77 @@ dojo.webui.widgets.HTMLComboBox = function(){
 	this.textInputNode = null;
 	this.optionsListNode = null;
 	this.downArrowNode = null;
+	this.searchTimer = null;
+	this.searchDelay = 100;
+	this.timeoutWrapperName = null;
+	this.dataUrl = "";
 
 	this,onKeyDown = function(evt){
 		dj_debug(evt);
 	}
 
 	this.onKeyUp = function(evt){
-		dj_debug(evt);
+		// esc is 27
+		if(evt.keyCode == 27){
+			this.closeResultList();
+			return;
+		}
+		if(this.searchTimer){
+			clearTimeout(this.searchTimer);
+		}
+		var _this = this;
+		if(!this.timeoutWrapperName){
+			this.timeoutWrapperName = dojo.event.nameAnonFunc(function(){
+				_this.startSearchFromInput();
+			}, dj_global);
+		}
+		this.searchTimer = setTimeout(this.timeoutWrapperName+"()", this.searchDelay);
 	}
 
 	this.fillInTemplate = function(){
 		// FIXME: add logic
+		if(this.dataUrl!=""){
+			var _this = this;
+			dojo.io.bind({
+				url: this.dataUrl,
+				load: function(type, data, evt){ 
+					if(type=="load"){
+						_this.dataProvider.setData(data);
+					}
+				},
+				mimetype: "text/javascript"
+			});
+		}
 	}
+
+	this.openResultList = function(results){
+		while(this.optionsListNode.firstChild){
+			this.optionsListNode.removeChild(this.optionsListNode.firstChild);
+		}
+		if(!results.length){
+			this.closeResultList();
+		}else{
+			this.optionsListNode.style.display = "";
+		}
+		while(results.length){
+			var tr = results.shift();
+			var td = document.createElement("div");
+			td.appendChild(document.createTextNode(tr[0]));
+			this.optionsListNode.appendChild(td);
+		}
+	}
+
+	this.closeResultList = function(){
+		this.optionsListNode.style.display = "none";
+		return;
+	}
+
+	this.startSearchFromInput = function(){
+		this.startSearch(this.textInputNode.value);
+	}
+
+	dojo.event.connect(this, "startSearch", this.dataProvider, "startSearch");
+	dojo.event.connect(this.dataProvider, "provideSearchResults", this, "openResultList");
 }
 
 dj_inherits(dojo.webui.widgets.HTMLComboBox, dojo.webui.widgets.DomComboBox);
