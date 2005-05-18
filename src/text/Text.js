06e55af964a1e5d3d5e8732ff34080bd24a47685
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-18 00:13:08 +0000

    added paramString function
    git-svn-id: http://svn.dojotoolkit.org/src@619 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/text/Text.js
+++ src/text/Text.js
@@ -10,6 +10,28 @@ dojo.text = new function(){
 		if(!iString.length){ return iString; }
 		return iString.replace(/^\s*/, "").replace(/\s*$/, "");
 	}
+
+	// Parameterized string function
+	//  str - formatted string with %{values} to be replaces
+	//  pairs - object of name: "value" value pairs
+	//  killExtra - remove all remaining %{values} after pairs are inserted
+	this.paramString = function(str, pairs, killExtra) {
+		if(typeof str != "string") { // allow String.prototype-ing
+			pairs = str;
+			str = this;
+		}
+
+		for(var name in pairs) {
+			var re = new RegExp("\\%\\{" + name + "\\}", "g");
+			str = str.replace(re, pairs[name]);
+		}
+
+		if(killExtra) {
+			str = str.replace(/%\{([^\}\s]+)\}/g, "");
+		}
+
+		return str;
+	}
 }
 
 dojo.text.Text = {}; // duh, alex.
