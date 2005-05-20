b800605b59a49c5c42759522868954b4cbc82904
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-20 19:40:58 +0000

    for pending changes to Widget
    git-svn-id: http://svn.dojotoolkit.org/src@650 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/lang/Lang.js
@@ -0,0 +1,18 @@
+dojo.hostenv.startPackage("dojo.lang.Lang");
+
+dojo.lang.mixin = function(obj, props){
+	var tobj = {};
+	for(var x in props){
+		if(typeof tobj[x] == "undefined"){
+			obj[x] = props[x];
+		}
+	}
+}
+
+dojo.lang.extend = function(ctor, props){
+	this.mixin(ctor.prototype, props);
+}
+
+dojo.lang.extendPrototype = function(obj, props){
+	this.extend(obj.constructor.prototype, props);
+}
