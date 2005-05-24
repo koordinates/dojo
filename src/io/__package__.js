8a745ff26094f8695de4b5d244cd9a42bcaac1ef
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-24 20:59:02 +0000

    added cookies to package
    git-svn-id: http://svn.dojotoolkit.org/src@677 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/io/__package__.js
+++ src/io/__package__.js
@@ -1,5 +1,5 @@
 dojo.hostenv.conditionalLoadModule({
 	common: ["dojo.io.IO", false, false],
 	rhino: ["dojo.io.RhinoIO", false, false],
-	browser: ["dojo.io.BrowserIO", false, false]
+	browser: [["dojo.io.BrowserIO", false, false], ["dojo.io.Cookies", false, false]]
 });
