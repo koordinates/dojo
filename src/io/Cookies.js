d9d832580c09efb58aa590c868baa2c148452e62
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-24 20:52:53 +0000

    polished cookie lib and added test case
    git-svn-id: http://svn.dojotoolkit.org/src@675 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/io/Cookies.js
+++ src/io/Cookies.js
@@ -1,63 +1,63 @@
 dojo.hostenv.startPackage("dojo.io.Cookies");
 
 dojo.io.cookies = new function() {
-	this.setCookie = function(name, value, days, path, noEscape) {
+	this.setCookie = function(name, value, days, path) {
 		var expires;
 		if(typeof days == "number") {
 			var d = new Date();
 			d.setTime(d.getTime()+(days*24*60*60*1000));
 			expires = d.toGMTString();
 		}
-		if(!noEscape) { value = escape(value); }
+		value = escape(value);
 		document.cookie = name + "=" + value + "; expires=" + expires + "; path=" + (path || "/");
 	}
 
-	this.getCookie = function(name, noEscape) {
-		var ck = document.cookie;
-		var idx = ck.indexOf(name+'=');
+	this.getCookie = function(name) {
+		var idx = document.cookie.indexOf(name+'=');
 		if(idx == -1) { return null; }
 		value = document.cookie.substring(idx+name.length+1);
 		var end = value.indexOf(';');
 		if(end == -1) { end = value.length; }
 		value = value.substring(0, end);
-		if(!noEscape) { value = unescape(value); }
+		value = unescape(value);
 		return value;
 	}
 
 	this.deleteCookie = function(name) {
-		this.setCookie(name, "-", "-1");
+		this.setCookie(name, "-", -1);
 	}
 
-	this.setObjectCookie = function(name, valuePairs, days, path, clearValues) {
+	this.setObjectCookie = function(name, obj, days, path, clearCurrent) {
 		var pairs = [], cookie, value = "";
-		if(!clearValues) { cookie = this.getLongCookie(name); }
+		if(!clearCurrent) { cookie = this.getObjectCookie(name); }
 		if(days >= 0) {
 			if(!cookie) { cookie = {}; }
-			for(var i = 0; i < valuePairs.length; i++) {
-				if(valuePairs[i][1] == null) {
-					delete cookie[ valuePairs[i][0] ];
-				} else {
-					cookie[ valuePairs[i][0] ] = escape(valuePairs[i][1]);
+			for(var prop in obj) {
+				if(prop == null) {
+					delete cookie[prop];
+				} else if(typeof obj[prop] == "string" || typeof obj[prop] == "number") {
+					cookie[prop] = obj[prop];
 				}
 			}
+			prop = null;
 			for(var prop in cookie) {
-				pairs.push(prop + "=" + cookie[prop]);
+				pairs.push(escape(prop) + "=" + escape(cookie[prop]));
 			}
 			value = pairs.join("&");
 		}
-		this.setCookie(name, value, days, path, true);
+		this.setCookie(name, value, days, path);
 	}
 
 	this.getObjectCookie = function(name) {
-		var values = null, cookie = this.getCookie(name, true);
+		var values = null, cookie = this.getCookie(name);
 		if(cookie) {
 			values = {};
-			var pairs = cookie.split("&"), set, value;
+			var pairs = cookie.split("&");
 			for(var i = 0; i < pairs.length; i++) {
-				set = pairs[i].split("=");
-				value = set[1];
-				if( isNaN(value) ) value = unescape(set[1]).replace("'", "\\'");
-				values[ set[0] ] = value;
+				var pair = pairs[i].split("=");
+				var value = pair[1];
+				if( isNaN(value) ) { value = unescape(pair[1]); }
+				values[ unescape(pair[0]) ] = value;
 			}
 		}
 		return values;
@@ -65,7 +65,7 @@ dojo.io.cookies = new function() {
 
 	this.isSupported = function() {
 		if(typeof navigator.cookieEnabled != "boolean") {
-			this.setCookie("__TestingYourBrowserForCookieSupport__", "CookiesAllowed", 90, null, null);
+			this.setCookie("__TestingYourBrowserForCookieSupport__", "CookiesAllowed", 90, null);
 			var cookieVal = this.getCookie("__TestingYourBrowserForCookieSupport__");
 			navigator.cookieEnabled = (cookieVal == "CookiesAllowed");
 			if(navigator.cookieEnabled) {
