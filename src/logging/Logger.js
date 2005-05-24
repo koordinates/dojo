4c90578e9776495ea1d41eeef372822792412306
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-24 06:26:19 +0000

    we are now minimally functional, although the calling syntax is still really shitty
    git-svn-id: http://svn.dojotoolkit.org/src@667 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/logging/Logger.js
+++ src/logging/Logger.js
@@ -35,10 +35,6 @@
 // TODO: write XML Formatter class
 // TODO: write HTTP Handler which uses POST to send log lines/sections
 
-// $Id: LogCore.js,v 1.7 2004/10/04 15:46:00 dylan Exp $
-// Copyright (c) 2000-2004 Alex Russell
-// Licensed under the Academic Free License version 1.2
-
 // Filename:	LogCore.js
 // Purpose:		a common logging infrastructure for dojo
 // Classes:		dojo.logging, dojo.logging.Logger, dojo.logging.Record, dojo.logging.LogFilter
@@ -46,6 +42,7 @@
 // Dependencies:	none
 
 dojo.hostenv.startPackage("dojo.logging.Logger");
+dojo.hostenv.loadModule("dojo.lang.*");
 
 /*
 	A simple data structure class that stores information for and about
@@ -81,173 +78,175 @@ dojo.logging.Logger = function(){
 	this.handlers = [];
 }
 
-dojo.logging.Logger.prototype.argsToArr = function(args){
-	// utility function, reproduced from __util__ here to remove dependency
-	var ret = [];
-	for(var x=0; x<args.length; x++){
-		ret.push(args[x]);
-	}
-	return ret;
-}
+dojo.lang.extend(dojo.logging.Logger, {
+	argsToArr: function(args){
+		// utility function, reproduced from __util__ here to remove dependency
+		var ret = [];
+		for(var x=0; x<args.length; x++){
+			ret.push(args[x]);
+		}
+		return ret;
+	},
 
-dojo.logging.Logger.prototype.setLevel = function(lvl){
-	this.cutOffLevel = parseInt(lvl);
-}
+	setLevel: function(lvl){
+		this.cutOffLevel = parseInt(lvl);
+	},
 
-dojo.logging.Logger.prototype.isEnabledFor = function(lvl){
-	return parseInt(lvl) >= this.cutOffLevel;
-}
+	isEnabledFor: function(lvl){
+		return parseInt(lvl) >= this.cutOffLevel;
+	},
 
-dojo.logging.Logger.prototype.getEffectiveLevel = function(){
-	if((this.cutOffLevel==0)&&(this.parent)){
-		return this.parent.getEffectiveLevel();
-	}
-	return this.cutOffLevel;
-}
+	getEffectiveLevel: function(){
+		if((this.cutOffLevel==0)&&(this.parent)){
+			return this.parent.getEffectiveLevel();
+		}
+		return this.cutOffLevel;
+	},
 
-dojo.logging.Logger.prototype.addFilter = function(flt){
-	this.filters.push(flt);
-	return this.filters.length-1;
-}
+	addFilter: function(flt){
+		this.filters.push(flt);
+		return this.filters.length-1;
+	},
 
-dojo.logging.Logger.prototype.removeFilterByIndex = function(fltIndex){
-	if(this.filters[fltIndex]){
-		delete this.filters[fltIndex];
-		return true;
-	}
-	return false;
-}
-
-dojo.logging.Logger.prototype.removeFilter = function(fltRef){
-	for(var x=0; x<this.filters.length; x++){
-		if(this.filters[x]===fltRef){
-			delete this.filters[x];
+	removeFilterByIndex: function(fltIndex){
+		if(this.filters[fltIndex]){
+			delete this.filters[fltIndex];
 			return true;
 		}
-	}
-	return false;
-}
+		return false;
+	},
 
-dojo.logging.Logger.prototype.removeAllFilters = function(){
-	this.filters = []; // clobber all of them
-}
+	removeFilter: function(fltRef){
+		for(var x=0; x<this.filters.length; x++){
+			if(this.filters[x]===fltRef){
+				delete this.filters[x];
+				return true;
+			}
+		}
+		return false;
+	},
 
-dojo.logging.Logger.prototype.filter = function(rec){
-	for(var x=0; x<this.filters.length; x++){
-		if((this.filters[x]["filter"])&&
-		   (!this.filters[x].filter(rec))||
-		   (rec.level<this.cutOffLevel)){
+	removeAllFilters: function(){
+		this.filters = []; // clobber all of them
+	},
+
+	filter: function(rec){
+		for(var x=0; x<this.filters.length; x++){
+			if((this.filters[x]["filter"])&&
+			   (!this.filters[x].filter(rec))||
+			   (rec.level<this.cutOffLevel)){
+				return false;
+			}
+		}
+		return true;
+	},
+
+	addHandler: function(hdlr){
+		this.handlers.push(hdlr);
+		return this.handlers.length-1;
+	},
+
+	handle: function(rec){
+		if((!this.filter(rec))||(rec.level<this.cutOffLevel)){ return false; }
+		for(var x=0; x<this.handlers.length; x++){
+			if(this.handlers[x]["handle"]){
+			   this.handlers[x].handle(rec);
+			}
+		}
+		// FIXME: not sure what to do about records to be propagated that may have
+		// been modified by the handlers or the filters at this logger. Should
+		// parents always have pristine copies? or is passing the modified record
+		// OK?
+		// if((this.propagate)&&(this.parent)){ this.parent.handle(rec); }
+		return true;
+	},
+
+	// the heart and soul of the logging system
+	log: function(lvl, msg){
+		if(	(this.propagate)&&(this.parent)&&
+			(this.parent.rec.level>=this.cutOffLevel)){
+			this.parent.log(lvl, msg);
 			return false;
 		}
-	}
-	return true;
-}
+		// FIXME: need to call logging providers here!
+		this.handle(new dojo.logging.Record(lvl, msg));
+		return true;
+	},
 
-dojo.logging.Logger.prototype.addHandler = function(hdlr){
-	this.handlers.push(hdlr);
-	return this.handlers.length-1;
-}
+	// logger helpers
+	debug:function(msg){
+		return this.logType("DEBUG", this.argsToArr(arguments));
+	},
 
-dojo.logging.Logger.prototype.handle = function(rec){
-	if((!this.filter(rec))||(rec.level<this.cutOffLevel)){ return false; }
-	for(var x=0; x<this.handlers.length; x++){
-		if(this.handlers[x]["handle"]){
-		   this.handlers[x].handle(rec);
+	info: function(msg){
+		return this.logType("INFO", this.argsToArr(arguments));
+	},
+
+	warning: function(msg){
+		return this.logType("WARNING", this.argsToArr(arguments));
+	},
+
+	warn: dojo.logging.Logger.prototype.warning,
+
+	error: function(msg){
+		return this.logType("ERROR", this.argsToArr(arguments));
+	},
+
+	err: dojo.logging.Logger.prototype.error,
+
+	critical: function(msg){
+		return this.logType("CRITICAL", this.argsToArr(arguments));
+	},
+
+	crit: dojo.logging.Logger.prototype.criticial,
+
+	exception: function(msg, e, squelch){
+		// FIXME: this needs to be modified to put the exception in the msg
+		// if we're on Moz, we can get the following from the exception object:
+		//		lineNumber
+		//		message
+		//		fileName
+		//		stack
+		//		name
+		// on IE, we get:
+		//		name
+		//		message (from MDA?)
+		//		number
+		//		description (same as message!)
+		if(e){
+			var eparts = [e.name, (e.description||e.message)];
+			if(e.fileName){
+				eparts.push(e.fileName);
+				eparts.push("line "+e.lineNumber);
+				// eparts.push(e.stack);
+			}
+			msg += " "+eparts.join(" : ");
 		}
-	}
-	// FIXME: not sure what to do about records to be propagated that may have
-	// been modified by the handlers or the filters at this logger. Should
-	// parents always have pristine copies? or is passing the modified record
-	// OK?
-	// if((this.propagate)&&(this.parent)){ this.parent.handle(rec); }
-	return true;
-}
 
-// the heart and soul of the logging system
-dojo.logging.Logger.prototype.log = function(lvl, msg){
-	if(	(this.propagate)&&(this.parent)&&
-		(this.parent.rec.level>=this.cutOffLevel)){
-		this.parent.log(lvl, msg);
-		return false;
-	}
-	// FIXME: need to call logging providers here!
-	this.handle(new dojo.logging.Record(lvl, msg));
-	return true;
-}
-
-// logger helpers
-dojo.logging.Logger.prototype.debug = function(msg){
-	return this.logType("DEBUG", this.argsToArr(arguments));
-}
-
-dojo.logging.Logger.prototype.info = function(msg){
-	return this.logType("INFO", this.argsToArr(arguments));
-}
-
-dojo.logging.Logger.prototype.warning = function(msg){
-	return this.logType("WARNING", this.argsToArr(arguments));
-}
-
-dojo.logging.Logger.prototype.warn = dojo.logging.Logger.prototype.warning;
-
-dojo.logging.Logger.prototype.error = function(msg){
-	return this.logType("ERROR", this.argsToArr(arguments));
-}
-
-dojo.logging.Logger.prototype.err = dojo.logging.Logger.prototype.error;
-
-dojo.logging.Logger.prototype.critical = function(msg){
-	return this.logType("CRITICAL", this.argsToArr(arguments));
-}
-
-dojo.logging.Logger.prototype.crit = dojo.logging.Logger.prototype.criticial;
-
-dojo.logging.Logger.prototype.exception = function(msg, e, squelch){
-	// FIXME: this needs to be modified to put the exception in the msg
-	// if we're on Moz, we can get the following from the exception object:
-	//		lineNumber
-	//		message
-	//		fileName
-	//		stack
-	//		name
-	// on IE, we get:
-	//		name
-	//		message (from MDA?)
-	//		number
-	//		description (same as message!)
-	if(e){
-		var eparts = [e.name, (e.description||e.message)];
-		if(e.fileName){
-			eparts.push(e.fileName);
-			eparts.push("line "+e.lineNumber);
-			// eparts.push(e.stack);
+		this.logType("ERROR", msg);
+		if(!squelch){
+			throw e;
 		}
-		msg += " "+eparts.join(" : ");
-	}
+	},
 
-	this.logType("ERROR", msg);
-	if(!squelch){
-		throw e;
+	logType: function(type, args){
+		var na = [dojo.logging.log.getLevel(type)];
+		if(typeof args == "array"){
+			na = na.concat(args);
+		}else if((typeof args == "object")&&(args["length"])){
+			na = na.concat(this.argsToArr(args));
+			/* for(var x=0; x<args.length; x++){
+				na.push(args[x]);
+			} */
+		}else{
+			na = na.concat(this.argsToArr(arguments).slice(1));
+			/* for(var x=1; x<arguments.length; x++){
+				na.push(arguments[x]);
+			} */
+		}
+		return this.log.apply(this, na);
 	}
-}
-
-dojo.logging.Logger.prototype.logType = function(type, args){
-	var na = [dojo.logging.getLevel(type)];
-	if(typeof args == "array"){
-		na = na.concat(args);
-	}else if((typeof args == "object")&&(args["length"])){
-		na = na.concat(this.argsToArr(args));
-		/* for(var x=0; x<args.length; x++){
-			na.push(args[x]);
-		} */
-	}else{
-		na = na.concat(this.argsToArr(arguments).slice(1));
-		/* for(var x=1; x<arguments.length; x++){
-			na.push(arguments[x]);
-		} */
-	}
-	return this.log.apply(this, na);
-}
+});
 
 // the Handler class
 dojo.logging.LogHandler = function(level){
@@ -287,7 +286,7 @@ dojo.logging.LogHandler.prototype.emit = function(record){
 }
 
 // set aliases since we don't want to inherit from dojo.logging.Logger
-(function(){ // begin globals protection closure
+dojo.logging.tmpFunc = function(){ // begin globals protection closure
 	var names = [
 		"setLevel", "addFilter", "removeFilterByIndex", "removeFilter",
 		"removeAllFilters", "filter"
@@ -297,7 +296,8 @@ dojo.logging.LogHandler.prototype.emit = function(record){
 	for(var x=0; x<names.length; x++){
 		tgt[names[x]] = src[names[x]];
 	}
-})(); // end globals protection closure
+}
+dojo.logging.tmpFunc(); // end globals protection closure
 
 dojo.logging.log = new dojo.logging.Logger();
 
@@ -334,6 +334,7 @@ dojo.logging.log.addLevelName = function(name, lvl){
 		return false;
 	}
 	this.levels.append({"name": name, "level": parseInt(lvl)});
+	return true;
 }
 
 dojo.logging.log.getLevel = function(name){
@@ -386,11 +387,12 @@ dojo.logging.logQueueHandler.emit = function(record){
 	// we should probably abstract this in the future
 	// also, what if a console is opened after some error messages pile up in
 	// the queue?  Do we dump them all to the queue?  Is this another pref?
-	if(dj_global["stdout"]){
+	if(dj_global["dj_debug"]){
 		dojo.logging.logQueueHandler.emit = function(record){
-			stdout(String(record.time.toLocaleTimeString())+" :"+dojo.logging.log.getLevelName(record.level)+": "+record.message);
+			dj_debug(String(record.time.toLocaleTimeString())+" :"+dojo.logging.log.getLevelName(record.level)+": "+record.message);
 		}
 	}
+	/*
 	switch(this.postType) {
 		case -1:
 			break;
@@ -404,6 +406,7 @@ dojo.logging.logQueueHandler.emit = function(record){
 			// need some sort of interval setter...
 			break;
 	}
+	*/
 
 	// it seems that we would't want to send a request to the server for every
 	// log file, so perhaps we want to send them in batches, or in time
