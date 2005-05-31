6cd552623c1f750cb7be6a033826a53b031ff661
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-05-31 15:50:53 +0000

    the boolean check to see if we shouldn't build the iframe for handling back button requests was b0rken. This should fix it.
    git-svn-id: http://svn.dojotoolkit.org/src@690 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/hostenv_browser.js
+++ src/hostenv_browser.js
@@ -240,6 +240,6 @@ dojo.hostenv.modulesLoadedListeners.push(function(){
 
 // we assume that we haven't hit onload yet. Lord help us.
 // document.write("<iframe style='border: 0px; width: 1px; height: 1px; position: absolute; bottom: 0px; right: 0px; visibility: visible;' name='djhistory' id='djhistory' src='"+((dojo.render.html.moz) ? 'about:blank' : (dojo.hostenv.base_relative_path_+'/blank.html'))+"'></iframe>");
-if((!window["djConfig"])&&(!window["djConfig"]["preventBackButtonFix"])){
+if((!window["djConfig"])||(!window["djConfig"]["preventBackButtonFix"])){
 	document.write("<iframe style='border: 0px; width: 1px; height: 1px; position: absolute; bottom: 0px; right: 0px; visibility: visible;' name='djhistory' id='djhistory' src='"+(dojo.hostenv.getBaseScriptUri()+'/blank.html')+"'></iframe>");
 }
