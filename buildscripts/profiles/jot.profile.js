442f30e7f688b2d52fe5949f2e396babb7bad840
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-06-14 21:02:00 +0000

    updated build profile
    git-svn-id: http://svn.dojotoolkit.org/src@734 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/profiles/jot.profile.js
+++ buildscripts/profiles/jot.profile.js
@@ -2,7 +2,9 @@ var dependencies = [
 	"dojo.io.IO",
 	"dojo.event.*",
 	"dojo.io.BrowserIO",
-	"dojo.webui.widgets.jot.*"
+	"dojo.webui.widgets.jot.*",
+	"dojo.webui.widgets.InlineEditBox",
+	"dojo.webui.widgets.DropdownButtons"
 ];
 
 load("getDependencyList.js");
