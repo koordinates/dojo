5c0280f4edc968ba155ea253791240cb459e3fee
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-06-20 18:22:44 +0000

    making Rhino detection a bit safer still. What hostenvs am I missing w/ the auto-detection?
    git-svn-id: http://svn.dojotoolkit.org/src@752 560b804f-0ae3-0310-86f3-f6aa0a117693

--- __package__.js
+++ __package__.js
@@ -1,5 +1,5 @@
 (function(){
-	var isRhino = (typeof this["load"] == "function");
+	var isRhino = ((typeof this["load"] == "function")&&(typeof this["Packages"] == "object"));
 	var tmps = ["bootstrap1.js", "hostenv_"+((isRhino) ? "rhino" : "browser")+".js", "bootstrap2.js"];
 	var root = ((this["djConfig"])&&(djConfig["baseScriptUri"])) ?  djConfig.baseScriptUri : ".";
 	for(var x in tmps){
