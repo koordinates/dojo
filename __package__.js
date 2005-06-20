2d8c0b23540e04de39cf73b743c1f743d9192a6d
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-06-20 18:20:37 +0000

    should now work for Rhino too
    git-svn-id: http://svn.dojotoolkit.org/src@751 560b804f-0ae3-0310-86f3-f6aa0a117693

--- __package__.js
+++ __package__.js
@@ -1,7 +1,13 @@
 (function(){
-	var tmps = ["bootstrap1.js", "hostenv_browser.js", "bootstrap2.js"];
-	var root = ((this["djConfig"])&&(djConfig["baseScriptUri"])) ?  djConfig.baseScriptUri : "";
+	var isRhino = (typeof this["load"] == "function");
+	var tmps = ["bootstrap1.js", "hostenv_"+((isRhino) ? "rhino" : "browser")+".js", "bootstrap2.js"];
+	var root = ((this["djConfig"])&&(djConfig["baseScriptUri"])) ?  djConfig.baseScriptUri : ".";
 	for(var x in tmps){
-		document.write("<script type='text/javascript' src='"+root+"/src/"+tmps[x]+"'></script>");
+		var spath = root+"/src/"+tmps[x];
+		if(isRhino){
+			load(spath);
+		}else{
+			document.write("<script type='text/javascript' src='"+spath+"'></script>");
+		}
 	}
 })();
