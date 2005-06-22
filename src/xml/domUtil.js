9fc50405b23ab1dbe304751291335082d301c888
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-06-22 04:40:25 +0000

    moved color functions from domUtil.js to color.js (aliases preserved) and added blend and blendHex functions
    git-svn-id: http://svn.dojotoolkit.org/src@754 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/xml/domUtil.js
+++ src/xml/domUtil.js
@@ -1,4 +1,5 @@
 dojo.hostenv.startPackage("dojo.xml.domUtil");
+dojo.hostenv.loadModule("dojo.graphics.color");
 
 // for loading script:
 dojo.xml.domUtil = new function(){
@@ -206,76 +207,10 @@ dojo.xml.domUtil = new function(){
 		}
 	}
 
-	// get RGB array from css-style color declarations
-	this.extractRGB = function(color) {
-		var hex = "0123456789abcdef";
-		color = color.toLowerCase();
-		if( color.indexOf("rgb") == 0 ) {
-			var matches = color.match(/rgba*\((\d+), *(\d+), *(\d+)/i);
-			var ret = matches.splice(1, 3);
-			return ret;
-		} else if( color.indexOf("#") == 0 ) {
-			var colors = [];
-			color = color.substring(1);
-			if( color.length == 3 ) {
-				colors[0] = color.charAt(0) + color.charAt(0);
-				colors[1] = color.charAt(1) + color.charAt(1);
-				colors[2] = color.charAt(2) + color.charAt(2);
-			} else {
-				colors[0] = color.substring(0, 2);
-				colors[1] = color.substring(2, 4);
-				colors[2] = color.substring(4, 6);
-			}
-
-			for(var i = 0; i < colors.length; i++) {
-				var c = colors[i];
-				colors[i] = hex.indexOf(c.charAt(0))*16 + hex.indexOf(c.charAt(1));
-			}
-			return colors;
-		} else {
-			// named color (how many do we support?)
-			switch(color) {
-				case "white": return [255,255,255];
-				case "black": return [0,0,0];
-				case "red": return[255,0,0];
-				case "green": return [0,255,0];
-				case "blue": return [0,0,255];
-				case "navy": return [0,0,128];
-				case "gray": return [128,128,128];
-				case "silver": return [192,192,192];
-			}
-		}
-		return [255,255,255]; // assume white if all else fails
-	}
-
-	this.hex2rgb = function(hex) {
-		var hexNum = "0123456789ABCDEF";
-		var rgb = new Array(3);
-		if( hex.indexOf("#") == 0 ) { hex = hex.substring(1); }
-		hex = hex.toUpperCase();
-		if( hex.length == 3 ) {
-			rgb[0] = hex.charAt(0) + hex.charAt(0)
-			rgb[1] = hex.charAt(1) + hex.charAt(1)
-			rgb[2] = hex.charAt(2) + hex.charAt(2);
-		} else {
-			rgb[0] = hex.substring(0, 2);
-			rgb[1] = hex.substring(2, 4);
-			rgb[2] = hex.substring(4);
-		}
-		for(var i = 0; i < rgb.length; i++) {
-			rgb[i] = hexNum.indexOf(rgb[i].charAt(0)) * 16 + hexNum.indexOf(rgb[i].charAt(1));
-		}
-		return rgb;
-	}
-
-	this.rgb2hex = function(r, g, b) {
-		if(r.constructor == Array) {
-			g = r[1] || 0;
-			b = r[2] || 0;
-			r = r[0] || 0;
-		}
-		return "#" + r.toString(16) + g.toString(16) + b.toString(16);
-	}
+	// referenced for backwards compatibility
+	this.extractRGB = dojo.graphics.color.extractRGB;
+	this.hex2rgb = dojo.graphics.color.hex2rgb;
+	this.rgb2hex = dojo.graphics.color.rgb2hex;
 
 	this.insertBefore = function(node, ref){
 		var pn = ref.parentNode;
