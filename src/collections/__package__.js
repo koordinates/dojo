a7415fcff797585a8ed14bf331ddc08617c6060c
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-07-08 19:17:22 +0000

    filling in the package file for dojo.collections.*. Files to be moved to match include directives shortly.
    git-svn-id: http://svn.dojotoolkit.org/src@838 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/collections/__package__.js
@@ -0,0 +1,9 @@
+dojo.hostenv.conditionalLoadModule({
+	common: ["dojo.collections.Collections",
+			 "dojo.collections.List",
+			 "dojo.collections.SortedList", 
+			 "dojo.collections.Dictionary", 
+			 "dojo.collections.Queue", 
+			 "dojo.collections.ArrayList", 
+			 "dojo.collections.Stack"]
+});
