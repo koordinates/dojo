9cfda5aa638e863324af7162acc4a6d6c3e42b56
psowden <psowden@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-07-17 01:22:32 +0000

    Added a Uri object and dojo.uri.dojoUri convenience method
    git-svn-id: http://svn.dojotoolkit.org/src@883 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/uri/Uri.js
+++ src/uri/Uri.js
@@ -6,6 +6,86 @@ dojo.uri = new function() {
 		for(var i = 0; i < arguments.length; i++) { arr.push(arguments[i]); }
 		return arr.join("/").replace(/\/{2,}/g, "/").replace(/((https*|ftps*):)/i, "$1/");
 	}
-}
+	
+	this.dojoUri = function (uri) {
+		// returns a Uri object resolved relative to the dojo root
+		return new dojo.uri.Uri(uri, dojo.hostenv.getBaseScriptUri());
+	}
+		
+	this.Uri = function (/*uri, relativeTo, [...]*/) {
+		// An object representing a Uri.
+		// Each argument is evaluated in order relative to the next until
+		// a conanical uri is producued. To get an absolute Uri relative
+		// to the current document use
+		//      new dojo.uri.Uri(uri, document.baseURI)
 
-dojo.hostenv.startPackage("dojo.uri.Uri");
+		// TODO: support for IPv6, see RFC 2732
+
+		// resolve uri components relative to each other
+		var uri = arguments[arguments.length - 1];
+		for (var i = arguments.length - 2; i >= 0; i--) {
+
+			var relobj = new this.constructor(arguments[i]);
+			var uriobj = new this.constructor(uri);
+
+			if (relobj.path == "" && relobj.scheme == null &&
+				relobj.authority == null && relobj.query == null)
+			{
+				if (relobj.fragment != null) { uriobj.fragment = relobj.fragment; }
+				relobj = uriobj;
+			}
+			else if (relobj.scheme == null) {
+				relobj.scheme = uriobj.scheme;
+			
+				if (relobj.authority == null) {
+					relobj.authority = uriobj.authority;
+					
+					if (relobj.path.charAt(0) != "/") {
+						var path = uriobj.path.substring(0,
+							uriobj.path.lastIndexOf("/") + 1) + relobj.path;
+						var segs = path.split("/");
+						for (var j = 0; j < segs.length; j++) {
+							if (segs[j] == ".") { segs.splice(j, 1); j--; }
+							else if (j != 0 && segs[j] == "..") {
+								segs.splice(j - 1, 2); j -= 2;
+							}
+						}
+						relobj.path = segs.join("/");
+					}
+				}
+			}
+
+			uri = "";
+			if (relobj.scheme != null) { uri += relobj.scheme + ":"; }
+			if (relobj.authority != null) { uri += "//" + relobj.authority; }
+			uri += relobj.path;
+			if (relobj.query != null) { uri += "?" + relobj.query; }
+			if (relobj.fragment != null) { uri += "#" + relobj.fragment; }
+		}
+		
+		this.uri = uri;
+
+		// break the uri into its main components
+		var regexp = "^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?$";
+	    var r = this.uri.match(new RegExp(regexp));
+
+		this.scheme = r[2] || (r[1] ? "" : null);
+		this.authority = r[4] || (r[3] ? "" : null);
+		this.path = r[5]; // can never be undefined
+		this.query = r[7] || (r[6] ? "" : null);
+		this.fragment  = r[9] || (r[8] ? "" : null);
+		
+		if (this.authority != null) {
+			// server based naming authority
+			regexp = "^((([^:]+:)?([^@]+))@)?([^:]+)(:([0-9]+))?$";
+			r = this.authority.match(new RegExp(regexp));
+			
+			this.user = r[3] || null;
+			this.password = r[4] || null;
+			this.host = r[5];
+			this.port = r[7] || null;
+		}
+	
+		this.toString = function () { return this.uri; }
+	}
+};
