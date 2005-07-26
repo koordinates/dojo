dcac10c158e470dec6ff6382759efaed34a3deab
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-07-26 23:42:08 +0000

    so you want Dojo? Here ya go.
    git-svn-id: http://svn.dojotoolkit.org/src@951 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/profiles/kitchen_sink.profile.js
@@ -0,0 +1,18 @@
+var dependencies = [ 
+	"dojo.alg.*",
+	"dojo.lang.*",
+	"dojo.event.*",
+	"dojo.logging.*",
+	"dojo.io.*",
+	"dojo.text.*",
+	"dojo.mop.*",
+	"dojo.xml.*",
+	"dojo.crypto.*",
+	"dojo.collections.*",
+	"dojo.graphics.*",
+	"dojo.animation.*",
+	"dojo.io.BrowserIO",
+	"dojo.webui.*",
+];
+
+load("getDependencyList.js");
