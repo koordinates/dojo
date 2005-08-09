5e566244f694be658395bc1c5db6fceba3275b76
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-08-09 20:39:14 +0000

    removing DnD dep for now
    git-svn-id: http://svn.dojotoolkit.org/src@1012 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/webui/__package__.js
+++ src/webui/__package__.js
@@ -2,7 +2,7 @@ dojo.hostenv.conditionalLoadModule({
 	common: ["dojo.xml.Parse", 
 			 "dojo.webui.Widget", 
 			 "dojo.webui.widgets.Parse", 
-			 "dojo.webui.DragAndDrop", 
+			 // "dojo.webui.DragAndDrop", 
 			 "dojo.webui.WidgetManager"],
 	browser: ["dojo.webui.DomWidget",
 			  "dojo.webui.HtmlWidget"],
