4b3b1c0b1f2507e3c3fd35863c2facd51fcafa24
psowden <psowden@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-08-11 22:53:53 +0000

    toCapitalized; it'd be awesome if someone filled this in!
    git-svn-id: http://svn.dojotoolkit.org/src@1043 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/text/Text.js
+++ src/text/Text.js
@@ -1,21 +1,20 @@
-// hostenv.loadModule("dojo.alg.*");
-dojo.hostenv.startPackage("dojo.text.Text");
+dojo.provide("dojo.text.Text");
 
-dojo.text = new function(){
-	this.trim = function(iString){
+dojo.text = {
+	trim: function(iString){
 		if(arguments.length == 0){ // allow String.prototyp-ing
 			iString = this; 
 		}
 		if(typeof iString != "string"){ return iString; }
 		if(!iString.length){ return iString; }
 		return iString.replace(/^\s*/, "").replace(/\s*$/, "");
-	}
+	},
 
 	// Parameterized string function
 	//  str - formatted string with %{values} to be replaces
 	//  pairs - object of name: "value" value pairs
 	//  killExtra - remove all remaining %{values} after pairs are inserted
-	this.paramString = function(str, pairs, killExtra) {
+	paramString: function(str, pairs, killExtra) {
 		if(typeof str != "string") { // allow String.prototype-ing
 			pairs = str;
 			str = this;
@@ -26,12 +25,20 @@ dojo.text = new function(){
 			str = str.replace(re, pairs[name]);
 		}
 
-		if(killExtra) {
-			str = str.replace(/%\{([^\}\s]+)\}/g, "");
-		}
-
+		if(killExtra) { str = str.replace(/%\{([^\}\s]+)\}/g, ""); }
 		return str;
+	},
+	
+	/** Uppercases the first letter of each word */
+	toCapitalized: function (string) {
+		if (arguments.length == 0) { string = this; }
+		// TODO: implement
+		return new String(string);
 	}
 }
 
-dojo.text.Text = {}; // duh, alex.
+/*
+ * We need to make sure that the dojo.text.Text Object exists, the
+ * above assignment causes it to be clobbered so we redefine it here.
+ */
+dojo.text.Text = {};
