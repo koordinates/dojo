50491e3cca4ae9197077beeb5736c27de8da5cc0
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-08-16 22:07:47 +0000

    fix for tabs cloning nodes w/o removing the original node from the document
    git-svn-id: http://svn.dojotoolkit.org/src@1098 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/HtmlTabs.js
+++ src/widget/HtmlTabs.js
@@ -5,6 +5,7 @@ dojo.require("dojo.io.*");
 dojo.require("dojo.widget.Manager");
 dojo.require("dojo.graphics.*");
 
+dojo.widget.HtmlTabs._savedNodes = {};
 dojo.widget.HtmlTabs = function() {
 	dojo.widget.HtmlWidget.call(this);
 
@@ -111,18 +112,21 @@ dojo.widget.HtmlTabs = function() {
 	this.getPanel = function(panel) {
 		if(!panel || panel.loaded) { return; }
 
-		var id = dojo.xml.domUtil.getUniqueId();
 		if(panel.url.charAt(0) == "#") {
-			var origNode = document.getElementById(panel.url.substring(1));
-			origNode.style.display = "none";
+			var id = panel.url.substring(1);
+			var origNode = dojo.widget.HtmlTabs._savedNodes[id];
+			if(!origNode) {
+				origNode = dojo.widget.HtmlTabs._savedNodes[id] = document.getElementById(id);
+				if(origNode.parentNode) { origNode.parentNode.removeChild(origNode); }
+			}
 			var node = origNode.cloneNode(true);
-			node.id = id;
+			node.style.display = "none";
 			this.panelContainer.appendChild(node);
 		} else {
 			var node = document.createElement("div");
 			node.innerHTML = "Loading...";
 			node.style.display = "none";
-			node.id = id;
+			node.id = dojo.xml.domUtil.getUniqueId();
 			this.panelContainer.appendChild(node);
 
 			dojo.io.bind({
