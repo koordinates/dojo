0eafcc2fd0dfb45dbe6a6a6f3998feca1d087977
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-08-30 00:06:08 +0000

    widgets profile
    git-svn-id: http://svn.dojotoolkit.org/src@1326 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tags/release-0.1.0/buildscripts/profiles/widgets.profile.js
@@ -0,0 +1,11 @@
+var dependencies = [ 
+	"dojo.io.IO",
+	"dojo.io.BrowserIO",
+	"dojo.event.*", // NOTE: this includes topic support
+	"dojo.alg.*",
+	"dojo.graphics.*",
+	"dojo.animation.*",
+	"dojo.widget.*"
+];
+
+load("getDependencyList.js");
