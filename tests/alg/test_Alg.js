1148c1991a18be14dc6b4495851292a1f2271ce2
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-09-12 07:54:11 +0000

    updating tests and source
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1420 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/alg/test_Alg.js
+++ tests/alg/test_Alg.js
@@ -10,7 +10,8 @@ function test_alg_find(){
 function test_alg_inArr(){
 	// jum.debug(testArr);
 	jum.assertTrue("test20", dojo.alg.inArr(testArr, "foo"))
-	jum.assertFalse("test30", dojo.alg.inArr(testArr, "foobar"))
+	jum.assertFalse("test21", dojo.alg.inArr(testArr, "foobar"))
+	jum.assertFalse("test22", dojo.alg.inArr(testArr, true))
 }
 
 function test_alg_has(){
@@ -20,10 +21,10 @@ function test_alg_has(){
 	}
 	tclass.prototype.xyzzy = true;
 	var tobj = new tclass();
-	jum.assertTrue("test40", dojo.alg.has(tobj, "foo"));
-	jum.assertTrue("test50", dojo.alg.has(tobj, "bar"));
-	jum.assertTrue("test60", dojo.alg.has(tobj, "xyzzy"));
-	jum.assertFalse("test70", dojo.alg.has(tobj, "baz"));
+	jum.assertTrue("test30", dojo.alg.has(tobj, "foo"));
+	jum.assertTrue("test31", dojo.alg.has(tobj, "bar"));
+	jum.assertTrue("test32", dojo.alg.has(tobj, "xyzzy"));
+	jum.assertFalse("test33", dojo.alg.has(tobj, "baz"));
 }
 
 function test_alg_getNameInObj(){
@@ -34,7 +35,42 @@ function test_alg_getNameInObj(){
 	}
 	var tobj = new tclass();
 
-	jum.assertEquals("test80", "foo", dojo.alg.getNameInObj(tobj, tobj.foo));
-	jum.assertEquals("test80", "bar", dojo.alg.getNameInObj(tobj, tobj.bar));
-	jum.assertEquals("test90", "baz", dojo.alg.getNameInObj(tobj, tobj.baz));
+	jum.assertEquals("test40", "foo", dojo.alg.getNameInObj(tobj, tobj.foo));
+	jum.assertEquals("test41", "bar", dojo.alg.getNameInObj(tobj, tobj.bar));
+	jum.assertEquals("test42", "baz", dojo.alg.getNameInObj(tobj, tobj.baz));
+}
+
+function test_alg_tryThese(){
+	jum.assertEquals("test50", 5, dojo.alg.tryThese(
+		function(){ throw "foo"; },
+		function(){ return 5; },
+		function(){ throw "bar"; }
+	));
+	var undef;
+	jum.assertEquals("test50", undef, dojo.alg.tryThese());
+}
+
+function test_alg_map(){
+	var foo = 0;
+	jum.alg.map([1, 2, 3], dj_global, function(arg){
+		foo+=arg;
+	});
+	jum.assertEquals("60", 6, foo);
+	foo = 0;
+	jum.alg.map([1, 2, 3, 4], function(arg){
+		foo+=arg;
+	});
+	jum.assertEquals("61", 10, foo);
+}
+
+function test_alg_forEach(){
+	var foo = 0;
+	jum.alg.forEach([1, 2, 3], function(arg){
+		foo+=arg;
+	});
+	jum.assertEquals("70", 6, foo);
+
+	var tarr = [1, 2, 3];
+	jum.alg.forEach(tarr, function(arg){ tarr.push(arg); }, true);
+	jum.assertEquals("71", 6, tarr.length);
 }
