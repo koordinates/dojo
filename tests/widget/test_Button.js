779fdaaa7ef5d38122b139aab59f98b54b8fee7f
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-09-12 06:47:09 +0000

    there's no longer a DomButton class  to test
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1419 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/widget/test_Button.js
+++ tests/widget/test_Button.js
@@ -6,11 +6,4 @@ function test_button_ctor(){
 	jum.assertTrue("test10", typeof b1 == "object");
 	jum.assertTrue("test20", b1.widgetType == "Button");
 	jum.assertTrue("test21", typeof b1["attachProperty"] == "undefined");
-
-	var db1 = new dojo.widget.DomButton();
-	jum.assertTrue("test30", typeof db1 == "object");
-	jum.assertTrue("test40", db1.widgetType == "Button");
-	jum.assertTrue("test50", db1.attachProperty == "dojoAttachPoint");
-	jum.assertTrue("test60", typeof db1.domNode != "undefined");
-
 }
