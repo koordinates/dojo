b93b7b2489d35fd17983803c9e6c43bdbc31619d
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-09-13 21:15:33 +0000

    dojo.graphics.htmlEffects --> dojo.fx/dojo.fx.html added dojo.graphics.color.Color object and cleaned up some of the other color bits added color test page
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1453 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/graphics/color.js
+++ src/graphics/color.js
@@ -1,94 +1,121 @@
 dojo.provide("dojo.graphics.color");
+dojo.require("dojo.lang");
 
-dojo.graphics.color = new function() {
-	// blend colors a and b (both as RGB array or hex strings) with weight from -1 to +1, 0 being a 50/50 blend
-	this.blend = function(a, b, weight) {
-		if(typeof a == "string") { return this.blendHex(a, b, weight); }
-		if(!weight) { weight = 0; }
-		else if(weight > 1) { weight = 1; }
-		else if(weight < -1) { weight = -1; }
-		var c = new Array(3);
-		for(var i = 0; i < 3; i++) {
-			var half = Math.abs(a[i] - b[i])/2;
-			c[i] = Math.floor(Math.min(a[i], b[i]) + half + (half * weight));
-		}
-		return c;
+// takes an r, g, b, a(lpha) value, [r, g, b, a] array, "rgb(...)" string, hex string (#aaa, #aaaaaa, aaaaaaa)
+dojo.graphics.color.Color = function(r, g, b, a) {
+	if(dojo.lang.isArray(r)) {
+		this.r = r[0];
+		this.g = r[1];
+		this.b = r[2];
+		this.a = r[3]||1.0;
+	} else if(dojo.lang.isString(r)) {
+		var rgb = dojo.graphics.color.extractRGB(r);
+		this.r = rgb[0];
+		this.g = rgb[1];
+		this.b = rgb[2];
+		this.a = b||1.0;
+	} else {
+		this.r = r;
+		this.g = g;
+		this.b = b;
 	}
+};
 
-	// very convenient blend that takes and returns hex values
-	// (will get called automatically by blend when blend gets strings)
-	this.blendHex = function(a, b, weight) {
-		return this.rgb2hex(this.blend(this.hex2rgb(a), this.hex2rgb(b), weight));
+dojo.graphics.color.Color.prototype.toRgb = function() {
+	return [this.r, this.g, this.b];
+};
+
+dojo.graphics.color.Color.prototype.toHex = function() {
+	return dojo.graphics.color.rgb2hex(this.toRgb());
+};
+
+dojo.graphics.color.Color.prototype.toCss = function() {
+	return "rgb(" + this.toRgb().join() + ")";
+}
+
+dojo.graphics.color.Color.prototype.toString = function() {
+	return this.toHex(); // decent default?
+}
+
+dojo.graphics.color.named = {
+	white:      [255,255,255],
+	black:      [0,0,0],
+	red:        [255,0,0],
+	green:	    [0,255,0],
+	blue:       [0,0,255],
+	navy:       [0,0,128],
+	gray:       [128,128,128],
+	silver:     [192,192,192]
+};
+
+// blend colors a and b (both as RGB array or hex strings) with weight from -1 to +1, 0 being a 50/50 blend
+dojo.graphics.color.blend = function(a, b, weight) {
+	if(typeof a == "string") { return dojo.graphics.color.blendHex(a, b, weight); }
+	if(!weight) { weight = 0; }
+	else if(weight > 1) { weight = 1; }
+	else if(weight < -1) { weight = -1; }
+	var c = new Array(3);
+	for(var i = 0; i < 3; i++) {
+		var half = Math.abs(a[i] - b[i])/2;
+		c[i] = Math.floor(Math.min(a[i], b[i]) + half + (half * weight));
 	}
+	return c;
+}
 
-	// get RGB array from css-style color declarations
-	this.extractRGB = function(color) {
-		var hex = "0123456789abcdef";
-		color = color.toLowerCase();
-		if( color.indexOf("rgb") == 0 ) {
-			var matches = color.match(/rgba*\((\d+), *(\d+), *(\d+)/i);
-			var ret = matches.splice(1, 3);
-			return ret;
-		} else if( color.indexOf("#") == 0 ) {
-			var colors = [];
-			color = color.substring(1);
-			if( color.length == 3 ) {
-				colors[0] = color.charAt(0) + color.charAt(0);
-				colors[1] = color.charAt(1) + color.charAt(1);
-				colors[2] = color.charAt(2) + color.charAt(2);
-			} else {
-				colors[0] = color.substring(0, 2);
-				colors[1] = color.substring(2, 4);
-				colors[2] = color.substring(4, 6);
-			}
+// very convenient blend that takes and returns hex values
+// (will get called automatically by blend when blend gets strings)
+dojo.graphics.color.blendHex = function(a, b, weight) {
+	return dojo.graphics.color.rgb2hex(dojo.graphics.color.blend(dojo.graphics.color.hex2rgb(a), dojo.graphics.color.hex2rgb(b), weight));
+}
 
-			for(var i = 0; i < colors.length; i++) {
-				var c = colors[i];
-				colors[i] = hex.indexOf(c.charAt(0))*16 + hex.indexOf(c.charAt(1));
-			}
+// get RGB array from css-style color declarations
+dojo.graphics.color.extractRGB = function(color) {
+	var hex = "0123456789abcdef";
+	color = color.toLowerCase();
+	if( color.indexOf("rgb") == 0 ) {
+		dj_debug("e1");
+		var matches = color.match(/rgba*\((\d+), *(\d+), *(\d+)/i);
+		var ret = matches.splice(1, 3);
+		return ret;
+	} else {
+		var colors = dojo.graphics.color.hex2rgb(color);
+		if(colors) {
 			return colors;
 		} else {
 			// named color (how many do we support?)
-			switch(color) {
-				case "white": return [255,255,255];
-				case "black": return [0,0,0];
-				case "red": return[255,0,0];
-				case "green": return [0,255,0];
-				case "blue": return [0,0,255];
-				case "navy": return [0,0,128];
-				case "gray": return [128,128,128];
-				case "silver": return [192,192,192];
-			}
+			return dojo.graphics.color.named[color] || [255, 255, 255];
 		}
-		return [255,255,255]; // assume white if all else fails
 	}
+}
 
-	this.hex2rgb = function(hex) {
-		var hexNum = "0123456789ABCDEF";
-		var rgb = new Array(3);
-		if( hex.indexOf("#") == 0 ) { hex = hex.substring(1); }
-		hex = hex.toUpperCase();
-		if( hex.length == 3 ) {
-			rgb[0] = hex.charAt(0) + hex.charAt(0)
-			rgb[1] = hex.charAt(1) + hex.charAt(1)
-			rgb[2] = hex.charAt(2) + hex.charAt(2);
-		} else {
-			rgb[0] = hex.substring(0, 2);
-			rgb[1] = hex.substring(2, 4);
-			rgb[2] = hex.substring(4);
-		}
-		for(var i = 0; i < rgb.length; i++) {
-			rgb[i] = hexNum.indexOf(rgb[i].charAt(0)) * 16 + hexNum.indexOf(rgb[i].charAt(1));
-		}
-		return rgb;
+dojo.graphics.color.hex2rgb = function(hex) {
+	var hexNum = "0123456789ABCDEF";
+	var rgb = new Array(3);
+	if( hex.indexOf("#") == 0 ) { hex = hex.substring(1); }
+	hex = hex.toUpperCase();
+	if(hex.replace(new RegExp("["+hexNum+"]", "g"), "") != "") {
+		return null;
 	}
+	if( hex.length == 3 ) {
+		rgb[0] = hex.charAt(0) + hex.charAt(0)
+		rgb[1] = hex.charAt(1) + hex.charAt(1)
+		rgb[2] = hex.charAt(2) + hex.charAt(2);
+	} else {
+		rgb[0] = hex.substring(0, 2);
+		rgb[1] = hex.substring(2, 4);
+		rgb[2] = hex.substring(4);
+	}
+	for(var i = 0; i < rgb.length; i++) {
+		rgb[i] = hexNum.indexOf(rgb[i].charAt(0)) * 16 + hexNum.indexOf(rgb[i].charAt(1));
+	}
+	return rgb;
+}
 
-	this.rgb2hex = function(r, g, b) {
-		if(r.constructor == Array) {
-			g = r[1] || 0;
-			b = r[2] || 0;
-			r = r[0] || 0;
-		}
-		return ["#", r.toString(16), g.toString(16), b.toString(16)].join("");
+dojo.graphics.color.rgb2hex = function(r, g, b) {
+	if(dojo.lang.isArray(r)) {
+		g = r[1] || "00";
+		b = r[2] || "00";
+		r = r[0] || "00";
 	}
+	return ["#", r.toString(16), g.toString(16), b.toString(16)].join("");
 }
