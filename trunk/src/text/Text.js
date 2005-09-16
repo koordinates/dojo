aa61f50adc56255077f5aae78a0e7d275f525486
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-09-16 16:21:21 +0000

    updated to point to dojo.string
    git-svn-id: http://svn.dojotoolkit.org/src@1513 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/text/Text.js
+++ trunk/src/text/Text.js
@@ -1,3 +1,5 @@
-dojo.provide("dojo.text.Text");
 dj_deprecated("dojo.text.Text is being replaced by dojo.string");
 dojo.require("dojo.string");
+
+dojo.text = dojo.string;
+dojo.provide("dojo.text.Text");
