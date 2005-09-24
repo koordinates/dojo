e01b9abbe3f82cf0326a06ecd459a301378e3a12
psowden <psowden@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-09-24 18:52:23 +0000

    moving IO files to represent their contents (they're not class definitions)
    git-svn-id: http://svn.dojotoolkit.org/src@1603 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/io/__package__.js
+++ trunk/src/io/__package__.js
@@ -1,6 +1,6 @@
 dojo.hostenv.conditionalLoadModule({
-	common: ["dojo.io.IO", false, false],
+	common: ["dojo.io", false, false],
 	rhino: ["dojo.io.RhinoIO", false, false],
-	browser: [["dojo.io.BrowserIO", false, false], ["dojo.io.Cookies", false, false]]
+	browser: [["dojo.io.BrowserIO", false, false], ["dojo.io.cookie", false, false]]
 });
 dojo.hostenv.moduleLoaded("dojo.io.*");
