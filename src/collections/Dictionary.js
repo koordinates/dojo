0bd7b5ae42b74376c7e32991fb51054ef7282ff7
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-09-30 21:08:09 +0000

    Deprecated List by having it return a Dictionary; added helper methods to Dictionary (getKeyList and getValueList) as well as allowed for a dictionary to be passed to the constructor.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1657 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/collections/Dictionary.js
+++ src/collections/Dictionary.js
@@ -1,9 +1,14 @@
 dojo.provide("dojo.collections.Dictionary");
 dojo.require("dojo.collections.Collections");
 
-dojo.collections.Dictionary = function(){
+dojo.collections.Dictionary = function(dictionary){
 	var items = {};
 	this.count = 0;
+	if (dictionary){
+		var e = dictionary.getIterator();
+		while (e.moveNext()) items[e.key] = new dojo.collections.DictionaryEntry(e.key, e.value);
+	}
+
 	this.add = function(k,v){
 		items[k] = new dojo.collections.DictionaryEntry(k,v);
 		this.count++;
@@ -13,9 +18,7 @@ dojo.collections.Dictionary = function(){
 		this.count = 0;
 	};
 	this.clone = function(){
-		var d = new dojo.collections.Dictionary();
-		for (var p in items) d.add(items[p].key, items[p].value);
-		return d;
+		return new dojo.collections.Dictionary(this);
 	};
 	this.contains = this.containsKey = function(k){
 		return (items[k] != null);
@@ -26,6 +29,18 @@ dojo.collections.Dictionary = function(){
 			if (e.value == v) return true;
 		}
 		return false;
+	};
+	this.getKeyList = function(){
+		var arr = [];
+		var e = this.getIterator();
+		while (e.moveNext()) arr.push(e.key);
+		return arr;
+	};
+	this.getValueList = function(){
+		var arr = [];
+		var e = this.getIterator();
+		while (e.moveNext()) arr.push(e.value);
+		return arr;
 	};
 	this.item = function(k){
 		return items[k];
