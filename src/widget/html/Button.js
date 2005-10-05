5ccea9ab127acf32db9e912862b82e39a584350d
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-05 18:11:35 +0000

    moving items out of the ctor and into the prototype seems to improve creation performance ~10%
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1684 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/html/Button.js
+++ src/widget/html/Button.js
@@ -6,25 +6,27 @@ dojo.widget.html.Button = function(){
 	// mix in the button properties
 	dojo.widget.Button.call(this);
 	dojo.widget.HtmlWidget.call(this);
-
-	this.templatePath = dojo.uri.dojoUri("src/widget/templates/HtmlButtonTemplate.html");
-	this.templateCssPath = dojo.uri.dojoUri("src/widget/templates/HtmlButtonTemplate.css");
-
-	// FIXME: freaking implement this already!
-	this.foo = function(){ alert("bar"); }
-
-	this.label = "huzzah!";
-	this.labelNode = null;
-
-	this.setLabel = function(){
-		this.labelNode.innerHTML = this.label;
-		// this.domNode.label = this.label;
-	}
-
-	this.fillInTemplate = function(){
-		this.setLabel();
-	}
-
-	this.onFoo = function(){ }
 }
 dojo.inherits(dojo.widget.html.Button, dojo.widget.HtmlWidget);
+dojo.lang.extend(dojo.widget.html.Button, {
+
+	templatePath: dojo.uri.dojoUri("src/widget/templates/HtmlButtonTemplate.html"),
+	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlButtonTemplate.css"),
+
+	// FIXME: freaking implement this already!
+	foo: function(){ alert("bar"); },
+
+	label: "huzzah!",
+	labelNode: null,
+	
+	setLabel: function(){
+		this.labelNode.innerHTML = this.label;
+		// this.domNode.label = this.label;
+	},
+
+	fillInTemplate: function(){
+		this.setLabel();
+	},
+
+	onFoo: function(){ }
+});
