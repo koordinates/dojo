3800ba5860e9be6a61b2486e77b02f39ece7343a
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-06 20:39:59 +0000

    a few very basic validation methods
    git-svn-id: http://svn.dojotoolkit.org/src@1699 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/validate.js
+++ trunk/src/validate.js
@@ -2,4 +2,21 @@ dojo.provide("dojo.validate");
 
 // currently a stub for dojo.validate
 
+
 dojo.validate = {};
+
+dojo.validate.isText = function(value) {
+	return (dojo.lang.isString(value) && value.search(/\S/) != -1);
+}
+
+dojo.validate.isInteger = function(value) {
+	if (!dojo.lang.isNumber(value)) {
+		return false;
+	}
+	value = value.replace(/^\+/, "");//remove leading plus sign
+	return (parseInt(value).toString() == value);
+}
+
+dojo.validate.isValidNumber = function(value) {
+	return dojo.lang.isNumber(value);
+}
