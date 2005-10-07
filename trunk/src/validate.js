74c74c468dbe96262d607b016c95b01cc6c3d0e2
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-07 00:03:18 +0000

    updates to regexp, more stuff
    git-svn-id: http://svn.dojotoolkit.org/src@1707 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/validate.js
+++ trunk/src/validate.js
@@ -1,9 +1,7 @@
 dojo.provide("dojo.validate");
+dojo.require("dojo.lang");
 
 // currently a stub for dojo.validate
-
-
-dojo.validate = {};
 
 dojo.validate.isText = function(value) {
 	return (dojo.lang.isString(value) && value.search(/\S/) != -1);
@@ -13,19 +11,26 @@ dojo.validate.isInteger = function(value) {
 	if (!dojo.lang.isNumber(value)) {
 		return false;
 	}
-	value = value.replace(/^\+/, "");//remove leading plus sign
-	return (parseInt(value).toString() == value);
+	return Math.round(value) == value;
 }
 
 dojo.validate.isValidNumber = function(value) {
 	return dojo.lang.isNumber(value);
 }
 
+// FIXME: may be too basic
 dojo.validate.isEmailAddress = function(value) {
-	// FIXME: very basic email validation
-	// strip out mailto: or <>
 	value = value.replace(/mailto:/i, "");
-	value = value.replace(/[<>]*/g, "");
-	//if((value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) 
-	return (!value.match(/[^0-9a-zA-Z+-._@]+/g);
+	return /^<?([\w\.\-]+)@([\w\.\-]+)>?$/i.test(value);
+}
+
+// FIXME: should this biggyback on isEmailAddress or just have its own RegExp?
+dojo.validate.isEmailAddressList = function(value) {
+	var values = value.split(/\s*[\s;,]\s*/gi);
+	for(var i = 0; i < values.length; i++) {
+		if(!dojo.validate.isEmailAddress(values[i])) {
+			return false;
+		}
+	}
+	return true;
 }
