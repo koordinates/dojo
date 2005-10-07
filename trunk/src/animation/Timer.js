b36a2bad0c8e8a88c4a502784ab893a7e2b28c19
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-07 14:17:00 +0000

    Very basic timer class
    git-svn-id: http://svn.dojotoolkit.org/src@1716 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/src/animation/Timer.js
@@ -0,0 +1,23 @@
+dojo.provide("dojo.animation.Timer");
+
+dojo.animation.Timer = function(intvl){
+	var _this = this;
+	var timer = null;
+	this.isRunning = false;
+	this.interval = intvl;
+
+	this.onTick = function(){};
+	this.onStart = null;
+	this.onStop = null;
+
+	this.start = function(){
+		if (typeof _this.onStart == "function") _this.onStart();
+		this.isRunning = true;
+		timer = window.setInterval(_this.onTick, _this.interval);
+	};
+	this.stop = function(){
+		if (typeof _this.onStop == "function") _this.onStop();
+		this.isRunning = false;
+		window.clearInterval(timer);
+	};
+};
