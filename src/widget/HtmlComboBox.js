f51107c82d452d248e9b6f40a75cd9211ff2fc48
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-15 20:08:58 +0000

    test to see if things in branches/ are now ignored by the mailer script
    git-svn-id: http://svn.dojotoolkit.org/src/branches/civicrm@1788 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/HtmlComboBox.js
+++ src/widget/HtmlComboBox.js
@@ -207,6 +207,19 @@ dojo.widget.HtmlComboBox = function(){
 				dojo.event.connect(this, "startSearch", this.dataProvider, "startSearch");
 				dojo.event.connect(this.dataProvider, "provideSearchResults", this, "openResultList");
 			}
+		}else{
+			// check to see if we can populate the list from <option> elements
+			var node = frag["dojo:"+this.widgetType.toLowerCase()]["nodeRef"];
+			if((node)&&(node.nodeName.toLowerCase() == "select")){
+				// NOTE: we're not handling <optgroup> here yet
+				var opts = node.getElementsByTagName("option");
+				var ol = opts.length;
+				var data = [];
+				for(var x=0; x<ol; x++){
+					data.push([new String(opts[x].innerHTML), new String(opts[x].value)]);
+				}
+				this.dataProvider.setData(data);
+			}
 		}
 	}
 
