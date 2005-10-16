bf4cf5d856c8bdd3dbc1648f14f88db2b0949d20
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-16 06:13:10 +0000

    another test
    git-svn-id: http://svn.dojotoolkit.org/src@1793 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/civicrm/src/widget/CiviCrmDatePicker.js
+++ branches/civicrm/src/widget/CiviCrmDatePicker.js
@@ -69,7 +69,7 @@ dojo.widget.HtmlCiviCrmDatePicker = function(){
 		}
 
 		if(this.mode.indexOf("date") != -1){
-			this.datePicker = dojo.widget.fromScript("DatePicker", {}, this.dateHolderTd);
+			this.datePicker = dojo.widget.fromScript("DatePicker", {}, this.dateHolderTd, "last");
 			dojo.event.connect(	this.datePicker, "onSetDate", 
 								this, "setDateSelects");
 
@@ -80,7 +80,7 @@ dojo.widget.HtmlCiviCrmDatePicker = function(){
 			}
 		}
 		if(this.mode.indexOf("time") != -1){
-			this.timePicker = dojo.widget.fromScript("TimePicker", {}, this.timeHolderTd);
+			this.timePicker = dojo.widget.fromScript("TimePicker", {}, this.timeHolderTd, "last");
 			dojo.event.connect(	this.timePicker, "onSetTime", 
 								this, "setTimeSelects");
 			var mfd = this.modeFormats.time;
