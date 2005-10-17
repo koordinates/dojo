174e6b9eda1911091ad8fcaf523c08e02975f42f
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-17 21:33:19 +0000

    updated to use fadeHide instead
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1814 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/HtmlComboBox.js
+++ src/widget/HtmlComboBox.js
@@ -314,10 +314,7 @@ dojo.lang.extend(dojo.widget.HtmlComboBox, {
 	},
 
 	hideResultList: function(){
-		// this.optionsListNode.style.display = "none";
-		dojo.fx.fadeOut(this.optionsListNode, 200, function(){
-			_this.optionsListNode.style.display = "none";
-		});
+		dojo.fx.fadeHide(this.optionsListNode, 200);
 		dojo.event.disconnect(dojo.html.body(), "onclick", this, "hideResultList");
 		this._result_list_open = false;
 		return;
