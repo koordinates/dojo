564513d71d61a14c4a471af0c6cfcc1caf1b4e4a
cal <cal@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-18 19:17:19 +0000

    added IE fixes, container constraints and titles for floating pane
    git-svn-id: http://svn.dojotoolkit.org/src@1820 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/HtmlFloatingPane.js
+++ trunk/src/widget/HtmlFloatingPane.js
@@ -27,7 +27,9 @@ dojo.lang.extend(dojo.widget.HtmlFloatingPane, {
 	dragBar: null,
 	dragOrigin: null,
 	posOrigin: null,
+	maxPosition: null,
 	title: 'Untitled',
+	constrainToContainer: 0,
 	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlFloatingPane.css"),
 
 
@@ -37,7 +39,6 @@ dojo.lang.extend(dojo.widget.HtmlFloatingPane, {
 			dojo.style.insertCssFile(this.templateCssPath, null, true);
 		}
 
-		//this.domNode.style.position = 'absolute';
 		dojo.html.addClass(this.domNode, 'dojoFloatingPane');
 
 		// this is our client area
@@ -46,6 +47,7 @@ dojo.lang.extend(dojo.widget.HtmlFloatingPane, {
 		dojo.dom.moveChildren(this.domNode, elm, 0);
 		dojo.html.addClass(elm, 'dojoFloatingPaneClient');
 		this.clientPane = this.createPane(elm, 'client');
+		this.clientPane.ownerPane = this;
 
 
 		// this is our chrome
@@ -54,9 +56,10 @@ dojo.lang.extend(dojo.widget.HtmlFloatingPane, {
 		elm.appendChild(document.createTextNode(this.title));
 		dojo.html.addClass(elm, 'dojoFloatingPaneDragbar');
 		this.dragBar = this.createPane(elm, 'top');
+		this.dragBar.ownerPane = this;
 
 		dojo.html.disableSelection(this.dragBar.domNode);
-		dojo.event.connect(this.dragBar.domNode, 'onmousedown', this, 'onDragStart');
+		dojo.event.connect(this.dragBar.domNode, 'onmousedown', this, 'onMyDragStart');
 
 		this.layoutSoon();
 	},
@@ -66,7 +69,9 @@ dojo.lang.extend(dojo.widget.HtmlFloatingPane, {
 		// attach our children
 
 		for(var i=0; i<this.children.length; i++){
-		//	this.domNode.appendChild(this.children[i].domNode);
+			if (this.children[i].ownerPane != this){
+				this.clientPane.domNode.appendChild(this.children[i].domNode);
+			}
 		}
 	},
 
@@ -79,33 +84,57 @@ dojo.lang.extend(dojo.widget.HtmlFloatingPane, {
 		return pane;
 	},
 
-	onDragStart: function(e){
+	onMyDragStart: function(e){
 
 		this.dragOrigin = {'x': e.pageX, 'y': e.pageY};
 		this.posOrigin = {'x': dojo.style.getNumericStyle(this.domNode, 'left'), 'y': dojo.style.getNumericStyle(this.domNode, 'top')};
 
-		//dojo.debugStruct(e);
+		if (this.constrainToContainer){
+			// get parent client size...
 
-		dojo.event.connect(window, 'onmousemove', this, 'onDragMove');
-		dojo.event.connect(window, 'onmouseup', this, 'onDragEnd');
+			if (this.domNode.parentNode.nodeName.toLowerCase() == 'body'){
+				var parentClient = {
+					'w': window.innerWidth,
+					'h': window.innerHeight
+				};
+			}else{
+				var parentClient = {
+					'w': this.domNode.parentNode.offsetWidth - dojo.style.getBorderWidth(this.domNode.parentNode),
+					'h': this.domNode.parentNode.offsetHeight - dojo.style.getBorderHeight(this.domNode.parentNode)
+				};
+			}
+
+			this.maxPosition = {
+				'x': parentClient.w - dojo.style.getOuterWidth(this.domNode),
+				'y': parentClient.h - dojo.style.getOuterHeight(this.domNode)
+			};
+		}
+
+		dojo.event.connect(window, 'onmousemove', this, 'onMyDragMove');
+		dojo.event.connect(window, 'onmouseup', this, 'onMyDragEnd');
 	},
 
-	onDragMove: function(e){
+	onMyDragMove: function(e){
 
-		var dx = e.pageX - this.dragOrigin.x;
-		var dy = e.pageY - this.dragOrigin.y;
+		var x = this.posOrigin.x + (e.pageX - this.dragOrigin.x);
+		var y = this.posOrigin.y + (e.pageY - this.dragOrigin.y);
 
-		this.domNode.style.left = (this.posOrigin.x + dx) + 'px';
-		this.domNode.style.top  = (this.posOrigin.y + dy) + 'px';
+		if (this.constrainToContainer){
+			if (x < 0){ x = 0; }
+			if (y < 0){ y = 0; }
+			if (x > this.maxPosition.x){ x = this.maxPosition.x; }
+			if (y > this.maxPosition.y){ y = this.maxPosition.y; }
+		}
 
-		//dojo.debug('drag '+dx+','+dy);
+		this.domNode.style.left = x + 'px';
+		this.domNode.style.top  = y + 'px';
 	},
 
-	onDragEnd: function(e){
+	onMyDragEnd: function(e){
 
-		dojo.event.disconnect(window, 'onmousemove', this, 'onDragMove');
-		dojo.event.disconnect(window, 'onmouseup', this, 'onDragEnd');
-	},
+		dojo.event.disconnect(window, 'onmousemove', this, 'onMyDragMove');
+		dojo.event.disconnect(window, 'onmouseup', this, 'onMyDragEnd');
+	}
 	
 });
 
