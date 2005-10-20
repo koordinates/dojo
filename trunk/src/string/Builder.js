57ecd198cfe6b4ef5669594982abf105cd223b07
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-20 00:57:42 +0000

    making the Builder methods return the builder instance most of the time so that methods can be chained and adding notes about places where the methods are b0rken.
    git-svn-id: http://svn.dojotoolkit.org/src@1845 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/string/Builder.js
+++ trunk/src/string/Builder.js
@@ -5,43 +5,59 @@ dojo.string.Builder = function(str){
 	var a = [];
 	var b = str || "";
 	var length = this.length = b.length;
-	if (!dojo.string.isBlank(b)) a.push(b);
+	if(!dojo.string.isBlank(b)){
+		a.push(b);
+	}
 	b = "";
 	this.toString = this.valueOf = function(){ 
 		return a.join(""); 
 	};
+
 	this.append = function(s){
 		a.push(s);
 		length += s.length;
 		this.length = length;
+		return this;
 	};
+
 	this.clear = function(){
 		a=[];
 		length = this.length = 0;
+		return this;
 	};
+
 	this.remove = function(f,l){
 		var s = ""; 
 		b = a.join(""); 
 		a=[];
-		if (f >0) s = b.substring(0, (f-1));
+		if(f>0){
+			s = b.substring(0, (f-1));
+		}
 		b = s + b.substring(f + l); 
 		a.push(b);
 		length = this.length = b.length; 
 		b="";
+		return this;
 	};
+
 	this.replace = function(o,n){
+		// FIXME: this is not passing our unit tests
 		b = a.join(""); 
 		a = []; 
 		b.replace(o,n); 
 		a.push(b);
 		length = this.length = b.length; 
 		b="";
+		return this;
 	};
+
 	this.insert = function(idx,s){
+		// FIXME: this is not passing our unit tests
 		b = a.join(""); 
 		a=[];
-		if (idx == 0) b = s + b;
-		else {
+		if(idx == 0){
+			b = s + b;
+		}else{
 			var start = b.substring(0, idx-1);
 			var end = b.substring(idx);
 			b = start + s + end;
@@ -49,5 +65,6 @@ dojo.string.Builder = function(str){
 		length = this.length = b.length; 
 		a.push(b); 
 		b="";
+		return this;
 	};
 };
