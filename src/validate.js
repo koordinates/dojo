6525cb00d64f1776a7760f63b57d018045f98830
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-10-24 06:31:50 +0000

    enhanced isEmailAddress
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@1877 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/validate.js
+++ src/validate.js
@@ -19,16 +19,21 @@ dojo.validate.isValidNumber = function(value) {
 }
 
 // FIXME: may be too basic
-dojo.validate.isEmailAddress = function(value) {
-	value = value.replace(/mailto:/i, "");
-	return /^<?([\w\.\-]+)@([\w\.\-]+)>?$/i.test(value);
+dojo.validate.isEmailAddress = function(value, allowLocal, allowCruft) {
+	if(allowCruft) { value = value.replace(/mailto:/i, ""); }
+	var part = "[\\w\\.\\-]+";
+	var cruft = allowCruft ? "<?" : "";
+	var local = allowLocal ? "" : "\\." + part;
+	// regexp: /^<?([\w\.\-]+)@([\w\.\-]+\.[\w\.\-]+)>?$/i
+	var re = new RegExp("^" + cruft + "(" + part + ")@(" + part + local + ")" + cruft + "$", "i");
+	return re.test(value);
 }
 
 // FIXME: should this biggyback on isEmailAddress or just have its own RegExp?
-dojo.validate.isEmailAddressList = function(value) {
+dojo.validate.isEmailAddressList = function(value, allowLocal, allowCruft) {
 	var values = value.split(/\s*[\s;,]\s*/gi);
 	for(var i = 0; i < values.length; i++) {
-		if(!dojo.validate.isEmailAddress(values[i])) {
+		if(!dojo.validate.isEmailAddress(values[i], allowLocal, allowCruft)) {
 			return false;
 		}
 	}
