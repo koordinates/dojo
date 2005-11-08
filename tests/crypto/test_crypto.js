e7e30a55927fbfe4d97eeb671e95a18d34a05ab9
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-11-08 23:50:00 +0000

    Unit tests for all crypto code.  Note that as of this checkin, only dojo.crypto passes.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2109 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tests/crypto/test_crypto.js
@@ -0,0 +1,25 @@
+dojo.require("dojo.crypto.*");
+
+function test_crypto_toByteArray(){
+	var r = dojo.crypto.toByteArray(" ,8Wbz");
+	var t = [0x57382c20, 0x00007A62];
+	jum.assertEquals("crypto_toByteArray", t, r);
+}
+
+function test_crypto_fromByteArray(){
+	var r = dojo.crypto.fromByteArray([0x57382c20, 0x00007A62]);
+	var t = " ,8Wbz";
+	jum.assertEquals("crypto_fromByteArray", t, r);
+}
+
+function test_crypto_toBase64(){
+	var r = dojo.crypto.toBase64([32,44,56,87,98,122]);
+	var t = "ICw4V2J6";
+	jum.assertEquals("crypto_toBase64", t, r);
+}
+
+function test_crypto_fromBase64(){
+	var r = dojo.crypto.fromBase64("ICw4V2J6");
+	var t = [32,44,56,87,98,122];
+	jum.assertEquals("crypto_fromBase64", t, r);
+}
