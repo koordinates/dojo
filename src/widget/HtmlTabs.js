368a499ce2cfe5115ae22b22cade54fc1e691387
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-11-12 13:28:33 +0000

    Implements Tabs using LayoutPane so that the sizing works, and also to eliminate duplicated code.   There's still some work to be done; in particular putting tab labels at the bottom (rather than the top) of the tab panel doesn't work well.  Also, need a way to do defer loading tabs (and any LayoutPane) until it is actually displayed.  Had to change the API for deploying tabs; hopefully that's OK.  Also, I changed LayoutPane to support multiple client panes, all overlapping (only one of which is displayed at a time)
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2146 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/HtmlTabs.js
+++ src/widget/HtmlTabs.js
@@ -1,254 +1,110 @@
-dojo.provide("dojo.widget.Tabs");
 dojo.provide("dojo.widget.HtmlTabs");
+dojo.provide("dojo.widget.HtmlTab");
 
-dojo.require("dojo.io.*");
 dojo.require("dojo.widget.*");
-dojo.require("dojo.dom");
+dojo.require("dojo.widget.HtmlLayoutPane");
+dojo.require("dojo.event.*");
 dojo.require("dojo.html");
+dojo.require("dojo.style");
 
+//////////////////////////////////////////
+// Tabs -- a set of tabs
+//////////////////////////////////////////
 dojo.widget.HtmlTabs = function() {
-	dojo.widget.HtmlWidget.call(this);
-	this.tabs = [];
-	this.panels = [];
+	dojo.widget.HtmlLayoutPane.call(this);
 }
-dojo.inherits(dojo.widget.HtmlTabs, dojo.widget.HtmlWidget);
+dojo.inherits(dojo.widget.HtmlTabs, dojo.widget.HtmlLayoutPane);
 
 dojo.lang.extend(dojo.widget.HtmlTabs, {
 
 	widgetType: "Tabs",
-	isContainer: true,
 
-	templatePath: null, // prolly not
+	labelPosition: "top",
+
 	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlTabs.css"),
 
-	domNode: null,
-	containerNode: null,
+	selected: null,		// currently selected tab
 
-	selected: -1,
-
-	tabTarget: "",
-	extractContent: false, // find the bits inside <body>
-	parseContent: false, // parse externally loaded pages for widgets
-
-	buildRendering: function(args, frag) {
+	fillInTemplate: function(args, frag) {
+		dojo.widget.HtmlTabs.superclass.fillInTemplate.call(this, args, frag);
+		
+		// TODO: prevent multiple includes of the same CSS file, when there are multiple
+		// tabs on the same screen.
 		dojo.style.insertCssFile(this.templateCssPath);
-		this.domNode = frag["dojo:"+this.widgetType.toLowerCase()]["nodeRef"];
-		if(!this.domNode) { dj_error("HTMLTabs: No node reference"); }
 
-		if(args["tabtarget"]) {
-			this.tabtarget = args["tabtarget"];
-			this.containerNode = document.getElementById(args["tabtarget"]);
-		} else {
-			this.containerNode = document.createElement("div");
-			var next = this.domNode.nextSibling;
-			if(next) {
-				this.domNode.parentNode.insertBefore(this.containerNode, next);
-			} else {
-				this.domNode.parentNode.appendChild(this.containerNode);
-			}
-		}
-		dojo.html.addClass(this.containerNode, "dojoTabPanelContainer");
-
-		var li = dojo.dom.getFirstChildElement(this.domNode);
-		while(li) {
-			var a = li.getElementsByTagName("a").item(0);
-			this.addTab(a);
-			li = dojo.dom.getNextSiblingElement(li);
-		}
-
-		if(this.selected == -1) { this.selected = 0; }
-		this.selectTab(null, this.tabs[this.selected]);
+		// Create panel to hold the tab labels (as a <ul> with special formatting)
+		// TODO: set "bottom" css tag if label is on bottom
+		this.filterAllowed('labelPosition', ['top', 'bottom']);
+		this.labelPanel = dojo.widget.fromScript("LayoutPane", {layoutAlign: this.labelPosition});
+		this.ul = document.createElement("ul");
+		dojo.html.addClass(this.ul, "tabs");
+		dojo.html.addClass(this.ul, this.labelPosition);
+		this.labelPanel.domNode.appendChild(this.ul);
+		this.addPane(this.labelPanel);
 	},
 
-	addTab: function(title, url, tabId, tabHandler) {
-		// TODO: make this an object proper
-		var panel = {
-			url: null,
-			title: null,
-			isLoaded: false,
-			id: null,
-			isLocal: false
-		};
-
-		if(title && title.tagName && title.tagName.toLowerCase() == "a") {
-			// init case
-			var a = title;
-			var li = a.parentNode;
-			title = a.innerHTML;
-			url = a.getAttribute("href");
-			var id = null;
-			var hash = url.indexOf("#");
-			if(hash == 0 || (hash > 0 && location.href.split("#")[0] == url.split("#")[0])) {
-				id = url.split("#")[1];
-				dj_debug("setting local id:", id);
-				url = "#" + id;
-				panel.isLocal = true;
-			} else {
-				id = a.getAttribute("tabid");
-			}
-
-			panel.url = url;
-			panel.title = title;
-			panel.id = id || dojo.html.getUniqueId();
-			dj_debug("panel id:", panel.id, "url:", panel.url);
-		} else {
-			// programmatically adding
-			var li = document.createElement("li");
-			var a = document.createElement("a");
-			a.innerHTML = title;
-			a.href = url;
-			li.appendChild(a);
-			this.domNode.appendChild(li);
-
-			panel.url = url;
-			panel.title = title;
-			panel.id = tabId || dojo.html.getUniqueId();
-			dj_debug("prg tab:", panel.id, "url:", panel.url);
-		}
-
-		if(panel.isLocal) {
-			var node = document.getElementById(id);
-			node.style.display = "none";
-			this.containerNode.appendChild(node);
-		} else {
-			var node = document.createElement("div");
-			node.style.display = "none";
-			node.id = panel.id;
-			this.containerNode.appendChild(node);
-		}
-
-		var handler = a.getAttribute("tabhandler") || tabHandler;
-		if(handler) {
-			this.setPanelHandler(handler, panel);
-		}
-
-		dojo.event.connect(a, "onclick", this, "selectTab");
-
-		this.tabs.push(li);
-		this.panels.push(panel);
-
-		if(this.selected == -1 && dojo.html.hasClass(li, "current")) {
-			this.selected = this.tabs.length-1;
-		}
-
-		return { "tab": li, "panel": panel };
-	},
-
-	selectTab: function(e, target) {
-		if(dojo.lang.isNumber(e)) {
-			target = this.tabs[e];
-		}
-		else if(e) {
-			if(e.target) {
-				target = e.target;
-				while(target && (target.tagName||"").toLowerCase() != "li") {
-					target = target.parentNode;
-				}
-			}
-			if(e.preventDefault) { e.preventDefault(); }
-		}
-
-		dojo.html.removeClass(this.tabs[this.selected], "current");
-
-		for(var i = 0; i < this.tabs.length; i++) {
-			if(this.tabs[i] == target) {
-				dojo.html.addClass(this.tabs[i], "current");
-				this.selected = i;
-				break;
-			}
-		}
-
-		var panel = this.panels[this.selected];
-		if(panel) {
-			this.getPanel(panel);
-			this.hidePanels(panel);
-			document.getElementById(panel.id).style.display = "";
+	registerChild: function(tab, insertionIndex){
+		dojo.widget.HtmlTabs.superclass.registerChild.call(this, tab, insertionIndex);
+		this.ul.appendChild(tab.li);
+		
+		if ( !this.selected ) {
+			this.onSelected(tab);
 		}
 	},
 
-	setPanelHandler: function(handler, panel) {
-		var fcn = dojo.lang.isFunction(handler) ? handler : window[handler];
-		if(!dojo.lang.isFunction(fcn)) {
-			throw new Error("Unable to set panel handler, '" + handler + "' not a function.");
-			return;
+	onSelected: function(tab) {
+		// Deselect old tab and select new one
+		if ( this.selected ) {
+			this.selected.hide();
 		}
-		this["tabHandler" + panel.id] = function() {
-			return fcn.apply(this, arguments);
-		}
-	},
-
-	runPanelHandler: function(panel) {
-		if(dojo.lang.isFunction(this["tabHandler" + panel.id])) {
-			this["tabHandler" + panel.id](panel, document.getElementById(panel.id));
-			return false;
-		}
-		return true;
-		/*
-		// in case we want to honor the return value?
-		var ret = true;
-		if(dojo.lang.isFunction(this["tabhandler" + panel.id])) {
-			var val = this["tabhandler" + panel.id](this, panel);
-			if(!dojo.lang.isUndefined(val)) {
-				ret = val;
-			}
-		}
-		return ret;
-		*/
-	},
-
-	getPanel: function(panel) {
-		if(this.runPanelHandler(panel)) {
-			if(panel.isLocal) {
-				// do nothing?
-			} else {
-				if(!panel.isLoaded || !this.useCache) {
-					this.setExternalContent(panel, panel.url, this.useCache);
-				}
-			}
-		}
-	},
-
-	setExternalContent: function(panel, url, useCache) {
-		var node = document.getElementById(panel.id);
-		node.innerHTML = "Loading...";
-
-		var extract = this.extractContent;
-		var parse = this.parseContent;
-
-		dojo.io.bind({
-			url: url,
-			useCache: useCache,
-			mimetype: "text/html",
-			handler: function(type, data, e) {
-				if(type == "load") {
-					if(extract) {
-						var matches = data.match(/<body[^>]*>\s*([\s\S]+)\s*<\/body>/im);
-						if(matches) { data = matches[1]; }
-					}
-					node.innerHTML = data;
-					panel.isLoaded = true;
-					if(parse) {
-						var parser = new dojo.xml.Parse();
-						var frag = parser.parseElement(node, null, true);
-						dojo.widget.getParser().createComponents(frag);
-					}
-				} else {
-					node.innerHTML = "Error loading '" + panel.url + "' (" + e.status + " " + e.statusText + ")";
-				}
-			}
-		});
-	},
-
-	hidePanels: function(except) {
-		for(var i = 0; i < this.panels.length; i++) {
-			if(this.panels[i] != except && this.panels[i].id) {
-				var p = document.getElementById(this.panels[i].id);
-				if(p) {
-					p.style.display = "none";
-				}
-			}
-		}
+		tab.show();
+		this.selected = tab;
 	}
 });
-
 dojo.widget.tags.addParseTreeHandler("dojo:tabs");
+
+//////////////////////////////////////////////////////
+// Tab - a single tab
+//////////////////////////////////////////////////////
+dojo.widget.HtmlTab = function() {
+	dojo.widget.HtmlLayoutPane.call(this);
+}
+dojo.inherits(dojo.widget.HtmlTab, dojo.widget.HtmlLayoutPane);
+
+dojo.lang.extend(dojo.widget.HtmlTab, {
+	widgetType: "Tab",
+	
+	label: "",
+	url: "inline",
+	handler: "none",
+	
+	fillInTemplate: function(args, frag) {
+		this.layoutAlign = "client";
+		dojo.widget.HtmlTab.superclass.fillInTemplate.call(this, args, frag);
+		dojo.html.prependClass(this.domNode, "dojoTabPanel");
+		this.domNode.style.display="none";
+
+		// Create label
+		this.li = document.createElement("li");
+		var span = document.createElement("span");
+		span.innerHTML = this.label;
+		this.li.appendChild(span);
+		dojo.event.connect(this.li, "onclick", this, "onSelected");
+	},
+	
+	onSelected: function() {
+		this.parent.onSelected(this);
+	},
+	
+	show: function() {
+		dojo.html.addClass(this.li, "current");
+		dojo.widget.HtmlTab.superclass.show.call(this);
+	},
+
+	hide: function() {
+		dojo.html.removeClass(this.li, "current");
+		dojo.widget.HtmlTab.superclass.hide.call(this);
+	}	
+});
+dojo.widget.tags.addParseTreeHandler("dojo:tab");
+
