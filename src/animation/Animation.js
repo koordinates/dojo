f213f31c91a4e6284965ed8dd26ebe76f63be2c2
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-11-20 23:39:17 +0000

    refactored Animation, AnimationEvent and AnimationSequence to use prototype instead of closures
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2199 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/animation/Animation.js
+++ src/animation/Animation.js
@@ -13,14 +13,11 @@ TODO:
 - make animations reusable by not killing repeatCount as we go
 */
 
-dojo.animation = {};
-
 dojo.animation.Animation = function(curve, duration, accel, repeatCount) {
 	// public properties
 	this.curve = curve;
 	this.duration = duration;
 	this.repeatCount = repeatCount || 0;
-	this.animSequence_ = null;
 	if(accel) {
 		if(dojo.lang.isFunction(accel.getValue)) {
 			this.accel = accel;
@@ -29,50 +26,58 @@ dojo.animation.Animation = function(curve, duration, accel, repeatCount) {
 			this.accel = new dojo.math.curves.CatmullRom([[0], [i], [1]], 0.45);
 		}
 	}
+}
+dojo.lang.extend(dojo.animation.Animation, {
+	// public properties
+	curve: null,
+	duration: 0,
+	repeatCount: 0,
+	accel: null,
 
-	// public events
-	this.onBegin = null;
-	this.onAnimate = null;
-	this.onEnd = null;
-	this.onPlay = null;
-	this.onPause = null;
-	this.onStop = null;
-	this.handler = null; // catch-all handler
+	// events
+	onBegin: null,
+	onAnimate: null,
+	onEnd: null,
+	onPlay: null,
+	onPause: null,
+	onStop: null,
+	handler: null,
 
-	// private properties
-	var startTime = null,
-		endTime = null,
-		lastFrame = null,
-		timer = null,
-		percent = 0,
-		active = false,
-		paused = false;
+	// "private" properties
+	_animSequence: null,
+	_startTime: null,
+	_endTime: null,
+	_lastFrame: null,
+	_timer: null,
+	_percent: 0,
+	_active: false,
+	_paused: false,
 
 	// public methods
-	this.play = function(gotoStart) {
+	play: function(gotoStart) {
 		if( gotoStart ) {
-			clearTimeout(timer);
-			active = false;
-			paused = false;
-			percent = 0;
-		} else if( active && !paused ) {
+			clearTimeout(this._timer);
+			this._active = false;
+			this._paused = false;
+			this._percent = 0;
+		} else if( this._active && !this._paused ) {
 			return;
 		}
 
-		startTime = new Date().valueOf();
-		if( paused ) {
-			startTime -= (this.duration * percent / 100);
+		this._startTime = new Date().valueOf();
+		if( this._paused ) {
+			this._startTime -= (this.duration * this._percent / 100);
 		}
-		endTime = startTime + this.duration;
-		lastFrame = startTime;
+		this._endTime = this._startTime + this.duration;
+		this._lastFrame = this._startTime;
 
-		var e = new dojo.animation.AnimationEvent(this, null, this.curve.getValue(percent),
-			startTime, startTime, endTime, this.duration, percent, 0);
+		var e = new dojo.animation.AnimationEvent(this, null, this.curve.getValue(this._percent),
+			this._startTime, this._startTime, this._endTime, this.duration, this._percent, 0);
 
-		active = true;
-		paused = false;
+		this._active = true;
+		this._paused = false;
 
-		if( percent == 0 ) {
+		if( this._percent == 0 ) {
 			e.type = "begin";
 			if(typeof this.handler == "function") { this.handler(e); }
 			if(typeof this.onBegin == "function") { this.onBegin(e); }
@@ -82,73 +87,74 @@ dojo.animation.Animation = function(curve, duration, accel, repeatCount) {
 		if(typeof this.handler == "function") { this.handler(e); }
 		if(typeof this.onPlay == "function") { this.onPlay(e); }
 
-		if(this.animSequence_) { this.animSequence_.setCurrent(this); }
+		if(this._animSequence) { this._animSequence.setCurrent(this); }
 
-		dojo.lang.hitch(this, cycle)();
-	}
+		//dojo.lang.hitch(this, cycle)();
+		this._cycle();
+	},
 
-	this.pause = function() {
-		clearTimeout(timer);
-		if( !active ) { return; }
-		paused = true;
-		var e = new dojo.animation.AnimationEvent(this, "pause", this.curve.getValue(percent),
-			startTime, new Date().valueOf(), endTime, this.duration, percent, 0);
+	pause: function() {
+		clearTimeout(this._timer);
+		if( !this._active ) { return; }
+		this._paused = true;
+		var e = new dojo.animation.AnimationEvent(this, "pause", this.curve.getValue(this._percent),
+			this._startTime, new Date().valueOf(), this._endTime, this.duration, this._percent, 0);
 		if(typeof this.handler == "function") { this.handler(e); }
 		if(typeof this.onPause == "function") { this.onPause(e); }
-	}
+	},
 
-	this.playPause = function() {
-		if( !active || paused ) {
+	playPause: function() {
+		if( !this._active || this._paused ) {
 			this.play();
 		} else {
 			this.pause();
 		}
-	}
+	},
 
-	this.gotoPercent = function(pct, andPlay) {
-		clearTimeout(timer);
-		active = true;
-		paused = true;
-		percent = pct;
+	gotoPercent: function(pct, andPlay) {
+		clearTimeout(this._timer);
+		this._active = true;
+		this._paused = true;
+		this._percent = pct;
 		if( andPlay ) { this.play(); }
-	}
+	},
 
-	this.stop = function(gotoEnd) {
-		clearTimeout(timer);
-		var step = percent / 100;
+	stop: function(gotoEnd) {
+		clearTimeout(this._timer);
+		var step = this._percent / 100;
 		if( gotoEnd ) {
 			step = 1;
 		}
 		var e = new dojo.animation.AnimationEvent(this, "stop", this.curve.getValue(step),
-			startTime, new Date().valueOf(), endTime, this.duration, percent, Math.round(fps));
+			this._startTime, new Date().valueOf(), this._endTime, this.duration, this._percent, Math.round(fps));
 		if(typeof this.handler == "function") { this.handler(e); }
 		if(typeof this.onStop == "function") { this.onStop(e); }
-		active = false;
-		paused = false;
-	}
+		this._active = false;
+		this._paused = false;
+	},
 
-	this.status = function() {
-		if( active ) {
-			return paused ? "paused" : "playing";
+	status: function() {
+		if( this._active ) {
+			return this._paused ? "paused" : "playing";
 		} else {
 			return "stopped";
 		}
-	}
+	},
 
-	// private methods
-	function cycle() {
-		clearTimeout(timer);
-		if( active ) {
+	// "private" methods
+	_cycle: function() {
+		clearTimeout(this._timer);
+		if( this._active ) {
 			var curr = new Date().valueOf();
-			var step = (curr - startTime) / (endTime - startTime);
-			fps = 1000 / (curr - lastFrame);
-			lastFrame = curr;
+			var step = (curr - this._startTime) / (this._endTime - this._startTime);
+			fps = 1000 / (curr - this._lastFrame);
+			this._lastFrame = curr;
 
 			if( step >= 1 ) {
 				step = 1;
-				percent = 100;
+				this._percent = 100;
 			} else {
-				percent = step * 100;
+				this._percent = step * 100;
 			}
 			
 			// Perform accelleration
@@ -157,30 +163,31 @@ dojo.animation.Animation = function(curve, duration, accel, repeatCount) {
 			}
 
 			var e = new dojo.animation.AnimationEvent(this, "animate", this.curve.getValue(step),
-				startTime, curr, endTime, this.duration, percent, Math.round(fps));
+				this._startTime, curr, this._endTime, this.duration, this._percent, Math.round(fps));
 
 			if(typeof this.handler == "function") { this.handler(e); }
 			if(typeof this.onAnimate == "function") { this.onAnimate(e); }
 
 			if( step < 1 ) {
-				timer = setTimeout(dojo.lang.hitch(this, cycle), 10);
+				this._timer = setTimeout(dojo.lang.hitch(this, "_cycle"), 10);
 			} else {
 				e.type = "end";
-				active = false;
+				this._active = false;
 				if(typeof this.handler == "function") { this.handler(e); }
 				if(typeof this.onEnd == "function") { this.onEnd(e); }
+
 				if( this.repeatCount > 0 ) {
 					this.repeatCount--;
 					this.play(true);
 				} else if( this.repeatCount == -1 ) {
 					this.play(true);
-				} else if( this.animSequence_ ) {
-					this.animSequence_.playNext();
+				} else if( this._animSequence ) {
+					this._animSequence.playNext();
 				}
 			}
 		}
 	}
-};
+});
 
 dojo.animation.AnimationEvent = function(anim, type, coords, sTime, cTime, eTime, dur, pct, fps) {
 	this.type = type; // "animate", "begin", "end", "play", "pause", "stop"
@@ -198,129 +205,134 @@ dojo.animation.AnimationEvent = function(anim, type, coords, sTime, cTime, eTime
 	this.duration = dur;
 	this.percent = pct;
 	this.fps = fps;
-
-	this.coordsAsInts = function() {
+};
+dojo.lang.extend(dojo.animation.AnimationEvent, {
+	coordsAsInts: function() {
 		var cints = new Array(this.coords.length);
 		for(var i = 0; i < this.coords.length; i++) {
 			cints[i] = Math.round(this.coords[i]);
 		}
 		return cints;
 	}
-
-	return this;
-};
+});
 
 dojo.animation.AnimationSequence = function(repeatCount) {
-	var anims = [];
-	var currAnim = -1;
-
 	this.repeatCount = repeatCount || 0;
+}
+dojo.lang.extend(dojo.animation.AnimationSequence, {
+	repeateCount: 0,
 
-	// event handlers
-	this.onBegin = null;
-	this.onEnd = null;
-	this.onNext = null;
-	this.handler = null;
+	_anims: [],
+	_currAnim: -1,
 
-	this.add = function() {
+	onBegin: null,
+	onEnd: null,
+	onNext: null,
+	handler: null,
+
+	add: function() {
 		for(var i = 0; i < arguments.length; i++) {
-			anims.push(arguments[i]);
-			arguments[i].animSequence_ = this;
+			this._anims.push(arguments[i]);
+			arguments[i]._animSequence = this;
 		}
-	}
+	},
 
-	this.remove = function(anim) {
-		for(var i = 0; i < anims.length; i++) {
-			if( anims[i] == anim ) {
-				anims[i].animSequence_ = null;
-				anims.splice(i, 1);
+	remove: function(anim) {
+		for(var i = 0; i < this._anims.length; i++) {
+			if( this._anims[i] == anim ) {
+				this._anims[i]._animSequence = null;
+				this._anims.splice(i, 1);
 				break;
 			}
 		}
-	}
+	},
 
-	this.removeAll = function() {
-		for(var i = 0; i < anims.length; i++) {
-			anims[i].animSequence_ = null;
+	removeAll: function() {
+		for(var i = 0; i < this._anims.length; i++) {
+			this._anims[i]._animSequence = null;
 		}
-		anims = [];
-		currAnim = -1;
-	}
+		this._anims = [];
+		this._currAnim = -1;
+	},
 
-	this.play = function(gotoStart) {
-		if( anims.length == 0 ) { return; }
-		if( gotoStart || !anims[currAnim] ) {
-			currAnim = 0;
+	clear: function() {
+		this.removeAll();
+	},
+
+	play: function(gotoStart) {
+		if( this._anims.length == 0 ) { return; }
+		if( gotoStart || !this._anims[this._currAnim] ) {
+			this._currAnim = 0;
 		}
-		if( anims[currAnim] ) {
-			if( currAnim == 0 ) {
-				var e = {type: "begin", animation: anims[currAnim]};
+		if( this._anims[this._currAnim] ) {
+			if( this._currAnim == 0 ) {
+				var e = {type: "begin", animation: this._anims[this._currAnim]};
 				if(typeof this.handler == "function") { this.handler(e); }
 				if(typeof this.onBegin == "function") { this.onBegin(e); }
 			}
-			anims[currAnim].play(gotoStart);
+			this._anims[this._currAnim].play(gotoStart);
 		}
-	}
+	},
 
-	this.pause = function() {
-		if( anims[currAnim] ) {
-			anims[currAnim].pause();
+	pause: function() {
+		if( this._anims[this._currAnim] ) {
+			this._anims[this._currAnim].pause();
 		}
-	}
+	},
 
-	this.playPause = function() {
-		if( anims.length == 0 ) { return; }
-		if( currAnim == -1 ) { currAnim = 0; }
-		if( anims[currAnim] ) {
-			anims[currAnim].playPause();
+	playPause: function() {
+		if( this._anims.length == 0 ) { return; }
+		if( this._currAnim == -1 ) { this._currAnim = 0; }
+		if( this._anims[this._currAnim] ) {
+			this._anims[this._currAnim].playPause();
 		}
-	}
+	},
 
-	this.stop = function() {
-		if( anims[currAnim] ) {
-			anims[currAnim].stop();
+	stop: function() {
+		if( this._anims[this._currAnim] ) {
+			this._anims[this._currAnim].stop();
 		}
-	}
+	},
 
-	this.status = function() {
-		if( anims[currAnim] ) {
-			return anims[currAnim].status();
+	status: function() {
+		if( this._anims[this._currAnim] ) {
+			return this._anims[this._currAnim].status();
 		} else {
 			return "stopped";
 		}
-	}
+	},
 
-	this.setCurrent = function(anim) {
-		for(var i = 0; i < anims.length; i++) {
-			if( anims[i] == anim ) {
-				currAnim = i;
+	setCurrent: function(anim) {
+		for(var i = 0; i < this._anims.length; i++) {
+			if( this._anims[i] == anim ) {
+				this._currAnim = i;
 				break;
 			}
 		}
-	}
+	},
 
-	this.playNext = function() {
-		if( currAnim == -1 || anims.length == 0 ) { return; }
-		currAnim++;
-		if( anims[currAnim] ) {
-			var e = {type: "next", animation: anims[currAnim]};
+	playNext: function() {
+		if( this._currAnim == -1 || this._anims.length == 0 ) { return; }
+		this._currAnim++;
+		if( this._anims[this._currAnim] ) {
+			var e = {type: "next", animation: this._anims[this._currAnim]};
 			if(typeof this.handler == "function") { this.handler(e); }
 			if(typeof this.onNext == "function") { this.onNext(e); }
-			anims[currAnim].play(true);
+			this._anims[this._currAnim].play(true);
 		} else {
-			var e = {type: "end", animation: anims[anims.length-1]};
+			var e = {type: "end", animation: this._anims[this._anims.length-1]};
 			if(typeof this.handler == "function") { this.handler(e); }
 			if(typeof this.onEnd == "function") { this.onEnd(e); }
 			if(this.repeatCount > 0) {
-				currAnim = 0;
+				this._currAnim = 0;
 				this.repeatCount--;
-				anims[currAnim].play(true);
+				this._anims[this._currAnim].play(true);
 			} else if(this.repeatCount == -1) {
-				currAnim = 0;
-				anims[currAnim].play(true);
+				this._currAnim = 0;
+				this._anims[this._currAnim].play(true);
 			} else {
-				currAnim = -1;
+				this._currAnim = -1;
 			}
 		}
 	}
-};
+});
