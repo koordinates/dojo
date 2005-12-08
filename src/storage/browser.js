32fcd38e09acbcdc016cf180b8fe9118b422008b
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-08 10:44:48 +0000

    last cleanups before I revert to the earlier versions of the storage code since this variant doesn't work.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2367 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/storage/browser.js
+++ src/storage/browser.js
@@ -2,12 +2,12 @@ dojo.provide("dojo.storage.browser");
 dojo.require("dojo.storage");
 dojo.require("dojo.uri.*");
 
-/** Storage provider that uses features in Flash to achieve permanent storage.
+/** 	Storage provider that uses features in Flash to achieve permanent storage.
 		Internally, it uses Flash Shared Objects (Flash 6+) and the External
-		Interface API (Flash 8+) to script and store information in Flash
-		that can later be retrieved. 
+		Interface API (Flash 8+) to script and store information in Flash that
+		can later be retrieved. 
 		
-		@author Alex Russel, alex@dojotoolkit.org
+		@author Alex Russell, alex@dojotoolkit.org
 		@author Brad Neuberg, bkn3@columbia.edu 
 */
 dojo.storage.browser.FlashStorageProvider = function(){
@@ -21,155 +21,52 @@ dojo.storage.browser.FlashStorageProvider = function(){
 	this.initialized = true;
 }
 
-dojo.inherits(dojo.storage.browser.FlashStorageProvider, 
-							dojo.storage.StorageProvider);
+dojo.inherits(	dojo.storage.browser.FlashStorageProvider, 
+				dojo.storage.StorageProvider);
 
 // static, class level methods
-/** Returns whether this storage provider is 
-    available on this platform. Static, class
-    level method that can be called to determine
-    if we can even instantiate this storage
-    provider on this platform.
+/*
+	Returns whether this storage provider is available on this platform.
+	Static, class level method that can be called to determine if we can even
+	instantiate this storage provider on this platform.
 
-    @returns True or false if this storage 
-    provider is supported.
- */
+    @returns True or false if this storage provider is supported.
+*/
 dojo.storage.browser.FlashStorageProvider.isAvailable = function(){
 	return true;
 }
 
-/** Returns whether this provider can be installed,
-		to upgrade a platform to have the features
-		necessary to use this storage provider. */
+/** Returns whether this provider can be installed, to upgrade a platform to
+ * have the features necessary to use this storage provider. */
 dojo.storage.browser.FlashStorageProvider.isInstallable = function(){
 	return false;
 }
 
-/** If this provider can be installed at runtime,
-		does so. */
-dojo.storage.browser.FlashStorageProvider.install = function(){
-}
+dojo.storage.browser.FlashStorageProvider.install = function(){ }
 
 
 // instance methods and properties
 dojo.lang.extend(dojo.storage.browser.FlashStorageProvider, {
 	initialized: false,
 	
-	/** Puts a key and value into this storage system.
-
-    @param key A string key to use when retrieving 
-           this value in the future.
-    @param value A value to store; this can be 
-           any JavaScript type.
-    @param resultsHandler A callback function 
-           that will receive two arguments.
-           The first argument is one of three 
-           values: dojo.storage.SUCCESS,
-           dojo.storage.FAILED, or 
-           dojo.storage.PENDING; these values 
-           determine how the put request went. 
-           In some storage systems users can deny
-           a storage request, resulting in a 
-           dojo.storage.FAILED, while in 
-           other storage systems a storage 
-           request must wait for user approval,
-           resulting in a dojo.storage.PENDING 
-           status until the request
-           is either approved or denied, 
-           resulting in another call back
-           with dojo.storage.SUCCESS. 
-    
-    The second argument in the call back is an 
-    optional message that details possible error 
-    messages that might have occurred during
-    the storage process.
-    
-    Example:
-      var resultsHandler = function(status, message){
-        alert("status="+status+", message="+message);
-      };
-      dojo.storage.put("test", "hello world", 
-                       resultsHandler);
-	*/
 	put: function(key, value, resultsHandler){
 		// FIXME: Modify Flash to do results handler callback
 		this.flash.set(key, value, dojo.storage.manager.namespace);
 	},
 
-	/** Gets the value with the given key. Returns null
-	    if this key is not in the storage system.
-	
-	    @param key A string key to get the value of.
-	    @returns Returns any JavaScript object type; 
-	    null if the key is not
-	    present. */
 	get: function(key){
 		var results = this.flash.get(key, dojo.storage.manager.namespace);
 		return results;
 	},
 
-	/** Determines whether the storage has the given 
-	    key. 
-	
-	      @returns Whether this key is 
-	               present or not. */
-	hasKey: function(key){
-		if (this.get(key) != null)
-			return true;
-		else
-			return false;
-	},
-
-	/** Enumerates all of the available keys in 
-	    this storage system.
-	
-	    @returns Array of string keys in this 
-	             storage system.
-	 */
-	getKeys: function(){},
-
-	/** Completely clears this storage system of all 
-	    of it's values and keys. */
-	clear: function(){},
-
-	/** Returns whether this storage provider's 
-	    values are persisted when this platform 
-	    is shutdown. 
-	
-	    @returns True or false whether this 
-	    storage is permanent. */
 	isPermanent: function(){
 		return false;
 	},
 
-	/** The maximum storage allowed by this provider.
-	
-	    @returns Returns the maximum storage size 
-	             supported by this provider, in 
-	             thousands of bytes (i.e., if it 
-	             returns 60 then this means that 60K 
-	             of storage is supported).
-	    
-	             If this provider can not determine 
-	             it's maximum size, then 
-	             dojo.storage.SIZE_NOT_AVAILABLE is 
-	             returned; if there is no theoretical
-	             limit on the amount of storage 
-	             this provider can return, then
-	             dojo.storage.SIZE_NO_LIMIT is 
-	             returned. */
 	getMaximumSize: function(){},
 
-	/** Determines whether this provider has a 
-	    settings UI.
-	
-	    @returns True or false if this provider has 
-	             the ability to show a
-	             a settings UI to change it's 
-	             values, change the amount of storage
-	             available, etc. */
 	hasSettingsUI: function(){
-		return false;
+		return true;
 	},
 
 	/** If this provider has a settings UI, it is 
