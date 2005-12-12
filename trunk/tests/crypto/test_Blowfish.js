a7aa4736ac92383d725f3979affc3785598f1252
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-12 18:47:27 +0000

    Working Blowfish implementation.  Works only in ECB mode right now.
    git-svn-id: http://svn.dojotoolkit.org/src@2400 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/tests/crypto/test_Blowfish.js
+++ trunk/tests/crypto/test_Blowfish.js
@@ -3,17 +3,16 @@ dojo.require("dojo.crypto.Blowfish");
 function test_Blowfish_encryption(){
 	var message = "The rain in Spain falls mainly on the plain.";
 	var key = "foobar";
-	var base64Encrypted = "0nFmvmtqbrp9AIOSbn4dWMCKcPnqgCQBDldoXZiiGh+8s7mol7Zp8H/QGrJLmkT9";
-
-	var result = dojo.crypto.toBase64(dojo.crypto.Blowfish.encrypt(message, key));
+	var base64Encrypted = "WI5J5BPPVBuiTniVcl7KlIyNMmCosmKTU6a/ueyQuoUXyC5dERzwwdzfFsiU4vBw";
+	result = dojo.crypto.Blowfish.encrypt(message, key);
 	jum.assertEquals("BlowfishEncryption", base64Encrypted, result);
 }
 
 function test_Blowfish_decryption(){
 	var message = "The rain in Spain falls mainly on the plain.";
 	var key = "foobar";
-	var base64Encrypted = "0nFmvmtqbrp9AIOSbn4dWMCKcPnqgCQBDldoXZiiGh+8s7mol7Zp8H/QGrJLmkT9";
+	var base64Encrypted = "WI5J5BPPVBuiTniVcl7KlIyNMmCosmKTU6a/ueyQuoUXyC5dERzwwdzfFsiU4vBw";
 
-	result = dojo.crypto.Blowfish.decrypt(dojo.crypto.fromBase64(base64Encrypted), key);
+	result = dojo.crypto.Blowfish.decrypt(base64Encrypted, key);
 	jum.assertEquals("BlowfishDecryption", message, result);
 }
