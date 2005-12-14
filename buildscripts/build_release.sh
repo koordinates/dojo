8c0d4f57b742511da6f9a00f14d251cfe7fd9156
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-14 23:52:43 +0000

    ensuring that the special meaning of requireAfterIf and requireAfter don't screw us ove when used inside of a build
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2497 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/build_release.sh
+++ buildscripts/build_release.sh
@@ -12,7 +12,7 @@ do
 	profile=`echo $pfile | sed 's/.profile.js//g'`
 	echo Building profile: $profile
 	ant -q -Ddocless=true -Dprofile=$profile release
-	proName=dojo-0.1.0-plus-$profile
+	proName=dojo-0.2.0-$profile
 	cd ../release
 	mv dojo $proName
 	tar -zcf $OUT_DIR/$proName.tar.gz $proName/
