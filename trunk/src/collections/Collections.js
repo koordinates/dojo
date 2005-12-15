de231bbccff1f2a3e8faab7c7720dd7339447537
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-15 18:27:01 +0000

    Bah.  AGAIN, AGAIN, AGAIN.
    git-svn-id: http://svn.dojotoolkit.org/src@2515 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/collections/Collections.js
+++ trunk/src/collections/Collections.js
@@ -12,14 +12,13 @@ dojo.collections.Iterator = function(a){
 	var obj = a;
 	var position = 0;
 	this.atEnd = (position>=obj.length-1);
-	if(this.atEnd) dojo.raise("dojo.collections.Iterator.ctor: array passed to ctor is empty.");
 	this.current = obj[position];
 	this.moveNext = function(){
-		if(++position==obj.length){
+		if(++position>=obj.length){
 			this.atEnd = true;
 		}
 		if(this.atEnd){
-			dojo.raise("dojo.collections.Iterator.moveNext: iterator is at end position.");
+			return;
 		}
 		this.current=obj[position];
 	}
@@ -34,17 +33,16 @@ dojo.collections.DictionaryIterator = function(obj){
 	for (var p in obj) arr.push(obj[p]) ;	//	fill it up
 	var position = 0 ;
 	this.atEnd = (position>=arr.length-1);
-	if(this.atEnd) dojo.raise("dojo.collections.DictionaryIterator.ctor: object passed to ctor has no properties.");
 	this.current = arr[position] ;
 	this.entry = this.current ;
 	this.key = this.entry.key ;
 	this.value = this.entry.value ;
 	this.moveNext = function() { 
-		if (++position == arr.length) {
+		if (++position>=arr.length) {
 			this.atEnd = true ;
 		}
 		if(this.atEnd){
-			dojo.raise("dojo.collections.DictionaryIterator.moveNext: iterator is at end position.");
+			return;
 		}
 		this.entry = this.current = arr[position] ;
 		if (this.entry) {
