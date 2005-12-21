96e75a95795faa6e78f6aaf2da85828a61de3b4a
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-21 18:58:16 +0000

    make htmlify dump content in the html sibling directory
    git-svn-id: http://svn.dojotoolkit.org/src@2572 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/documents/manual/rest/htmlify.py
+++ trunk/documents/manual/rest/htmlify.py
@@ -1,10 +1,15 @@
+#! /usr/bin/python
 import os, sys, glob
 
 ffiles = []
 for root, dirs, files in list(os.walk('.')):
    ffiles += [os.path.join(root,f) for f in files if f.endswith('.rest')]
 for source in ffiles:
-   print source
-   target = os.path.splitext(source)[0] + '.html'
+   (tdir, tfn) = os.path.split(source)
+   tdir = os.path.join("../html/", tdir)
+   if not os.path.isdir(tdir):
+   	os.makedirs(tdir)
+   target = os.path.join(tdir, os.path.splitext(tfn)[0]+'.html')
    print source, target
+   target = os.path.abspath(target)
    os.system('rst2html.py --stylesheet-path=./screen.css  %s %s' % (source, target))
