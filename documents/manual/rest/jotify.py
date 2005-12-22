dc0f085c2f95771402fedad23de3c2e1dcdbb751
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-22 12:37:18 +0000

    minor updates
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2576 560b804f-0ae3-0310-86f3-f6aa0a117693

--- documents/manual/rest/jotify.py
+++ documents/manual/rest/jotify.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-import os, sys, glob
+import os, sys, glob, shutil
 
 ffiles = []
 for root, dirs, files in list(os.walk('.')):
@@ -9,7 +9,9 @@ for source in ffiles:
    tdir = os.path.join("../html/", tdir)
    if not os.path.isdir(tdir):
    	os.makedirs(tdir)
-   target = os.path.join(tdir, os.path.splitext(tfn)[0]+'.xml')
+   target = os.path.join(tdir, os.path.splitext(tfn)[0]+'.html.xml')
    print source, target
    target = os.path.abspath(target)
-   os.system('rst2html.py --stylesheet-path=./screen.css  %s %s' % (source, target))
+   os.system('rst2html.py --stylesheet-path=./blank.css  %s %s' % (source, target))
+
+shutil.copy("index.html", "../html/index.html.xml")
