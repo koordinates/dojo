851236fbb25b0c9c498520aef35383bf3d840ce5
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2005-12-30 12:14:48 +0000

    updating to support the (obj, "funcname") calling syntax for addCallback and addErrback
    git-svn-id: http://svn.dojotoolkit.org/src@2612 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/rpc/DeferredRequest.js
+++ trunk/src/rpc/DeferredRequest.js
@@ -1,65 +1,70 @@
 dojo.provide("dojo.rpc.DeferredRequest");
 dojo.require("dojo.lang");
 
-dojo.rpc.DeferredRequest = function() {
-	this.Callbacks = [];
-	this.Errbacks = [];
+dojo.rpc.DeferredRequest = function(){
+	this._callbacks = [];
+	this._errbacks = [];
 	this.results = null;
 	this.error = null;
 };
 
 dojo.lang.extend(dojo.rpc.DeferredRequest, {
-
-	addCallback: function(cb) {
-
-		if (dojo.lang.isFunction(cb)) {
-			if (this.results != null) {
-				cb(this.results);
+	
+	getFunctionFromArgs: function(){
+		var a = arguments;
+		if((a[0])&&(!a[1])){
+			if(dojo.lang.isFunction(a[0])){
+				return a[0];
+			}else if(dojo.lang.isString(a[0])){
+				return dj_global[a[0]];
 			}
+		}else if((a[0])&&(a[1])){
+			return dojo.lang.hitch(a[0], a[1]);
+		}
+		return null;
+	},
 
-			this.Callbacks.push(cb);
-
-		} else {
+	addCallback: function(cb, cbfn){
+		var enclosed = this.getFunctionFromArgs(cb, cbfn)
+		if(enclosed){
+			this._callbacks.push(enclosed);
+			if(this.results){
+				enclosed(this.results);
+			}
+		}else{
 			dojo.raise("DeferredRequest: object supplied to addCallback is not a function");
 		}
-        },
+	},
 
-	addErrback: function(eb) {
-		if (dojo.lang.isFunction(eb)) {
-
-			if (this.error!=null) {
-				eb(this.error);
+	addErrback: function(eb, ebfn){
+		var enclosed = this.getFunctionFromArgs(eb, ebfn)
+		if(enclosed){
+			this._errbacks.push(enclosed);
+			if(this.error){
+				enclosed(this.error);
 			}	
-
-                	this.Errbacks.push(eb);
-		} else {
+		}else{
 			dojo.raise("DeferredRequest: object supplied to addErrback is not a function");
 		}
-        },
+	},
 
-	addBoth: function(cb, eb) {
+	addBoth: function(cb, eb){
 		this.addCallback(cb);
 		this.addErrback(eb);
 	}
 
-	callback: function(results) {
+	callback: function(results){
 		this.results = results;
-		
-		if (this.Callbacks.length > 0) {
-			for (var x=0; x<this.Callbacks.length;x++) {
-				this.Callbacks[x](results);
-			}
-		}
+		dojo.lang.forEach(this._callbacks, function(func){
+			func(results);
+		});
 	},
 
-	errback: function(error) {
+	errback: function(error){
 		this.error = error;
-		
-		if (this.Errbacks.length > 0) {
-			for (var x=0; x<this.Errbacks.length;x++) {
-				this.Errbacks[x](results);
-			}
-		}
+		dojo.lang.forEach(this._errbacks, function(func){
+			func(error);
+		});
 	}
 
 });
