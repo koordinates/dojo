a0b6e3620044315c2bb728929508ec8ad96c5009
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-02 06:43:00 +0000

    more incremental work on the svg HslColorPicker
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2640 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/svg/HslColorPicker.js
+++ src/widget/svg/HslColorPicker.js
@@ -10,14 +10,14 @@ dojo.require("dojo.graphics.color");
 dojo.widget.svg.HslColorPicker=function(){
 	dojo.widget.HslColorPicker.call(this);
 	dojo.widget.HtmlWidget.call(this);
+	this.hue = "0";
+	this.saturation = "0";
+	this.light = "0";
+	this.storedColor = "#000000";
 };
 dojo.inherits(dojo.widget.svg.HslColorPicker, dojo.widget.HtmlWidget);
 dojo.lang.extend(dojo.widget.svg.HslColorPicker, {
 	//	widget props
-	hue: "0",
-	saturation: "0",
-	light: "0",
-	storedColor: "#000000",
 	templatePath: dojo.uri.dojoUri("src/widget/templates/HslColorPicker.svg"),
 	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HslColorPicker.css"),
 	fillInTemplate: function() {
@@ -25,16 +25,36 @@ dojo.lang.extend(dojo.widget.svg.HslColorPicker, {
 		dojo.svg.g.suspend();
 		this.hueSliderNode.setAttributeNS(dojo.dom.xmlns.xlink, "href", dojo.uri.dojoUri("src/widget/templates/images/hue.png"));
 		dojo.svg.g.resume();
+		this.setSaturationStopColors();
+		this.setHueSlider();
+		this.setSaturationLightSlider();
 	},
-	onHueClick: function(evt) {
-		// get the position that was clicked on the element
-		// FIXME: handle document scrolling
-		var yPosition = evt.clientY - evt.target.getAttribute("y");
-		this.hue = 360 - parseInt((parseInt(yPosition))*(360/parseInt(this.height)));
+	setSaturationStopColors: function() {
 		this.leftGradientStopColor = "rgb(" + dojo.graphics.color.hsl2rgb(this.hue, 0, 50).join(", ") + ")";
 		this.rightGradientStopColor = "rgb(" + dojo.graphics.color.hsl2rgb(this.hue, 100, 50).join(", ") + ")";
 		this.leftGradientColorNode.setAttributeNS(null,'stop-color',leftGradientStopColor);
 		this.rightGradientColorNode.setAttributeNS(null,'stop-color',rightGradientStopColor);
+	},
+	setHue: function(hue) {
+		this.hue = hue;
+	},
+	setHueSlider: function() {
+		// FIXME: need to add some padding around the picker so you can see the slider at the top and bottom of the picker)
+		this.hueSliderNode.setAttribute("y", parseInt((hue/360) * parseInt(this.height) - 2) + "px" );
+	}
+	setSaturationLight: function(saturation, light) {
+		this.saturation = saturation;
+		this.light = light;
+	},
+	setSaturationLightSlider: function() {
+		// TODO
+	}
+	onHueClick: function(evt) {
+		// get the position that was clicked on the element
+		// FIXME: handle document scrolling
+		var yPosition = parseInt(evt.clientY - evt.target.getAttribute("y"));
+		this.setHue( 360 - parseInt(yPosition*(360/parseInt(this.height))) );
+		this.setSaturationStopColors();
 		this.setStoredColor(dojo.graphics.color.hsl2hex(this.hue, this.saturation, this.light).join(""));
 	},
 	onHueDrag: function(evt) {
