322939152165724830f1bdd7df4f31d558866269
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-02 06:25:16 +0000

    more incremental work on the svg HslColorPicker, add hsl2hex shortcut method to dojo.graphics.color
    git-svn-id: http://svn.dojotoolkit.org/src@2638 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/svg/HslColorPicker.js
+++ trunk/src/widget/svg/HslColorPicker.js
@@ -14,11 +14,39 @@ dojo.widget.svg.HslColorPicker=function(){
 dojo.inherits(dojo.widget.svg.HslColorPicker, dojo.widget.HtmlWidget);
 dojo.lang.extend(dojo.widget.svg.HslColorPicker, {
 	//	widget props
+	hue: "0",
+	saturation: "0",
+	light: "0",
+	storedColor: "#000000",
 	templatePath: dojo.uri.dojoUri("src/widget/templates/HslColorPicker.svg"),
 	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HslColorPicker.css"),
 	fillInTemplate: function() {
+		this.height = "131px";
 		dojo.svg.g.suspend();
-		this.hueSliderNode.setAttributeNS("http://www.w3.org/1999/xlink", "href", dojo.uri.dojoUri("src/widget/templates/images/hue.png"));
+		this.hueSliderNode.setAttributeNS(dojo.dom.xmlns.xlink, "href", dojo.uri.dojoUri("src/widget/templates/images/hue.png"));
 		dojo.svg.g.resume();
+	},
+	onHueClick: function(evt) {
+		// get the position that was clicked on the element
+		// FIXME: handle document scrolling
+		var yPosition = evt.clientY - evt.target.getAttribute("y");
+		this.hue = 360 - parseInt((parseInt(yPosition))*(360/parseInt(this.height)));
+		this.leftGradientStopColor = "rgb(" + dojo.graphics.color.hsl2rgb(this.hue, 0, 50).join(", ") + ")";
+		this.rightGradientStopColor = "rgb(" + dojo.graphics.color.hsl2rgb(this.hue, 100, 50).join(", ") + ")";
+		this.leftGradientColorNode.setAttributeNS(null,'stop-color',leftGradientStopColor);
+		this.rightGradientColorNode.setAttributeNS(null,'stop-color',rightGradientStopColor);
+		this.setStoredColor(dojo.graphics.color.hsl2hex(this.hue, this.saturation, this.light).join(""));
+	},
+	onHueDrag: function(evt) {
+		// TODO
+	},
+	onSaturationLightClick: function(evt) {
+		// TODO
+	},
+	onSaturationLightDrag: function(evt) {
+		// TODO
+	},
+	setStoredColor: function(rgbHexColor) {
+		this.storedColor = rgbHexColor;
 	}
 });
