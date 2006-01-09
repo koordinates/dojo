a6345002a9d3ebf14f4574a4a61e9f1ce0972aad
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-09 21:35:03 +0000

    util fix (.html, not.xml) plus some doc cleanup
    git-svn-id: http://svn.dojotoolkit.org/src@2724 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/documents/manual/rest/htmlify.py
+++ trunk/documents/manual/rest/htmlify.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 import os, sys, glob
 
 ffiles = []
@@ -9,7 +9,7 @@ for source in ffiles:
    tdir = os.path.join("../html/", tdir)
    if not os.path.isdir(tdir):
    	os.makedirs(tdir)
-   target = os.path.join(tdir, os.path.splitext(tfn)[0]+'.xml')
-   print source, target
+   target = os.path.join(tdir, os.path.splitext(tfn)[0]+'.html')
+   print os.path.basename(source), "->", os.path.basename(target)
    target = os.path.abspath(target)
    os.system('rst2html.py --stylesheet-path=./screen.css  %s %s' % (source, target))
