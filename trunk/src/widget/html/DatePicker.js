e8ea7a09080c0174adf96af84247417fb8d422cd
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-15 03:39:32 +0000

    Don't do dojo.require("dojo.widget.DatePicker.util"); It tries to pull in the file called DatePicker/util.js, which doesn't exist.
    Although this works in a filesystem environment but when accessing over a webserver (at
    least dojotoolkit.org), the server returns an HTML page saying "file not found" in lieu of
    a javascript file, which naturally causes the browser to print some error message about
    invalid javascript.
    
    Try demos/Mail.html for a testcase.
    
    git-svn-id: http://svn.dojotoolkit.org/src@2796 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/html/DatePicker.js
+++ trunk/src/widget/html/DatePicker.js
@@ -2,7 +2,6 @@ dojo.provide("dojo.widget.html.DatePicker");
 dojo.require("dojo.widget.*");
 dojo.require("dojo.widget.HtmlWidget");
 dojo.require("dojo.widget.DatePicker");
-dojo.require("dojo.widget.DatePicker.util");
 dojo.require("dojo.event.*");
 dojo.require("dojo.html");
 
