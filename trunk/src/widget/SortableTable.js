350d988d4dfbb69edafc352b0e42ce7c00ab7aa8
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-19 21:50:58 +0000

    test page passes now
    git-svn-id: http://svn.dojotoolkit.org/src@2870 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/SortableTable.js
+++ trunk/src/widget/SortableTable.js
@@ -1,5 +1,7 @@
 dojo.provide("dojo.widget.SortableTable");
-dojo.require("dojo.widget.DomWidget");
+dojo.require("dojo.widget.*");
+dojo.requireAfterIf("html", "dojo.widget.html.SortableTable");
+dojo.widget.tags.addParseTreeHandler("dojo:sortableTable");
 
 //	set up the general widget
 dojo.widget.SortableTable=function(){
@@ -21,44 +23,3 @@ dojo.widget.SortableTable=function(){
 	this.sortFunctions={};	//	you can add to this if needed.
 };
 dojo.inherits(dojo.widget.SortableTable, dojo.widget.Widget);
-dojo.widget.tags.addParseTreeHandler("dojo:sortableTable");
-dojo.requireAfterIf("html", "dojo.widget.html.SortableTable");
-
-dojo.lang.extend(dojo.widget.SortableTable, {
-	getTypeFromString:function(s){
-		var parts=s.split("."),i=0,obj=dj_global; 
-		do{obj=obj[parts[i++]];}while(i<parts.length&&obj); 
-		return(obj!=dj_global)?obj:null;
-	},
-	
-	compare:function(o1, o2){
-		//	we will compare property values, and only at the top level.
-		for(var p in o1){
-			if(!o2[p]) return false;
-			if(o1[p]!=o2[p]) return false;
-		}
-		return true;
-	},
-	getSelection:function(){
-		return this.selected;
-	},
-	parseData:function(data){
-		//	this is for receiving raw JSON objects.  We expect an array
-		//	of objects that have properties matching either the field of a
-		//	column or the label on the column.  If we don't find that
-		//	property, we set it to the type's default value.
-		//	NB: you MUST render manually.
-		this.data=[];
-		for(var i=0; i<data.length; i++){
-			var o={};	//	new data object.
-			for(var j=0; j<this.columns.length; j++){
-				var field=this.columns[j].getField();
-				var type=this.columns[j].getType();
-				var val=data[i][field];
-				if (val) o[field]=new type(val);
-				else o[field]=new type();	//	let it use the default.
-			}
-			this.data.push(o);
-		}
-	}
-});
--- trunk/src/widget/html/SortableTable.js
+++ trunk/src/widget/html/SortableTable.js
@@ -25,6 +25,45 @@ dojo.lang.extend(dojo.widget.html.SortableTable, {
 	templatePath:null,
 	templateCssPath:null,
 
+	getTypeFromString:function(s){
+		var parts=s.split("."),i=0,obj=dj_global; 
+		do{obj=obj[parts[i++]];}while(i<parts.length&&obj); 
+		return(obj!=dj_global)?obj:null;
+	},
+	
+	compare:function(o1, o2){
+		//	we will compare property values, and only at the top level.
+		for(var p in o1){
+			if(!o2[p]) return false;
+			if(o1[p]!=o2[p]) return false;
+		}
+		return true;
+	},
+
+	getSelection:function(){
+		return this.selected;
+	},
+
+	parseData:function(data){
+		//	this is for receiving raw JSON objects.  We expect an array
+		//	of objects that have properties matching either the field of a
+		//	column or the label on the column.  If we don't find that
+		//	property, we set it to the type's default value.
+		//	NB: you MUST render manually.
+		this.data=[];
+		for(var i=0; i<data.length; i++){
+			var o={};	//	new data object.
+			for(var j=0; j<this.columns.length; j++){
+				var field=this.columns[j].getField();
+				var type=this.columns[j].getType();
+				var val=data[i][field];
+				if (val) o[field]=new type(val);
+				else o[field]=new type();	//	let it use the default.
+			}
+			this.data.push(o);
+		}
+	}, 
+
 	//	widget-specific methods
 	getObjectFromRow:function(row){
 		var cells=row.getElementsByTagName("td");
