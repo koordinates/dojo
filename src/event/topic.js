89bdec1c3f3e763449676bf152fe005e75768b33
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-20 00:45:42 +0000

    support passing the arguments object along
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2879 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/event/topic.js
+++ src/event/topic.js
@@ -31,7 +31,7 @@ dojo.event.topic = new function(){
 		// if message is an array, we treat it as a set of arguments,
 		// otherwise, we just pass on the arguments passed in as-is
 		var args = [];
-		if(arguments.length == 2 && dojo.lang.isArray(message)){
+		if(arguments.length == 2 && (dojo.lang.isArray(message) || message.callee)){
 			args = message;
 		}else{
 			var args = [];
