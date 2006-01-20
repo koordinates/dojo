b056a70d1a23ce0cb2a2da3453576e8a410a4a95
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-20 21:24:37 +0000

    removing dojo.string dependency
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2899 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/Widget.js
+++ src/widget/Widget.js
@@ -4,7 +4,6 @@ dojo.provide("dojo.widget.tags");
 dojo.require("dojo.lang");
 dojo.require("dojo.widget.Manager");
 dojo.require("dojo.event.*");
-dojo.require("dojo.string");
 
 dojo.widget.Widget = function(){
 	// these properties aren't primitives and need to be created on a per-item
@@ -293,7 +292,7 @@ dojo.lang.extend(dojo.widget.Widget, {
 						for(var y=0; y<pairs.length; y++){
 							var si = pairs[y].indexOf(":");
 							if((si != -1)&&(pairs[y].length>si)){
-								this[x][dojo.string.trim(pairs[y].substr(0, si))] = pairs[y].substr(si+1);
+								this[x][pairs[y].substr(0, si).replace(/^\s+|\s+$/g, "")] = pairs[y].substr(si+1);
 							}
 						}
 					}else{
