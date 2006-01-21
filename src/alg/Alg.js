4c3fc41a64337c6d02c82ccec66b34812f70483d
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-21 03:13:05 +0000

    missed one
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2911 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/alg/Alg.js
+++ src/alg/Alg.js
@@ -7,3 +7,5 @@ dojo.alg = dojo.lang;
 dojo.alg.for_each = dojo.alg.forEach; // burst compat
 
 dojo.alg.for_each_call = dojo.alg.map; // burst compat
+
+dojo.alg.inArr = dojo.lang.inArray; // burst compat
