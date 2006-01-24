1bc00f0011ad5f2664975d2e9744800cde60d33a
david <david@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-01-24 18:56:05 +0000

    an undo manager, complete with nested transaction support
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@2966 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/undo/Manager.js
+++ src/undo/Manager.js
@@ -1,81 +1,122 @@
 dojo.provide("dojo.undo.Manager");
+dojo.require("dojo.lang");
 
-dojo.undo.Manager = function () {
+dojo.undo.Manager = function(parent) {
+	this.clear();
+	this._parent = parent;
+};
+dojo.lang.extend(dojo.undo.Manager, {
+	_parent: null,
+	_undoStack: null,
+	_redoStack: null,
+	_currentManager: null,
 
-	this._undoStack = [];
-	this._redoStack = [];
-
-	this._undoRegistrationLevel = 0;
-}
-
-dojo.undo.Manager.prototype = {
-
-//Registering undo operations
-	//registerUndoWithTarget:selector:object:
-	prepareWithInvocationTarget: function () {},
-	forwardInvocation: function () {},
-
-//Checking undo ability
 	canUndo: false,
 	canRedo: false,
 
-//Performing undo and redo
-	undo: function () {},
-	undoNestedGroup: function () {},
-	redo: function () {},
-
-//Limiting the undo stack
-	setLevelsOfUndo: function (levels) {
-		this.levelsOfUndo = levels;
-		if (levels != 0 && this._undoStack.length > levels) {
-			this._undoStack.splice(levels, this._undoStack.length - levels);
-		}
-	},
-	levelsOfUndo: 0,
-
-//Creating undo groups
-	beginUndoGrouping: function () {},
-	endUndoGrouping: function () {},
-	enableUndoRegistration: function () {
-		if (++this._undoRegistrationLevel >= 0) {
-			this._undoRegistrationLevel = 0;
-			this.isUndoRegistrationEnabled = true;
-		}
-	},
-	groupsByEvent: true,
-	setGroupsByEvent: function (bool) { this.groupsByEvent = bool; },
-	groupingLevel: 0,
-
-//Disabling undo
-	disableUndoRegistration = function () {
-		this.isUndoRegistrationEnabled = false;
-		this._undoRegistrationLevel--;
-	},
-	isUndoRegistrationEnabled: true,
-
-//Checking whether undo or redo is being performed
 	isUndoing: false,
 	isRedoing: false,
 
-//Clearing undo operations
-	removeAllActions: function () {
+	// events which you can use to munge the item to be
+	// undone/redone, usually you just use it for notification though
+	onUndo: function(top) {},
+	onRedo: function(top) {},
+
+	_updateStatus: function() {
+		this.canUndo = this._undoStack.length > 0;
+		this.canRedo = this._redoStack.length > 0;
+	},
+
+	clear: function() {
 		this._undoStack = [];
 		this._redoStack = [];
+		this._currentManager = this;
+		this.canUndo = false;
+		this.canRedo = false;
+		this.isUndoing = false;
+		this.isRedoing = false;
 	},
-	removeAllActionsWithTarget: function (target) {},
 
-//Setting and getting the action name
-	setActionName: function () {},
-	redoActionName: null,
-	undoActionName: null,
+	undo: function() {
+		if(!this.canUndo) { return false; }
 
-//Getting and localizing menu item title
-	redoMenuItemTitle: null,
-	undoMenuItemTitle: null,
-	redoMenuTitleForUndoActionName: function () {},
-	undoMenuTitleForUndoActionName: function () {},
-      
-//Working with run loops
-	runLoopModes: [],
-	setRunLoopModes: function () {}
-}
+		this.isUndoing = true;
+		this.onUndo(top);
+		var top = this._undoStack.pop();
+		if(top instanceof this.constructor) {
+			top.undoAll();
+		} else {
+			top.undo();
+		}
+		if(top.redo) {
+			this._redoStack.push(top);
+		}
+		this.isUndoing = false;
+
+		this._updateStatus();
+		return true;
+	},
+
+	redo: function() {
+		if(!this.canRedo) { return false; }
+
+		this.isRedoing = true;
+		var top = this._redoStack.pop();
+		this.onRedo(top);
+		if(top instanceof this.constructor) {
+			top.redoAll();
+		} else {
+			top.redo();
+		}
+		this._undoStack.push(top);
+		this.isRedoing = false;
+
+		this._updateStatus();
+		return true;
+	},
+
+	undoAll: function() {
+		while(this._undoStack.length > 0) {
+			this.undo();
+		}
+	},
+
+	redoAll: function() {
+		while(this._redoStack.length > 0) {
+			this.redo();
+		}
+	},
+
+	push: function(undo, redo /* optional */) {
+		if(this._currentManager == this) {
+			this._undoStack.push({
+				undo: undo,
+				redo: redo
+			});
+		} else {
+			this._currentManager.push.apply(this._currentManager, arguments);
+		}
+		this._updateStatus();
+	},
+
+	beginTransaction: function() {
+		if(this._currentManager == this) {
+			var mgr = new dojo.undo.Manager(this);
+			this._undoStack.push(mgr);
+			this._currentManager = mgr;
+		} else {
+			this._currentManager.beginTransaction.apply(this._currentManager, arguments);
+		}
+	},
+
+	endTransaction: function() {
+		if(this._currentManager == this) {
+			if(this._parent) {
+				this._parent._currentManager = this._parent;
+				this._parent.endTransaction.apply(this._parent, arguments);
+			}
+		} else {
+			this._currentManager.endTransaction.apply(this._currentManager, arguments);
+		}
+	}
+});
