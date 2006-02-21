f3c89ca915984c5b3ab7ddd9833039791c248b0b
pottedmeat <pottedmeat@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-02-21 02:52:30 +0000

    Sorting properly, dealing with packages properly
    git-svn-id: http://svn.dojotoolkit.org/src@3124 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/docscripts/doctool.js
+++ trunk/docscripts/doctool.js
@@ -9,28 +9,44 @@ function docToolInit(){
 		mimetype:	"text/json",
 		load:			function(type, data, evt){
 			var searchData = [];
-			
+
 			for(var key in data){
+				// Packages starting with _ have a parent of "dojo"
 				if(key.charAt(0) == "_"){
 					var new_key = "dojo" + key.substring(1, key.length);
 					data[new_key] = data[key];
 					delete data[key];
 					key = new_key;
 				}
-				if(!dojo.lang.inArray(data[key], key)){
-					searchData.push([key, key]);
-				}
+				// Function names starting with _ have a parent of their package name
 				for(var pkg_key in data[key]){
 					if(data[key][pkg_key].charAt(0) == "_"){
 						var new_value = key + data[key][pkg_key].substring(1, data[key][pkg_key].length);
 						data[key][pkg_key] = new_value;
 					}
+				}
+				// Add the package if it doesn't exist in its children
+				if(!dojo.lang.inArray(data[key], key)){
+					searchData.push([key, key]);
+				}
+				// Add the functions
+				for(var pkg_key in data[key]){
 					searchData.push([data[key][pkg_key], data[key][pkg_key]]);
 				}
 			}
-			dojo.widget.byId("search").dataProvider.setData(searchData);
+			dojo.widget.byId("search").dataProvider.setData(searchData.sort(docToolSort));
 		}
 	});
+}
+
+function docToolSort(a, b){
+	if(a[0] < b[0]){
+		return -1;
+	}
+	if(a[0] > b[0]){
+		return 1;
+	}
+  return 0
 }
 
 dojo.event.connect(window, "onload", docToolInit);
