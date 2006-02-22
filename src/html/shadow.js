2377522353884449b798a0645ae4f7b0e6dc427f
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-02-22 16:06:17 +0000

    move shadow stuff to CSS based system
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3157 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/html/shadow.js
+++ src/html/shadow.js
@@ -3,23 +3,46 @@ dojo.provide("dojo.html.shadow");
 dojo.require("dojo.lang");
 dojo.require("dojo.uri");
 
+if(dojo.render.html.ie){
+	dojo.style.insertCssFile(dojo.uri.dojoUri("src/html/shadowIE.css"));
+}else{
+	dojo.style.insertCssFile(dojo.uri.dojoUri("src/html/shadow.css"));
+}
+
 dojo.html.shadow = function(node) {
 	this.init(node);
 }
 
 dojo.lang.extend(dojo.html.shadow, {
-
 	shadowPng: dojo.uri.dojoUri("src/html/images/shadow"),
-	shadowThickness: 8,
 
 	init: function(node){
 		this.node=node;
 
+		var pieces = [ "T", "TL", "TR", "L", "R", "BL", "BR", "B" ];
+		var urlBase = this.shadowPng;
+		
+		dojo.lang.forEach(pieces,
+			function(piece) {
+				var child = document.createElement("div");
+				child.className="dojoShadow"+piece;
+				node.appendChild(child);
+				
+				// to get the pathnames right we have to generate the CSS :-(
+				if(dojo.render.html.ie){
+					var url = urlBase + piece + ".png";
+					sizing=(dojo.lang.inArray(['T','L','R','B'],piece)?"scale":"");
+					child.style.filter=
+						"progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+url+"'"+
+						(sizing?", sizingMethod='"+sizing+"'":"") + ")";
+				}
+			}
+		);
+/*		
 		var t= "top: -8px;";
 		var l= "left: -8px;";
 		var b= dojo.render.html.ie ? "bottom: -6px;" : "bottom: -8px;";
 		var r= dojo.render.html.ie ? "right: -6px;" : "right: -8px;";
-		
 		this._makePiece("UL", t + l + "width: 8px; height: 8px;");
 		this._makePiece("UR", t + r + "width: 8px; height: 8px;");
 		this._makePiece("BL", b + l + "width: 8px; height: 8px;");
@@ -29,6 +52,7 @@ dojo.lang.extend(dojo.html.shadow, {
 		this._makePiece("B", b + "left: 0px; width: 100%; height: 8px;", "scale");
 		this._makePiece("L", "top: 0px;" + l + "width: 8px; height: 100%;", "scale");
 		this._makePiece("R", "top: 0px;" + r + "width: 8px; height: 100%;", "scale");
+*/
 	},
 
 	_makePiece: function(name, cssText, sizing){
