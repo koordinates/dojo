854941e96aa1365ae439e9441a05294734f5aadc
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-02-28 13:32:50 +0000

    Changes from Kris Leite (CLA on file):
    1. support tab labels on top, bottom, left, or right
    (the left and right tabs have horizontal text but a
    subsequent checkin will support vertical labels also)
    
    2. do resizing via css rather than javascript calculation
    (this makes resizing faster, so it appears smoother).
    Also, TabPane no longer derives from LayoutPane
    
    
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3180 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/TabPane.js
+++ src/widget/TabPane.js
@@ -1,10 +1,9 @@
 dojo.provide("dojo.widget.TabPane");
 dojo.provide("dojo.widget.html.TabPane");
 dojo.provide("dojo.widget.Tab");
-dojo.provide("dojo.widget.html.Tab");
 
 dojo.require("dojo.widget.*");
-dojo.require("dojo.widget.LayoutPane");
+dojo.require("dojo.widget.HtmlWidget");
 dojo.require("dojo.event.*");
 dojo.require("dojo.html");
 dojo.require("dojo.style");
@@ -13,86 +12,114 @@ dojo.require("dojo.style");
 // TabPane -- a set of Tabs
 //////////////////////////////////////////
 dojo.widget.html.TabPane = function() {
-	dojo.widget.html.LayoutPane.call(this);
+	dojo.widget.HtmlWidget.call(this);
 }
-dojo.inherits(dojo.widget.html.TabPane, dojo.widget.html.LayoutPane);
+
+dojo.inherits(dojo.widget.html.TabPane, dojo.widget.HtmlWidget);
 
 dojo.lang.extend(dojo.widget.html.TabPane, {
 	widgetType: "TabPane",
+    isContainer: true,
 
 	// Constructor arguments
 	labelPosition: "top",
 	useVisibility: false,		// true-->use visibility:hidden instead of display:none
 
-
+	templatePath: dojo.uri.dojoUri("src/widget/templates/HtmlTabPane.html"),
 	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlTabPane.css"),
 
 	selectedTab: "",		// initially selected tab (widgetId)
 
 	fillInTemplate: function(args, frag) {
+		// Copy style info and id from input node to output node
+		var source = this.getFragNodeRef(frag);
+		this.domNode.style.cssText = source.style.cssText;
+		dojo.html.addClass(this.domNode, dojo.html.getClass(source));
 		dojo.widget.html.TabPane.superclass.fillInTemplate.call(this, args, frag);
-		
-		dojo.style.insertCssFile(this.templateCssPath, null, true);
-		dojo.html.prependClass(this.domNode, "dojoTabPane");
 	},
 
 	postCreate: function(args, frag) {
-		// Create <ul> with special formatting to store all the tab labels
-		// TODO: set "bottom" css tag if label is on bottom
-		this.ul = document.createElement("ul");
-		dojo.html.addClass(this.ul, "tabs");
-		dojo.html.addClass(this.ul, this.labelPosition);
-
 		// Load all the tabs, creating a label for each one
 		for(var i=0; i<this.children.length; i++){
 			this._setupTab(this.children[i]);
 		}
-		dojo.widget.html.TabPane.superclass.postCreate.call(this, args, frag);
 
-		// Put tab labels in a panel on the top (or bottom)
-		this.filterAllowed(this, 'labelPosition', ['top', 'bottom']);
-		this.labelPanel = dojo.widget.createWidget("LayoutPane", {layoutAlign: this.labelPosition});
-		this.labelPanel.domNode.appendChild(this.ul);
-		dojo.widget.html.TabPane.superclass.addChild.call(this, this.labelPanel);
-
-		// workaround CSS loading race condition bug
-		dojo.lang.setTimeout(this, this.onResized, 50);
+		dojo.html.addClass(this.dojoTabLabels, "dojoTabLabels-"+this.labelPosition);
+        this._setPadding();
+		this._Resized();
 	},
 
 	addChild: function(child, overrideContainerNode, pos, ref, insertIndex){
 		this._setupTab(child);
 		dojo.widget.html.TabPane.superclass.addChild.call(this,child, overrideContainerNode, pos, ref, insertIndex);
+
+		// in case the tab labels have overflowed from one line to two lines
+		this._setPadding();
 	},
 
 	_setupTab: function(tab){
-		tab.layoutAlign = "client";
 		tab.domNode.style.display="none";
-		dojo.html.prependClass(tab.domNode, "dojoTabPanel");
 
 		// Create label
-		tab.li = document.createElement("li");
+		tab.div = document.createElement("div");
+		dojo.html.addClass(tab.div, "dojoTabPaneTab");
 		var span = document.createElement("span");
 		span.innerHTML = tab.label;
 		dojo.html.disableSelection(span);
-		tab.li.appendChild(span);
-		this.ul.appendChild(tab.li);
+		tab.div.appendChild(span);
+		this.dojoTabLabels.appendChild(tab.div);
 		
 		var self = this;
-		dojo.event.connect(tab.li, "onclick", function(){ self.selectTab(tab); });
+		dojo.event.connect(tab.div, "onclick", function(){ self.selectTab(tab); });
 		
 		if(!this.selectedTabWidget || this.selectedTab==tab.widgetId || tab.selected){
-            this.selectTab(tab);
+    		this.selectedTabWidget = tab;
+//            this.selectTab(tab);
         } else {
             this._hideTab(tab);
         }
+	},
+
+	// Configure the content pane to take up all the space except for where the tab labels are
+	_setPadding: function(){
+		var t=dojo.style.getOuterHeight(this.dojoTabLabels);
+		var w=dojo.style.getInnerWidth(this.dojoTabLabels);
+		var c=dojo.style.getOuterHeight(this.containerNode);
+		if(t==0 || c==0){
+			// browser needs more time to compute sizes (maybe CSS hasn't downloaded yet)
+			dojo.lang.setTimeout(this, this._setPadding, 10);
+			return;
+		}
+		with(this.domNode.style){
+			switch (this.labelPosition) {
+				case 'top': 
+					paddingTop=t+"px";
+					break;
+				case 'bottom':
+					paddingBottom=t+"px";
+					break;
+				case 'left':
+					paddingLeft=w+"px";
+					break;
+				case 'right':
+					paddingRight=w+"px";
+					break;
+				case 'left-h':
+					paddingLeft=w+"px";
+					break;
+				case 'right-h':
+					paddingRight=w+"px";
+					break;
+			}
+		}
 	},
 
     removeChild: function(tab) {
         dojo.widget.html.TabPane.superclass.removeChild.call(this, tab);
 
         dojo.html.removeClass(tab.domNode, "dojoTabPanel");
-        this.ul.removeChild(tab.li);
-        delete(tab.li);
+        this.ul.removeChild(tab.div);
+        delete(tab.div);
 
         // FIXME: do we need to disconnect event handler?
 
@@ -102,6 +129,9 @@ dojo.lang.extend(dojo.widget.html.TabPane, {
                 this.selectTab(this.children[0]);
             }
         }
+
+		// in case the tab labels have overflowed from one line to two lines
+		this._setPadding();
     },
 
     selectTab: function(tab) {
@@ -115,7 +145,7 @@ dojo.lang.extend(dojo.widget.html.TabPane, {
 	},
 	
 	_showTab: function(tab) {
-		dojo.html.addClass(tab.li, "current");
+		dojo.html.addClass(tab.div, "current");
 		tab.selected=true;
 		if ( this.useVisibility && !dojo.render.html.ie ) {
 			tab.domNode.style.visibility="visible";
@@ -125,7 +155,7 @@ dojo.lang.extend(dojo.widget.html.TabPane, {
 	},
 
 	_hideTab: function(tab) {
-		dojo.html.removeClass(tab.li, "current");
+		dojo.html.removeClass(tab.div, "current");
 		tab.selected=false;
 		if( this.useVisibility ){
 			tab.domNode.style.visibility="hidden";
@@ -134,13 +164,19 @@ dojo.lang.extend(dojo.widget.html.TabPane, {
 		}
 	},
 
-	onResized: function() {
+	// TODO: why is this a separate function?  (also, name is weird)
+	_Resized: function() {
 		// Display the selected tab
 		if(this.selectedTabWidget){
 			this.selectTab(this.selectedTabWidget);
 		} else {
 			dojo.widget.html.TabPane.superclass.onResized.call(this);
 		}
+	},
+
+	onResized: function() {
+		// in case the tab labels have overflowed from one line to two lines
+		this._setPadding();
 	}
 });
 dojo.widget.tags.addParseTreeHandler("dojo:TabPane");
@@ -152,15 +188,4 @@ dojo.lang.extend(dojo.widget.Widget, {
 	label: "",
 	selected: false	// is this tab currently selected?
 });
-
-// Deprecated class.  TabPane can take any widget as input.
-// Use ContentPane, LayoutPane, etc.
-dojo.widget.html.Tab = function() {
-	dojo.widget.html.LayoutPane.call(this);
-}
-dojo.inherits(dojo.widget.html.Tab, dojo.widget.html.LayoutPane);
-dojo.lang.extend(dojo.widget.html.Tab, {
-	widgetType: "Tab"
-});
-dojo.widget.tags.addParseTreeHandler("dojo:Tab");
 
