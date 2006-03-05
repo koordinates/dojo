e316a128b703e0440d502dc3eec33907c5b52296
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-03-05 01:02:25 +0000

    merging James Burke's ScriptSrcIO patch. Fixes #379. All around a great patch.
    git-svn-id: http://svn.dojotoolkit.org/src@3219 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/tests/io/scriptsrc/Test.js
@@ -0,0 +1,8 @@
+myTasks = new Array();
+myTasks[0] = 'Take out trash.';
+myTasks[1] = 'Do dishes.';
+myTasks[2] = 'Brush teeth.';
+myTasks = new Array();
+myTasks[0] = 'Take out trash.';
+myTasks[1] = 'Do dishes.';
+myTasks[2] = 'Brush teeth.';
