0220f55df1b0a332ea2d772a9f5bf1e85d4dca36
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-03-11 07:20:48 +0000

    Fixed duplication of content for test files. The test pages for dojo.undo.browser and dojo.io.ScriptSrcTransport should work properly now.
    git-svn-id: http://svn.dojotoolkit.org/src@3260 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/tests/io/scriptsrc/Test.js
+++ trunk/tests/io/scriptsrc/Test.js
@@ -2,7 +2,4 @@ myTasks = new Array();
 myTasks[0] = 'Take out trash.';
 myTasks[1] = 'Do dishes.';
 myTasks[2] = 'Brush teeth.';
-myTasks = new Array();
-myTasks[0] = 'Take out trash.';
-myTasks[1] = 'Do dishes.';
-myTasks[2] = 'Brush teeth.';
+
