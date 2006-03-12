cde80c6cb4a59252c3ee67595adbda29353a4ea4
pottedmeat <pottedmeat@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-03-12 20:03:28 +0000

    Added topic system, readme, meta loading
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3275 560b804f-0ae3-0310-86f3-f6aa0a117693

--- docscripts/doctool.js
+++ docscripts/doctool.js
@@ -1,8 +1,35 @@
 dojo.require("dojo.widget.html.DocComboBox");
 
 dojo.doc.getMeta(_result, "dojo.animation.Animation");
+dojo.doc.getSrc(_result, "dojo.animation.Animation");
+dojo.doc.getDoc(_docResult, "dojo.animation.Animation.play");
 //dojo.doc.getDoc("dojo.animation.Animation.play");
 
-function _result(input){
-	dojo.debug(input.this_variables);
+var docCount = 0;
+
+function _docResult(input){
+	dojo.debug("_docResult");
+	var i = arguments.length;
+	while(i-- > 0){
+		dojo.debug(dojo.json.serialize(arguments[i]));
+	}
 }
+
+function _result(input){
+	if(dojo.lang.isObject(input)){
+		dojo.debug(dojo.json.serialize(input));
+	}else{
+		dojo.debug(input);
+	}
+}
+
+function docSubmit(){
+	dojo.event.topic.publish("docSearch", {selectKey: ++docCount, name: dojo.widget.byId("search").domNode.getElementsByTagName("input")[2].value});
+	return false;
+}
+
+function docResults(/*Object*/ input){
+	dojo.debug(dojo.json.serialize(input));
+}
+
+dojo.event.topic.subscribe("docResults", docResults);
