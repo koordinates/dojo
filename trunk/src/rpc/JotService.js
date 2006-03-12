635c871db262b849737fa33218afe3dd898b192a
pottedmeat <pottedmeat@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-03-12 05:29:44 +0000

    User/pass doesn't work
    git-svn-id: http://svn.dojotoolkit.org/src@3271 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/rpc/JotService.js
+++ trunk/src/rpc/JotService.js
@@ -5,8 +5,6 @@ dojo.require("dojo.json");
 
 dojo.rpc.JotService = function(){
 	this.serviceUrl = "/_/jsonrpc";
-	this.user = "";
-	this.pass = "";
 }
 
 dojo.inherits(dojo.rpc.JotService, dojo.rpc.JsonService);
@@ -21,9 +19,7 @@ dojo.lang.extend(dojo.rpc.JotService, {
 			method: "POST",
 			mimetype: "text/json",
 			load: this.resultCallback(deferredRequestHandler),
-			preventCache: true,
-			user: this.user,
-			pass: this.pass
+			preventCache: true
 		});
 	},
 
