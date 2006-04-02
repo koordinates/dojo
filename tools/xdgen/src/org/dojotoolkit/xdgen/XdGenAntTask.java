201c98a98e449bd7c2f8e9edf26f9d903cdcfc5f
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-02 05:50:57 +0000

    The start of the xdgen ant task (not been compiled yet).
    git-svn-id: http://svn.dojotoolkit.org/src/branches/xdomain@3422 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tools/xdgen/src/org/dojotoolkit/xdgen/XdGenAntTask.java
@@ -0,0 +1,40 @@
+package org.dojotoolkit.xdgen;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.apache.tools.ant.types.FileSet;
+
+public class XdGenAntTask extends MatchingTask{
+	private List mFileSetList = new ArrayList();
+
+	public void addConfiguredFileSet(FileSet fileset){
+		mFileSetList.add(fileset);
+	}
+
+	public void execute(){
+		if(mFileSetList.size() > 0){
+			ArrayList fileNameList = new ArrayList();
+			for(int listIndex = 0; listIndex < mFileSetList.size(); listIndex++){
+				DirectoryScanner directoryScanner = ((FileSet)mFileSetList
+						.get(listIndex)).getDirectoryScanner(getProject());
+				String[] files = directoryScanner.getIncludedFiles();
+
+				if(files != null){
+					for(int index = 0; index < files.length; index++){
+						fileNameList.add(directoryScanner.getBasedir()
+								.getAbsolutePath()
+								+ File.separator + files[index]);
+					}
+				}
+			}
+
+			String[] fileNames = (String[])fileNameList
+					.toArray(new String[fileNameList.size()]);
+			XdGen.main(fileName);
+		}
+	}
+}
