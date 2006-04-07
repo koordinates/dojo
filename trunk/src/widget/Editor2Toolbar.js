ee4aa6fa348e82212297b5ce7afae4e3cfdcfdbe
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-07 21:35:53 +0000

    first hack at Editor2, lots more to do to get it behaving correctly everywhere
    git-svn-id: http://svn.dojotoolkit.org/src@3465 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/Editor2Toolbar.js
+++ trunk/src/widget/Editor2Toolbar.js
@@ -5,45 +5,54 @@ dojo.require("dojo.widget.*");
 dojo.require("dojo.event.*");
 dojo.require("dojo.widget.RichText");
 
-dojo.widget.tags.addParseTreeHandler("dojo:Editor2Toolbar");
+dojo.widget.defineWidget(
+	"dojo.widget.html.Editor2Toolbar",
+	dojo.widget.HtmlWidget,
+	{
+		templatePath: dojo.uri.dojoUri("src/widget/templates/HtmlEditorToolbar.html"),
+		templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlEditorToolbar.css"),
 
-dojo.widget.html.Editor2Toolbar = function(){
-	dojo.widget.HtmlWidget.call(this);
-}
+		// DOM Nodes
+		wikiWordButton: null,
+		styleDropdownButton: null,
+		styleDropdownContainer: null,
+		copyButton: null,
+		boldButton: null,
+		italicButton: null,
+		underlineButton: null,
+		leftButton: null,
+		fullButton: null,
+		rightButton: null,
+		pasteButton: null,
+		undoButton: null,
+		redoButton: null,
 
-dojo.inherits(dojo.widget.html.Editor2Toolbar, dojo.widget.HtmlWidget);
-dojo.lang.extend(dojo.widget.html.Editor2Toolbar, {
-	widgetType: "Editor2Toolbar",
-	isContainer: false,
+		buttonClick: function(){ dojo.debug("buttonClick"); },
 
-	templatePath: dojo.uri.dojoUri("src/widget/templates/HtmlEditorToolbar.html"),
-	templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlEditorToolbar.css"),
+		// event signals
+		wikiWordClick: function(){ dojo.debug("wikiWordButtonClick"); },
+		styleDropdownClick: function(){
+			dojo.debug("styleDropdownClick:", this.styleDropdownContainer);
+			dojo.style.toggleShowing(this.styleDropdownContainer);
+		},
 
-	// DOM Nodes
-	wikiWordButton: null,
-	styleDropdownButton: null,
-	copyButton: null,
-	boldButton: null,
-	italicButton: null,
-	underlineButton: null,
-	leftButton: null,
-	fullButton: null,
-	rightButton: null,
-	pasteButton: null,
-	undoButton: null,
-	redoButton: null,
+		copyClick: function(){ this.exec("copy"); },
+		boldClick: function(){ this.exec("bold"); },
+		italicClick: function(){ this.exec("italic"); },
+		underlineClick: function(){ this.exec("underline"); },
+		leftClick: function(){ this.exec("justifyleft"); },
+		fullClick: function(){ this.exec("justifyfull"); },
+		rightClick: function(){ this.exec("justifyright"); },
+		pasteClick: function(){ this.exec("paste"); },
+		undoClick: function(){ this.exec("undo"); },
+		redoClick: function(){ this.exec("redo"); },
 
-	// event signals
-	wikiWordClick: function(){},
-	styleDropdownClick: function(){},
-	copyClick: function(){},
-	boldClick: function(){},
-	italicClick: function(){},
-	underlineClick: function(){},
-	leftClick: function(){},
-	fullClick: function(){},
-	rightClick: function(){},
-	pasteClick: function(){},
-	undoClick: function(){},
-	redoClick: function(){}
-});
+		normalTextClick: function(){ this.exec("formatblock", "p"); },
+		h1TextClick: function(){ this.exec("formatblock", "h1"); },
+		h2TextClick: function(){ this.exec("formatblock", "h2"); },
+		h3TextClick: function(){ this.exec("formatblock", "h3"); },
+		h4TextClick: function(){ this.exec("formatblock", "h4"); },
+
+		exec: function(what, arg){ }
+	}
+);
