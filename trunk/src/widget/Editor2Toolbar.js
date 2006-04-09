540f281e44cea25327f659121f64d56eb6780282
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-09 00:55:38 +0000

    more stuff
    git-svn-id: http://svn.dojotoolkit.org/src@3478 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/Editor2Toolbar.js
+++ trunk/src/widget/Editor2Toolbar.js
@@ -9,6 +9,18 @@ dojo.widget.defineWidget(
 	"dojo.widget.html.Editor2Toolbar",
 	dojo.widget.HtmlWidget,
 	{
+		commandList: [ "bold", "italic", "underline", "subscript", "superscript",
+			"fontname", "fontsize", "forecolor", "hilitecolor", "justifycenter",
+			"justifyfull", "justifyleft", "justifyright", "cut", "copy", "paste",
+			"delete", "undo", "redo", "createlink", "unlink", "removeformat",
+			"inserthorizontalrule", "insertimage", "insertorderedlist",
+			"insertunorderedlist", "indent", "outdent", "formatblock", "strikethrough", 
+			"inserthtml", "blockdirltr", "blockdirrtl", "dirltr", "dirrtl",
+			"inlinedirltr", "inlinedirrtl", "inserttable", "insertcell",
+			"insertcol", "insertrow", "deletecells", "deletecols", "deleterows",
+			"mergecells", "splitcell"
+		],
+
 		templatePath: dojo.uri.dojoUri("src/widget/templates/HtmlEditorToolbar.html"),
 		templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlEditorToolbar.css"),
 
@@ -20,14 +32,23 @@ dojo.widget.defineWidget(
 		boldButton: null,
 		italicButton: null,
 		underlineButton: null,
-		leftButton: null,
-		fullButton: null,
-		rightButton: null,
+		justifyleftButton: null,
+		justifyfullButton: null,
+		justifyrightButton: null,
 		pasteButton: null,
 		undoButton: null,
 		redoButton: null,
+		linkButton: null,
+		insertunorderedlistButton: null,
+		insertorderedlistButton: null,
+		forecolorButton: null,
+		hilitecolorButton: null,
+		formatSelectBox: null,
 
 		buttonClick: function(){ dojo.debug("buttonClick"); },
+
+		buttonMouseOver: function(e){  },
+		buttonMouseOut: function(e){  },
 
 		// event signals
 		wikiWordClick: function(){ dojo.debug("wikiWordButtonClick"); },
@@ -36,16 +57,36 @@ dojo.widget.defineWidget(
 			dojo.style.toggleShowing(this.styleDropdownContainer);
 		},
 
+		closeAllDropDowns: function(){
+		},
+
 		copyClick: function(){ this.exec("copy"); },
 		boldClick: function(){ this.exec("bold"); },
 		italicClick: function(){ this.exec("italic"); },
 		underlineClick: function(){ this.exec("underline"); },
-		leftClick: function(){ this.exec("justifyleft"); },
-		fullClick: function(){ this.exec("justifyfull"); },
-		rightClick: function(){ this.exec("justifyright"); },
+		justifyleftClick: function(){ this.exec("justifyleft"); },
+		justifyfullClick: function(){ this.exec("justifyfull"); },
+		justifyrightClick: function(){ this.exec("justifyright"); },
 		pasteClick: function(){ this.exec("paste"); },
 		undoClick: function(){ this.exec("undo"); },
 		redoClick: function(){ this.exec("redo"); },
+		linkClick: function(){ 
+			this.exec(	"createlink", 
+						prompt("Please enter the URL of the link:", "http://"));
+		},
+		insertunorderedlistClick: function(){ this.exec("insertunorderedlist"); },
+		insertorderedlistClick: function(){ this.exec("insertorderedlist"); },
+
+		formatSelectClick: function(){ 
+			// dojo.debug(this.formatSelectBox.value);
+			var sv = this.formatSelectBox.value.toLowerCase();
+			this.exec("formatblock", sv);
+			/*
+			if(sv == "blockquote"){
+				this.exec("italic");
+			}
+			*/
+		},
 
 		normalTextClick: function(){ this.exec("formatblock", "p"); },
 		h1TextClick: function(){ this.exec("formatblock", "h1"); },
@@ -53,6 +94,35 @@ dojo.widget.defineWidget(
 		h3TextClick: function(){ this.exec("formatblock", "h3"); },
 		h4TextClick: function(){ this.exec("formatblock", "h4"); },
 
-		exec: function(what, arg){ }
+		forecolorClick: function(){
+			//TODO
+		},
+
+		hilitecolorClick: function(){
+			//TODO
+		},
+
+		indentClick: function(){ this.exec("indent"); },
+		outdentClick: function(){ this.exec("outdent"); },
+
+		// stub that an observer can connect to
+		exec: function(what, arg){ },
+
+		hideUnusableButtons: function(){
+			dojo.lang.forEach(this.commandList,
+				function(cmd){
+					if(this[cmd+"Button"]){
+						var cb = this[cmd+"Button"];
+						if(!dojo.widget.html.RichText.prototype.queryCommandAvailable(cmd)){
+							cb.style.display = "none";
+						}
+					}
+				},
+				this);
+		}
+	},
+	"html",
+	function(){
+		dojo.event.connect(this, "fillInTemplate", this, "hideUnusableButtons");
 	}
 );
