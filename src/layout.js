7c4a6c953eb02f7313f02213c33e3f130586cfe6
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-14 17:05:06 +0000

    Split layout code into a separate file so that it can be used independently of LayoutContainer.  Also rewrote the layout code; reduced the code size quite a bit.
    I took out the minSize stuff because it needs to be
    reworked anyway; maybe should be part of HtmlWidget.js?
    
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3532 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/layout.js
@@ -0,0 +1,85 @@
+/**
+ * Layout a bunch of child dom nodes within a parent dom node
+ * Input is an array of objects like:
+ * @ container - parent node
+ * @ layoutPriority - "top-bottom" or "left-right"
+ * @ children an array like [ {domNode: foo, layoutAlign: "bottom" }, {domNode: bar, layoutAlign: "client"} ]
+ */
+ 
+dojo.provide("dojo.layout");
+
+dojo.require("dojo.lang");
+dojo.require("dojo.style");
+
+dojo.layout = function(container, children, layoutPriority) {
+	// container has to be either relative or absolute position so that we
+	// can position nodes inside of it
+	if(container.style.position != "absolute"){
+		container.style.position = "relative";
+	}
+
+	// copy input array and remove elements w/out layout
+	children = dojo.lang.filter(children, function(child){
+		return dojo.lang.inArray(["top","bottom","left","right","client","flood"], child.layoutAlign)
+	});
+
+	// order the children according to layoutPriority
+	if(layoutPriority){
+		var rank = function(child){
+			switch(child.layoutAlign){
+				case "flood":
+					return 1;
+				case "left":
+				case "right":
+					return (layoutPriority=="left-right") ? 2 : 3;
+				case "top":
+				case "bottom":
+					return (layoutPriority=="left-right") ? 3 : 2;
+				default:
+					return 4;
+			}
+		};
+		children.sort(function(a,b){ return rank(a)-rank(b); });
+	}
+
+	// record the width/height of all the floating elements added so far (plus padding)
+	var usedSpace={
+		'left': dojo.style.getPixelValue(container, "padding-left", true),
+		'right': dojo.style.getPixelValue(container, "padding-right", true),
+		'top': dojo.style.getPixelValue(container, "padding-top", true),
+		'bottom': dojo.style.getPixelValue(container, "padding-bottom", true)
+	};
+
+	// track how much space is remaining
+	var remainingWidth  = dojo.style.getContentWidth(container);
+	var remainingHeight =  dojo.style.getContentHeight(container);
+
+	// set positions/sizes
+	dojo.lang.forEach(children, function(child){
+		var elm=child.domNode;
+		var position=child.layoutAlign;
+
+		// set two of left/right/top/bottom properties
+		elm.style.position="absolute";
+		var lr = (position=="right")?"right":"left";
+		elm.style[lr]=usedSpace[lr] + "px";
+		var tb = (position=="bottom")?"bottom":"top";
+		elm.style[tb]=usedSpace[tb] + "px";
+
+		// set size && adjust record of remaining space
+		if ( (position=="top")||(position=="bottom") ) {
+			dojo.style.setOuterWidth(elm, remainingWidth);
+			var h = dojo.style.getOuterHeight(elm);
+			usedSpace[position] += h;
+			remainingHeight -= h;
+		} else if(position=="left" || position=="right"){
+			dojo.style.setOuterHeight(elm, remainingHeight);
+			var w = dojo.style.getOuterWidth(elm);
+			usedSpace[position] += w;
+			remainingWidth -= w;
+		} else if(position=="flood" || position=="client"){
+			dojo.style.setOuterWidth(elm, remainingWidth);		
+			dojo.style.setOuterHeight(elm, remainingHeight);
+		}
+	});
+};
