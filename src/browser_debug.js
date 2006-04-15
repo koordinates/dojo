df81cdd0e3b5488f077a5d8c02f9eb290dfe17a5
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-15 05:32:45 +0000

    Getting closer with xd loader. just smaller todos left. Also added support for kwCompoundRequire conditionalRequire for the build/load scripts that look for dependency calls.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/xdomain@3546 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/browser_debug.js
+++ src/browser_debug.js
@@ -19,7 +19,7 @@ dojo.hostenv.getRequiresAndProvides = function(contents){
 	var deps = [];
 	var tmp;
 	RegExp.lastIndex = 0;
-	var testExp = /dojo.(hostenv.loadModule|hosetnv.require|require|requireIf|hostenv.conditionalLoadModule|hostenv.startPackage|hostenv.provide|provide)\([\w\W]*?\)/mg;
+	var testExp = /dojo.(hostenv.loadModule|hosetnv.require|require|requireIf|kwCompoundRequire|conditionalRequire|hostenv.conditionalLoadModule|hostenv.startPackage|hostenv.provide|provide)\([\w\W]*?\)/mg;
 	while((tmp = testExp.exec(contents)) != null){
 		deps.push(tmp[0]);
 	}
