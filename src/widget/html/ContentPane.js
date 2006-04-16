777f1100e3846f8ef2a6a68bc3fba6a3105ec07c
mumme <mumme@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-16 10:58:56 +0000

    fix #616, dont fix relative paths on anchor tags
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3564 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/html/ContentPane.js
+++ src/widget/html/ContentPane.js
@@ -212,7 +212,7 @@ dojo.lang.extend(dojo.widget.html.ContentPane, {
 					case "src":// falltrough
 					case "href":
 						// this hopefully covers most common protocols
-						if(attr[3].search(/^(https?|ftps?|file|javascript|mailto|news):/)==-1){ 
+						if(attr[3].search(/^(?:[#]|(?:(?:https?|ftps?|file|javascript|mailto|news):))/)==-1){ 
 							fixedPath = (new dojo.uri.Uri(url, attr[3]).toString());
 						} else {
 							pos2 = pos2 + attr[3].length;
