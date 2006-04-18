cdddfb24846612a7c324f773d2f1600f2ce820f2
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-04-18 20:37:53 +0000

    Added some functionality to the map, see the test for what.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3604 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/html/GoogleMap.js
+++ src/widget/html/GoogleMap.js
@@ -1,26 +1,122 @@
 dojo.provide("dojo.widget.html.GoogleMap");
 dojo.require("dojo.event.*");
 dojo.require("dojo.html");
+dojo.require("dojo.math");
 dojo.require("dojo.widget.HtmlWidget");
 dojo.require("dojo.widget.GoogleMap");
 
 dojo.widget.html.GoogleMap=function(){
 	dojo.widget.HtmlWidget.call(this);
 	dojo.widget.GoogleMap.call(this);
+
+	var gm=dojo.widget.GoogleMap;
+
+	this.map=null;
+	this.center="";
+	this.zoom="12";
+	this.bounds=null;
+	this.plot=[];
+	this.points=[];
+	this.controls=[gm.Controls.LargeMap,gm.Controls.Scale,gm.Controls.MapType];
 };
 dojo.inherits(dojo.widget.html.GoogleMap, dojo.widget.HtmlWidget);
 
 dojo.lang.extend(dojo.widget.html.GoogleMap, {
 	templatePath:null,
 	templateCssPath:null,
-	map:null,
+
+	setControls:function(){
+		if(!this.map){
+			return;
+		}
+		var c=dojo.widget.GoogleMap.Controls;
+		for(var i=0; i<this.controls.length; i++){
+			var type=this.controls[i];
+			switch(type){
+				case c.LargeMap:{
+					this.map.addControl(new GLargeMapControl());
+					break;
+				}
+				case c.SmallMap:{
+					this.map.addControl(new GSmallMapControl());
+					break;
+				}
+				case c.SmallZoom:{
+					this.map.addControl(new GSmallZoomControl());
+					break;
+				}
+				case c.Scale:{
+					this.map.addControl(new GScaleControl());
+					break;
+				}
+				case c.MapType:{
+					this.map.addControl(new GMapTypeControl());
+					break;
+				}
+				case c.Overview:{
+					this.map.addControl(new GOverviewMapControl());
+					break;
+				}
+				default:{
+					break;
+				}
+			}
+		}
+	},
+	setMarkers:function(){
+		if(!this.map){
+			return;
+		}
+		for(var i=0; i<this.points.length; i++){
+			this.map.addOverlay(new GMarker(this.points[i]));
+		}
+	},
+	createPoint:function(lat, long){
+		return new GLatLng(parseFloat(lat), parseFloat(long));
+	},
+	center:function(point, zoom){
+		if(this.map){
+			this.map.setCenter(point, zoom);
+		}
+	},
+	panTo:function(point){
+		if(this.map){
+			this.map.panTo(point);
+		}
+	},
+	findCenter:function(){
+		if(!this.bounds){
+			this.bounds=new GLatLngBounds();
+		}
+		var clat=(this.bounds.getNorthEast().lat()+this.bounds.getSouthWest().lat())/2;
+		var clng=(this.bounds.getNorthEast().lng()+this.bounds.getSouthWest().lng())/2;
+		return new GLatLng(clat,clng);
+	},
+	findZoom:function(){
+		if(!this.bounds){
+			this.bounds=new GLatLngBounds();
+		}
+		return this.map.getBoundsZoomLevel(this.bounds);
+	},
 
 	postCreate:function(){
 		if(!GMap2){
 			dojo.raise("dojo.widget.GoogleMap: The Google Map script must be included (with a proper API key) in order to use this widget.");
 		}
+		if(!this.bounds){
+			this.bounds=new GLatLngBounds();
+		}
+		for(var i=0; i<this.plot.length; i++){
+			var a=this.plot[i].split(",");
+			var p=new GLatLng(parseFloat(a[0]),parseFloat(a[1]));
+			this.points.push(p);
+			this.bounds.extend(p);
+		}
+		this.domNode.style.position="relative";
+		
 		this.map=new GMap2(this.domNode);
-		this.map.setCenter(new GLatLng(37.4419, -122.1419), 13);
-
+		this.setControls();
+		this.map.setCenter(this.findCenter(), this.findZoom());
+		this.setMarkers();
 	}
 });
