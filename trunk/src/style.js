60100e615ebd021ba266b288a93282c0bd80d33f
sjmiles <sjmiles@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-05 21:11:03 +0000

    I suck
    git-svn-id: http://svn.dojotoolkit.org/src@3773 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/style.js
+++ trunk/src/style.js
@@ -652,7 +652,7 @@ dojo.require("dojo.lang.common");
 		node = dojo.byId(node);
 		if(ds.getStyleProperty(node, 'display')=='none'){
 			ds.setStyle(node, 'display', (node.dojoDisplayCache||''));
-			if(!('dojoDisplayCache' in node)){ // it could == '', so we cannot say !node.dojoDisplayCount
+			if('dojoDisplayCache' in node){ // it could == '', so we cannot say if (node.dojoDisplayCount)
 				// IE throws if the prop doesn't exist
 				delete node.dojoDisplayCache;	
 			}
