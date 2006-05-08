a86cf85bde09bfcfa6b6977d8f88ab9167ee2353
BryanForbes <BryanForbes@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-08 18:00:53 +0000

    Added a DropdownDatePicker and test.
    git-svn-id: http://svn.dojotoolkit.org/src@3836 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/src/widget/DropdownDatePicker.js
@@ -0,0 +1,48 @@
+dojo.provide("dojo.widget.DropdownDatePicker");
+dojo.require("dojo.widget.*");
+dojo.require("dojo.widget.DropdownContainer");
+dojo.require("dojo.widget.DatePicker");
+dojo.require("dojo.event.*");
+dojo.require("dojo.html");
+
+dojo.widget.defineWidget(
+	"dojo.widget.DropdownDatePicker",
+	dojo.widget.DropdownContainer,
+	{
+		iconURL: dojo.uri.dojoUri("src/widget/templates/images/dateIcon.gif"),
+		iconAlt: "Select a Date",
+		iconTitle: "Select a Date",
+		
+		datePicker: null,
+		
+		dateFormat: "%m/%d/%Y",
+		
+		fillInTemplate: function(args, frag){
+			dojo.widget.DropdownDatePicker.superclass.fillInTemplate.call(this, args, frag);
+			var source = this.getFragNodeRef(frag);
+			
+			if(args.dateFormat){ this.dateFormat = args.dateFormat; }
+			
+			var dpNode = document.createElement("div");
+			this.containerNode.appendChild(dpNode);
+			
+			this.datePicker = dojo.widget.createWidget("DatePicker", { widgetContainerId: this.widgetId }, dpNode);
+			dojo.event.connect(this.datePicker, "onSetDate", this, "onPopulate");
+		},
+		
+		onPopulate: function(){
+			this.inputNode.value = dojo.date.format(this.datePicker.date, this.dateFormat);
+			this.onHide();
+		},
+		
+		onInputChange: function(){
+			var tmp = new Date(this.inputNode.value);
+			this.datePicker.date = tmp;
+			this.datePicker.setDate(dojo.widget.DatePicker.util.toRfcDate(tmp));
+			this.datePicker.initUI();
+		}
+	},
+	"html"
+);
+
+dojo.widget.tags.addParseTreeHandler("dojo:dropdowndatepicker");
