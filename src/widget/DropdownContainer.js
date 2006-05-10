bc1ae3c398aaea174831bd608a39c522eeaf2498
BryanForbes <BryanForbes@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-10 17:20:26 +0000

    DropdownContainer.js: added inputId parameter to set the input node's dom ID; added a BackgroundIframe so selects don't show through in IE; wrapped entire widget in a div, changed span to only wrap input and button, and then added a br between the input/button and the container to fix issue with IE putting container next to input/button
    DropdownDatePicker.js: added date parameter to set default date; changed onPopulate to onSetDate
    
    test_DropdownDatePicker.html: added a select to check for IE bleed-through
    
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3883 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/DropdownContainer.js
+++ src/widget/DropdownContainer.js
@@ -15,6 +15,7 @@ dojo.widget.defineWidget(
 		snarfChildDomOutput: true,
 		
 		inputWidth: "7em",
+		inputId: "",
 		inputName: "",
 		iconURL: null,
 		iconAlt: null,
@@ -24,12 +25,13 @@ dojo.widget.defineWidget(
 		containerNode: null,
 		subWidgetNode: null,
 
-		templateString: '<span style="white-space:nowrap"><input type="text" value="" style="vertical-align:middle;" dojoAttachPoint="inputNode" /> <img src="" alt="" dojoAttachPoint="buttonNode" dojoAttachEvent="onclick: onDropdown;" style="vertical-align:middle; cursor:pointer; cursor:hand;" /><div dojoAttachPoint="containerNode" style="display:none;position:absolute;width:12em;background-color:#fff;"></div></span>',
+		templateString: '<div><span style="white-space:nowrap"><input type="text" value="" style="vertical-align:middle;" dojoAttachPoint="inputNode" /> <img src="" alt="" dojoAttachPoint="buttonNode" dojoAttachEvent="onclick: onIconClick;" style="vertical-align:middle; cursor:pointer; cursor:hand;" /></span><br /><div dojoAttachPoint="containerNode" style="display:none;position:absolute;width:12em;background-color:#fff;"></div></div>',
 		templateCssPath: "",
 
 		fillInTemplate: function(args, frag){
 			var source = this.getFragNodeRef(frag);
-
+			
+			if(args.inputId){ this.inputId = args.inputId; }
 			if(args.inputName){ this.inputName = args.inputName; }
 			if(args.iconURL){ this.iconURL = args.iconURL; }
 			if(args.iconAlt){ this.iconAlt = args.iconAlt; }
@@ -37,25 +39,51 @@ dojo.widget.defineWidget(
 			this.containerNode.style.left = "";
 			this.containerNode.style.top = "";
 
-			this.inputNode.name = this.inputName;
+			if(this.inputId){ this.inputNode.id = this.inputId; }
+			if(this.inputName){ this.inputNode.name = this.inputName; }
 			this.inputNode.style.width = this.inputWidth;
 
 			if(this.iconURL){ this.buttonNode.src = this.iconURL; }
 			if(this.iconAlt){ this.buttonNode.alt = this.iconAlt; }
 
-			dojo.event.connect(this.valueInputNode, "onchange", this, "onInputChange");
+			dojo.event.connect(this.inputNode, "onchange", this, "onInputChange");
+			
+			this.containerIframe = new dojo.html.BackgroundIframe(this.containerNode);
+			this.containerIframe.size([0,0,0,0]);
 		},
 
-		onDropdown: function(evt){
+		onIconClick: function(evt){
 			this.toggleContainerShow();
 		},
 
 		toggleContainerShow: function(){
-			dojo.html.toggleShowing(this.containerNode);
+			if(dojo.html.isShowing(this.containerNode)){
+				dojo.html.hide(this.containerNode);
+			}else{
+				this.showContainer();
+			}
+		},
+		
+		showContainer: function(){
+			dojo.html.show(this.containerNode);
+			this.sizeBackgroundIframe();
 		},
 		
 		onHide: function(evt){
 			dojo.html.hide(this.containerNode);
+		},
+		
+		sizeBackgroundIframe: function(){
+			var w = dojo.style.getOuterWidth(this.containerNode);
+			var h = dojo.style.getOuterHeight(this.containerNode);
+			if(w==0||h==0){
+				// need more time to calculate size
+				dojo.lang.setTimeout(this, "sizeBackgroundIframe", 100);
+				return;
+			}
+			if(dojo.html.isShowing(this.containerNode)){
+				this.containerIframe.size([0,0,w,h]);
+			}
 		},
 
 		onInputChange: function(){}
