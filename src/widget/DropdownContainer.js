17ec75e2ab7de97efca64916f325505f59726930
dylan <dylan@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-11 04:33:14 +0000

    doh, reverting my previous checkin as it is redundant, which means that #447 was probably already fixed, and if not, it is because that use of the DatePicker was likely not wrapped in the dropdown container
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@3929 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/DropdownContainer.js
+++ src/widget/DropdownContainer.js
@@ -50,10 +50,6 @@ dojo.widget.defineWidget(
 			
 			this.containerIframe = new dojo.html.BackgroundIframe(this.containerNode);
 			this.containerIframe.size([0,0,0,0]);
-			
-			if(dojo.render.html.ie) {
-				new dojo.html.BackgroundIframe(this.domNode);
-			}
 		},
 
 		onIconClick: function(evt){
