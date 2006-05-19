d9e2b8b910bda5507816c58c1cb2c4ad0eb0c2cd
dmachi <dmachi@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-19 01:01:42 +0000

    added custom build profile for dojoWebsite
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@4069 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/profiles/dojoWebsite.profile.js
@@ -0,0 +1,11 @@
+var dependencies = [ 
+	"dojo.widget.*",
+	"dojo.io.*",
+	"dojo.event.*",
+	"dojo.widget.DemoEngine",
+	"dojo.widget.TabContainer",
+	"dojo.widget.ContentPane",
+	"dojo.widget.Button"
+];
+
+load("getDependencyList.js");
