ac588d286706bd0bb5760b2702ff553af4db0930
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-19 14:57:56 +0000

    Fixed the footer link.
    git-svn-id: http://svn.dojotoolkit.org/src@4072 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/documents/web0.3/wp_theme/dojo_of_summer/footer.php
+++ trunk/documents/web0.3/wp_theme/dojo_of_summer/footer.php
@@ -2,7 +2,7 @@
 		<div id="footer">
 			<div class="links">
 				<span><a href="http://dojotoolkit.org/">Home</a></span>
-				<span><a href="http://dojotoolkit.org/developer/">Developers</a></span>
+				<span><a href="http://dojotoolkit.org/developers/">Developers</a></span>
 				<span><a href="http://dojo.jot.com/">Wiki</a></span>
 				<span><a href="http://trac.dojotoolkit.org/">Bug Tracking</a></span>
 				<span><a href="http://dojotoolkit.org/foundation/">About the Foundation</a></span>
