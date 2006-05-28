41f74c64dce7f774dff8374ec2f45ee4237a200a
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-05-28 00:40:23 +0000

    prevent SwtWidget from interfering with test runs
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@4194 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/SwtWidget.js
+++ src/widget/SwtWidget.js
@@ -6,43 +6,49 @@ dojo.require("dojo.uri.*");
 dojo.require("dojo.lang.func");
 dojo.require("dojo.lang.extras");
 
-importPackage(Packages.org.eclipse.swt.widgets);
+try{
+	importPackage(Packages.org.eclipse.swt.widgets);
 
-dojo.declare("dojo.widget.SwtWidget", dojo.widget.Widget, {
-	initializer: function() {
-		if((arguments.length>0)&&(typeof arguments[0] == "object")){
-			this.create(arguments[0]);
+	dojo.declare("dojo.widget.SwtWidget", dojo.widget.Widget, {
+		initializer: function() {
+			if((arguments.length>0)&&(typeof arguments[0] == "object")){
+				this.create(arguments[0]);
+			}
+		},
+
+		display: null,
+		shell: null,
+
+		show: function(){ },
+		hide: function(){ },
+
+		addChild: function(){ },
+		registerChild: function(){ },
+		addWidgetAsDirectChild: function(){ },
+		removeChild: function(){ },
+		cleanUp: function(){ },
+		destroyRendering: function(){ },
+		postInitialize: function(){ },
+	});
+
+	// initialize SWT runtime
+
+	dojo.widget.SwtWidget.prototype.display = new Display();
+	dojo.widget.SwtWidget.prototype.shell = new Shell(dojo.widget.SwtWidget.prototype.display);
+
+	dojo.widget.manager.startShell = function(){
+		var sh = dojo.widget.SwtWidget.prototype.shell;
+		var d = dojo.widget.SwtWidget.prototype.display;
+		sh.open();
+		while(!sh.isDisposed()){
+			dojo.widget.manager.doNext();
+			if(!d.readAndDispatch()){
+				d.sleep();
+			}
 		}
-	},
-
-	display: null,
-	shell: null,
-
-	show: function(){ },
-	hide: function(){ },
-
-	addChild: function(){ },
-	registerChild: function(){ },
-	addWidgetAsDirectChild: function(){ },
-	removeChild: function(){ },
-	cleanUp: function(){ },
-	destroyRendering: function(){ },
-	postInitialize: function(){ },
-});
-
-// initialize SWT runtime
-
-dojo.widget.SwtWidget.prototype.display = new Display();
-dojo.widget.SwtWidget.prototype.shell = new Shell(dojo.widget.SwtWidget.prototype.display);
-
-(function(){
-	var sh = dojo.widget.SwtWidget.prototype.shell;
-	var d = dojo.widget.SwtWidget.prototype.display;
-	sh.open();
-	while(!sh.isDisposed()){
-		if(!d.readAndDispatch()){
-			d.sleep();
-		}
-	}
-	d.dispose();
-})();
+		d.dispose();
+	};
+}catch(e){
+	// seems we didn't have the SWT classes in the environment. Log it.
+	dojo.debug("dojo.widget.SwtWidget not loaded. SWT classes not available");
+}
