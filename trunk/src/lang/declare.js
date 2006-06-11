koordinates/dojo

commit 19aa8084988b810800d7718a0534318a38710199

Fix declare.js to repair test failures reported in trac #851
git-svn-id: http://svn.dojotoolkit.org/src@4323 560b804f-0ae3-0310-86f3-f6aa0a117693

trunk/src/lang/declare.js
@@ -8,14 +8,14 @@ dojo.require("dojo.lang.extras");
  *
  * - inherits from "superclass(es)" 
  *
- *   "superclass" argument may be a function, or an array of 
- *   functions. 
+ *   "superclass" argument may be a Function, or an array of 
+ *   Functions. 
  *
  *   If "superclass" is an array, the first element is used 
- *   as the prototypical ancestor and any following functions 
+ *   as the prototypical ancestor and any following Functions 
  *   become mixin ancestors. 
  * 
- *   All "superclass(es)" must be functions (not mere Objects).
+ *   All "superclass(es)" must be Functions (not mere Objects).
  *
  *   Using mixin ancestors provides a type of multiple
  *   inheritance. Mixin ancestors prototypical 
@@ -27,13 +27,14 @@ dojo.require("dojo.lang.extras");
  * - name of the class ("className" argument) is stored in 
  *   "declaredClass" property
  * 
- * An initializer function can be specified in the "init" 
- * argument, or by including a function called "initializer" 
- * in "props".
+ * - An initializer function can be specified in the "init" 
+ *   argument, or by including a function called "initializer" 
+ *   in "props".
  * 
- * Superclass methods (inherited methods) can be invoked using "inherited" method:
+ * - Superclass methods (inherited methods) can be invoked using "inherited" method:
  *
  * this.inherited(<method name>[, <argument array>]);
+ * 
  * - inherited will continue up the prototype chain until it finds an implementation of method
  * - nested calls to inherited are supported (i.e. inherited method "A" can succesfully call inherited("A"), and so on)
  *
@@ -81,11 +82,14 @@ dojo.lang.declare = function(className /*string*/, superclass /*function || arra
 		dojo.lang.extend(ctor, mixins[i].prototype);
 	}
 	ctor.prototype.declaredClass = className;
-	props = (props)||({});
-	if (props){dojo.lang.extend(ctor, props);}
+	if (dojo.lang.isArray(props)){
+		dojo.lang.extend.apply(dojo.lang, [ctor].concat(props));
+	}else{
+		dojo.lang.extend(ctor, (props)||{});
+	}
 	dojo.lang.extend(ctor, dojo.lang.declare.base);
 	ctor.prototype.constructor = ctor;
-	ctor.prototype.initializer=(props.initializer)||(init)||(function(){});
+	ctor.prototype.initializer=(ctor.prototype.initializer)||(init)||(function(){});
 	dojo.lang.setObjPathValue(className, ctor, null, true);
 }
 
@@ -125,9 +129,9 @@ dojo.lang.declare.base = {
 	// invokes ctor.prototype.method, with args, in our context 
 	inheritedFrom: function(ctor, prop, args){
 		var p = ((ctor)&&(ctor.prototype)&&(ctor.prototype[prop]));
-		return (dojo.lang.isFunction(p) ? p.apply(this, args) : p);
+		return (dojo.lang.isFunction(p) ? p.apply(this, (args||[])) : p);
 	},
-	// searches backward thru prototype chain to find nearest ancestral implementation of method
+	// searches backward thru prototype chain to find nearest ancestral instance of prop
 	inherited: function(prop, args){
 		var p = this._getPropContext();
 		do{

commit 2647ae4fad407a4941664eda8f7bbb3daf77a424

fix initializer bug introduced in r4323 remove some bogus commented debug statements
git-svn-id: http://svn.dojotoolkit.org/src@4332 560b804f-0ae3-0310-86f3-f6aa0a117693

trunk/src/lang/declare.js
@@ -81,6 +81,7 @@ dojo.lang.declare = function(className /*string*/, superclass /*function || arra
 	for(var i=0,l=mixins.length; i<l; i++){
 		dojo.lang.extend(ctor, mixins[i].prototype);
 	}
+	ctor.prototype.initializer = null;
 	ctor.prototype.declaredClass = className;
 	if(dojo.lang.isArray(props)){
 		dojo.lang.extend.apply(dojo.lang, [ctor].concat(props));

trunk/src/widget/Widget.js
@@ -550,10 +550,8 @@ dojo.widget._defineWidget = function(widgetClass /*string*/, renderer /*string*/
 	var namespace = widgetClass.split(".");
 	var type = namespace.pop(); // type <= WidgetName, namespace <= foo.bar.baz<.renderer>
 	var regx = "\\.(" + (renderer ? renderer + '|' : '') + dojo.widget.defineWidget.renderers + ")\\.";
-	//dojo.debug(regx);
 	var r = widgetClass.search(new RegExp(regx));
 	namespace = (r < 0 ? namespace.join(".") : widgetClass.substr(0, r));
-	//dojo.debug(r + ': ' + widgetClass + ': ' + namespace);
 
 	dojo.widget.manager.registerWidgetPackage(namespace);
 	dojo.widget.tags.addParseTreeHandler("dojo:"+type.toLowerCase());