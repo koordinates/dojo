33cb07a28ef89b866bdf16934af1379dd1041e11
BryanForbes <BryanForbes@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-06-12 18:05:11 +0000

    Moved contents of html/style/opacity.js to html/style/display.js and removed it.  Moved getBackgroundColor to html/style/color.js.  Modified lfx to require color now.
    git-svn-id: http://svn.dojotoolkit.org/src@4370 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/html-refactor/src/html/style/display.js
+++ branches/html-refactor/src/html/style/display.js
@@ -1,26 +1,7 @@
 dojo.provide("dojo.html.style.display");
 
 dojo.require("dojo.html.style");
-dojo.require("dojo.graphics.color");
 dojo.require("dojo.lang.common");
-
-dojo.html.getBackgroundColor = function(node){
-	node = dojo.byId(node);
-	var color;
-	do{
-		color = dojo.html.getStyle(node, "background-color");
-		// Safari doesn't say "transparent"
-		if(color.toLowerCase() == "rgba(0, 0, 0, 0)") { color = "transparent"; }
-		if(node == document.getElementsByTagName("body")[0]) { node = null; break; }
-		node = node.parentNode;
-	}while(node && dojo.lang.inArray(color, ["transparent", ""]));
-	if(color == "transparent"){
-		color = [255, 255, 255, 0];
-	}else{
-		color = dojo.graphics.color.extractRGB(color);
-	}
-	return color;
-}
 
 dojo.html._toggle = function(node, tester, setter){
 	node = dojo.byId(node);
@@ -125,4 +106,78 @@ dojo.html.isVisible = function(node){
 // return the new value of isVisible()
 dojo.html.toggleVisibility = function(node){
 	return dojo.html._toggle(node, dojo.html.isVisible, dojo.html.setVisibility);
+}
+
+/* float between 0.0 (transparent) and 1.0 (opaque) */
+dojo.html.setOpacity = function(node, opacity, dontFixOpacity){
+	node = dojo.byId(node);
+	var h = dojo.render.html;
+	if(!dontFixOpacity){
+		if( opacity >= 1.0){
+			if(h.ie){
+				dojo.html.clearOpacity(node);
+				return;
+			}else{
+				opacity = 0.999999;
+			}
+		}else if( opacity < 0.0){ opacity = 0; }
+	}
+	if(h.ie){
+		if(node.nodeName.toLowerCase() == "tr"){
+			// FIXME: is this too naive? will we get more than we want?
+			var tds = node.getElementsByTagName("td");
+			for(var x=0; x<tds.length; x++){
+				tds[x].style.filter = "Alpha(Opacity="+opacity*100+")";
+			}
+		}
+		node.style.filter = "Alpha(Opacity="+opacity*100+")";
+	}else if(h.moz){
+		node.style.opacity = opacity; // ffox 1.0 directly supports "opacity"
+		node.style.MozOpacity = opacity;
+	}else if(h.safari){
+		node.style.opacity = opacity; // 1.3 directly supports "opacity"
+		node.style.KhtmlOpacity = opacity;
+	}else{
+		node.style.opacity = opacity;
+	}
+}
+
+dojo.html.clearOpacity = function clearOpacity(node){
+	node = dojo.byId(node);
+	var ns = node.style;
+	var h = dojo.render.html;
+	if(h.ie){
+		try {
+			if( node.filters && node.filters.alpha ){
+				ns.filter = ""; // FIXME: may get rid of other filter effects
+			}
+		} catch(e) {
+			/*
+			 * IE7 gives error if node.filters not set;
+			 * don't know why or how to workaround (other than this)
+			 */
+		}
+	}else if(h.moz){
+		ns.opacity = 1;
+		ns.MozOpacity = 1;
+	}else if(h.safari){
+		ns.opacity = 1;
+		ns.KhtmlOpacity = 1;
+	}else{
+		ns.opacity = 1;
+	}
+}
+
+dojo.html.getOpacity = function getOpacity (node){
+	node = dojo.byId(node);
+	var h = dojo.render.html;
+	if(h.ie){
+		var opac = (node.filters && node.filters.alpha &&
+			typeof node.filters.alpha.opacity == "number"
+			? node.filters.alpha.opacity : 100) / 100;
+	}else{
+		var opac = node.style.opacity || node.style.MozOpacity ||
+			node.style.KhtmlOpacity || 1;
+	}
+	return opac >= 0.999999 ? 1.0 : Number(opac);
 }
