0361a2b9003e57280287e3da5329e2178774caba
liucougar <liucougar@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-06-28 04:12:05 +0000

    fixed typo in comment
    git-svn-id: http://svn.dojotoolkit.org/src@4544 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/event/browser.js
+++ trunk/src/event/browser.js
@@ -236,7 +236,7 @@ dojo.event.browser = new function(){
 			if(!evt.layerX){ evt.layerX = evt.offsetX; }
 			if(!evt.layerY){ evt.layerY = evt.offsetY; }
 			// FIXME: scroll position query is duped from dojo.html to avoid dependency on that entire module
-			// is it save to use dojo.body() here?
+			// is it safe to use dojo.body() here?
 			var docBody = ((dojo.render.html.ie55)||(document["compatMode"] == "BackCompat")) ? document.body : document.documentElement;
 			if(!evt.pageX){ evt.pageX = evt.clientX + (docBody.scrollLeft || 0) }
 			if(!evt.pageY){ evt.pageY = evt.clientY + (docBody.scrollTop || 0) }
