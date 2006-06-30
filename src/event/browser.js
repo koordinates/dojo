7b110add9b7f8d70d300bb4e1b4ba6859b6b6fbd
liucougar <liucougar@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-06-30 02:14:08 +0000

    in IE, document.documentElement should be use to retrieve scrollLeft and scrollTop, instead of document.body so dojo.body() should not be used here. This also fixed popupmenu2 regression in IE (pop up in wrong place if scrolled the window)
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@4575 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/event/browser.js
+++ src/event/browser.js
@@ -236,7 +236,9 @@ dojo.event.browser = new function(){
 			if(!evt.layerX){ evt.layerX = evt.offsetX; }
 			if(!evt.layerY){ evt.layerY = evt.offsetY; }
 			// FIXME: scroll position query is duped from dojo.html to avoid dependency on that entire module
-			var docBody = dojo.body();
+			// DONOT replace the following to use dojo.body(), in IE, document.documentElement should be used
+			// here rather than document.body
+			var docBody = ((dojo.render.html.ie55)||(document["compatMode"] == "BackCompat")) ? document.body : document.documentElement;
 			if(!evt.pageX){ evt.pageX = evt.clientX + (docBody.scrollLeft || 0) }
 			if(!evt.pageY){ evt.pageY = evt.clientY + (docBody.scrollTop || 0) }
 			// mouseover
