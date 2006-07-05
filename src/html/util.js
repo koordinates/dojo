664c9a35b78c25ad14a04a7835134966e691adff
BryanForbes <BryanForbes@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-07-05 16:21:53 +0000

    Extras.js is no more, moved things to util.js. Fixed deps for selection.js. Moved sumAncestorProperties to layout.js. Moved stylesheet and css functions away from util.js into style.js. Moved get*Window functions into util.js. Updated more tests.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/html-refactor@4612 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/html/util.js
+++ src/html/util.js
@@ -1,185 +1,398 @@
 dojo.provide("dojo.html.util");
 
-dojo.html.sumAncestorProperties = function(node, prop){
-	node = dojo.byId(node);
-	if(!node){ return 0; } // FIXME: throw an error?
+// Get the window object where the element is placed in.
+dojo.html.getElementWindow = function(element){
+	return dojo.html.getDocumentWindow( element.ownerDocument );
+}
+
+// Get window object associated with document doc
+dojo.html.getDocumentWindow = function(doc){
+	// With Safari, there is not wa to retrieve the window from the document, so we must fix it.
+	if(dojo.render.html.safari && !doc._parentWindow){
+		var w=window.top;
+		w.document.parentWindow=w;
+		for(var i=0; i<w.frames.length; i++){
+			w.frames[i].document.parentWindow=w.frames[i];
+		}
+	}
+
+	//In some IE versions (at least 6.0), document.parentWindow does not return a 
+	//reference to the real window object (maybe a copy), so we must fix it as well
+	if(dojo.render.html.ie && window !== document.parentWindow && !doc._parentWindow){
+		/*
+		In IE 6, only the variable "window" can be used to connect events (others
+		may be only copies). We use IE specific execScript to attach the real window
+		reference to document._parentWindow for later use
+		*/
+		doc.parentWindow.execScript("document._parentWindow = window;", "Javascript");
+	}
+
+	return doc._parentWindow || doc.parentWindow || doc.defaultView;
+}
+
+/*
+	This is a Safari specific function that fix the reference to the parent
+	window from the document object.
+*/
+dojo.html._fixSafariDocumentParentWindow = function( targetWindow ){
+	targetWindow.document.parentWindow = targetWindow;
 	
-	var retVal = 0;
-	while(node){
-		var val = node[prop];
-		if(val){
-			retVal += val - 0;
-			if(node==dojo.body()){ break; }// opera and khtml #body & #html has the same values, we only need one value
-		}
-		node = node.parentNode;
+	for (var i = 0; i < targetWindow.frames.length; i++){
+		dojo.html._fixSafariDocumentParentWindow(targetWindow.frames[i]);
 	}
-	return retVal;
 }
 
-dojo.html.styleSheet = null;
+/**
+ * Calculates the mouse's direction of gravity relative to the centre
+ * of the given node.
+ * <p>
+ * If you wanted to insert a node into a DOM tree based on the mouse
+ * position you might use the following code:
+ * <pre>
+ * if (gravity(node, e) & gravity.NORTH) { [insert before]; }
+ * else { [insert after]; }
+ * </pre>
+ *
+ * @param node The node
+ * @param e		The event containing the mouse coordinates
+ * @return		 The directions, NORTH or SOUTH and EAST or WEST. These
+ *						 are properties of the function.
+ */
+dojo.html.gravity = function(node, e){
+	node = dojo.byId(node);
+	var mouse = dojo.html.getCursorPosition(e);
 
-// FIXME: this is a really basic stub for adding and removing cssRules, but
-// it assumes that you know the index of the cssRule that you want to add 
-// or remove, making it less than useful.  So we need something that can 
-// search for the selector that you you want to remove.
-dojo.html.insertCssRule = function(selector, declaration, index) {
-	if (!dojo.html.styleSheet) {
-		if (document.createStyleSheet) { // IE
-			dojo.html.styleSheet = document.createStyleSheet();
-		} else if (document.styleSheets[0]) { // rest
-			// FIXME: should create a new style sheet here
-			// fall back on an exsiting style sheet
-			dojo.html.styleSheet = document.styleSheets[0];
-		} else { return null; } // fail
+	with (dojo.html) {
+		var absolute = getAbsolutePosition(node, true);
+		var inner = getBorderBox(node);
+		var nodecenterx = absolute.x + (inner.width / 2);
+		var nodecentery = absolute.y + (inner.height / 2);
 	}
-
-	if (arguments.length < 3) { // index may == 0
-		if (dojo.html.styleSheet.cssRules) { // W3
-			index = dojo.html.styleSheet.cssRules.length;
-		} else if (dojo.html.styleSheet.rules) { // IE
-			index = dojo.html.styleSheet.rules.length;
-		} else { return null; } // fail
+	
+	with (dojo.html.gravity) {
+		return ((mouse.x < nodecenterx ? WEST : EAST) |
+			(mouse.y < nodecentery ? NORTH : SOUTH));
 	}
-
-	if (dojo.html.styleSheet.insertRule) { // W3
-		var rule = selector + " { " + declaration + " }";
-		return dojo.html.styleSheet.insertRule(rule, index);
-	} else if (dojo.html.styleSheet.addRule) { // IE
-		return dojo.html.styleSheet.addRule(selector, declaration, index);
-	} else { return null; } // fail
 }
 
-dojo.html.removeCssRule = function(index){
-	if(!dojo.html.styleSheet){
-		dojo.debug("no stylesheet defined for removing rules");
-		return false;
+dojo.html.gravity.NORTH = 1;
+dojo.html.gravity.SOUTH = 1 << 1;
+dojo.html.gravity.EAST = 1 << 2;
+dojo.html.gravity.WEST = 1 << 3;
+
+dojo.html.overElement = function(element, e){
+	element = dojo.byId(element);
+	var mouse = dojo.html.getCursorPosition(e);
+
+	with(dojo.html){
+		var inner = getBorderBox(element);
+		var absolute = getAbsolutePosition(element, true);
+		var top = absolute.y;
+		var bottom = top + inner.height;
+		var left = absolute.x;
+		var right = left + inner.width;
 	}
-	if(dojo.html.render.ie){
-		if(!index){
-			index = dojo.html.styleSheet.rules.length;
-			dojo.html.styleSheet.removeRule(index);
-		}
-	}else if(document.styleSheets[0]){
-		if(!index){
-			index = dojo.html.styleSheet.cssRules.length;
-		}
-		dojo.html.styleSheet.deleteRule(index);
-	}
-	return true;
+	
+	return (mouse.x >= left && mouse.x <= right &&
+		mouse.y >= top && mouse.y <= bottom);
 }
 
-// calls css by XmlHTTP and inserts it into DOM as <style [widgetType="widgetType"]> *downloaded cssText*</style>
-dojo.html._insertedCssFiles = []; // cache container needed because IE reformats cssText when added to DOM
-dojo.html.insertCssFile = function(URI, doc, checkDuplicates){
-	if(!URI){ return; }
-	if(!doc){ doc = document; }
-	var cssStr = dojo.hostenv.getText(URI);
-	cssStr = dojo.html.fixPathsInCssText(cssStr, URI);
-
-	if(checkDuplicates){
-		var idx = -1, node, ent = dojo.html._insertedCssFiles;
-		for(var i = 0; i < ent.length; i++){
-			if((ent[i].doc == doc) && (ent[i].cssText == cssStr)){
-				idx = i; node = ent[i].nodeRef;
-				break;
-			}
-		}
-		// make sure we havent deleted our node
-		if(node){
-			var styles = doc.getElementsByTagName("style");
-			for(var i = 0; i < styles.length; i++){
-				if(styles[i] == node){
-					return;
+/**
+ * Attempts to return the text as it would be rendered, with the line breaks
+ * sorted out nicely. Unfinished.
+ */
+dojo.html.renderedTextContent = function(node){
+	node = dojo.byId(node);
+	var result = "";
+	if (node == null) { return result; }
+	for (var i = 0; i < node.childNodes.length; i++) {
+		switch (node.childNodes[i].nodeType) {
+			case 1: // ELEMENT_NODE
+			case 5: // ENTITY_REFERENCE_NODE
+				var display = "unknown";
+				try {
+					display = dojo.html.getStyle(node.childNodes[i], "display");
+				} catch(E) {}
+				switch (display) {
+					case "block": case "list-item": case "run-in":
+					case "table": case "table-row-group": case "table-header-group":
+					case "table-footer-group": case "table-row": case "table-column-group":
+					case "table-column": case "table-cell": case "table-caption":
+						// TODO: this shouldn't insert double spaces on aligning blocks
+						result += "\n";
+						result += dojo.html.renderedTextContent(node.childNodes[i]);
+						result += "\n";
+						break;
+					
+					case "none": break;
+					
+					default:
+						if(node.childNodes[i].tagName && node.childNodes[i].tagName.toLowerCase() == "br") {
+							result += "\n";
+						} else {
+							result += dojo.html.renderedTextContent(node.childNodes[i]);
+						}
+						break;
 				}
-			}
-			// delete this entry
-			dojo.html._insertedCssFiles.shift(idx, 1);
+				break;
+			case 3: // TEXT_NODE
+			case 2: // ATTRIBUTE_NODE
+			case 4: // CDATA_SECTION_NODE
+				var text = node.childNodes[i].nodeValue;
+				var textTransform = "unknown";
+				try {
+					textTransform = dojo.html.getStyle(node, "text-transform");
+				} catch(E) {}
+				switch (textTransform){
+					case "capitalize":
+						var words = text.split(' ');
+						for(var i=0; i<words.length; i++){
+							words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+						}
+						text = words.join(" ");
+						break;
+					case "uppercase": text = text.toUpperCase(); break;
+					case "lowercase": text = text.toLowerCase(); break;
+					default: break; // leave as is
+				}
+				// TODO: implement
+				switch (textTransform){
+					case "nowrap": break;
+					case "pre-wrap": break;
+					case "pre-line": break;
+					case "pre": break; // leave as is
+					default:
+						// remove whitespace and collapse first space
+						text = text.replace(/\s+/, " ");
+						if (/\s$/.test(result)) { text.replace(/^\s/, ""); }
+						break;
+				}
+				result += text;
+				break;
+			default:
+				break;
 		}
 	}
-
-	var style = dojo.html.insertCssText(cssStr);
-	dojo.html._insertedCssFiles.push({'doc': doc, 'cssText': cssStr, 'nodeRef': style});
-
-	// insert custom attribute ex dbgHref="../foo.css" usefull when debugging in DOM inspectors, no?
-	if(style && djConfig.isDebug){
-		style.setAttribute("dbgHref", URI);
-	}
-	return style
+	return result;
 }
 
-// DomNode Style  = insertCssText(String ".dojoMenu {color: green;}"[, DomDoc document, dojo.uri.Uri Url ])
-dojo.html.insertCssText = function(cssStr, doc, URI){
-	if(!cssStr){ return; }
-	if(!doc){ doc = document; }
-	if(URI){// fix paths in cssStr
-		cssStr = dojo.html.fixPathsInCssText(cssStr, URI);
+dojo.html.createNodesFromText = function(txt, trim){
+	if(trim) { txt = txt.replace(/^\s+|\s+$/g, ""); }
+
+	var tn = dojo.doc().createElement("div");
+	// tn.style.display = "none";
+	tn.style.visibility= "hidden";
+	dojo.body().appendChild(tn);
+	var tableType = "none";
+	if((/^<t[dh][\s\r\n>]/i).test(txt.replace(/^\s+/))) {
+		txt = "<table><tbody><tr>" + txt + "</tr></tbody></table>";
+		tableType = "cell";
+	} else if((/^<tr[\s\r\n>]/i).test(txt.replace(/^\s+/))) {
+		txt = "<table><tbody>" + txt + "</tbody></table>";
+		tableType = "row";
+	} else if((/^<(thead|tbody|tfoot)[\s\r\n>]/i).test(txt.replace(/^\s+/))) {
+		txt = "<table>" + txt + "</table>";
+		tableType = "section";
 	}
-	var style = doc.createElement("style");
-	style.setAttribute("type", "text/css");
-	// IE is b0rken enough to require that we add the element to the doc
-	// before changing it's properties
-	var head = doc.getElementsByTagName("head")[0];
-	if(!head){ // must have a head tag 
-		dojo.debug("No head tag in document, aborting styles");
-		return;
-	}else{
-		head.appendChild(style);
+	tn.innerHTML = txt;
+	if(tn["normalize"]){
+		tn.normalize();
 	}
-	if(style.styleSheet){// IE
-		style.styleSheet.cssText = cssStr;
-	}else{ // w3c
-		var cssText = doc.createTextNode(cssStr);
-		style.appendChild(cssText);
+
+	var parent = null;
+	switch(tableType) {
+		case "cell":
+			parent = tn.getElementsByTagName("tr")[0];
+			break;
+		case "row":
+			parent = tn.getElementsByTagName("tbody")[0];
+			break;
+		case "section":
+			parent = tn.getElementsByTagName("table")[0];
+			break;
+		default:
+			parent = tn;
+			break;
 	}
-	return style;
+
+	/* this doesn't make much sense, I'm assuming it just meant trim() so wrap was replaced with trim
+	if(wrap){ 
+		var ret = [];
+		// start hack
+		var fc = tn.firstChild;
+		ret[0] = ((fc.nodeValue == " ")||(fc.nodeValue == "\t")) ? fc.nextSibling : fc;
+		// end hack
+		// tn.style.display = "none";
+		dojo.body().removeChild(tn);
+		return ret;
+	}
+	*/
+	var nodes = [];
+	for(var x=0; x<parent.childNodes.length; x++){
+		nodes.push(parent.childNodes[x].cloneNode(true));
+	}
+	tn.style.display = "none"; // FIXME: why do we do this?
+	dojo.body().removeChild(tn);
+	return nodes;
 }
 
-// String cssText = fixPathsInCssText(String cssStr, dojo.uri.Uri URI)
-// usage: cssText comes from dojoroot/src/widget/templates/HtmlFoobar.css
-// 	it has .dojoFoo { background-image: url(images/bar.png);} 
-//	then uri should point to dojoroot/src/widget/templates/
-dojo.html.fixPathsInCssText = function(cssStr, URI){
-	if(!cssStr || !URI){ return; }
-	var match, str = "", url = "";
-	var regex = /url\(\s*([\t\s\w()\/.\\'"-:#=&?]*)\s*\)/;
-	var regexProtocol = /(file|https?|ftps?):\/\//;
-	var regexTrim = /^[\s]*(['"]?)([\w()\/.\\'"-:#=&?]*)\1[\s]*?$/;
-	while(match = regex.exec(cssStr)){
-		url = match[1].replace(regexTrim, "$2");
-		if(!regexProtocol.exec(url)){
-			url = (new dojo.uri.Uri(URI, url).toString());
-		}
-		str += cssStr.substring(0, match.index) + "url(" + url + ")";
-		cssStr = cssStr.substr(match.index + match[0].length);
+/* TODO: merge placeOnScreen and placeOnScreenPoint to make 1 function that allows you
+ * to define which corner(s) you want to bind to. Something like so:
+ *
+ * kes(node, desiredX, desiredY, "TR")
+ * kes(node, [desiredX, desiredY], ["TR", "BL"])
+ *
+ * TODO: make this function have variable call sigs
+ *
+ * kes(node, ptArray, cornerArray, padding, hasScroll)
+ * kes(node, ptX, ptY, cornerA, cornerB, cornerC, paddingArray, hasScroll)
+ */
+
+/**
+ * Keeps 'node' in the visible area of the screen while trying to
+ * place closest to desiredX, desiredY. The input coordinates are
+ * expected to be the desired screen position, not accounting for
+ * scrolling. If you already accounted for scrolling, set 'hasScroll'
+ * to true. Set padding to either a number or array for [paddingX, paddingY]
+ * to put some buffer around the element you want to position.
+ * NOTE: node is assumed to be absolutely or relatively positioned.
+ *
+ * Alternate call sig:
+ *  placeOnScreen(node, [x, y], padding, hasScroll)
+ *
+ * Examples:
+ *  placeOnScreen(node, 100, 200)
+ *  placeOnScreen("myId", [800, 623], 5)
+ *  placeOnScreen(node, 234, 3284, [2, 5], true)
+ */
+dojo.html.placeOnScreen = function(node, desiredX, desiredY, padding, hasScroll) {
+	if(desiredX instanceof Array || typeof desiredX == "array") {
+		hasScroll = padding;
+		padding = desiredY;
+		desiredY = desiredX[1];
+		desiredX = desiredX[0];
 	}
-	return str + cssStr;
+
+	if(!isNaN(padding)) {
+		padding = [Number(padding), Number(padding)];
+	} else if(!(padding instanceof Array || typeof padding == "array")) {
+		padding = [0, 0];
+	}
+
+	var scroll = dojo.html.getScrollOffset();
+	var view = dojo.html.getViewportSize();
+
+	node = dojo.byId(node);
+	var w = node.offsetWidth + padding[0];
+	var h = node.offsetHeight + padding[1];
+
+	if(hasScroll) {
+		desiredX -= scroll.x;
+		desiredY -= scroll.y;
+	}
+
+	var x = desiredX + w;
+	if(x > view.w) {
+		x = view.w - w;
+	} else {
+		x = desiredX;
+	}
+	x = Math.max(padding[0], x) + scroll.x;
+
+	var y = desiredY + h;
+	if(y > view.h) {
+		y = view.h - h;
+	} else {
+		y = desiredY;
+	}
+	y = Math.max(padding[1], y) + scroll.y;
+
+	node.style.left = x + "px";
+	node.style.top = y + "px";
+
+	var ret = [x, y];
+	ret.x = x;
+	ret.y = y;
+	return ret;
 }
 
-dojo.html.setActiveStyleSheet = function(title){
-	var i = 0, a, els = dojo.doc().getElementsByTagName("link");
-	while (a = els[i++]) {
-		if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")){
-			a.disabled = true;
-			if (a.getAttribute("title") == title) { a.disabled = false; }
-		}
+/**
+ * Like placeOnScreenPoint except that it attempts to keep one of the node's
+ * corners at desiredX, desiredY.  Favors the bottom right position
+ *
+ * Examples placing node at mouse position (where e = [Mouse event]):
+ *  placeOnScreenPoint(node, e.clientX, e.clientY);
+ */
+dojo.html.placeOnScreenPoint = function(node, desiredX, desiredY, padding, hasScroll) {
+	if(desiredX instanceof Array || typeof desiredX == "array") {
+		hasScroll = padding;
+		padding = desiredY;
+		desiredY = desiredX[1];
+		desiredX = desiredX[0];
 	}
-}
 
-dojo.html.getActiveStyleSheet = function(){
-	var i = 0, a, els = dojo.doc().getElementsByTagName("link");
-	while (a = els[i++]) {
-		if (a.getAttribute("rel").indexOf("style") != -1 &&
-			a.getAttribute("title") && !a.disabled) { return a.getAttribute("title"); }
+	if(!isNaN(padding)) {
+		padding = [Number(padding), Number(padding)];
+	} else if(!(padding instanceof Array || typeof padding == "array")) {
+		padding = [0, 0];
 	}
-	return null;
-}
 
-dojo.html.getPreferredStyleSheet = function(){
-	var i = 0, a, els = dojo.doc().getElementsByTagName("link");
-	while (a = els[i++]) {
-		if(a.getAttribute("rel").indexOf("style") != -1
-			&& a.getAttribute("rel").indexOf("alt") == -1
-			&& a.getAttribute("title")) { return a.getAttribute("title"); }
+	var scroll = dojo.html.getScrollOffset();
+	var view = dojo.html.getViewportSize();
+
+	node = dojo.byId(node);
+	var oldDisplay = node.style.display;
+	node.style.display="";
+	var w = dojo.html.getInnerWidth(node);
+	var h = dojo.html.getInnerHeight(node);
+	node.style.display=oldDisplay;
+
+	if(hasScroll) {
+		desiredX -= scroll.x;
+		desiredY -= scroll.y;
 	}
-	return null;
+
+	var x = -1, y = -1;
+	//dojo.debug((desiredX+padding[0]) + w, "<=", view.w, "&&", (desiredY+padding[1]) + h, "<=", view.h);
+	if((desiredX+padding[0]) + w <= view.w && (desiredY+padding[1]) + h <= view.h) { // TL
+		x = (desiredX+padding[0]);
+		y = (desiredY+padding[1]);
+		//dojo.debug("TL", x, y);
+	}
+
+	//dojo.debug((desiredX-padding[0]), "<=", view.w, "&&", (desiredY+padding[1]) + h, "<=", view.h);
+	if((x < 0 || y < 0) && (desiredX-padding[0]) <= view.w && (desiredY+padding[1]) + h <= view.h) { // TR
+		x = (desiredX-padding[0]) - w;
+		y = (desiredY+padding[1]);
+		//dojo.debug("TR", x, y);
+	}
+
+	//dojo.debug((desiredX+padding[0]) + w, "<=", view.w, "&&", (desiredY-padding[1]), "<=", view.h);
+	if((x < 0 || y < 0) && (desiredX+padding[0]) + w <= view.w && (desiredY-padding[1]) <= view.h) { // BL
+		x = (desiredX+padding[0]);
+		y = (desiredY-padding[1]) - h;
+		//dojo.debug("BL", x, y);
+	}
+
+	//dojo.debug((desiredX-padding[0]), "<=", view.w, "&&", (desiredY-padding[1]), "<=", view.h);
+	if((x < 0 || y < 0) && (desiredX-padding[0]) <= view.w && (desiredY-padding[1]) <= view.h) { // BR
+		x = (desiredX-padding[0]) - w;
+		y = (desiredY-padding[1]) - h;
+		//dojo.debug("BR", x, y);
+	}
+
+	if(x < 0 || y < 0 || (x + w > view.w) || (y + h > view.h)) {
+		return dojo.html.placeOnScreen(node, desiredX, desiredY, padding, hasScroll);
+	}
+
+	x += scroll.x;
+	y += scroll.y;
+
+	node.style.left = x + "px";
+	node.style.top = y + "px";
+
+	var ret = [x, y];
+	ret.x = x;
+	ret.y = y;
+	return ret;
 }
