b2aa979298b2e069ef56e29974fdba55285cee57
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-07-24 04:35:11 +0000

    Minor fix for FF users with no Firebug installed.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@4889 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/debug/Firebug.js
+++ src/debug/Firebug.js
@@ -1,7 +1,7 @@
 dojo.provide("dojo.debug.Firebug");
 
 if (dojo.render.html.moz) {
-	if (console.log) {
+	if (console && console.log) {
 		dojo.hostenv.println=console.log;
 	} else {
 		dojo.debug("dojo.debug.Firebug requires Firebug > 0.4");
