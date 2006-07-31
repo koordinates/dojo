6f8547dc9006838be571509aa44bd248522e3f1b
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-07-31 05:50:21 +0000

    more ant hacking.  build resources in nls/ directory, append code to dojo.js to load them
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5010 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/flattenResources.js
+++ buildscripts/flattenResources.js
@@ -1,6 +1,22 @@
+// This little utility is invoked by the build to flatten all of the JSON resource bundles used
+// by dojo.requireLocalization(), much like the main build itself, to optimize so that multiple
+// web hits will not be necessary to load these resources.  Normally, a request for a particular
+// bundle in a locale like "en-us" would result in three web hits: one looking for en_us/ another
+// for en/ and another for ROOT/.  All of this multiplied by the number of bundles used can result
+// in a lot of web hits and latency.  This script uses Dojo to actually load the resources into
+// memory, then flatten the object and spit it out using dojo.json.serialize.  The bootstrap
+// will be modified to download exactly one of these files, whichever is closest to the user's
+// locale.
+
+// The input {reqFile} is the list of dojo.requireLocalization() commands grepped from a build.
+// A list of locales to build is loaded.  Any applicable locales plus all partial locales will
+// be generated in the appropriate files at {destDir}/{prefix}_{locale}.js.  Lastly, the list of
+// actual translations found and processed is returned as an array in stdout.
+
 var reqFile = arguments[0];
 var destDir = arguments[1];
-var localeList = arguments[2].split(',');
+var prefix = arguments[2];
+var localeList = arguments[3].split(',');
 
 djConfig={
 	locale: 'xx',
@@ -28,10 +44,10 @@ load(reqFile);
 var djBundlesByLocale = {};
 var locale, entry, bundle;
 
-for (var i = 0; i < djLoadedBundles.length; i++) {
+for (var i = 0; i < djLoadedBundles.length; i++){
 	entry = djLoadedBundles[i];
 	bundle = dojo.hostenv.findModule(entry.modulename)._nls[entry.bundlename];
-	for (locale in bundle) {
+	for (locale in bundle){
 		if (!djBundlesByLocale[locale]){djBundlesByLocale[locale]=[];}
 		djBundlesByLocale[locale].push(entry);
 	}
@@ -39,14 +55,24 @@ for (var i = 0; i < djLoadedBundles.length; i++) {
 
 localeList = [];
 
-for (locale in djBundlesByLocale) {
-	for (var j = 0; j < djLoadedBundles.length; j++) {
-		entry = djLoadedBundles[j];
-		var pkg = [entry.modulename,"_nls",entry.bundlename,locale].join(".");
-		print("dojo.hostenv.startPackage(\""+pkg+"\");");
-//		print("dojo.hostenv.loaded_modules_[bundlepackage] = bundle;");
-		bundle = dojo.hostenv.findModule(entry.modulename)._nls[entry.bundlename];
-		print(pkg+"="+dojo.json.serialize(bundle[locale]));
+var mkdir = false;
+var dir = new java.io.File(destDir);
+for (locale in djBundlesByLocale){
+	if(!mkdir){ dir.mkdir(); mkdir = true; }
+	var outFile = new java.io.File(dir, prefix + "_" + locale + ".js");
+	var os = new java.io.BufferedWriter(
+			new java.io.OutputStreamWriter(new java.io.FileOutputStream(outFile), "utf-8"));
+	try{
+		for (var j = 0; j < djLoadedBundles.length; j++){
+			entry = djLoadedBundles[j];
+			var pkg = [entry.modulename,"_nls",entry.bundlename,locale].join(".");
+			os.write("dojo.hostenv.startPackage(\""+pkg+"\");");
+//			os.write("dojo.hostenv.loaded_modules_[bundlepackage] = bundle;");
+			bundle = dojo.hostenv.findModule(entry.modulename)._nls[entry.bundlename];
+			os.write(pkg+"="+dojo.json.serialize(bundle[locale]));
+		}
+	}finally{
+		os.close();
 	}
 	localeList.push(locale);
 }
