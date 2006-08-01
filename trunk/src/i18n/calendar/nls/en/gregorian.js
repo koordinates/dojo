de5504f9fadb90bc700d6f8d3771cd6878f97350
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-01 03:26:10 +0000

    i18n data: month/day names
    git-svn-id: http://svn.dojotoolkit.org/src@5016 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/src/i18n/calendar/nls/en/gregorian.js
@@ -0,0 +1,8 @@
+({
+	'months-format-wide': ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+	'months-format-abbr': ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+	'months-standAlone-narrow': ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+	'days-format-wide': ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+	'days-format-abbr': ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+	'days-standAlone-narrow': ["S", "M", "T", "W", "T", "F", "S"]
+})
