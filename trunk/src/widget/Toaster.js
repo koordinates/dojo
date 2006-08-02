820a2b6480de275be6da07b53d2a7708010feac3
BryanForbes <BryanForbes@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-02 16:39:11 +0000

    Added more positions and animation directions to Toaster.
    git-svn-id: http://svn.dojotoolkit.org/src@5046 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/Toaster.js
+++ trunk/src/widget/Toaster.js
@@ -10,11 +10,13 @@ dojo.widget.defineWidget(
 	"dojo.widget.Toaster",
 	dojo.widget.HtmlWidget,
 	{
-		templateString: '<div dojoAttachPoint="domNode" dojoAttachEvent="onClick:onSelect"><div dojoAttachPoint="messageNode"></div></div>',
+		templateString: '<div dojoAttachPoint="clipNode"><div dojoAttachPoint="containerNode" dojoAttachEvent="onClick:onSelect"><div dojoAttachPoint="contentNode"></div></div></div>',
 		templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlToaster.css"),
 		
+		clipNode: null,
+
 		messageTopic: "",
-		messageNode: null,
+		contentNode: null,
 		
 		// possible message types
 		messageTypes: {
@@ -25,15 +27,16 @@ dojo.widget.defineWidget(
 		},
 
 		// css classes
-		nodeCssClass: "toasterBlock",
-		contentCssClass: "toasterContent",
-		messageCssClass: "toasterMessage",
-		warningCssClass: "toasterWarning",
-		errorCssClass: "toasterError",
-		fatalCssClass: "toasterFatal",
-		overflowCssClass: "toasterBodyOverflow",
-
-		position: "bottom", // can be "bottom", "top", "left", or "right"
+		clipCssClass: "dojoToasterClip",
+		containerCssClass: "dojoToasterContainer",
+		contentCssClass: "dojoToasterContent",
+		messageCssClass: "dojoToasterMessage",
+		warningCssClass: "dojoToasterWarning",
+		errorCssClass: "dojoToasterError",
+		fatalCssClass: "dojoToasterFatal",
+		
+		positionDirection: "br-up",
+		positionDirectionTypes: ["br-up", "br-left", "bl-up", "bl-right", "tr-down", "tr-left", "tl-down", "tl-right"],
 		showDelay: 2000,
 
 		slideAnim: null,
@@ -41,11 +44,14 @@ dojo.widget.defineWidget(
 
 		postCreate: function(){
 			this.hide();
-			dojo.html.addClass(this.domNode, this.nodeCssClass);
-			this.domNode.style.position = "absolute";
-			dojo.html.setClass(this.messageNode, this.contentCssClass);
+			dojo.html.setClass(this.clipNode, this.clipCssClass);
+			dojo.html.addClass(this.containerNode, this.containerCssClass);
+			dojo.html.setClass(this.contentNode, this.contentCssClass);
 			if(this.messageTopic){
 				dojo.event.topic.subscribe(this.messageTopic, this, "setContent");
+			}
+			if(!this.positionDirection || !dojo.lang.inArray(this.positionDirectionTypes, this.positionDirection)){
+				this.positionDirection = this.positionDirectionTypes.BRU;
 			}
 		},
 
@@ -61,37 +67,42 @@ dojo.widget.defineWidget(
 				if(this.fadeAnim) this.fadeAnim.stop();
 			}
 			if(!msg){
-				dojo.debug("Toaster.setContent() incoming content was null, ignoring.");
+				dojo.debug(this.widgetId + ".setContent() incoming content was null, ignoring.");
 				return;
+			}
+			if(!this.positionDirection || !dojo.lang.inArray(this.positionDirectionTypes, this.positionDirection)){
+				dojo.raise(this.widgetId + ".positionDirection is an invalid value: " + this.positionDirection);
 			}
 
 			// determine type of content and apply appropriately
-			dojo.html.removeClass(this.domNode, this.messageCssClass);
-			dojo.html.removeClass(this.domNode, this.warningCssClass);
-			dojo.html.removeClass(this.domNode, this.errorCssClass);
-			dojo.html.removeClass(this.domNode, this.fatalCssClass);
+			dojo.html.removeClass(this.containerNode, this.messageCssClass);
+			dojo.html.removeClass(this.containerNode, this.warningCssClass);
+			dojo.html.removeClass(this.containerNode, this.errorCssClass);
+			dojo.html.removeClass(this.containerNode, this.fatalCssClass);
+
+			dojo.html.clearOpacity(this.containerNode);
 			
 			if(msg instanceof String || typeof msg == "string"){
-				this.messageNode.innerHTML = msg;
+				this.contentNode.innerHTML = msg;
 			}else if(dojo.html.isNode(msg)){
-				this.messageNode.innerHTML = dojo.html.getContentAsString(msg);
+				this.contentNode.innerHTML = dojo.html.getContentAsString(msg);
 			}else{
 				dojo.raise("Toaster.setContent(): msg is of unknown type:" + msg);
 			}
 
 			switch(messageType){
 				case this.messageTypes.WARNING:
-					dojo.html.addClass(this.domNode, this.warningCssClass);
+					dojo.html.addClass(this.containerNode, this.warningCssClass);
 					break;
 				case this.messageTypes.ERROR:
-					dojo.html.addClass(this.domNode, this.errorCssClass);
+					dojo.html.addClass(this.containerNode, this.errorCssClass);
 					break
 				case this.messageTypes.FATAL:
-					dojo.html.addClass(this.domNode, this.fatalCssClass);
+					dojo.html.addClass(this.containerNode, this.fatalCssClass);
 					break;
 				case this.messageTypes.MESSAGE:
 				default:
-					dojo.html.addClass(this.domNode, this.messageCssClass);
+					dojo.html.addClass(this.containerNode, this.messageCssClass);
 					break;
 			}
 
@@ -102,41 +113,64 @@ dojo.widget.defineWidget(
 
 			this.show();
 
-			var nodeSize = dojo.html.getMarginBox(this.domNode);
-
-			// sets node below bottom right hand corner so it can animate "up"
-			// TODO: set up different position and slide-out directions
-			this.domNode.style.left=(view.width - nodeSize.width - 1 - scroll.left)+"px";
-			this.domNode.style.top=(view.height + nodeSize.height + 10 + scroll.top)+"px";
+			var nodeSize = dojo.html.getMarginBox(this.containerNode);
+			var endCoords = { top: 0, left: 0 };
 
 			// TODO: Add scroll change event lisener similar to Dialog.js widget so
 			// that notify window stays with bottom of screen when it is moved
 
-			// disable scrolling so node being below screen doesn't create scrollbars
-			if(!(dojo.body().offsetHeight > view.height) // only if not already scrolling
-			   && !dojo.html.hasClass(dojo.body(), this.overflowCssClass)){
-				dojo.html.addClass(dojo.body(), this.overflowCssClass);
+			// sets up the size of the clipping node
+			this.clipNode.style.height = nodeSize.height+"px";
+			this.clipNode.style.width = nodeSize.width+"px";
+
+			// sets up the position of the clipping node
+			if(this.positionDirection.match(/^t/)){
+				this.clipNode.style.top = scroll.top+"px";
+			}else if(this.positionDirection.match(/^b/)){
+				this.clipNode.style.top = (view.height - nodeSize.height - 2 + scroll.top)+"px";
 			}
-			
-			var endCoords = {
-				top: view.height - nodeSize.height - 2 + scroll.top,
-				left: view.width - nodeSize.width - 1 - scroll.left
-			};
+			if(this.positionDirection.match(/^[tb]r-/)){
+				this.clipNode.style.left = (view.width - nodeSize.width - 1 - scroll.left)+"px";
+			}else if(this.positionDirection.match(/^[tb]l-/)){
+				this.clipNode.style.left = 0 + "px";
+			}
+
+			this.clipNode.style.clip = "rect(0px, " + nodeSize.width + "px, " + nodeSize.height + "px, 0px)";
+
+			// sets up initial position of container node and slide-out direction
+			if(this.positionDirection.indexOf("-up") >= 0){
+				this.containerNode.style.left=0+"px";
+				this.containerNode.style.top=nodeSize.height + 10 + "px";
+			}else if(this.positionDirection.indexOf("-left") >= 0){
+				this.containerNode.style.left=nodeSize.width + 10 +"px";
+				this.containerNode.style.top=0+"px";
+			}else if(this.positionDirection.indexOf("-right") >= 0){
+				this.containerNode.style.left = 0 - nodeSize.width - 10 + "px";
+				this.containerNode.style.top = 0+"px";
+			}else if(this.positionDirection.indexOf("-down") >= 0){
+				this.containerNode.style.left = 0+"px";
+				this.containerNode.style.top = 0 - nodeSize.height - 10 + "px";
+			}else{
+				dojo.raise(this.widgetId + ".positionDirection is an invalid value: " + this.positionDirection);
+			}
 
 			this.slideAnim = dojo.lfx.html.slideTo(
-				this.domNode,
-				endCoords,
+				this.containerNode,
+				{ top: 0, left: 0 },
 				450,
 				null,
 				dojo.lang.hitch(this, function(nodes, anim){
 					dojo.html.removeClass(dojo.body(), this.overflowCssClass);
 					dojo.lang.setTimeout(dojo.lang.hitch(this, function(evt){
-						this.fadeAnim = dojo.lfx.html.fadeHide(
-							this.domNode,
+						// can't do a fadeHide because we're fading the
+						// inner node rather than the clipping node
+						this.fadeAnim = dojo.lfx.html.fadeOut(
+							this.containerNode,
 							1000,
 							null,
 							dojo.lang.hitch(this, function(evt){
-								dojo.html.setOpacity(this.domNode, 1.0);
+								this.hide();
+								dojo.html.setOpacity(this.containerNode, 1.0);
 							})).play();
 					}), this.showDelay);
 				})).play();
