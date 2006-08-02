5f46bd2250e3c2ae4deec04170e07bc2820c7fd7
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-02 19:00:59 +0000

    Finnish translations from joose (CLA signed)  Other translations are available at unicode.org/cldr and JSON should be generated someday
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5052 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/i18n/calendar/nls/fi/gregorian.js
@@ -0,0 +1,8 @@
+({
+	'months-format-wide': ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"],
+	'months-format-abbr': ["tammi", "helmi", "maalis", "huhti", "touko", "kesä", "heinä", "elo", "syys", "loka", "marras", "joulu"],
+	'months-standAlone-narrow': ["T", "H", "M", "H", "T", "K", "H", "E", "S", "L", "M", "J"],
+	'days-format-wide': ["sunnuntai", "maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai"],
+	'days-format-abbr': ["su", "ma", "ti", "ke", "to", "pe", "la"],
+	'days-standAlone-narrow': ["S", "M", "T", "K", "T", "P", "L"]
+})
