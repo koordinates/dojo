79dee26746e09d0fdf4867867c4f39bc0da6e58c
liucougar <liucougar@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-05 16:56:13 +0000

    Add support in PopupContainer to be used as a mixin Mixin PopupContainer into Tooltip
    git-svn-id: http://svn.dojotoolkit.org/src@5124 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/Tooltip.js
+++ trunk/src/widget/Tooltip.js
@@ -1,6 +1,6 @@
 dojo.provide("dojo.widget.Tooltip");
 dojo.require("dojo.widget.ContentPane");
-dojo.require("dojo.widget.Menu2"); //for dojo.widget.PopupContainer
+dojo.require("dojo.widget.Menu2");
 dojo.require("dojo.uri");
 dojo.require("dojo.widget.*");
 dojo.require("dojo.event");
@@ -10,7 +10,7 @@ dojo.require("dojo.html.iframe");
 
 dojo.widget.defineWidget(
 	"dojo.widget.Tooltip",
-	dojo.widget.ContentPane,
+	[dojo.widget.ContentPane, dojo.widget.PopupContainer],
 	{
 		isContainer: true,
 
@@ -23,57 +23,43 @@ dojo.widget.defineWidget(
 		templateCssPath: dojo.uri.dojoUri("src/widget/templates/HtmlTooltipTemplate.css"),
 
 		connectNode: null,
-		popupWidget: null,
-	
-		fillInTemplate: function(args, frag){
-			//the line below is required to prevent opera 9 from crashing
-			this.domNode.style.display="none";
 
+		fillInTemplate: function(args, frag){
 			if(this.caption != ""){
 				this.domNode.appendChild(document.createTextNode(this.caption));
 			}
-			this.connectNode = dojo.byId(this.connectId);		
+			this.connectNode = dojo.byId(this.connectId);
 			dojo.widget.Tooltip.superclass.fillInTemplate.call(this, args, frag);
 
-			//wrap this.domNode in a PopupContainer
-			//and pass animation setting to it
-			this.popupWidget = dojo.widget.createWidget("PopupContainer", {toggle: this.toggle, toggleDuration: this.toggleDuration});
-			this.popupWidget.addChild(this);
-			dojo.html.addClass(this.popupWidget.domNode, "dojoTooltip");
+			this.addOnLoad(this, "_LoadedContent");
+			dojo.html.addClass(this.domNode, "dojoTooltip");
 
 			//copy style from input node to output node
 			var source = this.getFragNodeRef(frag);
-			dojo.html.copyStyle(this.popupWidget.domNode, source);
+			dojo.html.copyStyle(this.domNode, source);
 
-			//reset animation on this widget
-			this.toggle="plain";
-			this.toggleDuration=150;
-			//re-create animation object
-			this.postMixInProperties();
-
-			this.popupWidget.domNode.style.display="none";
-
-			dojo.body().appendChild(this.popupWidget.domNode);
+			//apply the necessary css rules to the node so that it can popup
+			this.applyPopupBasicStyle();
 		},
-		
+
 		postCreate: function(args, frag){
 			dojo.event.connect(this.connectNode, "onmouseover", this, "onMouseOver");
 			dojo.widget.Tooltip.superclass.postCreate.call(this, args, frag);
 		},
-		
+
 		onMouseOver: function(e) {
 			this.mouse = {x: e.pageX, y: e.pageY};
-	
+
 			if(!this.showTimer){
-				this.showTimer = setTimeout(dojo.lang.hitch(this, "show"), this.showDelay);
+				this.showTimer = setTimeout(dojo.lang.hitch(this, "open"), this.showDelay);
 				dojo.event.connect(document.documentElement, "onmousemove", this, "onMouseMove");
 			}
 		},
-	
+
 		onMouseMove: function(e) {
 			this.mouse = {x: e.pageX, y: e.pageY};
 
-			if(dojo.html.overElement(this.connectNode, e) || dojo.html.overElement(this.popupWidget.domNode, e)){
+			if(dojo.html.overElement(this.connectNode, e) || dojo.html.overElement(this.domNode, e)){
 				// If the tooltip has been scheduled to be erased, cancel that timer
 				// since we are hovering over element/tooltip again
 				if(this.hideTimer) {
@@ -88,22 +74,29 @@ dojo.widget.defineWidget(
 					clearTimeout(this.showTimer);
 					delete this.showTimer;
 				}
-				if(this.popupWidget.isShowingNow && !this.hideTimer){
-					this.hideTimer = setTimeout(dojo.lang.hitch(this, "hide"), this.hideDelay);
+				if(this.isShowingNow && !this.hideTimer){
+					this.hideTimer = setTimeout(dojo.lang.hitch(this, "close"), this.hideDelay);
 				}
 			}
 		},
-	
-		show: function() {
-			if (this.popupWidget.isShowingNow) { return; }
-	
-			dojo.widget.Tooltip.superclass.show.call(this);
-			
-			this.popupWidget.open(this.mouse.x, this.mouse.y, this, [this.mouse.x, this.mouse.y], "TL,TR,BL,BR", [10,15]);
+
+		open: function() {
+			if (this.isShowingNow) { return; }
+
+			dojo.widget.PopupContainer.prototype.open.call(this, this.mouse.x, this.mouse.y, null, [this.mouse.x, this.mouse.y], "TL,TR,BL,BR", [10,15]);
 		},
-	
-		hide: function() {
-			if (this.popupWidget.isShowingNow) {
+
+		//call dojo.widget.ContentPane.show/hide here, otherwise
+		//dojo.widget.PopupContainer.show/hide is used
+		show: function(){
+			dojo.widget.Tooltip.superclass.show.call(this);
+		},
+		hide: function(){
+			dojo.widget.Tooltip.superclass.hide.call(this);
+		},
+
+		close: function() {
+			if (this.isShowingNow) {
 				if ( this.showTimer ) {
 					clearTimeout(this.showTimer);
 					delete this.showTimer;
@@ -113,26 +106,21 @@ dojo.widget.defineWidget(
 					delete this.hideTimer;
 				}
 				dojo.event.disconnect(document.documentElement, "onmousemove", this, "onMouseMove");
-				this.popupWidget.close();
+				dojo.widget.PopupContainer.prototype.close.call(this);
 			}
-		},
-	
-		onHide: function(){
-			dojo.widget.Tooltip.superclass.hide.call(this);
 		},
 
 		position: function(){
-			this.popupWidget.move(this.mouse.x, this.mouse.y, [10,15], "TL,TR,BL,BR");
+			this.move(this.mouse.x, this.mouse.y, [10,15], "TL,TR,BL,BR");
 		},
 
-		onLoad: function(){
-			if(this.popupWidget.isShowingNow){
+		_LoadedContent: function(){
+			if(this.isShowingNow){
 				// the tooltip has changed size due to downloaded contents, so reposition it
 				this.position();
-				dojo.widget.Tooltip.superclass.onLoad.apply(this, arguments);
 			}
 		},
-	
+
 		checkSize: function() {
 			// checkSize() is called when the user has resized the browser window,
 			// but that doesn't affect this widget (or this widget's children)
