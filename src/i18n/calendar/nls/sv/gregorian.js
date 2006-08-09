74885570160c938e6b173fd1f7ff77917c4ff772
mumme <mumme@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-09 21:59:56 +0000

    Added swedish locales to calendar
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5212 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/i18n/calendar/nls/sv/gregorian.js
@@ -0,0 +1,8 @@
+({
+	'months-format-wide': ["januari", "februari", "mars", "april", "maj", "juni", "juli", "augusti", "september", "oktober", "november", "december"],
+	'months-format-abbr': ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+	'months-standAlone-narrow': ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+	'days-format-wide': ["söndag", "måndag", "tisdag", "onsdag", "torsdag", "fredag", "lördag"],
+	'days-format-abbr': ["sön", "mån", "tis", "ons", "tor", "fre", "lör"],
+	'days-standAlone-narrow': ["S", "M", "T", "O", "T", "F", "L"]
+})
