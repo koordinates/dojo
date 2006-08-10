960adef612d342d5c49abab2cff80d48e33edf15
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-10 02:53:48 +0000

    more calendar localizations + README
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5217 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/i18n/calendar/nls/de/gregorian.js
@@ -0,0 +1,8 @@
+({
+	'months-format-wide': ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+	'months-format-abbr': ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+	'months-standAlone-narrow': ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+	'days-format-wide': ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+	'days-format-abbr': ["So", "Mo", "Di", "Mi", "Do", "Fri", "Sat"],
+	'days-standAlone-narrow': ["S", "M", "D", "M", "D", "F", "S"]
+})
---
+++ src/i18n/calendar/nls/es/gregorian.js
@@ -0,0 +1,8 @@
+({
+	'months-format-wide': ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+	'months-format-abbr': ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
+	'months-standAlone-narrow': ["E", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
+	'days-format-wide': ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
+	'days-format-abbr': ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+	'days-standAlone-narrow': ["D", "L", "M", "M", "J", "V", "S"]
+})
