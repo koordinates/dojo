583d4f62de353cb1cdbb1d442c47cc3646df2788
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-11 05:31:21 +0000

    start work on i18n date formatting
    git-svn-id: http://svn.dojotoolkit.org/src@5235 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/i18n/calendar/nls/de/gregorian.js
+++ trunk/src/i18n/calendar/nls/de/gregorian.js
@@ -4,5 +4,14 @@
 	'months-standAlone-narrow': ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"],
 	'days-format-wide': ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
 	'days-format-abbr': ["So", "Mo", "Di", "Mi", "Do", "Fri", "Sat"],
-	'days-standAlone-narrow': ["S", "M", "D", "M", "D", "F", "S"]
+	'days-standAlone-narrow': ["S", "M", "D", "M", "D", "F", "S"],
+
+	'dateFormat-full': "EEEE, d. MMMM yyyy",
+	'dateFormat-long': "d. MMMM yyyy",
+	'dateFormat-medium': "dd.MM.yyyy",
+	'dateFormat-short': "dd.MM.yy",
+
+	'timeFormat-full': "H:mm' Uhr 'z",
+
+	eras: ['v. Chr.','n. Chr.']
 })
