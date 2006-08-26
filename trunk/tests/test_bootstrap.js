5070a8d9e1bf9d848f991f80c51bd6dc8bfccdc9
jkuhnert <jkuhnert@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-26 21:12:11 +0000

    Applying changes to tests that aren't needed anymore. Fixes #1351
    git-svn-id: http://svn.dojotoolkit.org/src@5394 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/tests/test_bootstrap.js
+++ trunk/tests/test_bootstrap.js
@@ -1,5 +1,4 @@
 
 function test_dojo_doc () {
-    // when fixed, remove the workaround in test_dom.js#test_dom_getUniqueId
 	jum.assertTrue("document is undefined in test environment", typeof(dojo.doc()) != "undefined");
 }
