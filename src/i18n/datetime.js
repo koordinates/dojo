09b23628b676164aa547540e03d1368600cb5643
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-29 20:41:21 +0000

    complete getFirstDayOfWeek
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5418 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/i18n/datetime.js
+++ src/i18n/datetime.js
@@ -523,16 +523,6 @@ dojo.i18n.datetime.strftime = function (/*Date*/dateObject, /*String*/format) {
 	return string;
 };
 
-dojo.i18n.datetime.getFirstDayOfWeek = function(/*String?*/locale){
-// summary:
-//		Returns a zero-based index for first day of the week, as used by the local (Gregorian) calendar.
-//		e.g. Sunday (returns 0), or Monday (returns 1)
-	locale = dojo.normalizeLocale(locale);
-	// FIXME: put this in resources, or see if it can be pulled from the CLDR, perhaps?
-	var variants = locale.split("-");
-	return (variants[1] == "no" || variants[1] == "fi" || variants[1] == "se") ? 1 : 0; // Just a few for testing, for now
-};
-
 dojo.i18n.datetime.getNames = function(/*String*/item, /*String*/type, /*String?*/use, /*String?*/locale){
 //
 // summary:
@@ -597,4 +587,28 @@ dojo.i18n.datetime.getMonthName = function(/*Date*/dateObject, /*String?*/locale
 
 dojo.i18n.datetime.getMonthShortName = function(/*Date*/dateObject, /*String?*/locale){
 	return dojo.i18n.datetime.getNames('months', 'abbr', 'format', locale)[dateObject.getMonth()]; /*String*/
+};
+
+// Supplemental
+
+dojo.i18n.datetime.getFirstDayOfWeek = function(/*String?*/locale){
+// summary:
+//		Returns a zero-based index for first day of the week, as used by the local (Gregorian) calendar.
+//		e.g. Sunday (returns 0), or Monday (returns 1)
+
+	// from http://www.unicode.org/cldr/data/common/supplemental/supplementalData.xml:supplementalData/weekData/firstDay
+	var firstDay = {/*default is 1*/
+		mv:5,
+		ae:6,af:6,bh:6,dj:6,dz:6,eg:6,er:6,et:6,iq:6,ir:6,jo:6,ke:6,kw:6,lb:6,ly:6,ma:6,om:6,qa:6,sa:6,
+		sd:6,so:6,tn:6,ye:6,
+		as:0,au:0,az:0,bw:0,ca:0,cn:0,fo:0,ge:0,gl:0,gu:0,hk:0,ie:0,il:0,is:0,jm:0,jp:0,kg:0,kr:0,la:0,
+		mh:0,mo:0,mp:0,mt:0,nz:0,ph:0,pk:0,sg:0,th:0,tt:0,tw:0,um:0,us:0,uz:0,vi:0,za:0,zw:0,
+		et:0,mw:0,ng:0,tj:0,
+		gb:0,
+		sy:4
+	};
+
+	locale = dojo.normalizeLocale(locale);
+	var variants = locale.split("-");
+	return firstDay[variants[1]]|| 1;
 };
