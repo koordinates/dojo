1671fbb6db5ecd0f6a7ff5e62b4b20a55ca21032
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-08-31 20:03:06 +0000

    Fix on Timer.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5461 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/animation/Timer.js
+++ src/animation/Timer.js
@@ -1,8 +1,6 @@
 dojo.provide("dojo.animation.Timer");
-
-dojo.deprecated("dojo.animation.Timer is now dojo.lang.timing.Timer", "0.5");
 dojo.require("dojo.lang.timing.Timer");
 
-dojo.animation.Timer = function(/* int */interval){
-	dojo.lang.timing.Timer.call(this, interval);
-};
+dojo.deprecated("dojo.animation.Timer is now dojo.lang.timing.Timer", "0.5");
+
+dojo.animation.Timer = dojo.lang.timing.Timer;
