9dccbcd04566b09b914f302d922fde4d4297388f
elazutkin <elazutkin@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-02 19:16:45 +0000

    major refactoring of SVG and VML renderers, API is updated
    git-svn-id: http://svn.dojotoolkit.org/src@5482 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/gfx/__package__.js
+++ trunk/src/gfx/__package__.js
@@ -1,22 +1,13 @@
-dojo.require("dojo.gfx.m2d");
+dojo.kwCompoundRequire({
+	common: [
+		"dojo.gfx.color",
+		"dojo.gfx.m2d",
+		"dojo.gfx.common"
+	]
+});
+
+// include a renderer conditionally
+dojo["requireIf"](dojo.render.svg.capable, "dojo.gfx.svg");
+dojo["requireIf"](!dojo.render.svg.capable && dojo.render.vml.capable, "dojo.gfx.vml");
+
 dojo.provide("dojo.gfx.*");
-
-dojo.requireIf(dojo.render.svg.capable, "dojo.gfx.svg");
-dojo.requireIf(!dojo.render.svg.capable && dojo.render.vml.capable, "dojo.gfx.vml");
-
-dojo.gfx.defaultRenderer.init();
-dojo.gfx.normalizeParameters = function (existed, update) {
-    if(update) {
-        for(x in existed) {
-            if(x in update) {
-                existed[x] = update[x];
-            }
-        }
-    }
-    return existed;
-}
-
-dojo.gfx.GUID = 1;
-dojo.gfx.guid = function(){
-    return dojo.gfx.GUID++;
-};
