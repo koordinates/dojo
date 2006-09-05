9ec10c03556bf4d99d0f59d7c28510853fe6ba3c
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-05 21:00:01 +0000

    fixes #1401, strftime fixes and enhancements, patch from Matthew Eernisse (CLA on file)
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5527 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/date/test_format.js
+++ tests/date/test_format.js
@@ -29,10 +29,131 @@ function test_date_format() {
 
 function test_date_strftime() {
 	var date = new Date(2006, 7, 11, 0, 55, 12, 3456);
-	jum.assertEquals("strftime_test1", "06/08/11", dojo.date.strftime(date, "%y/%m/%d"));
-	jum.assertEquals("strftime_test2", "06/08/11", dojo.date.strftime(date, "%y/%m/%d"));
-	jum.assertEquals("strftime_test1depr", "06/08/11", dojo.date.format(date, "%y/%m/%d"));
-	jum.assertEquals("strftime_test2depr", "06/08/11", dojo.date.format(date, "%y/%m/%d"));
+	jum.assertEquals("strftime_test0", "06/08/11", dojo.date.strftime(date, "%y/%m/%d"));
+	jum.assertEquals("strftime_test0depr", "06/08/11", dojo.date.format(date, "%y/%m/%d"));
+
+	var dt = null; // Date to test
+	var fmt = ''; // Format to test
+	var res = ''; // Expected result
+	
+	dt = new Date(2006, 0, 1, 18, 23);
+	fmt = '%a';
+	res = 'Sun';
+	jum.assertEquals("strftime_test1", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%A';
+	res = 'Sunday';
+	jum.assertEquals("strftime_test2", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%b';
+	res = 'Jan';
+	jum.assertEquals("strftime_test3", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%B';
+	res = 'January';
+	jum.assertEquals("strftime_test4", res, dojo.date.strftime(dt, fmt, 'en'));
+
+//dojo.date.format can't print timezones yet	
+	//fmt = '%c';
+	//res = 'Sunday, January 1, 2006 6:23:00 PM';
+	//jum.assertEquals("strftime_test5", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%C';
+	res = '20';
+	jum.assertEquals("strftime_test6", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%d';
+	res = '01';
+	jum.assertEquals("strftime_test7", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%D';
+	res = '01/01/06';
+	jum.assertEquals("strftime_test8", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%e';
+	res = ' 1';
+	jum.assertEquals("strftime_test9", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%h';
+	res = 'Jan';
+	jum.assertEquals("strftime_test10", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%H';
+	res = '18';
+	jum.assertEquals("strftime_test11", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%I';
+	res = '06';
+	jum.assertEquals("strftime_test12", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%j';
+	res = '001';
+	jum.assertEquals("strftime_test13", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%k';
+	res = '18';
+	jum.assertEquals("strftime_test14", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%l';
+	res = ' 6';
+	jum.assertEquals("strftime_test15", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%m';
+	res = '01';
+	jum.assertEquals("strftime_test16", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%M';
+	res = '23';
+	jum.assertEquals("strftime_test17", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%p';
+	res = 'PM';
+	jum.assertEquals("strftime_test18", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%r';
+	res = '06:23:00 PM';
+	jum.assertEquals("strftime_test19", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%R';
+	res = '18:23';
+	jum.assertEquals("strftime_test20", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%S';
+	res = '00';
+	jum.assertEquals("strftime_test21", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%T';
+	res = '18:23:00';
+	jum.assertEquals("strftime_test22", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%u';
+	res = '7';
+	jum.assertEquals("strftime_test23", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%w';
+	res = '0';
+	jum.assertEquals("strftime_test24", res, dojo.date.strftime(dt, fmt));
+
+	fmt = '%x';
+	res = 'Sunday, January 1, 2006';
+	jum.assertEquals("strftime_test25", res, dojo.date.strftime(dt, fmt, 'en'));
+
+// dojo.date.format can't print timezones yet
+//	fmt = '%X';
+//	res = '6:23:00 PM';
+//	jum.assertEquals("strftime_test26", res, dojo.date.strftime(dt, fmt, 'en'));
+	
+	fmt = '%y';
+	res = '06';
+	jum.assertEquals("strftime_test27", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%Y';
+	res = '2006';
+	jum.assertEquals("strftime_test28", res, dojo.date.strftime(dt, fmt));
+	
+	fmt = '%%';
+	res = '%';
+	jum.assertEquals("strftime_test29", res, dojo.date.strftime(dt, fmt));
 }
 
 function test_date_parse() {
