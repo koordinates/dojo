7ff6cc82b5087997fdbd416744c99ccef81c4853
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-11 04:58:22 +0000

    whoops. put back failing unit tests
    git-svn-id: http://svn.dojotoolkit.org/src@5613 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/validate/jp.js
+++ trunk/src/validate/jp.js
@@ -10,7 +10,7 @@ dojo.require("dojo.validate.common");
 dojo.validate.isJapaneseCurrency = function(value) {
 	var flags = {
 		symbol: "\u00a5",
-		cents: false
+		fractional: false
 	};
 	return dojo.validate.isCurrency(value, flags);
 }
