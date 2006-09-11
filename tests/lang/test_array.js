cc4adee0aa28c1d3a55567d32fce512f25092666
skinner <skinner@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-11 06:07:52 +0000

    Cleaned up unit tests for dojo.lang.find()
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5620 560b804f-0ae3-0310-86f3-f6aa0a117693

--- tests/lang/test_array.js
+++ tests/lang/test_array.js
@@ -4,12 +4,12 @@ function test_lang_find() {
 	var foo = new Array(128, 256, 512);
 	var bar = new Array("aaa", "bbb", "ccc");
 	
-	jum.assertTrue("500", dojo.lang.find(56, [45, 56, 85]) == 1);
+	jum.assertTrue("500", dojo.lang.find([45, 56, 85], 56) == 1);
 	jum.assertTrue("501", dojo.lang.find([Number, String, Date], String) == 1);
-	jum.assertTrue("502", dojo.lang.find(String, [Number, String, Date]) == 1); // bug #348 -- http://trac.dojotoolkit.org/ticket/348
-	jum.assertTrue("503", dojo.lang.find(foo[2], foo) == 2);
+	// jum.assertTrue("502", dojo.lang.find(String, [Number, String, Date]) == 1); // bug #348 -- http://trac.dojotoolkit.org/ticket/348
+	jum.assertTrue("503", dojo.lang.find(foo, foo[1]) == 1);
 	jum.assertTrue("504", dojo.lang.find(foo, foo[2]) == 2);
-	jum.assertTrue("505", dojo.lang.find(bar[2], bar) == 2);
+	jum.assertTrue("505", dojo.lang.find(bar, bar[1]) == 1);
 	jum.assertTrue("506", dojo.lang.find(bar, bar[2]) == 2);
 	
 	foo.push(bar);
