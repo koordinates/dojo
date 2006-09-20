4049ac860bd7b71964a08f2b685d54cab7d50c89
liucougar <liucougar@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-20 22:23:37 +0000

    added missing dojo.require in contextmenu plugin for editor2
    git-svn-id: http://svn.dojotoolkit.org/src@5736 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/Editor2Plugin/ContextMenu.js
+++ trunk/src/widget/Editor2Plugin/ContextMenu.js
@@ -1,5 +1,7 @@
 dojo.provide("dojo.widget.Editor2Plugin.ContextMenu");
 
+dojo.require("dojo.widget.Menu2");
+
 dojo.event.topic.subscribe("dojo.widget.Editor2::onLoad", function(editor){
 	var p = new dojo.widget.Editor2Plugin.ContextMenu(editor);
 });
