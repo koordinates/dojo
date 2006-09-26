300ae5352e9b46d15c66f9153369d066bcc38a55
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-26 12:29:12 +0000

    first pass at rewriting Accordion to have proper sliding effect, and to be able to contain arbitrary children (like LayoutContainer)
    git-svn-id: http://svn.dojotoolkit.org/src@5834 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/AccordionPane.js
+++ trunk/src/widget/AccordionPane.js
@@ -1,84 +1,55 @@
 dojo.provide("dojo.widget.AccordionPane");
 
 dojo.require("dojo.widget.*");
-dojo.require("dojo.widget.TitlePane");
+dojo.require("dojo.html.*");
 dojo.require("dojo.html.selection");
+dojo.require("dojo.widget.html.layout");
 
+/*
+ * AccordionPane is a box with a title that contains another widget (often a ContentPane).
+ * It works in conjunction w/an AccordionContainer.
+ */
 dojo.widget.defineWidget(
 	"dojo.widget.AccordionPane",
-	dojo.widget.TitlePane,
+	dojo.widget.HtmlWidget,
 {
 	// parameters
 	open: false,
-	allowCollapse: true,
 	label: "",
 	labelNodeClass: "",
 	containerNodeClass: "",
 	
+	open: true,
+	templatePath: dojo.uri.dojoUri("src/widget/templates/AccordionPane.html"),
+
+	isContainer: true,
+
 	// methods
-    postCreate: function() {
-		dojo.widget.AccordionPane.superclass.postCreate.call(this);
-		this.domNode.widgetType=this.widgetType;
-		this.setSizes();
-		dojo.html.addClass(this.labelNode, this.labelNodeClass);
+    fillInTemplate: function() {
+		dojo.widget.AccordionPane.superclass.fillInTemplate.call(this);
 		dojo.html.disableSelection(this.labelNode);
-		dojo.html.addClass(this.containerNode, this.containerNodeClass);
-    },
-
-	collapse: function() {
-		//dojo.fx.html.wipeOut(this.containerNode,250);
-		//var anim = dojo.fx.html.wipe(this.containerNode, 1000, this.containerNode.offsetHeight, 0, null, true);
-		this.containerNode.style.display="none";
-		this.open=false;
 	},
 
-	expand: function() {
-		//dojo.fx.html.wipeIn(this.containerNode,250);
-		this.containerNode.style.display="block";
-		//var anim = dojo.fx.html.wipe(this.containerNode, 1000, 0, this.containerNode.scrollHeight, null, true);
-		this.open=true;
+	setLabel: function(label) {
+		this.labelNode.innerHTML=label;
 	},
-
-	getCollapsedHeight: function() {
-		return dojo.html.getMarginBox(this.labelNode).height+1;
-	},
-
-	setSizes: function() {
-		var siblings = this.domNode.parentNode.childNodes;
-		var height=dojo.html.getBorderBox(this.domNode.parentNode).height-this.getCollapsedHeight();
-
-		this.siblingWidgets = [];
 	
-		for (var x=0; x<siblings.length; x++) {
-			if (siblings[x].widgetType==this.widgetType) {
-				if (this.domNode != siblings[x]) {
-					var ap = dojo.widget.byNode(siblings[x]);
-					this.siblingWidgets.push(ap);
-					height -= ap.getCollapsedHeight();
-				}
-			}
-		}
-	
-		for (var x=0; x<this.siblingWidgets.length; x++) {
-			dojo.html.setMarginBox(this.siblingWidgets[x].containerNode,{ height: height });
-		}
+	resizeTo: function(width, height){
+		dojo.html.setMarginBox(this.domNode, {width: width, height: height});
+		var children = [
+			{domNode: this.labelNode, layoutAlign: "top"},
+			{domNode: this.containerNode, layoutAlign: "client"}
+		];
+		dojo.widget.html.layout(this.domNode, children);
+		var childSize = dojo.html.getContentBox(this.containerNode);
+		this.children[0].resizeTo(childSize.width, childSize.height);
+	},
 
-		dojo.html.setMarginBox(this.containerNode,{ height: height});
+	getLabelHeight: function() {
+		return dojo.html.getMarginBox(this.labelNode).height;
 	},
 
 	onLabelClick: function() {
-		this.setSizes();
-		if (!this.open) { 
-			for (var x=0; x<this.siblingWidgets.length;x++) {
-				if (this.siblingWidgets[x].open) {
-					this.siblingWidgets[x].collapse();
-				}
-			}
-			this.expand();
-		} else {
-			if (this.allowCollapse) {
-				this.collapse();
-			}
-		}
+		this.parent.selectPage(this);
 	}
 });
