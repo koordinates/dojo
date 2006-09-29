a1634cedf3aedc1a9cc431d9aba3f939753a6037
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-09-29 19:09:03 +0000

    combine date and internet "validate" widgets.  fix docs.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5915 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/validate/DateTextbox.js
+++ src/widget/validate/DateTextbox.js
@@ -3,31 +3,62 @@ dojo.provide("dojo.widget.validate.DateTextbox");
 dojo.require("dojo.widget.validate.ValidationTextbox");
 dojo.require("dojo.validate.datetime");
 
-/*
-  ****** DateTextbox ******
-
-  A subclass of ValidationTextbox.
-  Over-rides isValid to test if input is in a valid date format.
-
-  @attr format  Described in dojo.validate.js.  Default is  "MM/DD/YYYY".
-*/
 dojo.widget.defineWidget(
 	"dojo.widget.validate.DateTextbox",
 	dojo.widget.validate.ValidationTextbox,
 	{
-		mixInProperties: function(localProperties, frag) {
+	// summary: A TextBox which tests for a valid date
+	// format: Style as described in v0.3 in dojo.validate.  Default is  "MM/DD/YYYY".
+
+		mixInProperties: function(/*Object*/localProperties){
+			// summary: see dojo.widget.Widget
+
 			// First initialize properties in super-class.
 			dojo.widget.validate.DateTextbox.superclass.mixInProperties.apply(this, arguments);
 	
 			// Get properties from markup attributes, and assign to flags object.
-			if ( localProperties.format ) { 
+			if(localProperties.format){ 
 				this.flags.format = localProperties.format;
 			}
 		},
 
-		// Over-ride for date validation
-		isValid: function() { 
+		isValid: function(){ 
+			// summary: see dojo.widget.validate.ValidationTextbox
 			return dojo.validate.isValidDate(this.textbox.value, this.flags.format);
+		}
+	}
+);
+
+dojo.widget.defineWidget(
+	"dojo.widget.validate.TimeTextbox",
+	dojo.widget.validate.ValidationTextbox,
+	{
+	// summary: A TextBox which tests for a valid time
+	// format: Described in v0.3 in dojo.validate.  Default is  "h:mm:ss t".
+	// amSymbol: The symbol used for AM.  Default is "AM" or "am".
+	// pmSymbol: The symbol used for PM.  Default is "PM" or "pm".
+
+		mixInProperties: function(/*Object*/localProperties){
+			// summary: see dojo.widget.Widget
+
+			// First initialize properties in super-class.
+			dojo.widget.validate.TimeTextbox.superclass.mixInProperties.apply(this, arguments);
+	
+			// Get properties from markup attributes, and assign to flags object.
+			if(localProperties.format){ 
+				this.flags.format = localProperties.format;
+			}
+			if(localProperties.amsymbol){ 
+				this.flags.amSymbol = localProperties.amsymbol;
+			}
+			if(localProperties.pmsymbol){ 
+				this.flags.pmSymbol = localProperties.pmsymbol;
+			}
+		},
+
+		isValid: function(){ 
+			// summary: see dojo.widget.validate.ValidationTextbox
+			return dojo.validate.isValidTime(this.textbox.value, this.flags);
 		}
 	}
 );
