cee724849a1d37731e7b0ef93d52e0f1520de73e
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-10-03 23:42:40 +0000

    Very first rev of new charting engine.  Only SVG line chart is supported with this checkin, VML to follow.
    git-svn-id: http://svn.dojotoolkit.org/src@5972 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/src/widget/charting/engine/vml/Plotters.js
@@ -0,0 +1,12 @@
+dojo.provide("dojo.widget.charting.engine.vml.Plotters");
+dojo.require("dojo.lang.common");
+
+/*
+ *	Mixin the VML-specific plotter object.
+ */
+dojo.mixin(dojo.widget.engine.Plotters, {
+
+
+
+
+});
