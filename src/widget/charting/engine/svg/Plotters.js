ac5df43f93e7ffd1fba9d9996a616f025b18ac38
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-10-04 00:21:10 +0000

    SVG Area added.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@5973 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/charting/engine/svg/Plotters.js
+++ src/widget/charting/engine/svg/Plotters.js
@@ -61,5 +61,68 @@ dojo.require("dojo.svg");
 		path.setAttribute("d", cmd.join(" "));
 		return line;
 	};
+	p.Area = function(
+		/* array */data, 
+		/* dojo.widget.charting.engine.PlotArea */plotarea,
+		/* dojo.widget.charting.engine.Plot */plot,
+		/* function? */applyTo
+	){
+		var tension = 3;
+		var area = plotarea.getArea();
+		var line = document.createElementNS(dojo.svg.xmlns.svg, "g");
+		var path = document.createElementNS(dojo.svg.xmlns.svg, "path");
+		line.appendChild(path);
+
+		path.setAttribute("fill", data[0].series.color);
+		path.setAttribute("fill-opacity", "0.4");
+		path.setAttribute("stroke", data[0].series.color);
+		path.setAttribute("stroke-width" , "1");
+		path.setAttribute("stroke-opacity", "0.85");
+		if(data[0].series.label != null){
+			path.setAttribute("title", data[0].series.label);
+		}
+
+		var cmd=[];
+		for(var i=0; i<data.length; i++){
+			var x = plot.axisX.getCoord(data[i].x, plotarea, plot);
+			var y = plot.axisY.getCoord(data[i].y, plotarea, plot);
+			var dx = area.left + 1;
+			var dy = area.bottom;
+			if(i>0){
+				dx = x - plot.axisX.getCoord(data[i-1].x, plotarea, plot);
+				dy = plot.axisY.getCoord(data[i-1].y, plotarea, plot);
+			}
+
+			if(i==0){ cmd.push("M"); }
+			else {
+				cmd.push("C");
+				var cx = x-(tension-1) * (dx/tension);
+				cmd.push(cx + "," + dy);
+				cx = x - (dx/tension);
+				cmd.push(cx + "," + y);
+			}
+			cmd.push(x+","+y);
+			
+			//	points on the line
+			var c=document.createElementNS(dojo.svg.xmlns.svg, "circle");
+			c.setAttribute("cx",x);
+			c.setAttribute("cy",y);
+			c.setAttribute("r","3");
+			c.setAttribute("fill", data[i].series.color);
+			c.setAttribute("fill-opacity", "0.65");
+			c.setAttribute("stroke-width", "1");
+			c.setAttribute("stroke-opacity", "0.85");
+			line.appendChild(c);
+			if(applyTo){ applyTo(c, data[i].src); }
+		}
+		//	finish it off
+		cmd.push("L");
+		cmd.push(x + "," + plot.axisY.getCoord(0, plotarea, plot));
+		cmd.push("L");
+		cmd.push( plot.axisX.getCoord(0, plotarea, plot) + "," +  plot.axisY.getCoord(0, plotarea, plot));
+		cmd.push("Z");
+		path.setAttribute("d", cmd.join(" "));
+		return line;
+	};
 	p["Default"] = p.Line;
 })();
