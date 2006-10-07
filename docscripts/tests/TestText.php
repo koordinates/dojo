fb35d8b39fcf34446d61415fe64bc79b8a6b740d
pottedmeat <pottedmeat@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-10-07 17:24:43 +0000

    New tests/benchmarks and refactoring
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6040 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ docscripts/tests/TestText.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('../lib/simpletest/unit_tester.php');
+require_once('../lib/simpletest/reporter.php');
+require_once('../inc/Text.php');
+
+class TestText extends UnitTestCase
+{
+  function testTrim()
+  {
+    $this->assertEqual(implode('|', Text::toArray("A string")), 'A| |s|t|r|i|n|g');
+  }
+  
+  function testFindTermination()
+  {
+    $source_array = array(' null,');
+    list($line_number, $position) = Text::findTermination($source_array, ',');
+    $this->assertEqual($line_number . '/' . $position, '0/5');
+    
+    $source_array = array(' function(key, value){},');
+    list($line_number, $position) = Text::findTermination($source_array, ',', '()[]{}');
+    $this->assertEqual($line_number . '/' . $position, '0/23');
+    
+    $source_array = array(' "     ")');
+    list($line_number, $position) = Text::findTermination($source_array, ')', '()');
+    $this->assertEqual($line_number . '/' . $position, '0/8');
+  }
+}
+
+$test = &new TestText();
+$test->run(new HtmlReporter());
