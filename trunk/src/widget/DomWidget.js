d92a601f94b1682a5b586fe2ff566caa17d524a5
becky <becky@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-10-10 15:15:08 +0000

    added doc for accessibility apis
    git-svn-id: http://svn.dojotoolkit.org/src@6106 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/DomWidget.js
+++ trunk/src/widget/DomWidget.js
@@ -94,20 +94,37 @@ dojo.widget.attachProperties = ["dojoAttachPoint", "id"];
 dojo.widget.eventAttachProperty = "dojoAttachEvent";
 // String: property name of code to evaluate when the widget is constructed
 dojo.widget.onBuildProperty = "dojoOnBuild";
+// Array:  possible accessibility values to set on widget elements - role or state
 dojo.widget.waiNames  = ["waiRole", "waiState"];
-// FIXME: TODOC
+// Object: Contains functions to set accessibility roles and states 
+// 	onto widget elements
 dojo.widget.wai = {
-	waiRole: { 	name: "waiRole", 
+	waiRole: { 	
+				// String: information for mapping accessibility role
+				name: "waiRole", 
+				// String: URI of the namespace for the set of roles
 				"namespace": "http://www.w3.org/TR/xhtml2", 
+				// String: alias to assign the namespace
 				alias: "x2",
+				// String: prefix to assign to the role value
 				prefix: "wairole:"
 	},
-	waiState: { name: "waiState", 
+	waiState: { 
+				// String: informatin for mapping accessibility state
+				name: "waiState", 
+				// String: URI of the namespace for the set of states
 				"namespace": "http://www.w3.org/2005/07/aaa", 
+				// String: alias to assign the namespace
 				alias: "aaa",
+				// String: empty string - state value does not require prefix
 				prefix: ""
 	},
-	setAttr: function(node, ns, attr, value){
+	setAttr: function(/*DomNode*/node, /*String*/ ns, /*String*/ attr, /*String|Boolean*/value){
+		// Summary: Use appropriate API to set the role or state attribute onto the element.
+		// Description: In IE use the generic setAttribute() api.  Append a namespace
+		//   alias to the attribute name and appropriate prefix to the value. 
+		//   Otherwise, use the setAttribueNS api to set the namespaced attribute. Also
+		//   add the appropriate prefix to the attribute value.
 		if(dojo.render.html.ie){
 			node.setAttribute(this[ns].alias+":"+ attr, this[ns].prefix+value);
 		}else{
@@ -115,12 +132,29 @@ dojo.widget.wai = {
 		}
 	},
 
-	getAttr: function(node, ns, attr){
+	getAttr: function(/*DomNode*/ node, /*String*/ ns, /*String|Boolena*/ attr){
+		// Summary:  Use the appropriate API to retrieve the role or state value
+		// Description: In IE use the generic getAttribute() api.  An alias value 
+		// 	was added to the attribute name to simulate a namespace when the attribute
+		//  was set.  Otherwise use the getAttributeNS() api to retrieve the state value
 		if(dojo.render.html.ie){
-			 return node.getAttribute(this[ns].alias+":"+attr);
+			return node.getAttribute(this[ns].alias+":"+attr);
 		}else{
 			return node.getAttributeNS(this[ns]["namespace"], attr);
 		}
+	},
+	removeAttr: function(/*DomNode*/ node, /*String*/ ns, /*String|Boolena*/ attr){
+		// Summary:  Use the appropriate API to remove the role or state value
+		// Description: In IE use the generic removeAttribute() api.  An alias value 
+		// 	was added to the attribute name to simulate a namespace when the attribute
+		//  was set.  Otherwise use the removeAttributeNS() api to remove the state value
+		var success = true; //only IE returns a value
+		if(dojo.render.html.ie){
+			 success = node.removeAttribute(this[ns].alias+":"+attr);
+		}else{
+			node.removeAttributeNS(this[ns]["namespace"], attr);
+		}
+		return success;
 	}
 };
 
