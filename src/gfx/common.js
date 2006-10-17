4918b51f8349d4528ad2c1c14f69c69623182005
elazutkin <elazutkin@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-10-17 06:21:51 +0000

    1st draft of inline documentation for common, matrix, and path modules
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6210 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/gfx/common.js
+++ src/gfx/common.js
@@ -5,9 +5,10 @@ dojo.require("dojo.lang.declare");
 dojo.require("dojo.lang.extras");
 dojo.require("dojo.dom");
 
-//dojo.gfx.defaultRenderer.init();
-
 dojo.lang.mixin(dojo.gfx, {
+	// summary: defines constants, prototypes, and utility functions
+	
+	// default shapes, which is used to fill in missing parameters
 	defaultPath:     {type: "path",     path: ""},
 	defaultPolyline: {type: "polyline", points: []},
 	defaultRect:     {type: "rect",     x: 0, y: 0, width: 100, height: 100, r: 0},
@@ -16,6 +17,7 @@ dojo.lang.mixin(dojo.gfx, {
 	defaultLine:     {type: "line",     x1: 0, y1: 0, x2: 100, y2: 100},
 	defaultImage:    {type: "image",    width: 0, height: 0, src: ""},
 
+	// default geometric attributes (a stroke, and fills)
 	defaultStroke: {color: "black", width: 1, cap: "butt", join: 4},
 	defaultLinearGradient: {type: "linear", x1: 0, y1: 0, x2: 100, y2: 100, 
 		colors: [{offset: 0, color: "black"}, {offset: 1, color: "white"}]},
@@ -23,10 +25,14 @@ dojo.lang.mixin(dojo.gfx, {
 		colors: [{offset: 0, color: "black"}, {offset: 1, color: "white"}]},
 	defaultPattern: {type: "pattern", x: 0, y: 0, width: 0, height: 0, src: ""},
 
-	normalizeColor: function(color){
-		return (color instanceof dojo.gfx.color.Color) ? color : new dojo.gfx.color.Color(color);
+	normalizeColor: function(/* Color */ color){
+		// summary: converts any legal color representation to normalized Color object
+		return (color instanceof dojo.gfx.color.Color) ? color : new dojo.gfx.color.Color(color); // dojo.gfx.color.Color
 	},
-	normalizeParameters: function(existed, update){
+	normalizeParameters: function(/* Object */ existed, /* Object */ update){
+		// summary: updates an existing object with properties from the "update" object
+		// existed: the "target" object to be updated
+		// update: the "update" object, whose properties will be used to update the existed object
 		if(update){
 			var empty = {};
 			for(var x in existed){
@@ -35,9 +41,12 @@ dojo.lang.mixin(dojo.gfx, {
 				}
 			}
 		}
-		return existed;
+		return existed;	// Object
 	},
-	makeParameters: function(defaults, update){
+	makeParameters: function(/* Object */ defaults, /* Object */ update){
+		// summary: copies the original object, and all copied properties from the "update" object
+		// defaults: the object to be cloned before updating
+		// update: the object, which properties are to be cloned during updating
 		if(!update) return dojo.lang.shallowCopy(defaults, true);
 		var result = {};
 		for(var i in defaults){
@@ -45,9 +54,12 @@ dojo.lang.mixin(dojo.gfx, {
 				result[i] = dojo.lang.shallowCopy((i in update) ? update[i] : defaults[i], true);
 			}
 		}
-		return result;
+		return result; // Object
 	},
-	formatNumber: function(x, addSpace){
+	formatNumber: function(/* Number */ x, /* Boolean, optional */ addSpace){
+		// summary: converts a number to a string using a fixed notation
+		// x: number to be converted
+		// addSpace: add a space before a positive number
 		var val = x.toString();
 		if(val.indexOf("e") >= 0){
 			val = x.toFixed(4);
@@ -58,18 +70,24 @@ dojo.lang.mixin(dojo.gfx, {
 			}
 		}
 		if(x < 0){
-			return val;
+			return val; // String
 		}
-		return addSpace ? " " + val : val;
+		return addSpace ? " " + val : val; // String
 	},
+	// a constant used to split a SVG/VML path into primitive components
 	pathRegExp: /([A-Za-z]+)|(\d+(\.\d+)?)|(\.\d+)|(-\d+(\.\d+)?)|(-\.\d+)/g
 });
 
-// this is a Surface object
 dojo.declare("dojo.gfx.Surface", null, {
+	// summary: a surface object to be used for drawings
 	initializer: function(){
+		// summary: a constructor
+		
 		// underlying node
 		this.rawNode = null;
 	},
-	getEventSource: function(){ return this.rawNode; }
+	getEventSource: function(){
+		// summary: returns a node, which can be used to attach event listeners
+		return this.rawNode; // Node
+	}
 });
