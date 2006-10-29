3914d9aa4fee1985131071699ce6c7067e3ca65d
pottedmeat <pottedmeat@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-10-29 02:48:14 +0000

    Missed one in my last checkin. Plugins class
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6353 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ docscripts/inc/Plugins.php
@@ -0,0 +1,76 @@
+<?php
+
+class Plugins
+{
+  private $dir;
+  private static $json;
+  private static $formats = array('xml', 'json');
+  private static $types = array('local', 'remote');
+  
+  public function __construct($dir)
+  {
+    $this->dir = $dir;
+    $this->json = new Services_JSON();
+  }
+  
+  public function write($output)
+  {
+    $dir = $this->dir . '/plugins/output/';
+    $output_dir = $this->dir . '/output/';
+    $this->delTree($output_dir);
+
+    $files = scandir($dir);
+    foreach ($files as $file) {
+      if (substr($file, -4) == '.php') {
+        include_once($dir . $file);
+        $name = substr($file, 0, -4);
+        foreach (self::$types as $type) {
+          foreach (self::$formats as $format) {
+            $function = "{$name}_{$type}_{$format}";
+            if (function_exists($function)) {
+              if(!file_exists($output_dir)) {
+                mkdir($output_dir, 0777);
+              }
+              if (!file_exists("{$output_dir}{$type}")) {
+                mkdir("{$output_dir}{$type}", 0777);
+              }
+              if (!file_exists("{$output_dir}{$type}/{$format}")) {
+                mkdir("{$output_dir}{$type}/{$format}", 0777);
+              }
+              
+              $data = call_user_func($function, $output);
+              foreach ($data as $file => $contents) {
+                $file = "{$output_dir}{$type}/{$format}/{$file}";
+                file_put_contents($file, $this->json->encode($contents));
+                chmod($file, 0755);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  private function delTree($directory, $last_directory = false)
+  { 
+    if($last_directory){ 
+      $directory .= '/' . $last_directory; 
+    } 
+  
+    if (file_exists($directory)) {
+      $files = scandir($directory); 
+      foreach($files as $file){ 
+        if($file{0} != '.'){ 
+          if(is_dir($directory . '/' . $file)){ 
+            deltree($directory, $file);
+            rmdir($directory . '/' . $file);
+          }else{ 
+            unlink($directory . '/' . $file); 
+          } 
+        }
+      }
+    }
+  }
+}
+
+?>
