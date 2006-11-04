15f87cf27a210861a37bd64e258fb3d8f78c2d6d
jkuhnert <jkuhnert@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-11-04 19:01:40 +0000

    Fixed Record() constructor to properly check messsage and set this.msgArgs.
    git-svn-id: http://svn.dojotoolkit.org/src@6415 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/tests/logging/test_Logger.js
+++ trunk/tests/logging/test_Logger.js
@@ -1,47 +1,49 @@
 dojo.require("dojo.logging.Logger");
 
 function test_logging_defaultInstalledHandler(){
-	jum.assertTrue("test10", (dojo.logging.logQueueHandler instanceof dojo.logging.MemoryLogHandler));
-	jum.assertEquals("test11", "object", (typeof dojo.logging.logQueueHandler.data));
-	jum.assertTrue("test12", (dojo.logging.logQueueHandler instanceof dojo.logging.MemoryLogHandler));
-	jum.assertTrue("test13", (dojo.logging.logQueueHandler.data instanceof Array));
+	jum.assertTrue((dojo.logging.logQueueHandler instanceof dojo.logging.MemoryLogHandler));
+	jum.assertEquals("object", (typeof dojo.logging.logQueueHandler.data));
+	jum.assertTrue((dojo.logging.logQueueHandler instanceof dojo.logging.MemoryLogHandler));
+	jum.assertTrue((dojo.logging.logQueueHandler.data instanceof Array));
+	
 	dojo.log.debug("dojo.log.debug() working correctly");
-	jum.assertTrue("test14", (dojo.logging.logQueueHandler.data.length >= 1));
+	
+	jum.assertTrue((dojo.logging.logQueueHandler.data.length >= 1));
 }
 
 function test_logging_debug(){
 	var msg = "dojo.log.debug() working correctly";
 	dojo.log.debug(msg);
 	var last = dojo.logging.logQueueHandler.data.pop();
-	jum.assertEquals("test20", msg, last.message);
+	jum.assertEquals(msg, last.message);
 }
 
 function test_logging_info(){
 	var msg = "dojo.log.info() working correctly";
 	dojo.log.info(msg);
 	var last = dojo.logging.logQueueHandler.data.pop();
-	jum.assertEquals("test30", msg, last.message);
+	jum.assertEquals(msg, last.message);
 }
 
 function test_logging_warn(){
 	var msg = "dojo.log.warn() working correctly";
 	dojo.log.warn(msg);
 	var last = dojo.logging.logQueueHandler.data.pop();
-	jum.assertEquals("test40", msg, last.message);
+	jum.assertEquals(msg, last.message);
 }
 
 function test_logging_err(){
 	var msg = "dojo.log.err() working correctly";
 	dojo.log.err(msg);
 	var last = dojo.logging.logQueueHandler.data.pop();
-	jum.assertEquals("test50", msg, last.message);
+	jum.assertEquals(msg, last.message);
 }
 
 function test_logging_err(){
 	var msg = "dojo.log.crit() working correctly";
 	dojo.log.crit(msg);
 	var last = dojo.logging.logQueueHandler.data.pop();
-	jum.assertEquals("test60", msg, last.message);
+	jum.assertEquals(msg, last.message);
 }
 
 function test_logging_exception(){
