446a0c54025ccb626d50f30dd24678d856682db9
liucougar <liucougar@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-11-13 19:52:58 +0000

    added offset parameter to smoothscroll
    git-svn-id: http://svn.dojotoolkit.org/src@6506 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/lfx/scroll.js
+++ trunk/src/lfx/scroll.js
@@ -5,18 +5,21 @@ dojo.require("dojo.lfx.*");
 
 dojo.lfx.smoothScroll = function(/*DOMNode*/ node,
 											/*Window*/ win,
+											/*Object*/offset,
 											/*int?*/ duration,
 											/*function?*/ easing,
 											/*Object?*/ handlers){
 	// summary: Returns an animation that will smooth-scroll to a node (specified in setup())
 	// description: This implementation support either horizental or vertical scroll, as well as
 	//		both. In addition, element in iframe can be scrolled to correctly.
+	// offset: {x: int, y: int} this will be added to the target position
 	// duration: Duration of the animation in milliseconds.
 	// easing: An easing function.
 	// handlers: { handler: Function?, onstart: Function?, onstop: Function?, onanimate: Function? }
 
 	var targs = {
 		"window": win,
+		"offset": offset || {x:0,y:0},
 		"target": dojo.html.getAbsolutePositionExt(node,true,dojo.html.boxSizing.BORDER_BOX, win),
 		"duration": duration,
 		"easing": easing||dojo.lfx.easeOut
@@ -26,7 +29,7 @@ dojo.lfx.smoothScroll = function(/*DOMNode*/ node,
 			beforeBegin: function(){
 				var current = dojo.withGlobal(targs.window,dojo.html.getScroll).offset;
 				delete this.curve;
-				anim.curve = new dojo.lfx.Line([current.x,current.y],[targs.target.x,targs.target.y]);
+				anim.curve = new dojo.lfx.Line([current.x,current.y],[targs.target.x+targs.offset.x,targs.target.y+targs.offset.y]);
 			},
 			onAnimate: function(value){
 				targs.window.scrollTo(value[0],value[1]);
