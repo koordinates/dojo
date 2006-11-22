d6d1eef78a2850014cc5c05d09130e7d09fa78f2
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-11-22 16:12:03 +0000

    remove trailing comma
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6680 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/nls/Editor2.js
+++ src/widget/nls/Editor2.js
@@ -7,5 +7,5 @@
 	linkWindowTop: "Top Window",
 	createLinkDialogTitle: "Insert/Edit Link",
 	insertImageDialogTitle: "Insert/Edit Image",
-	createLinkDialogSelectError: "Please select some text to create a link.",
+	createLinkDialogSelectError: "Please select some text to create a link."
 })
