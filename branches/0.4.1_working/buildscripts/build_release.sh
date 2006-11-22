021ebc8827044bf2486dbf97e7bb6e9515e536be
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-11-22 05:29:32 +0000

    Fixes #1937. Add xdomain ajax to release build set (merge from trunk).
    git-svn-id: http://svn.dojotoolkit.org/src@6673 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/0.4.1_working/buildscripts/build_release.sh
+++ branches/0.4.1_working/buildscripts/build_release.sh
@@ -4,17 +4,22 @@
 DOJO=dojo-`date +%F`
 OUT_DIR=../release/
 
-# Build profiles
-echo Build profiles...
-ant # get it setup
-for pfile in $(cd profiles; ls *.profile.js; cd ..)
-do
-	profile=`echo $pfile | sed 's/.profile.js//g'`
+
+doBuild(){
+	profile=`echo $1 | sed 's/.profile.js//g'`
+	version=$2
+	loader=$3
+	proName=dojo-$version-$profile
+	if [ "$loader" == "xdomain" ]; then
+		proName=dojo-$version-xdomain-$profile
+		version=$version"xdomain"
+	fi
+
 	echo Building profile: $profile
-	CLASSPATH="/home/alex/.ant/lib/js.jar:/home/alex/.ant/lib/jython.jar" ant -q -Dversion=$1 -Ddocless=true -Dprofile=$profile release
+	CLASSPATH="/home/alex/.ant/lib/js.jar:/home/alex/.ant/lib/jython.jar" ant -q -Dversion=$version -Ddocless=true -Dprofile=$profile -DdojoLoader=$loader release
 	# the release task now includes tests by default
 	# cp -r ../tests/* ../release/dojo/tests/
-	proName=dojo-$1-$profile
+
 	cd ../release
 	rm -f dojo/src/widget/templates/images/hue.png dojo/tests/widget/test_HslColorPicker.xhtml dojo/tests/widget/test_HslColorPicker.xml dojo/src/widget/svg/HslColorPicker.js dojo/src/widget/HslColorPicker.js dojo/src/widget/templates/HslColorPicker.svg
 	mv dojo $proName
@@ -22,4 +27,16 @@ do
 	zip -rq $proName.zip $proName/
 	rm -rf $proName
 	cd ../buildscripts
+}
+
+# Build profiles
+echo Build profiles...
+ant # get it setup
+for pfile in $(cd profiles; ls *.profile.js; cd ..)
+do
+	doBuild $pfile $1 "default"
 done
+
+# Make one xdomain build, for ajax.
+doBuild "ajax.profile.js" $1 "xdomain"
+
