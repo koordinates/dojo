19e188975d48783b7d1350f6198c2a418e1d69ec
ttrenka <ttrenka@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-11-26 17:27:41 +0000

    Fixes #1841.  Custom sort functions should expect the contents of the fields in question.
    git-svn-id: http://svn.dojotoolkit.org/src@6713 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/FilteringTable.js
+++ trunk/src/widget/FilteringTable.js
@@ -649,12 +649,15 @@ dojo.widget.defineWidget(
 				if(dojo.html.hasAttribute(rowA,"emptyRow")){ return 1; }
 				if(dojo.html.hasAttribute(rowB,"emptyRow")){ return -1; }
 
-				//	TODO: check for markup and compare by rendered text.
 				var a = self.store.getField(self.getDataByRow(rowA), field);
 				var b = self.store.getField(self.getDataByRow(rowB), field);
-				var ret = 0;
-				if(a > b) ret = 1;
-				if(a < b) ret = -1;
+				if(field.sortFunction!=null){
+					var ret = field.sortFunction(a, b);
+				} else {
+					var ret = 0;
+					if(a > b) ret = 1;
+					if(a < b) ret = -1;
+				}
 				return dir * ret;
 			}
 		}
