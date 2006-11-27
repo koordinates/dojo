5a9210beb3ae2accc3b177bfa627d9b83d61357c
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-11-27 13:17:27 +0000

    style cleanups and a fix for incorrect deletion. Fixes #1702.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/0.4.1_working@6719 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/io/cookie.js
+++ src/io/cookie.js
@@ -1,9 +1,11 @@
 dojo.provide("dojo.io.cookie");
 
-dojo.io.cookie.setCookie = function(/*String*/name, /*String*/value, /*Number?*/days, /*String?*/path, /*String?*/domain, /*boolean?*/secure) {
+dojo.io.cookie.setCookie = function(/*String*/name, /*String*/value, 
+									/*Number?*/days, /*String?*/path, 
+									/*String?*/domain, /*boolean?*/secure){
 	//summary: sets a cookie.
 	var expires = -1;
-	if(typeof days == "number" && days >= 0) {
+	if((typeof days == "number")&&(days >= 0)){
 		var d = new Date();
 		d.setTime(d.getTime()+(days*24*60*60*1000));
 		expires = d.toGMTString();
@@ -18,7 +20,7 @@ dojo.io.cookie.setCookie = function(/*String*/name, /*String*/value, /*Number?*/
 
 dojo.io.cookie.set = dojo.io.cookie.setCookie;
 
-dojo.io.cookie.getCookie = function(/*String*/name) {
+dojo.io.cookie.getCookie = function(/*String*/name){
 	//summary: Gets a cookie with the given name.
 
 	// FIXME: Which cookie should we return?
@@ -38,12 +40,15 @@ dojo.io.cookie.getCookie = function(/*String*/name) {
 
 dojo.io.cookie.get = dojo.io.cookie.getCookie;
 
-dojo.io.cookie.deleteCookie = function(/*String*/name) {
+dojo.io.cookie.deleteCookie = function(/*String*/name){
 	//summary: Deletes a cookie with the given name.
 	dojo.io.cookie.setCookie(name, "-", 0);
 }
 
-dojo.io.cookie.setObjectCookie = function(/*String*/name, /*Object*/obj, /*Number?*/days, /*String?*/path, /*String?*/domain, /*boolean?*/secure, /*boolean?*/clearCurrent) {
+dojo.io.cookie.setObjectCookie = function(	/*String*/name, /*Object*/obj, 
+											/*Number?*/days, /*String?*/path, 
+											/*String?*/domain, /*boolean?*/secure, 
+											/*boolean?*/clearCurrent){
 	//summary: Takes an object, serializes it to a cookie value, and either
 	//sets a cookie with the serialized value.
 	//description: If clearCurrent is true, then any current cookie value
@@ -52,24 +57,26 @@ dojo.io.cookie.setObjectCookie = function(/*String*/name, /*Object*/obj, /*Numbe
 	//with any changes from the new object value.
 	//Objects must be simple name/value pairs where the value is either a string
 	//or a number. Any other value will be ignored.
-	if(arguments.length == 5) { // for backwards compat
+	if(arguments.length == 5){ // for backwards compat
 		clearCurrent = domain;
 		domain = null;
 		secure = null;
 	}
 	var pairs = [], cookie, value = "";
-	if(!clearCurrent) { cookie = dojo.io.cookie.getObjectCookie(name); }
-	if(days >= 0) {
-		if(!cookie) { cookie = {}; }
-		for(var prop in obj) {
-			if(prop == null) {
+	if(!clearCurrent){
+		cookie = dojo.io.cookie.getObjectCookie(name);
+	}
+	if(days >= 0){
+		if(!cookie){ cookie = {}; }
+		for(var prop in obj){
+			if(obj[prop] == null){
 				delete cookie[prop];
-			} else if(typeof obj[prop] == "string" || typeof obj[prop] == "number") {
+			}else if((typeof obj[prop] == "string")||(typeof obj[prop] == "number")){
 				cookie[prop] = obj[prop];
 			}
 		}
 		prop = null;
-		for(var prop in cookie) {
+		for(var prop in cookie){
 			pairs.push(escape(prop) + "=" + escape(cookie[prop]));
 		}
 		value = pairs.join("&");
@@ -77,31 +84,31 @@ dojo.io.cookie.setObjectCookie = function(/*String*/name, /*Object*/obj, /*Numbe
 	dojo.io.cookie.setCookie(name, value, days, path, domain, secure);
 }
 
-dojo.io.cookie.getObjectCookie = function(/*String*/name) {
+dojo.io.cookie.getObjectCookie = function(/*String*/name){
 	//summary: Gets an object value for the given cookie name. The complement of
 	//dojo.io.cookie.setObjectCookie().
 	var values = null, cookie = dojo.io.cookie.getCookie(name);
-	if(cookie) {
+	if(cookie){
 		values = {};
 		var pairs = cookie.split("&");
-		for(var i = 0; i < pairs.length; i++) {
+		for(var i = 0; i < pairs.length; i++){
 			var pair = pairs[i].split("=");
 			var value = pair[1];
-			if( isNaN(value) ) { value = unescape(pair[1]); }
+			if( isNaN(value) ){ value = unescape(pair[1]); }
 			values[ unescape(pair[0]) ] = value;
 		}
 	}
 	return values;
 }
 
-dojo.io.cookie.isSupported = function() {
+dojo.io.cookie.isSupported = function(){
 	//summary: Tests the browser to see if cookies are enabled.
-	if(typeof navigator.cookieEnabled != "boolean") {
+	if(typeof navigator.cookieEnabled != "boolean"){
 		dojo.io.cookie.setCookie("__TestingYourBrowserForCookieSupport__",
 			"CookiesAllowed", 90, null);
 		var cookieVal = dojo.io.cookie.getCookie("__TestingYourBrowserForCookieSupport__");
 		navigator.cookieEnabled = (cookieVal == "CookiesAllowed");
-		if(navigator.cookieEnabled) {
+		if(navigator.cookieEnabled){
 			// FIXME: should we leave this around?
 			this.deleteCookie("__TestingYourBrowserForCookieSupport__");
 		}
@@ -110,4 +117,4 @@ dojo.io.cookie.isSupported = function() {
 }
 
 // need to leave this in for backwards-compat from 0.1 for when it gets pulled in by dojo.io.*
-if(!dojo.io.cookies) { dojo.io.cookies = dojo.io.cookie; }
+if(!dojo.io.cookies){ dojo.io.cookies = dojo.io.cookie; }
