4c867650459728eac3dd526779f0ff47d45b96e6
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-12-07 12:21:52 +0000

    RadioButton widget (it's a subclass of the Checkbox widget). Icons are placeholders until Torrey makes some real ones. Fixes #862.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6838 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/Checkbox.js
+++ src/widget/Checkbox.js
@@ -16,6 +16,14 @@ dojo.widget.defineWidget(
 		templatePath: dojo.uri.dojoUri('src/widget/templates/Checkbox.html'),
 		templateCssPath: dojo.uri.dojoUri('src/widget/templates/Checkbox.css'),
 
+		// class: String
+		//	CSS class name for widget
+		"class": "dojoCheckbox",
+
+		//	Value of "type" attribute for <input>, and waiRole attribute also.
+		//	User probably shouldn't adjust this.
+		_type: "checkbox",
+
 		// name: String
 		//	name used when submitting form; same as "name" attribute or plain HTML elements
 		name: "",
@@ -38,15 +46,18 @@ dojo.widget.defineWidget(
 		//	the value when form is submitted)
 		value: "on",
 
+		// This shared object keeps track of all widgets, grouped by name
+		_groups: { },
+
 		postMixInProperties: function(){
 			dojo.widget.Checkbox.superclass.postMixInProperties.apply(this, arguments);
 			
 			// set tabIndex="0" because if tabIndex=="" user won't be able to tab to the field
-			if(!this.disabled && this.tabIndex==""){ this.tabIndex="0"; }
+			if(!this.disabled && this.tabIndex==""){ this.tabIndex="0"; }		
 		},
 
 		fillInTemplate: function(){
-			this._setInfo();
+			this._setValue(this.checked);
 		},
 
 		postCreate: function(){
@@ -71,6 +82,11 @@ dojo.widget.defineWidget(
 			this._connectEvents(this.domNode);
 			// this is needed here for IE
 			this.inputNode.checked=this.checked;
+			this._register();
+		},
+
+		uninitialize: function(){
+			this._deregister();
 		},
 
 		_connectEvents: function(/*DomNode*/ node){
@@ -83,20 +99,30 @@ dojo.widget.defineWidget(
 
 		_onClick: function(/*Event*/ e){
 			if(this.disabled == false){
-				this.checked = !this.checked;
-				this._setInfo();
+				this.setValue(!this.checked);
 			}
 			e.preventDefault();
 			e.stopPropagation();
 			this.onClick();
 		},
 
+		_register: function(){
+			// summary: add this widget to _groups
+			if(this._groups[this.name] == null){
+				this._groups[this.name]=[];
+			}
+			this._groups[this.name].push(this);
+		},
+
+		_deregister: function(){
+			// summary: remove this widget from _groups
+			var idx = dojo.lang.find(this._groups[this.name], this, true);
+			this._groups[this.name].splice(idx, 1);
+		},
+
 		setValue: function(/*boolean*/ bool){
 			// summary: set the checkbox state
-			if(this.disabled == false){
-				this.checked = bool;
-				this._setInfo();
-			}
+			this._setValue(bool);
 		},
 
 		onClick: function(){
@@ -124,7 +150,7 @@ dojo.widget.defineWidget(
 		_hover: function(/*Event*/ e, /*Boolean*/ isOver){
 			if (this.disabled == false){
 				var state = this.checked ? "On" : "Off";
-				var style = "dojoHtmlCheckbox" + state + "Hover";
+				var style = this["class"] + state + "Hover";
 				if (isOver){
 					dojo.html.addClass(this.imageNode, style);
 				}else{
@@ -133,12 +159,14 @@ dojo.widget.defineWidget(
 			}
 		},
 
-		_setInfo: function(){
+		_setValue: function(/*Boolean*/ bool){
 			// summary:
-			//	set state of hidden checkbox node to correspond to displayed value.
+			//	sets checkbox to given value
+			//	set state of hidden checkbox node to correspond to given value.
 			//	also set CSS class string according to checked/unchecked and disabled/enabled state
-			var state = "dojoHtmlCheckbox" + (this.disabled ? "Disabled" : "") + (this.checked ? "On" : "Off");
-			dojo.html.setClass(this.imageNode, "dojoHtmlCheckbox " + state);
+			this.checked = bool;
+			var state = this["class"] + (this.disabled ? "Disabled" : "") + (this.checked ? "On" : "Off");
+			dojo.html.setClass(this.imageNode, this["class"] + " " + state);
 			this.inputNode.checked = this.checked;
 			if(this.disabled){
 				this.inputNode.setAttribute("disabled",true);
@@ -159,19 +187,75 @@ dojo.widget.defineWidget(
 
 		templatePath: dojo.uri.dojoUri('src/widget/templates/CheckboxA11y.html'),
 
-		fillInTemplate: function(){
-		},
-
 		postCreate: function(args, frag){
 			this.inputNode.checked=this.checked;
 			//only set disabled if true since FF interprets any value for disabled as true
 			if (this.disabled){
 				this.inputNode.setAttribute("disabled",true);
-			} 
+			}
+			this._register();
 		},
 
-		_onClick: function(){
+		_onClick: function(/*Event*/ e){
+			if(this.disabled == false){
+				this.setValue(!this.checked);
+			}
 			this.onClick();
+		},
+
+		_setValue: function(/*Boolean*/ bool){
+			// summary:
+			//	internal function to set checkbox value
+			//	set state of hidden checkbox node to correspond to given value.
+			//	also set CSS class string according to checked/unchecked and disabled/enabled state
+			this.checked = bool;
+			this.inputNode.checked = bool;
 		}
+	}
+);
+
+dojo.declare(
+	"dojo.widget.RadioButtonBase",
+	null,
+	{
+		// summary
+		//	Base class for radio button widgets
+
+		// class: String
+		//	CSS class name for widget
+		"class": "dojoRadioButton",
+
+		_type: "radio",
+		
+		setValue: function(/*boolean*/ bool){
+			this._setValue(bool);
+
+			// if turning this widget on, then turn others in same group off
+			if(bool){
+				dojo.lang.forEach(this._groups[this.name], function(widget){
+					if(widget != this){
+						widget._setValue(false);
+					}
+				}, this);
+			}
+		}
+	}
+);
+
+dojo.widget.defineWidget(
+	"dojo.widget.RadioButton",
+	[dojo.widget.Checkbox, dojo.widget.RadioButtonBase],
+	{
+		// summary
+		//	Same as an HTML radio button, but with fancy styling
+	}
+);
+
+dojo.widget.defineWidget(
+	"dojo.widget.a11y.RadioButton",
+	[dojo.widget.a11y.Checkbox, dojo.widget.RadioButtonBase],
+	{
+		// summary
+		//	variation on RadioButton widget to be display on monitors in high-contrast mode (that don't display CSS background images)
 	}
 );
