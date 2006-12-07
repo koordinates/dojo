5fd9c6b9ea083c1682fba8c9fdbc63ae12c1feee
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-12-07 15:43:55 +0000

    more removal of deprecated code
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6849 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/TabContainer.js
+++ src/widget/TabContainer.js
@@ -78,11 +78,6 @@ dojo.widget.defineWidget("dojo.widget.TabContainer", dojo.widget.PageContainer, 
 		}
 	},
 
-	selectTab: function(tab, callingWidget){
-		dojo.deprecated("use selectChild() rather than selectTab(), selectTab() will be removed in", "0.5");
-		this.selectChild(tab, callingWidget);
-	},
-
 	onKey: function(e){
 		// summary
 		//	Keystroke handling for keystrokes on the tab panel itself (that were bubbled up to me)
