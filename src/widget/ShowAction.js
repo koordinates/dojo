bdcf37db8db1292797a0b151f2317fed62914cf7
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-12-07 14:16:53 +0000

    combine all the Show widgets (Show, ShowAction, and ShowSlide) into one file, since they are always used together
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@6841 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/widget/ShowAction.js
+++ src/widget/ShowAction.js
@@ -1,20 +1,3 @@
 dojo.provide("dojo.widget.ShowAction");
-dojo.require("dojo.widget.*");
 
-dojo.widget.defineWidget(
-	"dojo.widget.ShowAction",
-	dojo.widget.HtmlWidget,
-{
-	on: "",
-	action: "fade",
-	duration: 350,
-	from: "",
-	to: "",
-	auto: "false",
-	postMixInProperties: function(){ 
-		// fix for very strange Opera 9 bug
-		if(dojo.render.html.opera){
-			this.action = this.action.split("/").pop();
-		}
-	}
-});
+dojo.deprecated("dojo.widget.ShowAction", "just do dojo.require('dojo.widget.Show')", "0.6");
