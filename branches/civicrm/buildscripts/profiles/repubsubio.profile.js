5dfd7d9a5d023f6ea39b947dfe12419f2ade50ce
alex <alex@560b804f-0ae3-0310-86f3-f6aa0a117693>
2006-12-29 23:09:44 +0000

    removing junk branch
    git-svn-id: http://svn.dojotoolkit.org/src@6944 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/civicrm/buildscripts/profiles/repubsubio.profile.js
+++
@@ -1,8 +0,0 @@
-var dependencies = [ 
-	"dojo.io.IO",
-	"dojo.event.*",
-	"dojo.io.BrowserIO",
-	"dojo.io.RepubsubIO"
-];
-
-load("getDependencyList.js");
--- branches/civicrm/tests/widget/test_Widget.js
+++
@@ -1,9 +0,0 @@
-dojo.require("dojo.widget.Widget");
-
-function test_widget_ctor(){
-	jum.debug("in widget.ctor");
-	var obj1 = new dojo.widget.Widget();
-
-	jum.assertTrue("test1", typeof obj1 == "object");
-//	jum.assertTrue("test2", obj1.widgetType == "Widget");
-}
