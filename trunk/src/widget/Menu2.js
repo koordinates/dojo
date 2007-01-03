aa253bbe734c6ae21b3aa5e36100ebe998bc304e
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-01-03 01:57:10 +0000

    Rework hiearchy so Menu2Bar isn't a PopupContainer (since it doesn't popup). Fixes problem that after previous changes, Menu2Bar was being initialized as display:none.
    git-svn-id: http://svn.dojotoolkit.org/src@6970 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/widget/Menu2.js
+++ trunk/src/widget/Menu2.js
@@ -2,13 +2,12 @@ dojo.provide("dojo.widget.Menu2");
 
 dojo.require("dojo.widget.PopupContainer");
 
-dojo.widget.defineWidget(
-	"dojo.widget.PopupMenu2",
-	dojo.widget.PopupContainer,
+dojo.declare(
+	"dojo.widget.MenuBase",
+	null,
 	function(){
 		// summary
-		//	provides a menu that can be used as a context menu (typically shown by right-click),
-		//	or as the drop down on a DropDownButton, ComboButton, etc.
+		//		Base class for PopupMenu and MenuBar
 
 		// targetNodeIds: String[]
 		//	Array of dom node ids of nodes to attach to.
@@ -309,8 +308,16 @@ dojo.widget.defineWidget(
 	}
 });
 
-// summary
-//	A line item in a Menu2
+dojo.widget.defineWidget(
+	"dojo.widget.PopupMenu2",
+	[dojo.widget.HtmlWidget, dojo.widget.MenuBase, dojo.widget.PopupContainerBase],
+	{
+		// summary
+		//	provides a menu that can be used as a context menu (typically shown by right-click),
+		//	or as the drop down on a DropDownButton, ComboButton, etc.
+	}
+);
+
 dojo.widget.defineWidget(
 	"dojo.widget.MenuItem2",
 	dojo.widget.HtmlWidget,
@@ -320,6 +327,9 @@ dojo.widget.defineWidget(
 		};
 	},
 {
+	// summary
+	//	A line item in a Menu2
+
 	// Make 4 columns
 	//   icon, label, accelerator-key, and right-arrow indicating sub-menu
 	templateString:
@@ -567,7 +577,7 @@ dojo.widget.defineWidget(
 //	A menu bar, listing menu choices horizontally, like the "File" menu in most desktop applications
 dojo.widget.defineWidget(
 	"dojo.widget.MenuBar2",
-	dojo.widget.PopupMenu2,
+	[dojo.widget.HtmlWidget, dojo.widget.MenuBase],
 {
 	menuOverlap: 2,
 
