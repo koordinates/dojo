024a4531498e276e9d49839da7858df8f90f7065
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-01-24 10:32:59 +0000

    dojo.dot.ui now correctly embeds the offline widget UI into the page -- there is just a placeholder for now, saying Hello World in the HTML -- Moxie is tied in
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@7070 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/dot/ui.js
+++ src/dot/ui.js
@@ -1,1 +1,53 @@
 dojo.provide("dojo.dot.ui");
+
+dojo.require("dojo.io.*");
+dojo.require("dojo.event.*");
+
+
+dojo.dot.ui = {
+	// autoEmbed: boolean
+	//	Whether to automatically auto-embed the default Dojo Offline
+	//	widget into this page; default is true. 
+	autoEmbed: true,
+	
+	// autoEmbedID: String
+	//	The ID of the DOM element that will contain our
+	//	Dojo Offline widget; defaults to the ID 'dot-widget'.
+	autoEmbedID: "dot-widget",
+	
+	_htmlTemplatePath: "src/dot/ui-template/widget.html",
+	_cssTemplatePath: "src/dot/ui-template/widget.css",
+
+	_onPageLoad: function(){
+		if(this.autoEmbed == true){
+			this._doAutoEmbed();
+		}
+	},
+	
+	_doAutoEmbed: function(){
+		// fetch our HTML for the offline widget
+		var templatePath = djConfig.baseRelativePath + this._htmlTemplatePath;
+		var bindArgs = {
+			url:	 templatePath,
+			sync:		false,
+			mimetype:	"text/html",
+			error:		function(type, errObj){
+				dojo.dot.enabled = false;
+				alert("Error loading the Dojo Offline Widget from "
+						+ templatePath + ": " + errObj);
+			},
+			load:		dojo.lang.hitch(this, this._templateLoaded)	 
+		};
+		
+		// dispatch the request
+		dojo.io.bind(bindArgs);
+	},
+	
+	_templateLoaded: function(type, data, evt){
+		// inline our HTML
+		var container = dojo.byId(this.autoEmbedID);
+		container.innerHTML = data;
+	}
+};
+
+dojo.event.connect(window, "onload", dojo.dot.ui, "_onPageLoad");
