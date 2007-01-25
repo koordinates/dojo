286f86b022df8eac2d3a0b2ef680a3d31ce8beb1
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-01-25 01:33:47 +0000

    Finished Learn How page, including its dynamic customizations based on framework settings and defaults; continuing to build out Offline Widget UI -- working on Synchronize messages currently and hooking into Moxie
    git-svn-id: http://svn.dojotoolkit.org/src@7079 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/dot.js
+++ trunk/src/dot.js
@@ -9,5 +9,36 @@ dojo.lang.mixin(dojo.dot, {
 	//	to dojo.dot.goOnline() and dojo.dot.goOffline(),
 	//	or it was set the last time we called
 	//	dojo.dot.isSiteAvailable()
-	isOnline: false
+	isOnline: false,
+	
+	// requireDurableCache: boolean
+	//	A durable cache is a cache that can correctly and
+	//	truely cache the UI resources of an application for
+	//	offline use, such as its HTML, JavaScript, etc. A
+	//	durable cache won't remove these files from it's
+	//	offline cache, for example. No browser's currently
+	//	support native durable cache's; FireFox 3 has plans
+	//	for one. If true, then we require a durable cache,
+	//	and must either install one or have one natively
+	//	supported by this browser; if false, then we will
+	//	rely on the browser's ordinary cache, which can be
+	//	made to work but is not always reliable. Defaults to
+	//	true.
+	requireDurableCache: true,
+	
+	hasDurableCache: function(){ /* boolean */
+		// summary: 
+		//	Returns whether we have a durable cache available
+		// description:
+		//	Determines if a durable cache is available or installed;
+		//	a durable cache is a facility that can truely cache offline
+		//	resource, such as JavaScript, HTML, etc. in such a way that
+		//	they won't be removed from the cache inappropriately like
+		//	a browser cache would. If this is false, and 
+		//	dojo.dot.requireDurableCache is true, then a durable cache
+		//	will be installed
+	
+		// FIXME: Implement
+		return true;
+	}
 });
--- trunk/src/dot/ui.js
+++ trunk/src/dot/ui.js
@@ -5,6 +5,13 @@ dojo.require("dojo.event.*");
 
 
 dojo.lang.mixin(dojo.dot.ui, {
+	// appName: String
+	//	This application's name, such as "Foobar". Note that
+	//	this is a string, not HTML, so embedded markup will
+	//	not work, including entities. Only the following
+	//	characters are allowed: numbers, letters, and spaces
+	appName: "Define dojo.dot.ui.appName",
+	
 	// autoEmbed: boolean
 	//	Whether to automatically auto-embed the default Dojo Offline
 	//	widget into this page; default is true. 
@@ -15,28 +22,112 @@ dojo.lang.mixin(dojo.dot.ui, {
 	//	Dojo Offline widget; defaults to the ID 'dot-widget'.
 	autoEmbedID: "dot-widget",
 	
+	// runLink: String
+	//	The URL that should be navigated to to run this 
+	//	application offline; this will be placed inside of a
+	//	link that the user can drag to their desktop and double
+	//	click. Note that this URL must exactly match the URL
+	//	of the main page of our resource that is offline for
+	//	it to be retrieved from the offline cache correctly.
+	//	For example, if you have cached your main page as
+	//	http://foobar.com/index.html, and you set this to
+	//	http://www.foobar.com/index.html, the run link will
+	//	not work. By default this value is automatically set to 
+	//	the URL of this page, so it does not need to be set
+	//	manually unless you have unusual needs.
+	runLink: window.location.href,
+	
+	// runLinkTitle: String
+	//	The text that will be inside of the link that a user
+	//	can drag to their desktop to run this application offline.
+	//	By default this is automatically set to "Run " plus your
+	//	application's name.
+	runLinkTitle: "Run Application",
+	
 	// learnHowPath: String
 	//	The path to a web page that has information on 
 	//	how to use this web app offline; defaults to
-	//	src/dot/ui-tempalte/learnhow.html, relative to
-	//	your Dojo installation
+	//	src/dot/ui-template/learnhow.html, relative to
+	//	your Dojo installation. Make sure to set
+	//	dojo.to.ui.customLearnHowPath to true if you want
+	//	a custom Learn How page.
 	learnHowPath: djConfig.baseRelativePath
 					+ "src/dot/ui-template/learnhow.html",
-					
-	// appName: String
-	//	This applications name, such as "Foobar". Note that
-	//	this is HTML, so you must correctly encode reserved
-	//	characters. For example, if you wanted to use brackets
-	//	around your appname, you would have "&lt;Foobar&gt;".
-	//	HTML markup may also appear in here.
-	appName: "Define dojo.dot.ui.appName",
+	
+	// customLearnHowPath: boolean
+	//	Whether the developer is using their own custom page
+	//	for the Learn How instructional page; defaults to false.
+	//	Use in conjunction with dojo.dot.ui.learnHowPath.
+	customLearnHowPath: false,
 	
 	_htmlTemplatePath: "src/dot/ui-template/widget.html",
 	_cssTemplatePath: "src/dot/ui-template/widget.css",
 	_onlineImagePath: "src/dot/ui-template/greenball.png",
 	_offlineImagePath: "src/dot/ui-template/redball.png",
+	_rollerImagePath: "src/dot/ui-template/roller.gif",
+	
+	onStart: function(){
+		// summary:
+		//	Updates our UI when synchronization first starts.
+		
+		dojo.debug("sync started");
+		this._updateSyncUI();
+	},
+	
+	onRefreshUI: function(){
+		// summary:
+		//	Updates our UI when synchronization starts
+		//	refreshing offline UI resources
+		
+		dojo.debug("onRefreshUI");
+	},
+	
+	onUpload: function(){
+		// summary:
+		//	Updates our UI when synchronization starts
+		//	uploading locally changed data
+		
+		dojo.debug("onUpload");
+	},
+	
+	onDownload: function(){
+		// summary:
+		//	Updates our UI when synchronization starts
+		//	download new server data
+		
+		dojo.debug("onDownload");
+	},
+	
+	onFinished: function(){
+		// summary:
+		//	Updates our UI when synchronization
+		//	is finished
+		
+		dojo.debug("onFinished");
+	},
 
 	_onPageLoad: function(){
+		// make sure our app name is correct
+		if(this._validateAppName(this.appName) == false){
+			alert("You must set dojo.dot.ui.appName; it can only contain "
+					+ "letters, numbers, and spaces; right now it "
+					+ "is set to " + dojo.dot.ui.appName);
+			dojo.dot.enabled = false;
+			return;
+		}
+		
+		// set our run link text to its default
+		this.runLinkText = "Run " + this.appName;
+		
+		// setup our event listeners for sync events
+		// to update our UI
+		dojo.sync.onStart = dojo.lang.hitch(this, this.onStart);
+		dojo.sync.onRefreshUI = dojo.lang.hitch(this, this.onRefreshUI);
+		dojo.sync.onUpload = dojo.lang.hitch(this, this.onUpload);
+		dojo.sync.onDownload = dojo.lang.hitch(this, this.onDownload);
+		dojo.sync.onFinished = dojo.lang.hitch(this, this.onFinished);
+		
+		// embed the offline widget UI
 		if(this.autoEmbed == true){
 			this._doAutoEmbed();
 		}
@@ -79,6 +170,15 @@ dojo.lang.mixin(dojo.dot.ui, {
 		
 		// update our 'Learn How' text
 		this._initLearnHow();
+		
+		// update our sync UI
+		this._updateSyncUI();
+		
+		// register our event listeners for buttons
+		var syncButton = dojo.byId("dot-sync-button");
+		if(syncButton){
+			dojo.event.connect(syncButton, "onclick", this, this._synchronize);
+		}
 	},
 	
 	_updateNetworkIndicator: function(){
@@ -105,6 +205,17 @@ dojo.lang.mixin(dojo.dot.ui, {
 			return;
 		}
 		
+		if(this.customLearnHowPath == false){
+			// add parameters to URL so the Learn How page
+			// can customize itself and display itself
+			// correctly
+			this.learnHowPath += "?appName=" + encodeURIComponent(this.appName)
+									+ "&requireDurableCache=" + dojo.dot.requireDurableCache
+									+ "&hasDurableCache=" + dojo.dot.hasDurableCache()
+									+ "&runLink=" + encodeURIComponent(this.runLink)
+									+ "&runLinkText=" + encodeURIComponent(this.runLinkText);
+		}
+		
 		learnHow.setAttribute("href", this.learnHowPath);
 		
 		var appName = dojo.byId("dot-widget-learn-how-app-name");
@@ -113,8 +224,61 @@ dojo.lang.mixin(dojo.dot.ui, {
 			return;
 		}
 		
-		appName.innerHTML = this.appName;
+		appName.innerHTML = "";
+		appName.appendChild(document.createTextNode(this.appName));
+	},
+	
+	_validateAppName: function(appName){
+		if(appName == null || typeof appName == "undefined"){
+			return false;
+		}
+		
+		return (/^[a-z0-9 ]*$/i.test(appName));
+	},
+	
+	_updateSyncUI: function(){
+		var syncButtons = dojo.byId("dot-sync-buttons");
+		var syncingButtons = dojo.byId("dot-syncing-buttons");
+		var roller = dojo.byId("dot-roller");
+		
+		if(dojo.sync.isSyncing == true){
+			if(syncButtons){
+				syncButtons.style.display = "none";
+			}
+			
+			if(syncingButtons){
+				syncingButtons.style.display = "block";
+			}
+			
+			dojo.debug("roller="+roller);
+			if(roller){
+				var src = djConfig.baseRelativePath + this._rollerImagePath;
+				dojo.debug("src="+src);
+				roller.setAttribute("src", src);
+				roller.style.visibility = "visible";
+			}
+		}else{
+			if(syncButtons){
+				syncButtons.style.display = "block";
+			}
+			
+			if(syncingButtons){
+				syncingButtons.style.display = "none";
+			}
+			
+			if(roller){
+				roller.style.visibility = "none";
+			}
+		}
+	},
+	
+	_synchronize: function(evt){
+		// cancel the button's default behavior
+		evt.preventDefault();
+		evt.stopPropagation();
+		
+		dojo.sync.synchronize();
 	}
 });
 
-dojo.event.connect(window, "onload", dojo.dot.ui, "_onPageLoad");
+dojo.event.connect(window, "onload", dojo.dot.ui, dojo.dot.ui._onPageLoad);
