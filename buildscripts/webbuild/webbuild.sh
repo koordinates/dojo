91ddbce251155bf7da15ba6258cea5c3d75d2017
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-04 23:36:39 +0000

    References #2366: web build plumbing. Introduce a script that pulls the code from svn and makes the appropriate zip file to be used as the web build and xdomain dojo
    git-svn-id: http://svn.dojotoolkit.org/src/branches/0.4@7183 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/webbuild/webbuild.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Define variables used for rest of the script
+version=0.4.2ALPHA
+xdDojoUrl=http://dojotoolkit.org/~jburke/$version
+versionSuffix=xdomain
+
+#Clean up from a previous run.
+#Make a temp area to do the work.
+rm -rf ./webbuildtemp
+mkdir ./webbuildtemp
+cd ./webbuildtemp
+
+#Get the source
+svn export http://svn.dojotoolkit.org/dojo/branches/0.4
+cd ./0.4/buildscripts
+
+#Do xdajax build.
+#Mark the dojo.js has an xdomain build, complete with xdomain path for Dojo.
+ant -Dprofile=ajax -DreleaseName=$version-xdajax -Dversion=$version$versionSuffix -DxdDojoUrl=$xdDojoUrl clean release intern-strings xd-dojo-config
+
+#src folders/buildscripts for the webbuild stuff.
+mkdir ../release
+
+cd ../release
+mkdir web
+cd web
+cp -r ../../src .
+cp -r ../../buildscripts .
+cp -r ../../dojo.js .
+cp -r ../../iframe_history.html .
+cp -r ../../flash6_gateway.swf .
+cp -r ../../DojoFileStorageProvider.jar .
+cp -r ../../storage_dialog.swf .
+cp -r ../../Storage_version6.swf .
+cp -r ../../Storage_version8.swf .
+
+#Stamp the web build with the xd Dojo URL.
+#xxx
+
+cd buildscripts/webbuild
+cat index.html | sed "s/@VERSION@/$version/g" | sed "s/@XD_DOJO_URL@/$xdDojoUrl/g" > index.out.html
+mv index.out.html index.html
+
+#Generate the list of modules for the web build process.
+cd buildscripts/webbuild
+java -jar ../lib/custom_rhino.jar makeWebBuildModuleList.js ../../src treeData.js
+cd ../../..
+
+#Now in release folder. Bundle it all up.
+zip -r dojo-$version.zip $version-xdajax/* web/*
+mv dojo-$version.zip ../..
+
+#Return to start directory.
+cd ../../..
