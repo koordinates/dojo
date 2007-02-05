e16aec1af90e272d851768b33ac1e0bc30a30c47
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-05 06:38:03 +0000

    Fixes #2301 and #2404, and references #2366. web built dojo.js now has strings interned, and for regular builds, all widget js files have strings interned now for the intern-strings target. To get those things to work, I moved the internStrings.js functions into buildUtil.js, but I did it slightly differently than the patch in #2301.
    git-svn-id: http://svn.dojotoolkit.org/src@7186 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/0.4/buildscripts/buildUtil.js
+++ branches/0.4/buildscripts/buildUtil.js
@@ -605,6 +605,173 @@ buildUtil.mapResourceToPath = function(resourceName, baseRelativePath, prefixes)
 	return finalPath;
 }
 
+
+function makeResourceUri(resourceName, templatePath, srcRoot, prefixes){
+	var bestPrefix = "";
+	var bestPrefixPath = ""
+	if(prefixes){
+		for (var i = 0; i < prefixes.length; i++){
+			var prefix = prefixes[i];
+			//Prefix must match from the start of the resourceName string.
+			if(resourceName.indexOf(prefix[0]) == 0){
+				if(prefix[0].length > bestPrefix.length){
+					bestPrefix = prefix[0];
+					bestPrefixPath = prefix[1];
+				}
+			}
+		}
+
+		if(bestPrefixPath != ""){
+			//Convert resourceName to a path
+			resourceName = resourceName.replace(bestPrefix, "");
+			if(resourceName.indexOf(".") == 0){
+				resourceName = resourceName.substring(1, resourceName.length);
+			}
+			resourceName = resourceName.replace(/\./g, "/");
+
+			//Final path construction
+			var finalPath = srcRoot;
+			finalPath += bestPrefixPath + "/";
+			if(resourceName){
+				finalPath += resourceName + "/";
+			}
+			finalPath += templatePath;
+
+			return finalPath;
+		}
+	}
+
+	return srcRoot + templatePath;
+}
+
+buildUtil.internTemplateStrings = function(profileFile, loader, releaseDir, srcRoot){
+	loader = loader || "default";
+	releaseDir = releaseDir || "../release/dojo";
+	srcRoot = srcRoot || "../";
+	
+	print("loader: " + loader);
+	print("releaseDir - " + releaseDir);
+	var resourceFile = releaseDir + "/dojo.js";
+
+	//Load Dojo so we can use readText() defined in hostenv_rhino.js.
+	//Also gives us the ability to use all the neato toolkit features.
+	djConfig={
+		baseRelativePath: "../"
+	};
+	load('../dojo.js');
+	dojo.require("dojo.string.extras");
+	dojo.require("dojo.i18n.common");
+	dojo.require("dojo.json");
+	
+	//Find the bundles that need to be flattened.
+	load("buildUtil.js");
+
+	var prefixes = buildUtil.getDependencyPropertyFromProfile(profileFile, "prefixes");
+	//Make sure dojo is in the list.
+	var dojoPath = releaseDir.replace(/^.*(\/|\\)release(\/|\\)/, "release/");
+	prefixes.push(["dojo", dojoPath + "/src"]);
+
+	var skiplist = buildUtil.getDependencyPropertyFromProfile(profileFile, "internSkipList");
+	
+	buildUtil.internTemplateStringsInFile(loader, resourceFile, srcRoot, prefixes, skiplist);
+
+	//Intern strings for all files in widget dir (xdomain and regular files)
+	var fileList = buildUtil.getFilteredFileList(releaseDir + "/src/widget",
+		/\.js$/, true);
+
+	if(fileList){
+		for(var i = 0; i < fileList.length; i++){
+			buildUtil.internTemplateStringsInFile(loader, fileList[i], srcRoot, prefixes, skiplist)
+		}
+	}
+}
+
+buildUtil.internTemplateStringsInFile = function(loader, resourceFile, srcRoot, prefixes, skiplist){
+	var resourceContent = new String(readText(resourceFile));
+	resourceContent = buildUtil.interningRegexpMagic(loader, resourceContent, srcRoot, prefixes, skiplist);
+	buildUtil.saveUtf8File(resourceFile, resourceContent);
+}
+
+buildUtil.interningDojoUriRegExpString = "(((templatePath|templateCssPath)\\s*(=|:)\\s*)|dojo\\.uri\\.cache\\.allow\\(\\s*)dojo\\.uri\\.(dojo|module)?Uri\\(\\s*?[\\\"\\']([\\w\\.\\/]+)[\\\"\\'](([\\,\\s]*)[\\\"\\']([\\w\\.\\/]*)[\\\"\\'])?\\s*\\)";
+buildUtil.interningGlobalDojoUriRegExp = new RegExp(buildUtil.interningDojoUriRegExpString, "g");
+buildUtil.interningLocalDojoUriRegExp = new RegExp(buildUtil.interningDojoUriRegExpString);
+
+//WARNING: This function assumes dojo.string.escapeString() has been loaded.
+buildUtil.interningRegexpMagic = function(loader, resourceContent, srcRoot, prefixes, skiplist){
+	return resourceContent.replace(buildUtil.interningGlobalDojoUriRegExp, function(matchString){
+		var parts = matchString.match(buildUtil.interningLocalDojoUriRegExp);
+
+		var filePath = "";
+		var resourceNsName = "";
+		if(parts[5] == "dojo"){
+			if(parts[6].match(/(\.htm|\.html|\.css)$/)){
+				print("Dojo match: " + parts[6]);
+				filePath = srcRoot + parts[6]
+				resourceNsName = "dojo:" + parts[6];
+			}
+		}else{
+			print("Module match: " + parts[6] + " and " + parts[9]);
+			filePath = makeResourceUri(parts[6], parts[9], srcRoot, prefixes);
+			resourceNsName = parts[6] + ':' + parts[9];		
+		}
+
+		if(!filePath || buildUtil.isValueInArray(resourceNsName, skiplist)){
+			if(filePath){
+				print("Skip intern resource: " + filePath);
+			}
+		}else{
+			print("Interning resource path: " + filePath);
+			//dojo.string.escapeString will add starting and ending double-quotes.
+			var jsEscapedContent = dojo.string.escapeString(new String(readText(filePath)));
+			if(jsEscapedContent){
+				if(matchString.indexOf("dojo.uri.cache.allow") != -1){
+					//Handle dojo.uri.cache-related interning.
+					var parenIndex = matchString.lastIndexOf(")");
+					matchString = matchString.substring(0, parenIndex + 1) + ", " + jsEscapedContent;
+					matchString = matchString.replace("dojo.uri.cache.allow", "dojo.uri.cache.set");
+				}else{
+					//Handle templatePath/templateCssPath-related interning.
+					if(parts[3] == "templatePath"){
+						//Replace templatePaths
+						matchString = "templateString" + parts[4] + jsEscapedContent;
+					}else{
+						//Dealing with templateCssPath
+						//For the CSS we need to keep the template path in there
+						//since the widget loading stuff uses the template path to
+						//know whether the CSS has been processed yet.
+						//Could have matched assignment via : or =. Need different statement separators at the end.
+						var assignSeparator = parts[4];
+						var statementSeparator = ",";
+						var statementPrefix = "";
+			
+						//FIXME: this is a little weak because it assumes a "this" in front of the templateCssPath
+						//when it is assigned using an "=", as in 'this.templateCssPath = dojo.uri.dojoUri("some/path/to/Css.css");'
+						//In theory it could be something else, but in practice it is not, and it gets a little too weird
+						//to figure out, at least for now.
+						if(assignSeparator == "="){
+							statementSeparator = ";";
+							statementPrefix = "this.";
+						}
+						matchString = "templateCssString" + assignSeparator + jsEscapedContent + statementSeparator + statementPrefix + parts[0];
+					}
+				}
+			}
+		}
+
+		return matchString;
+	});
+}
+
+buildUtil.isValueInArray = function(value, ary){
+	for(var i = 0; i < ary.length; i++){
+		if(ary[i] == value){
+			return true;
+		}
+	}
+	return false;
+}
+
+
 //Recurses startDir and finds matches to the files that match regExpFilter.
 //Ignores files/directories that start with a period (.).
 buildUtil.getFilteredFileList = function(startDir, regExpFilter, makeUnixPaths, startDirIsJavaObject){
