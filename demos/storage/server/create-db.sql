e864c4363868d2a3c5db292e8339588a1238ea4e
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-05 19:56:05 +0000

    Building up SQL for Moxie server-side database to start work on dojo sync and dojo data saving and manipulation -- beginning on server end from actual app to ground work
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@7196 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ demos/storage/server/create-db.sql
@@ -0,0 +1,27 @@
+/*
+	SQL script to create the Moxie database.
+	This SQL is MySQL specific.
+	To use:
+	
+	
+	Warning: This script first deletes any possible
+	pre-existing Moxie database before creating it anew.
+
+	@author Brad Neuberg, bkn3@columbia.edu
+*/
+
+DROP DATABASE IF EXISTS moxie;
+
+/* Set default character encoding so we i18n ready */
+CREATE DATABASE moxie 
+	DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+USE moxie;
+
+CREATE TABLE document (
+	id 				INT(50) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	file_name		VARCHAR(256) UNIQUE NOT NULL,
+	created_on		DATETIME NOT NULL,
+	last_updated		DATETIME NOT NULL,
+	content			TEXT 
+) ENGINE=MyISAM;
