9a13ef132c3a27ff8966fe6d9c6e1223dbad29f3
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-08 22:35:45 +0000

    Stubbing out server side sync classes
    git-svn-id: http://svn.dojotoolkit.org/src@7250 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ trunk/demos/storage/server/org/dojo/moxie/sync/SyncServlet.java
@@ -0,0 +1,11 @@
+package org.dojo.moxie;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class SyncServlet extends HttpServlet{
+}
+
---
+++ trunk/demos/storage/server/org/dojo/moxie/util/JSONDeserializer.java
@@ -0,0 +1,7 @@
+package org.moxie.util;
+
+/**
+	@author Brad Neuberg, bkn3@columbia.edu
+*/
+public class JSONDeserializer{
+}
