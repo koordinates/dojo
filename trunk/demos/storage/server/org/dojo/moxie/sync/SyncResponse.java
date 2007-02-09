40676689008f79caa7ddfadce01bd1f3c3d422ab
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-09 13:30:15 +0000

    Implementing server side sync
    git-svn-id: http://svn.dojotoolkit.org/src@7271 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/demos/storage/server/org/dojo/moxie/sync/SyncResponse.java
+++ trunk/demos/storage/server/org/dojo/moxie/sync/SyncResponse.java
@@ -1,5 +1,6 @@
 package org.dojo.moxie.sync;
 
+import java.util.*;
 import net.sf.json.*;
 
 /**
@@ -33,6 +34,46 @@ public class SyncResponse{
 	}
 	
 	public String toJSON(){
-		return JSONSerializer.toJSON(this).toString(2);
+		StringBuffer results = new StringBuffer();
+		
+		results.append("{\n");
+		results.append("  serverTime: " + serverTime + ",\n");
+		results.append("  commandLog: [\n");
+		
+		Iterator<Command> iter = log.iterator();
+		while(iter.hasNext()){
+			Command c = iter.next();
+			// clean up JSON output - it incorrectly converts
+			// nulls into empty strings and the number 0 in
+			// some cases
+			// FIXME: Fix this bug inside the JSON-Lib library
+			// itself
+			JSONObject jsonCommand = JSONObject.fromObject(c);
+			if(jsonCommand.has("status")
+				&& jsonCommand.getString("status").equals("")){
+				jsonCommand.remove("status");
+			}
+			
+			if(jsonCommand.has("timeoffset")
+				&& jsonCommand.getLong("timeoffset") == 0){
+				jsonCommand.remove("timeoffset");
+			}
+			
+			if(jsonCommand.has("timestamp")
+				&& jsonCommand.getLong("timestamp") == 0){
+				jsonCommand.remove("timestamp");
+			}
+			
+			results.append(jsonCommand.toString());
+			if(iter.hasNext()){
+				results.append(", \n");
+			}
+		}
+		
+		results.append("  ]\n");
+		
+		results.append("}\n");
+		
+		return results.toString();
 	}
 }
