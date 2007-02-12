14a2659dddab8228d9b25a36ab0c07870e157d13
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-12 06:54:14 +0000

    References #2366. Get tree double-click to work, and add some protection to php script so it doesn't allow fetching any file from the server.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/0.4@7284 560b804f-0ae3-0310-86f3-f6aa0a117693

--- buildscripts/makeDojoJsWeb.js
+++ buildscripts/makeDojoJsWeb.js
@@ -9,25 +9,50 @@ var version = new String(arguments[2]);
 var xdDojoUrl = new String(arguments[3]);
 
 depList = depList.split(",");
-provideList = provideList.split(",");
 
-var dependencyResult;
-eval('dependencyResult = {depList: ["' + depList.join('","') + '"], provideList: ["' + provideList.join('","') + '"]};');
-
-//Load dojo (needed for string interning)
-djConfig={
-	baseRelativePath: "../"
-};
-load('../dojo.js');
-dojo.require("dojo.string.extras");
-
-var contents = buildUtil.makeDojoJs(dependencyResult, version).dojoContents;
-
-//Add copyright, and intern strings.
-contents = new String(readFile("copyright.txt")) + buildUtil.interningRegexpMagic("xdomain", contents, djConfig.baseRelativePath, [["dojo", "src"]], [], true);
-
-if(xdDojoUrl){
-	contents = buildUtilXd.setXdDojoConfig(contents, xdDojoUrl);
+//Check if there were no provideList (the caller can send string "null"
+//to indicate that the command line parameter is empty. We need some string
+//to make sure all the arguments are in the right spot.
+if(provideList == "null"){
+	provideList = "[]";
+}else{
+	provideList = provideList.split(",");
+	provideList = '["' + provideList.join('","') + '"]';
 }
 
-print(contents);
+var dependencyResult;
+eval('dependencyResult = {depList: ["' + depList.join('","') + '"], provideList: ' + provideList + '};');
+
+//Make sure we are dealing with JS files and that the paths are not outside
+//of the working area. Do this to discourage fetching arbitrary files from
+//the server.
+var deps = dependencyResult.depList;
+var isInputOk = true;
+for(var i = 0; i < deps.length; i++){
+	var matches = deps[i].match(/\.\./g);
+	if((matches && matches.length > 1) || !deps[i].match(/\.js$/) || deps[i].indexOf(0) == '/'){
+		print("Error: Invalid file set.");
+		isInputOk = false;
+		break;
+	}
+}
+
+if(isInputOk){
+	//Load dojo (needed for string interning)
+	djConfig={
+		baseRelativePath: "../"
+	};
+	load('../dojo.js');
+	dojo.require("dojo.string.extras");
+	
+	var contents = buildUtil.makeDojoJs(dependencyResult, version).dojoContents;
+	
+	//Add copyright, and intern strings.
+	contents = new String(readFile("copyright.txt")) + buildUtil.interningRegexpMagic("xdomain", contents, djConfig.baseRelativePath, [["dojo", "src"]], [], true);
+	
+	if(xdDojoUrl){
+		contents = buildUtilXd.setXdDojoConfig(contents, xdDojoUrl);
+	}
+	
+	print(contents);
+}
