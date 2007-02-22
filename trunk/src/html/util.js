ada4acefcd6c6aea77460c94e167eec9abebd262
peller <peller@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-02-22 22:35:12 +0000

    Enable BiDi for Menu2 widget.  Fixes #2485
    git-svn-id: http://svn.dojotoolkit.org/src@7403 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/html/util.js
+++ trunk/src/html/util.js
@@ -507,3 +507,16 @@ dojo.html.scrollIntoView = function(/* HTMLElement */node){
 		}
 	}
 }
+
+dojo.html.isLeftToRight = function(/*HtmlElement*/node){
+	// summary
+	// Walks the DOM parent chain looking for a dir attribute to determine direction of text (BiDi)
+	// Stops if a value is found and returns true if the attribute is set to "ltr".  If none is found,
+	// the default direction is left-to-right, so true is returned.
+	for(; node; node = node.parentNode){
+		if(node.dir){
+			return node.dir == "ltr";
+		}
+	}
+	return true; // Boolean
+}
