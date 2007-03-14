50c036edbe8fd07d6cad930467324a12a36aa4f8
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-03-14 00:47:46 +0000

    (merge from trunk) Fixes #2583
    git-svn-id: http://svn.dojotoolkit.org/src@7583 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/0.4/src/html/style.js
+++ branches/0.4/src/html/style.js
@@ -316,7 +316,7 @@ if (dojo.render.html.ie) {
 		// summary
 		//  Extract value in pixels from styleValue (IE version).
 		//  If a value cannot be extracted, zero is returned.
-		if(!styleValue){ return 0; }
+		if(!styleValue){return 0;}
 		if(styleValue.slice(-2) == 'px'){return parseFloat(styleValue);}
 		var pixelValue = 0;
 		with(element){
@@ -342,7 +342,7 @@ if (dojo.render.html.ie) {
 		// summary
 		//  Extract value in pixels from styleValue (non-IE version).
 		//  If a value cannot be extracted, zero is returned.
-		return (styleValue.slice(-2) == 'px' ? parseFloat(styleValue) : 0); // Number
+		return (styleValue && (styleValue.slice(-2)=='px') ? parseFloat(styleValue) : 0); // Number
 	}
 }
 
