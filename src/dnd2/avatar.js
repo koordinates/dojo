2f2ffe1f0d35bf5feeaebaef3eba5b6e80009b00
elazutkin <elazutkin@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-03-21 03:15:49 +0000

    dnd2: switch all class names to camelCase, cosmetic changes
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@7724 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/dnd2/avatar.js
+++ src/dnd2/avatar.js
@@ -11,12 +11,12 @@ dojo.dnd2.Avatar = function(manager){
 dojo.extend(dojo.dnd2.Avatar, {
 	construct: function(){
 		var a = dojo.doc().createElement("table");
-		dojo.html.addClass(a, "dojo_dnd_avatar");
+		dojo.html.addClass(a, "dojoDndAvatar");
 		a.style.position = "absolute";
 		a.style.zIndex = 999;
 		var b = dojo.doc().createElement("tbody");
 		var tr = dojo.doc().createElement("tr");
-		dojo.html.addClass(tr, "dojo_dnd_avatar_header");
+		dojo.html.addClass(tr, "dojoDndAvatarHeader");
 		var td = dojo.doc().createElement("td");
 		td.innerHTML = this._generateText();
 		tr.appendChild(td);
@@ -25,7 +25,7 @@ dojo.extend(dojo.dnd2.Avatar, {
 		var k = Math.min(5, this.manager.nodes.length);
 		for(var i = 0; i < k; ++i){
 			tr = dojo.doc().createElement("tr");
-			dojo.html.addClass(tr, "dojo_dnd_avatar_item");
+			dojo.html.addClass(tr, "dojoDndAvatarItem");
 			td = dojo.doc().createElement("td");
 			var t = this.manager.source.node_creator(this.manager.source.map[this.manager.nodes[i].id].data, "avatar");
 			td.appendChild(t.node);
@@ -41,12 +41,12 @@ dojo.extend(dojo.dnd2.Avatar, {
 		this.node = false;
 	},
 	update: function(){
-		dojo.html[(this.manager.can_drop ? "add" : "remove") + "Class"](this.node, "dojo_dnd_avatar_can_drop");
+		dojo.html[(this.manager.can_drop ? "add" : "remove") + "Class"](this.node, "dojoDndAvatarCanDrop");
 		// replace text
 		var t = this.node.getElementsByTagName("td");
 		for(var i = 0; i < t.length; ++i){
 			var n = t[i];
-			if(dojo.html.hasClass(n.parentNode, "dojo_dnd_avatar_header")){
+			if(dojo.html.hasClass(n.parentNode, "dojoDndAvatarHeader")){
 				n.innerHTML = this._generateText();
 				break;
 			}
--- src/dnd2/selector.js
+++ src/dnd2/selector.js
@@ -9,8 +9,8 @@ dojo.require("dojo.dnd2.container");
 /*
 	Container item states:
 		""			- an item is not selected
-		"selected"	- an item is selected
-		"anchor"	- an item is selected, and is an anchor for a "shift" selection
+		"Selected"	- an item is selected
+		"Anchor"	- an item is selected, and is an anchor for a "shift" selection
 */
 
 dojo.declare("dojo.dnd2.Selector", dojo.dnd2.Container, 
@@ -41,7 +41,7 @@ function(node, filter, creator, singular){
 				for(var i in this.selection){
 					if(!(i in empty)){
 						var n = dojo.byId(i);
-						this.removeItemClass(n, "selected");
+						this.removeItemClass(n, "Selected");
 					}
 				}
 				this.selection = {};
@@ -54,7 +54,7 @@ function(node, filter, creator, singular){
 					if(this.node_filter(n)){ break; }
 				}
 				this.anchor = c[i];
-				this.addItemClass(this.anchor, "anchor");
+				this.addItemClass(this.anchor, "Anchor");
 			}
 			this.selection[this.anchor.id] = 1;
 			if(this.anchor != this.current){
@@ -68,45 +68,45 @@ function(node, filter, creator, singular){
 					var n = c[i];
 					if(!this.node_filter(n)){ continue; }
 					if(n == this.anchor || n == this.current){ break; }
-					this.addItemClass(n, "selected");
+					this.addItemClass(n, "Selected");
 					this.selection[n.id] = 1;
 				}
-				this.addItemClass(this.current, "selected");
+				this.addItemClass(this.current, "Selected");
 				this.selection[this.current.id] = 1;
 			}
 		}else{
 			if(this.singular){
 				if(this.anchor == this.current){
 					if(e.ctrlKey){
-						this.removeItemClass(this.anchor, "anchor");
+						this.removeItemClass(this.anchor, "Anchor");
 						this.anchor = null;
 						this.selection = {};
 					}
 				}else{
 					if(this.anchor){
-						this.removeItemClass(this.anchor, "anchor");
+						this.removeItemClass(this.anchor, "Anchor");
 					}
 					this.anchor = this.current;
-					this.addItemClass(this.anchor, "anchor");
+					this.addItemClass(this.anchor, "Anchor");
 					this.selection = {};
 					this.selection[this.current.id] = 1;
 				}
 			}else{
 				if(e.ctrlKey){
 					if(this.anchor == this.current){
-						this.removeItemClass(this.anchor, "anchor");
+						this.removeItemClass(this.anchor, "Anchor");
 						delete this.selection[this.anchor.id];
 						this.anchor = null;
 					}else{
 						if(this.current.id in this.selection){
-							this.removeItemClass(this.current, "selected");
+							this.removeItemClass(this.current, "Selected");
 							delete this.selection[this.current.id];
 						}else{
 							if(this.anchor){
-								dojo.html.replaceClass(this.anchor, "dojo_dnd_item_selected", "dojo_dnd_item_anchor");
+								dojo.html.replaceClass(this.anchor, "dojoDndItemSelected", "dojoDndItemAnchor");
 							}
 							this.anchor = this.current;
-							this.addItemClass(this.current, "anchor");
+							this.addItemClass(this.current, "Anchor");
 							this.selection[this.current.id] = 1;
 						}
 					}
@@ -115,15 +115,15 @@ function(node, filter, creator, singular){
 					for(var i in this.selection){
 						if(!(i in empty)){
 							var n = dojo.byId(i);
-							this.removeItemClass(n, "selected");
+							this.removeItemClass(n, "Selected");
 						}
 					}
 					if(this.anchor){
-						this.removeItemClass(this.anchor, "anchor");
+						this.removeItemClass(this.anchor, "Anchor");
 					}
 					this.selection = {};
 					this.anchor = this.current;
-					this.addItemClass(this.current, "anchor");
+					this.addItemClass(this.current, "Anchor");
 					this.selection[this.current.id] = 1;
 				}
 			}
@@ -139,7 +139,7 @@ function(node, filter, creator, singular){
 		this.selectNone();
 		if(this.current){
 			this.anchor = this.current;
-			this.addItemClass(this.anchor, "anchor");
+			this.addItemClass(this.anchor, "Anchor");
 			this.selection[this.current.id] = 1;
 		}
 	},
@@ -158,11 +158,11 @@ function(node, filter, creator, singular){
 		var empty = {};
 		for(var i in this.selection){
 			if(!(i in empty)){
-				this.removeItemClass(dojo.byId(i), "selected");
+				this.removeItemClass(dojo.byId(i), "Selected");
 			}
 		}
 		if(this.anchor){
-			this.removeItemClass(this.anchor, "anchor");
+			this.removeItemClass(this.anchor, "Anchor");
 			this.anchor = null;
 		}
 		this.selection = {};
@@ -170,14 +170,14 @@ function(node, filter, creator, singular){
 	},
 	selectAll: function(){
 		if(this.anchor){
-			this.removeItemClass(this.anchor, "anchor");
+			this.removeItemClass(this.anchor, "Anchor");
 			this.anchor = null;
 		}
 		var c = this.node.tagName.toLowerCase() == "table" ? this.parent.getElementsByTagName("tr") : this.node.childNodes;
 		for(var i = 0; i < c.length; ++i){
 			var n = c[i];
 			if(this.node_filter(n)){
-				this.addItemClass(n, "selected");
+				this.addItemClass(n, "Selected");
 				this.selection[n.id] = 1;
 			}
 		}
@@ -202,7 +202,7 @@ function(node, filter, creator, singular){
 			var _this = this;
 			this.node_creator = function(d){
 				var t = old_creator(d);
-				_this.addItemClass(t.node, "selected");
+				_this.addItemClass(t.node, "Selected");
 				_this.selection[t.node.id] = 1;
 				return t;
 			};
