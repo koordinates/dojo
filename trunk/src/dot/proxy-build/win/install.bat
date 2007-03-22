81104a704baf584a87b2587b328c4a30690f688a
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-03-22 06:53:08 +0000

    Updated install.bat with comments, made it more generic for other environments, and now copy everything to a c:\dot directory complete with the latest sources to make debugging on windows easier when using gdb
    git-svn-id: http://svn.dojotoolkit.org/src@7742 560b804f-0ae3-0310-86f3-f6aa0a117693

--- trunk/src/dot/proxy-build/win/install.bat
+++ trunk/src/dot/proxy-build/win/install.bat
@@ -1,8 +1,31 @@
+rem Move new distributions to a place on Windows we can run them.
+rem
+rem Define WINDOWS_BUILD to the full path to where your Windows build
+rem files are (such as z:\), and SOURCE_PATH to where your Dojo Offline
+rem local proxy source files are (such as y:dev\dojo\offline\trunk\src\dot\proxy).
+rem
+rem On my machine, Z:\ is a mapped network drive that actually maps to a Samba
+rem mounted drive, through Parallels, to a directory on my Mac OS X box
+rem (~/dev/dojo/offline/trunk/src/dot/proxy), while Y:\ is a mapped network 
+rem drive that maps through Samba and Parallels, to my home directory. Note
+rem that Windows-style network paths will not work for these variables, since
+rem DOS can not handle them (such as \\.psf\bradneuberg for example).
+rem
+rem This file will place all DOT configuration files in the root of C:, and
+rem will place the executables and source files into C:\dot
+rem  
+rem @author Brad Neuberg, bkn3@columbia.edu
+rem
+rem
+rmdir c:\dot
+mkdir c:\dot
 rmdir c:\.offline-cache
 del c:\.offline-list
 mkdir c:\.offline-cache
-copy dot.exe c:\
-copy config c:\
-copy offline-pac c:\offline-pac
+copy %WINDOWS_BUILD%\dot.exe c:\dot
+copy %WINDOWS_BUILD%\config c:\dot
+copy %WINDOWS_BUILD%\offline-pac c:\offline-pac
 move c:\offline-pac c:\.offline-pac
+copy %SOURCE_PATH%\*.c c:\dot
+copy %SOURCE_PATH%\*.h c:\dot
 
