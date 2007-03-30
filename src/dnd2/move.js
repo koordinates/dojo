844de5c92f8272c0239c24e3347651775b5b81e9
elazutkin <elazutkin@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-03-30 20:44:11 +0000

    dnd2: added the "move" --- move absolutely positioned nodes around with mouse; this functionality is required by widgets, and some custom code; note that it is completely separate from the general DnD2 code.
    git-svn-id: http://svn.dojotoolkit.org/src/trunk@7850 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/dnd2/move.js
@@ -0,0 +1,67 @@
+dojo.provide("dojo.dnd2.move");
+
+dojo.require("dojo.event.*");
+dojo.require("dojo.html.layout");
+
+dojo.dnd2.Mover = function(node, e){
+	this.node = node;
+	node.style.position = "absolute"; // enforcing the absolute mode
+	this.mouse_x = e.pageX;
+	this.mouse_y = e.pageY;
+	var h = dojo.html;
+	this.node_pos    = h.abs(node, true);
+	this.node_pos.x -= h.getMarginExtent(node, "left") + h.getBorderExtent(node, "left") + h.getPaddingExtent(node, "left");
+	this.node_pos.y -= h.getMarginExtent(node, "top")  + h.getBorderExtent(node, "top")  + h.getPaddingExtent(node, "top");
+	dojo.event.connect(dojo.doc(), "onmousemove", this, "onMouseMove");
+	dojo.event.connect(dojo.doc(), "onmouseup",   this, "onMouseUp");
+	// cancel text selection and text dragging
+	dojo.event.connect(dojo.doc(), "ondragstart",   dojo.event.browser, "stopEvent");
+	dojo.event.connect(dojo.doc(), "onselectstart", dojo.event.browser, "stopEvent");
+};
+
+dojo.extend(dojo.dnd2.Mover, {
+	// mouse event processors
+	onMouseMove: function(e){
+		var s = this.node.style;
+		s.left = (e.pageX - this.mouse_x + this.node_pos.x) + "px";
+		s.top  = (e.pageY - this.mouse_y + this.node_pos.y) + "px";
+	},
+	onMouseUp: function(e){
+		this.cancel();
+	},
+	// utilities
+	cancel: function(){
+		dojo.event.disconnect(dojo.doc(), "onmousemove", this, "onMouseMove");
+		dojo.event.disconnect(dojo.doc(), "onmouseup",   this, "onMouseUp");
+		dojo.event.disconnect(dojo.doc(), "ondragstart",   dojo.event.browser, "stopEvent");
+		dojo.event.disconnect(dojo.doc(), "onselectstart", dojo.event.browser, "stopEvent");
+		this.node = null;
+	}
+});
+
+dojo.dnd2.move = function(node, e){ return new dojo.dnd2.Mover(node, e); };
+
+dojo.dnd2.Moveable = function(node, handle){
+	if(!handle){ handle = node; }
+	this.node = node;
+	this.handle = handle;
+	dojo.event.connect(handle, "onmousedown", this, "onMouseDown");
+	// cancel text selection and text dragging
+	dojo.event.connect(handle, "ondragstart",   dojo.event.browser, "stopEvent");
+	dojo.event.connect(handle, "onselectstart", dojo.event.browser, "stopEvent");
+};
+
+dojo.extend(dojo.dnd2.Moveable, {
+	// mouse event processors
+	onMouseDown: function(e){
+		dojo.dnd2.move(this.node, e);
+		dojo.event.browser.stopEvent(e);
+	},
+	// utilities
+	cancel: function(){
+		dojo.event.disconnect(this.handle, "onmousedown", this, "onMouseDown");
+		dojo.event.disconnect(this.handle, "ondragstart",   dojo.event.browser, "stopEvent");
+		dojo.event.disconnect(this.handle, "onselectstart", dojo.event.browser, "stopEvent");
+		this.node = this.handle = null;
+	}
+});
