ff37a53e84e45374c2c50ea383fedd661e794f92
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-03 00:45:26 +0000

    Refs #2386. Create xd files for layered files.
    git-svn-id: http://svn.dojotoolkit.org/src@7911 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/0.4/buildscripts/makeDojoJs.js
+++ branches/0.4/buildscripts/makeDojoJs.js
@@ -1,12 +1,17 @@
 //START of the "main" part of the script.
 //This is the entry point for this script file.
 load("buildUtil.js");
+load("buildUtilXd.js");
 
 var profileFile = arguments[0];
 var releaseDir = arguments[1];
 var dojoFileName = arguments[2];
 var version = arguments[3];
 var lineSeparator = java.lang.System.getProperty("line.separator");
+
+var dependencyResult = buildUtil.loadDependencyList(profileFile);
+var isXDomain = (buildUtil.getDojoLoader(dependencyResult.dependencies) == "xdomain");
+var prefixes = dependencyResult.dependencies.prefixes || [];
 
 var result = buildUtil.makeDojoJs(buildUtil.loadDependencyList(profileFile), version);
 
@@ -16,10 +21,18 @@ buildUtil.saveFile(releaseDir + "/" + dojoFileName, result[0].contents);
 //Save the other layers, if there are any.
 for(var i = 1; i < result.length; i++){
 	var layerName = releaseDir + "/" + result[i].layerName;
+	var layerContents = result[i].contents;
 	var uncompressedLayerName = layerName + ".uncompressed.js";
 
-	buildUtil.saveFile(uncompressedLayerName, result[i].contents);
+	buildUtil.saveFile(uncompressedLayerName, layerContents);
 	
+	//If doing xd build, save an uncompressed xd file.
+	if(isXDomain){
+		var xdUncompressedFileName = uncompressedLayerName.replace(/\.js$/, ".xd.js");
+		var xdContents = buildUtilXd.makeXdContents(layerContents, "../", prefixes);
+		buildUtil.saveFile(xdUncompressedFileName, xdContents);
+	}
+
 	//Compress the layer files. It is clunky to do it here and have dojo.js
 	//compression done elsewhere. This will be fixed in 0.9 re-org.
 	print("Compressing file: " + layerName);
@@ -32,6 +45,13 @@ for(var i = 1; i < result.length; i++){
 	
 	//Save the optimized file.
 	buildUtil.saveFile(layerName, fileContents);
+
+	//If xdomain, optimize the xd.js file too. 
+	if(isXDomain){
+		xdContents = new String(buildUtil.readFile(xdUncompressedFileName));
+		xdContents = buildUtil.optimizeJs(xdUncompressedFileName, xdContents, copyright, true);
+		buildUtil.saveFile(layerName.replace(/\.js$/, ".xd.js"), xdContents);
+	}
 }
 
 //Save the dependency list to build.txt
