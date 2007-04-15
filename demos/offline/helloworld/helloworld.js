ba11698d0f470f9103658e746fbebc1997f69cd8
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-15 02:19:36 +0000

    Building up Hello World example
    git-svn-id: http://svn.dojotoolkit.org/src/branches/dot@8117 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ demos/offline/helloworld/helloworld.js
@@ -0,0 +1,33 @@
+dojo.require("dojo.storage.*");
+dojo.require("dojo.off.*");
+dojo.require("dojo.off.ui");
+dojo.require("dojo.sync");
+
+// configure how we should work offline
+
+// set our application name
+dojo.off.ui.appName = "Hello World";
+
+// add our list resources we need offline
+// Hello World resources
+dojo.off.files.cache([
+					"helloworld.html",
+					"helloworld.js"
+					]);
+
+// Dojo resources
+dojo.off.files.cache([
+					djConfig.baseRelativePath + "dojo.js"
+					]);
+					
+var HelloWorld = {
+	initialize: function(){
+		dojo.debug("Dojo Offline and the page are finished initializing");
+	}
+}
+					
+// Wait until Dojo Offline and the default Offline Widget are ready
+// before we initialize ourselves. When this gets called the page
+// is also finished loading.
+dojo.off.ui.onLoad = dojo.lang.hitch(HelloWorld, HelloWorld.initialize);
+					
