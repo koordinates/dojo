7a9c8920e56e5b9b4638da7a97b0d9259ee19b5b
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-15 02:20:56 +0000

    Baseline DOT profile until we can work to make it smaller and have less dependencies
    git-svn-id: http://svn.dojotoolkit.org/src/branches/dot@8120 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ buildscripts/profiles/off.profile.js
@@ -0,0 +1,17 @@
+var dependencies = [ 
+	"dojo.string.*",
+	"dojo.uri.*",
+	"dojo.html.common",
+	"dojo.io.*",
+	"dojo.event.*",
+	"dojo.lang.*",
+	"dojo.storage",
+	"dojo.storage.browser",
+	"dojo.flash",
+	"dojo.json",
+	"dojo.off.*",
+	"dojo.off.ui",
+	"dojo.sync"
+];
+
+load("getDependencyList.js");
