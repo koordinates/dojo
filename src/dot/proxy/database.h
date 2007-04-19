a428e7582050bc7fec116a3f1dabb1431cd15dbc
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-19 10:28:09 +0000

    SQLite experimentation from a few weeks ago that I hadn't checked in
    git-svn-id: http://svn.dojotoolkit.org/src/branches/dot@8160 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ src/dot/proxy/database.h
@@ -0,0 +1,18 @@
+#ifndef __database_h
+
+/**
+	This file contains our local SQL data
+	cache, one for each offline host.
+	
+	Author: Brad Neuberg, bkn3@columbia.edu
+*/
+
+void preinitDatabase(void);
+
+/**
+	Initializes the offline database support.
+*/
+void initDatabase(void);
+
+
+#endif /* __database_h */
