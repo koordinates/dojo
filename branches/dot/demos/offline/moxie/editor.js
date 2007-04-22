b98f49e2872d3212c86f8683a358d90c3fcf5b01
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-22 00:16:37 +0000

    Fix for 'disappearing toolbar' that can occur on IE 7 when offline occassionally
    git-svn-id: http://svn.dojotoolkit.org/src@8233 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/dot/demos/offline/moxie/editor.js
+++ branches/dot/demos/offline/moxie/editor.js
@@ -36,6 +36,11 @@ dojo.off.files.cache([
 					djConfig.baseRelativePath + "src/widget/templates/images/toolbar-bg.gif",
 					djConfig.baseRelativePath + "src/widget/templates/buttons/aggregate.gif"
 					]);
+					
+// work-around for bug in Dojo Editor Widget
+dojo.off.files.cache([
+					djConfig.baseRelativePath + "demos/src/widget/templates/buttons/aggregate.gif"
+					]);
 
 var Moxie = {
 	_availableKeys: null,
