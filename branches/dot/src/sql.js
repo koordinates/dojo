678e048d3a1a5a585bf6dd88f56eb3285c4fec91
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-27 09:39:12 +0000

    We now send our SQL to the local proxy through the dojo.sql.exec() method
    git-svn-id: http://svn.dojotoolkit.org/src@8319 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/dot/src/sql.js
+++ branches/dot/src/sql.js
@@ -3,10 +3,56 @@ dojo.provide("dojo.sql");
 dojo.require("dojo.off");
 
 dojo.lang.mixin(dojo.sql, {
+	_iframe: null,
+	_postForm: null,
+	_postSql: null,
+	
 	exec: function(sql){
 		dojo.debug("dojo.sql.exec, sql="+sql);
+	
+		if(this._iframe == null){
+			this._createIframe();
+		}
 		
-		//dojo.off._talkToOfflineCache("execSQL", dojo.lang.hitch(this, this._execResults));
+		this._postSql.value = sql;
+		this._postForm.submit();
+	},
+	
+	_createIframe: function(){
+		var f = document.createElement("iframe");
+		f.style.position = "absolute";
+		f.style.top = "-1000px";
+		f.style.left = "-1000px";
+		
+		var body = document.getElementsByTagName("body")[0];
+		body.appendChild(f);
+		
+		// write a form out into the iframe that we will
+		// use to post SQL to the local proxy and get results
+		// back
+		var contents = 
+						  "<html>"
+						+ "<body>"
+						+ "<form id='postForm' method='POST' action='/__polipo/__offline?execSQL'>"
+						+ "<textarea id='sql' name='sql'></textarea>"
+						+ "</form>"
+						+ "</body>"
+						+ "</html>";
+		
+		var doc = null;
+		if(f.contentDocument){ // Mozilla
+    		doc = f.contentDocument; 
+		}else if(f.contentWindow){ // IE
+			doc = f.contentWindow.document;
+		}
+		
+		doc.open();
+		doc.write(contents);
+		doc.close();
+		
+		this._iframe = f;
+		this._postForm = doc.getElementById("postForm");
+		this._postSql = doc.getElementById("sql");
 	},
 	
 	_execResults: function(name, results){
