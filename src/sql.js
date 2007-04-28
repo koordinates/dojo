53d10ff175851d8ca6a9e5fecbb4aae6e1272915
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-28 07:48:53 +0000

    Calls to dojo.sql.exec() can now be synchronous or asychronous -- we automatically determine which one based on whether there is a callback or not. This can make coding easier by just doing everything synchronous, or for more advanced programmers who have large datasets can make the call asychronous by adding a callback. Refactored open() and close() JavaScript methods as well
    git-svn-id: http://svn.dojotoolkit.org/src/branches/dot@8338 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/sql.js
+++ src/sql.js
@@ -7,75 +7,90 @@ dojo.lang.mixin(dojo.sql, {
 	_DB_URL: "/__polipo/__offline",
 	
 	open: function(){ /* boolean */
-		var bindArgs = {
-			url:	 	this._DB_URL + "?open",
-			sync:		true,
-			mimetype:	"text/javascript",
-			error:		function(type, errObj){
-				var msg = "Unable to open database: "
-						+ errObj.message;
-				throw msg;
-			},
-			load:		function(type, data, evt){
-				if(data == true){
-					return true;
-				}else{
-					return false;
-				}
-			}
-		};
-		
-		// dispatch the request
-		dojo.io.bind(bindArgs);
+		return this._openClose(this._DB_URL + "?open");
 	},
 	
 	close: function(){ /* boolean */
-		var bindArgs = {
-			url:	 	this._DB_URL + "?close",
-			sync:		true,
-			mimetype:	"text/javascript",
-			error:		function(type, errObj){
-				var msg = "Unable to open database: "
-						+ errObj.message;
-				throw msg;
-			},
-			load:		function(type, data, evt){
-				if(data == true){
-					return true;
-				}else{
-					return false;
-				}
-			}
-		};
-		
-		// dispatch the request
-		dojo.io.bind(bindArgs);
+		return this._openClose(this._DB_URL + "?close");
 	},
 	
 	exec: function(sql /* string */, 
 					callback /* function(resultSet, sql, errMessage) */ ){ /* void */
-		dojo.debug("dojo.sql.exec, sql="+sql);
+		var sync = false;
+		if(typeof callback == "undefined" || callback == null){
+			sync = true;
+		}
+		
+		var results = {hasError: false, errorMsg: null, returnMe: null};
 		
 		var requestArgs = {
 			sql: sql
 		};
+		
 		var bindArgs = {
 			url:	 	this._DB_URL + "?execSQL",
 			method: 	"post",
-			sync:		false,
+			sync:		sync,
 			content: 	requestArgs, 
 			mimetype:	"text/javascript",
 			error:		function(type, errObj){
 				var msg = "Unable to execute SQL: "
 						+ sql + ": " + errObj.message;
-				callback(null, sql, msg);
+				if(sync == false){
+					callback(null, sql, msg);
+				}else{
+					results.hasError = true;
+					results.errorMsg = msg;
+				}
 			},
 			load:		function(type, data, evt){
-				callback(data, sql, null);
+				if(sync == false){
+					callback(data, sql, null);
+				}else{
+					results.returnMe = data;
+				}	
 			}
 		};
 		
 		// dispatch the request
 		dojo.io.bind(bindArgs);
+		
+		if(results.hasError){
+			throw results.errorMsg;
+		}
+		
+		return results.returnMe;
+	},
+	
+	_openClose: function(url){
+				var results = {hasError: false, errorMsg: null, returnMe: null};
+		
+		var bindArgs = {
+			url:	 	url,
+			sync:		true,
+			mimetype:	"text/javascript",
+			error:		function(type, errObj){
+				var msg = "Unable to open database: "
+						+ errObj.message;
+				results.hasError = true;
+				results.errorMsg = msg;
+			},
+			load:		function(type, data, evt){
+				if(data == true){
+					results.returnMe = true;
+				}else{
+					results.returnMe = false;
+				}
+			}
+		};
+		
+		// dispatch the request
+		dojo.io.bind(bindArgs);
+		
+		if(results.hasError){
+			throw results.errorMsg;
+		}
+		
+		return results.returnMe;
 	}
 });
