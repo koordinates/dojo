a2028ee565440e0bd05d65ad70578b5d7063ac6a
BradNeuberg <BradNeuberg@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-04-28 09:04:05 +0000

    Big refactoring: open SQLite databases can't be shared with a forked child process, so we have to open and close the database on each call to prevent thread errors; this simplifies the API for the programmer, who doesn't have to call dojo.sql.open() or dojo.sql.close() anymore which have been removed. Also fixed a bug where we weren't null terminating a string causing a memory overflow bug, and fixed a bug where we weren't correctly aborting the request if an error occured
    git-svn-id: http://svn.dojotoolkit.org/src@8340 560b804f-0ae3-0310-86f3-f6aa0a117693

--- branches/dot/src/dot/proxy/database.c
+++ branches/dot/src/dot/proxy/database.c
@@ -2,11 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "../sqlite/build/sqlite3.h"
-
 #include "polipo.h"
-
-sqlite3* db = NULL;
 
 int first_row;
 
@@ -16,14 +12,14 @@ char *escapeJSON(const char *escapeMe) {
     return (char *)escapeMe;
 }
 
-int openDb(char *host) {
+int openDb(sqlite3 **db, const char *host) {
     int status;
     
     do_log(L_INFO, "Opening database for host %s\n", host);
     
-    status = sqlite3_open("./countries.db", &db);
+    status = sqlite3_open("./countries.db", db);
 
-    if(db == NULL || db == 0 || status != SQLITE_OK) {
+    if(*db == NULL || *db == 0 || status != SQLITE_OK) {
         do_log(L_ERROR, "Could not open database\n");
         return 1;
     }
@@ -31,7 +27,7 @@ int openDb(char *host) {
     return 0;
 }
 
-int closeDb(char *host) {
+int closeDb(sqlite3 *db, const char *host) {
     int status;
     
     do_log(L_INFO, "Closing database for host %s\n", host);
@@ -46,8 +42,9 @@ int closeDb(char *host) {
     return 0;
 }
 
-void execSQL(const char *sql, ObjectPtr object) {
+int execSQL(const char *host, const char *sql, ObjectPtr object) {
     char buffer[1024];
+    sqlite3* db = NULL;
     sqlite3_stmt *ppStmt;
     const char *errmsg;
     const char *colName;
@@ -55,6 +52,13 @@ void execSQL(const char *sql, ObjectPtr object) {
     int result, numCols, i;
     int firstRow = 1;
     int offset = 0;
+    int status;
+    
+    status = openDb(&db, host);
+    if(status == 1) {
+        abortObject(object, 500, internAtomF("Unable to open database for host %s\n", host));  
+        return 1;
+    }
 
     do_log(L_INFO, "Executing SQL: %s\n", sql);
 
@@ -63,8 +67,11 @@ void execSQL(const char *sql, ObjectPtr object) {
     
     if(result != SQLITE_OK) {
         errmsg = sqlite3_errmsg(db);
-        do_log(L_ERROR, "Error in select statement %s [%s].\n", sql, errmsg);
-        return;
+        do_log(L_ERROR, "Error in select statement: %s: %s\n", sql, errmsg);
+        abortObject(object, 400, 
+                    internAtomF("Error in select statement %s: %s\n", sql, errmsg));
+        closeDb(db, host);
+        return 1;
     }
     
     objectPrintf(object, offset, "[\n");
@@ -123,6 +130,14 @@ void execSQL(const char *sql, ObjectPtr object) {
     object->message = internAtom("Okay");
     object->code = 200;
     object->flags &= ~OBJECT_INITIAL;
+    
+    status = closeDb(db, host);
+    if(status == 1) {
+        abortObject(object, 500, internAtomF("Unable to close database for host %s\n", host));  
+        return 1;
+    }
+    
+    return 0;
 }
 
 void preinitDatabase(void){
