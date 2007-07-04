2a91884442c07f4bcab2e282071e066f1fe9a260
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2007-07-04 00:38:23 +0000

    Creating tag for 0.9.0beta release. This is the dojox part of the tag
    git-svn-id: http://svn.dojotoolkit.org/src@9462 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ tags/release-0.9.0beta/dojox/data/tests/module.js
@@ -0,0 +1,12 @@
+dojo.provide("dojox.tests.module");
+
+try{
+	dojo.require("dojox.data.tests.stores.CsvStore");
+	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.stores.HtmlTableStore");
+	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.stores.OpmlStore");
+	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.stores.XmlStore");
+	dojo.requireIf(dojo.isBrowser, "dojox.data.tests.dom");
+}catch(e){
+	doh.debug(e);
+}
+
