abf336b027f7d7fc77a9337338f0ed80aa3c7bbd
bill <bill@560b804f-0ae3-0310-86f3-f6aa0a117693>
2008-03-05 02:03:19 +0000

    Using r12880 to create a tag for the 1.1.0b3 release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.1.0b3@12884 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dojox/rpc/tests/module.js
@@ -0,0 +1,10 @@
+dojo.provide("dojox.rpc.tests.module");
+
+try{
+	dojo.require("dojox.rpc.tests.Service");
+	dojo.require("dojox.rpc.tests.JsonReferencing");
+	dojo.require("dojox.rpc.tests.Yahoo");
+}catch(e){
+	doh.debug(e);
+}
+
