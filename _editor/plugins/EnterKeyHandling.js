koordinates/dojo

commit 09feca53bb151dafe62703265543523ea20cddf0

fixes #6372: remove trailing br in LI elements in FF minor cleanup in RichText
git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13257 560b804f-0ae3-0310-86f3-f6aa0a117693

_editor/plugins/EnterKeyHandling.js
@@ -54,7 +54,7 @@ dojo.declare("dijit._editor.plugins.EnterKeyHandling", dijit._editor._Plugin, {
 			if(dojo.withGlobal(this.editor.window, 'isCollapsed', dijit)){
 				if(!dojo.withGlobal(this.editor.window, 'hasAncestorElement', dijit._editor.selection, ['LI'])){
 					//circulate the undo detection code by calling RichText::execCommand directly
-					dijit._editor.RichText.prototype.execCommand.apply(this.editor, ['formatblock',this.blockNodeForEnter]);
+					dijit._editor.RichText.prototype.execCommand.call(this.editor, 'formatblock',this.blockNodeForEnter);
 					//set the innerHTML of the new block node
 					var block = dojo.withGlobal(this.editor.window, 'getAncestorElement', dijit._editor.selection, [this.blockNodeForEnter]);
 					if(block){
@@ -77,7 +77,8 @@ dojo.declare("dijit._editor.plugins.EnterKeyHandling", dijit._editor._Plugin, {
 				}
 			}
 			this._checkListLater = false;
-		}else if(this._pressedEnterInBlock){
+		}
+		if(this._pressedEnterInBlock){
 			//the new created is the original current P, so we have previousSibling below
 			this.removeTrailingBr(this._pressedEnterInBlock.previousSibling);
 			delete this._pressedEnterInBlock;
@@ -134,6 +135,10 @@ dojo.declare("dijit._editor.plugins.EnterKeyHandling", dijit._editor._Plugin, {
 		var block = dijit.range.getBlockAncestor(range.endContainer, null, this.editor.editNode);
 
 		if((this._checkListLater = (block.blockNode && block.blockNode.tagName == 'LI'))){
+		    if(dojo.isMoz){
+				//press enter in middle of P may leave a trailing <br/>, let's remove it later
+				this._pressedEnterInBlock = block.blockNode;
+			}
 			return true;
 		}
 

_editor/RichText.js
@@ -1418,11 +1418,10 @@ dojo.declare("dijit._editor.RichText", dijit._Widget, {
 			.replace(/<(\/)?em([ \>])/gi, '<$1i$2' ); // String
 	},
 
-	_srcInImgRegex	: /(?:(<img(?=\s).*?\ssrc=)("|')(.*?)\2)|(?:(<img\s.*?src=)([^"'][^ >]+))/gi ,
-	_hrefInARegex	: /(?:(<a(?=\s).*?\shref=)("|')(.*?)\2)|(?:(<a\s.*?href=)([^"'][^ >]+))/gi ,
-
 	_preFixUrlAttributes: function(/* String */ html){
-		return html.replace(this._hrefInARegex, '$1$4$2$3$5$2 _djrealurl=$2$3$5$2')
-			.replace(this._srcInImgRegex, '$1$4$2$3$5$2 _djrealurl=$2$3$5$2'); // String
+		return html.replace(/(?:(<img(?=\s).*?\ssrc=)("|')(.*?)\2)|(?:(<img\s.*?src=)([^"'][^ >]+))/gi, 
+		        '$1$4$2$3$5$2 _djrealurl=$2$3$5$2')
+			.replace(/(?:(<a(?=\s).*?\shref=)("|')(.*?)\2)|(?:(<a\s.*?href=)([^"'][^ >]+))/gi,
+			    '$1$4$2$3$5$2 _djrealurl=$2$3$5$2'); // String
 	}
 });

commit 2d7f41884c4f70e2f7c7c388c377344a63c9faf5

refs #6372: fixed a regression introduced in r13257 (when user press enter twice in a list, an error occurs)
git-svn-id: http://svn.dojotoolkit.org/src/dijit/trunk@13263 560b804f-0ae3-0310-86f3-f6aa0a117693

_editor/plugins/EnterKeyHandling.js
@@ -80,7 +80,9 @@ dojo.declare("dijit._editor.plugins.EnterKeyHandling", dijit._editor._Plugin, {
 		}
 		if(this._pressedEnterInBlock){
 			//the new created is the original current P, so we have previousSibling below
-			this.removeTrailingBr(this._pressedEnterInBlock.previousSibling);
+			if(this._pressedEnterInBlock.previousSibling){
+			    this.removeTrailingBr(this._pressedEnterInBlock.previousSibling);
+			}
 			delete this._pressedEnterInBlock;
 		}
 	},