2fe7ed1a68a591a48658e0a0d61da0a507d7abd4
ktiedt <ktiedt@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-04-02 17:22:30 +0000

    Using r17178 to create a tag for the  release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-@17182 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dojox/date/tests/module.js
@@ -0,0 +1,10 @@
+dojo.provide("dojox.date.tests.module");
+
+try{
+	dojo.require("dojox.date.tests.hebrew.Date");
+	dojo.require("dojox.date.tests.IslamicDate");
+	dojo.require("dojox.date.tests.posix");
+}catch(e){
+	doh.debug(e);
+}
+
