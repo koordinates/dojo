7cfa3ab68606b42937a286456ad85666d023f497
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-10 20:38:39 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@17848 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/analytics/plugins/idle.js
+++ dojox/analytics/plugins/idle.js
@@ -1,30 +1,31 @@
 dojo.provide("dojox.analytics.plugins.idle");
 
 // window startup data
-dojox.analytics.plugins.idle = new (function(){
+dojox.analytics.plugins.idle = (function(){
 	this.addData = dojo.hitch(dojox.analytics, "addData", "idle");
-	this.idleTime=dojo.config["idleTime"] || 60000;
+	this.idleTime=dojo.config.idleTime || 60000;
 	this.idle=true;
 
 	this.setIdle = function(){
 		this.addData("isIdle");
 		this.idle=true;
 
-	}
+	};
 	
 	dojo.addOnLoad(dojo.hitch(this, function(){
 		var idleResets=["onmousemove","onkeydown","onclick","onscroll"];
+		var fn = function(e){
+			if (this.idle){
+				this.idle=false;
+				this.addData("isActive");
+				this.idleTimer=window.setTimeout(dojo.hitch(this,"setIdle"), this.idleTime);
+			}else{
+				window.clearTimeout(this.idleTimer);
+				this.idleTimer=window.setTimeout(dojo.hitch(this,"setIdle"), this.idleTime);
+			}
+		};
 		for (var i=0;i<idleResets.length;i++){
-			dojo.connect(dojo.doc,idleResets[i],this, function(e){ 
-				if (this.idle){
-					this.idle=false;
-					this.addData("isActive");
-					this.idleTimer=setTimeout(dojo.hitch(this,"setIdle"), this.idleTime);
-				}else{
-					clearTimeout(this.idleTimer);
-					this.idleTimer=setTimeout(dojo.hitch(this,"setIdle"), this.idleTime);
-				}
-			});
+			dojo.connect(dojo.doc,idleResets[i],this, fn);
 		}
 	}));
 })();
