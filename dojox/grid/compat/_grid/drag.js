7d595caedc775762efd745cc95d81cff7f1a6c09
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-20 00:25:30 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18155 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/grid/compat/_grid/drag.js
+++ dojox/grid/compat/_grid/drag.js
@@ -2,41 +2,39 @@ dojo.provide("dojox.grid.compat._grid.drag");
 
 // summary:
 //	utility functions for dragging as used in grid.
-// begin closure
+
 (function(){
 
-var dgdrag = dojox.grid.drag = {};
-
-dgdrag.dragging = false;
-dgdrag.hysteresis = 2;
+var dgdrag = dojox.grid.drag = { dragging:false, hysteresis: 2 };
 
 dgdrag.capture = function(inElement) {
-	//console.debug('dojox.grid.drag.capture');
-	if (inElement.setCapture)
-		inElement.setCapture();
+	if (inElement.setCapture) {
+		inElement.setCapture();}
 	else {
-		document.addEventListener("mousemove", inElement.onmousemove, true);
-		document.addEventListener("mouseup", inElement.onmouseup, true);
-		document.addEventListener("click", inElement.onclick, true);
+		dojo.doc.addEventListener("mousemove", inElement.onmousemove, true);
+		dojo.doc.addEventListener("mouseup", inElement.onmouseup, true);
+		dojo.doc.addEventListener("click", inElement.onclick, true);
 	}
-}
+};
 
 dgdrag.release = function(inElement) {
 	//console.debug('dojox.grid.drag.release');
 	if(inElement.releaseCapture){
 		inElement.releaseCapture();
 	}else{
-		document.removeEventListener("click", inElement.onclick, true);
-		document.removeEventListener("mouseup", inElement.onmouseup, true);
-		document.removeEventListener("mousemove", inElement.onmousemove, true);
+		dojo.doc.removeEventListener("click", inElement.onclick, true);
+		dojo.doc.removeEventListener("mouseup", inElement.onmouseup, true);
+		dojo.doc.removeEventListener("mousemove", inElement.onmousemove, true);
 	}
-}
+};
 
 dgdrag.start = function(inElement, inOnDrag, inOnEnd, inEvent, inOnStart){
-	if(/*dgdrag.elt ||*/ !inElement || dgdrag.dragging){
+	if(/*dgdrag.elt ||*/ !inElement || dojo.grid.drag.dragging){
 		console.debug('failed to start drag: bad input node or already dragging');
 		return;
 	}
+	var dgdrag = dojo.grid.drag;
+
 	dgdrag.dragging = true;
 	dgdrag.elt = inElement;
 	dgdrag.events = {
@@ -54,10 +52,11 @@ dgdrag.start = function(inElement, inOnDrag, inOnEnd, inEvent, inOnStart){
 	inElement.onmouseup = dgdrag.mouseup;
 	inElement.onclick = dgdrag.click;
 	dgdrag.capture(dgdrag.elt);
-}
+};
 
 dgdrag.end = function(){
-	//console.debug("dojox.grid.drag.end");
+	var dgdrag = dojo.grid.drag;
+
 	dgdrag.release(dgdrag.elt);
 	dgdrag.elt.onmousemove = dgdrag.events.oldmove;
 	dgdrag.elt.onmouseup = dgdrag.events.oldup;
@@ -70,18 +69,24 @@ dgdrag.end = function(){
 	}finally{
 		dgdrag.dragging = false;
 	}
-}
+};
 
 dgdrag.calcDelta = function(inEvent){
+	var dgdrag = dojo.grid.drag;
+
 	inEvent.deltaX = inEvent.screenX - dgdrag.positionX;
 	inEvent.deltaY = inEvent.screenY - dgdrag.positionY;
-}
+};
 
 dgdrag.hasMoved = function(inEvent){
+	var dgdrag = dojo.grid.drag;
+
 	return Math.abs(inEvent.deltaX) + Math.abs(inEvent.deltaY) > dgdrag.hysteresis;
-}
+};
 
 dgdrag.mousemove = function(inEvent){
+	var dgdrag = dojo.grid.drag;
+
 	inEvent = dojo.fixEvent(inEvent);
 	dojo.stopEvent(inEvent);
 	dgdrag.calcDelta(inEvent);
@@ -92,18 +97,17 @@ dgdrag.mousemove = function(inEvent){
 	if(dgdrag.started){
 		dgdrag.events.drag(inEvent);
 	}
-}
+};
 
 dgdrag.mouseup = function(inEvent){
-	//console.debug("dojox.grid.drag.mouseup");
 	dojo.stopEvent(dojo.fixEvent(inEvent));
-	dgdrag.end();
-}
+	dojo.grid.drag.end();
+};
 
 dgdrag.click = function(inEvent){
 	dojo.stopEvent(dojo.fixEvent(inEvent));
-	//dgdrag.end();
-}
+};
+
+dgdrag = null;
 
 })();
-// end closure
