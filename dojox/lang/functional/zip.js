05d99bd8178023f1bd9837475e3169fe01f27e42
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-21 20:18:47 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18238 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/lang/functional/zip.js
+++ dojox/lang/functional/zip.js
@@ -19,10 +19,10 @@ dojo.provide("dojox.lang.functional.zip");
 			//	array is truncated to match the length of the shortest input
 			//	array.
 			var n = arguments[0].length, m = arguments.length, i = 1, t = new Array(n), j, p;
-			for(; i < m; n = Math.min(n, arguments[i++].length));
+			for(; i < m; n = Math.min(n, arguments[i++].length)) {}
 			for(i = 0; i < n; ++i){
 				p = new Array(m);
-				for(j = 0; j < m; p[j] = arguments[j][i], ++j);
+				for(j = 0; j < m; p[j] = arguments[j][i], ++j) {}
 				t[i] = p;
 			}
 			return t;	// Array
