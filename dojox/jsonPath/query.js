a18dc6a1ea6914aaf3f9d6dadd814e880bc2fec0
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-21 19:35:33 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18225 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/jsonPath/query.js
+++ dojox/jsonPath/query.js
@@ -9,13 +9,11 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 	//		resultType: "VALUE"||"BOTH"||"PATH"} (defaults to value)
 	//		evalType: "RESULT"||"ITEM"} (defaults to ?)
 
-	var re = dojox.jsonPath._regularExpressions;
+	var $, loc;
 	if (!arg){arg={};}
 
 	var strs = [];
-	function _str(i){ return strs[i];}
-	var acc;
-	if (arg.resultType == "PATH" && arg.evalType == "RESULT") throw Error("RESULT based evaluation not supported with PATH based results");
+	if (arg.resultType == "PATH" && arg.evalType == "RESULT") { throw new Error("RESULT based evaluation not supported with PATH based results"); }
 	var P = {
 		resultType: arg.resultType || "VALUE",
 		normalize: function(expr){
@@ -33,7 +31,7 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 			ll = -1;
 			while(ll!=expr){
 				ll=expr;
-					 expr = expr.replace(/#([0-9]+)/g, function($0,$1){return subx[$1];});
+				expr = expr.replace(/#([0-9]+)/g, function($0,$1){return subx[$1];});
 			}
 			return expr.split(";");
 		},
@@ -41,8 +39,9 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 			for (var j=0;j<paths.length;j++){
 			var p = "$";
 			var x= paths[j];
-			for (var i=1,n=x.length; i<n; i++)
+			for (var i=1,n=x.length; i<n; i++) {
 				p += /^[0-9*]+$/.test(x[i]) ? ("["+x[i]+"]") : ("['"+x[i]+"']");
+			}
 			paths[j]=p;
 		  }
 			return paths;
@@ -51,12 +50,14 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 			var path = ['$'];
 			var result=rb?val:[val];
 			var paths=[path];
-			function add(v, p,def){
-			  if (v && v.hasOwnProperty(p) && P.resultType != "VALUE") paths.push(path.concat([p]));
-				if (def) 
-				  result = v[p];
-			  else if (v && v.hasOwnProperty(p))  
-					result.push(v[p]);
+			function add(v, p, def){
+			  if (v && dojo.isOwnProperty(v, p) && P.resultType != "VALUE") { paths.push(path.concat([p])); }
+			  if (def) {
+				result = v[p];
+		          }
+			  else if (v && dojo.isOwnProperty(v, p)) {
+			        result.push(v[p]);
+			  }
 			}
 			function desc(v){
 				result.push(v);
@@ -71,13 +72,14 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 				});
 			}
 			function slice(loc, val){
-				if (val instanceof Array){
+				if (dojo.isArray(val)){
 					var len=val.length, start=0, end=len, step=1;
-					loc.replace(/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/g, function($0,$1,$2,$3){start=parseInt($1||start);end=parseInt($2||end);step=parseInt($3||step);});
+					loc.replace(/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/g, function($0,$1,$2,$3){start=parseInt($1||start, 10);end=parseInt($2||end, 10);step=parseInt($3||step, 10);});
 					start = (start < 0) ? Math.max(0,start+len) : Math.min(len,start);
 					end = (end < 0) ? Math.max(0,end+len) : Math.min(len,end);
-				  	for (var i=start; i<end; i+=step)
+				  	for (var i=start; i<end; i+=step) {
 						add(val,i);
+					}
 				}
 			}
 			function repStr(str){
@@ -85,62 +87,76 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 				return i?strs[i[1]]:str;
 			}
 			function oper(val){
-				if (/^\(.*?\)$/.test(loc)) // [(expr)]
+				if (/^\(.*?\)$/.test(loc)) { // [(expr)]
 					add(val, P.eval(loc, val),rb);
+				}
 				else if (loc === "*"){
-					P.walk(val, rb && val instanceof Array ? // if it is result based, there is no point to just return the same array
-					function(i){P.walk(val[i],function(j){ add(val[i],j); })} :
+					P.walk(val, rb && dojo.isArray(val) ? // if it is result based, there is no point to just return the same array
+					function(i){P.walk(val[i],function(j){ add(val[i],j); });} :
 					function(i){ add(val,i); });
 				}
-				else if (loc === "..") 
+				else if (loc === "..") {
 					desc(val);
-				else if (/,/.test(loc)){ // [name1,name2,...]
-					for (var s=loc.split(/'?,'?/),i=0,n=s.length; i<n; i++)
-						add(val,repStr(s[i])); 
 				}
-				else if (/^\?\(.*?\)$/.test(loc)) // [?(expr)]
-					P.walk(val, function(i){ if (P.eval(loc.replace(/^\?\((.*?)\)$/,"$1"),val[i])) add(val,i); });
-				else if (/^(-?[0-9]*):(-?[0-9]*):?([0-9]*)$/.test(loc)) // [start:end:step]  python slice syntax
+				else if (/,/.test(loc)){ // [name1,name2,...]
+					for (var s=loc.split(/'?,'?/),i=0,n=s.length; i<n; i++) {
+						add(val,repStr(s[i]));
+					}
+				}
+				else if (/^\?\(.*?\)$/.test(loc)) { // [?(expr)]
+					P.walk(val, function(i){ if (P.eval(loc.replace(/^\?\((.*?)\)$/,"$1"),val[i])) { add(val,i); } });
+				}
+				else if (/^(-?[0-9]*):(-?[0-9]*):?([0-9]*)$/.test(loc)) { // [start:end:step]  python slice syntax
 					slice(loc, val);
+				}
 				else {
 					loc=repStr(loc);
-					if (rb && val instanceof Array && !/^[0-9*]+$/.test(loc)) 
-						P.walk(val, function(i){ add(val[i], loc)});
-					else 
-						add(val,loc,rb);		
+					if (rb && dojo.isArray(val) && !/^[0-9*]+$/.test(loc)) {
+						P.walk(val, function(i){ add(val[i], loc);});
+					}
+					else {
+						add(val,loc,rb);
+					}
 				}
 
 			}
 			while (locs.length){
-				var loc = locs.shift();
-				if ((val = result) === null || val===undefined) return val;
+				loc = locs.shift();
+				if ((val = result) === null || val===undefined) { return val; }
 				result = [];
 				var valPaths = paths;
 				paths = [];
-				if (rb) 
-					oper(val)
-				else
-					P.walk(val,function(i){path=valPaths[i]||path;oper(val[i])});
+				if (rb) {
+					oper(val);
+				}
+				else {
+					P.walk(val,function(i){path=valPaths[i]||path;oper(val[i]);});
+				}
 			}
 			if (P.resultType == "BOTH"){
 				paths = P.asPaths(paths);
 				var newResult = [];
-				for (var i =0;i <paths.length;i++)
+				for (var i =0;i <paths.length;i++) {
 					newResult.push({path:paths[i],value:result[i]});
+				}
 				return newResult;
 			}
 			return P.resultType == "PATH" ? P.asPaths(paths):result;
 		},
 		walk: function(val, f){
-			if (val instanceof Array){
-				for (var i=0,n=val.length; i<n; i++)
-					if (i in val)
+			if (dojo.isArray(val)){
+				for (var i=0,n=val.length; i<n; i++) {
+					if (i in val) {
 						f(i);
+					}
+				}
 			}
 			else if (typeof val === "object"){
-				for (var m in val)
-					if (val.hasOwnProperty(m))
+				for (var m in val) {
+					if (dojo.isOwnProperty(val, m)) {
 						f(m);
+					}
+				}
 			}
 		},
 		eval: function(x, _v){
@@ -149,7 +165,7 @@ dojox.jsonPath.query = function(/*Object*/obj, /*String*/expr, /*Object*/arg){
 		}
 	};
 
-	var $ = obj;
+	$ = obj;
 	if (expr && obj){
 		return P.exec(P.normalize(expr).slice(1), obj, arg.evalType == "RESULT");
 	}	
