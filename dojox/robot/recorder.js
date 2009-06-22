ca92875f57e73f460c717ea000124e363524e086
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-22 01:52:38 +0000

    Cleanup and removed browser detection
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18267 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/robot/recorder.js
+++ dojox/robot/recorder.js
@@ -39,11 +39,11 @@ var start = function(){
 	// summary:
 	//	Starts recording the user's input.
 	//
-	alert("Started recording.");
+	window.alert("Started recording.");
 	commands = [];
 	startTime = new Date();
 	prevTime = new Date();
-}
+};
 
 var addCommand = function(name, args){
 	// summary:
@@ -57,14 +57,14 @@ var addCommand = function(name, args){
 	//
 
 	// omit start/stop record
-	if(startTime == null 
-		|| name=="doh.robot.keyPress" 
-		&& args[0]==dojo.keys.ENTER 
-		&& eval("("+args[2]+")").ctrl 
-		&& eval("("+args[2]+")").alt){ return; }
+	if(startTime === null || startTime === undefined ||
+		name=="doh.robot.keyPress" &&
+		args[0]==dojo.keys.ENTER &&
+		eval("("+args[2]+")").ctrl &&
+		eval("("+args[2]+")").alt){ return; }
 	var dt = Math.max(Math.min(Math.round((new Date()).getTime() - prevTime.getTime()),MAXIMUM_DELAY),1);
 	// add in dt
-	// is usually args[1] but there are exceptions
+	// is usually args[1] but there is an exception
 	if(name == "doh.robot.mouseMove"){
 		args[2]=dt;
 	}else{
@@ -72,7 +72,7 @@ var addCommand = function(name, args){
 	}
 	commands.push({name:name,args:args});
 	prevTime = new Date();
-}
+};
 
 var _optimize = function(){
 	// make the stack more human-readable and remove any odditites
@@ -80,24 +80,24 @@ var _optimize = function(){
 
 	// INITIAL OPTIMIZATIONS
 	// remove starting ENTER press
-	if(c[0].name == "doh.robot.keyPress" 
-		&& (c[0].args[0] == dojo.keys.ENTER || c[0].args[0] == 77)){
+	if(c[0].name == "doh.robot.keyPress" &&
+		(c[0].args[0] == dojo.keys.ENTER || c[0].args[0] == 77)){
 		c.splice(0,1);
 	}
 	// remove ending CTRL + ALT keypresses in IE
 	for(var i = c.length-1; (i >= c.length-2) && (i>=0); i-- ){
-		if(c[i].name == "doh.robot.keyPress" 
-			&& c[i].args[0]==dojo.keys.ALT || c[i].args[0]==dojo.keys.CTRL){
+		if(c[i].name == "doh.robot.keyPress" &&
+			c[i].args[0]==dojo.keys.ALT || c[i].args[0]==dojo.keys.CTRL){
 			c.splice(i,1);
 		}
 	}
 	// ITERATIVE OPTIMIZATIONS
 	for(i = 0; i<c.length; i++){
 		var next, nextdt;
-		if(c[i+1] 
-			&& c[i].name=="doh.robot.mouseMove" 
-			&& c[i+1].name==c[i].name 
-			&& c[i+1].args[2]<MOUSEMOVE_MAXIMUM_DELAY){
+		if(c[i+1] &&
+			c[i].name=="doh.robot.mouseMove" &&
+			c[i+1].name==c[i].name &&
+			c[i+1].args[2]<MOUSEMOVE_MAXIMUM_DELAY){
 			// mouse movement optimization
 			// if the movement is temporally close, collapse it
 			// example: mouseMove(a,b,delay 5)+mouseMove(x,y,delay 10)+mousePress(delay 1) becomes mouseMove(x,y,delay 5)+mousePress(delay 11)
@@ -114,9 +114,9 @@ var _optimize = function(){
 
 			next = c[i+1];
 			nextdt = 0;
-			while(next 
-				&& next.name==c[i].name 
-				&& next.args[2]<MOUSEMOVE_MAXIMUM_DELAY){
+			while(next && 
+				next.name==c[i].name &&
+				next.args[2]<MOUSEMOVE_MAXIMUM_DELAY){
 				// cut out next
 				c.splice(i + 1,1);
 				// add next's delay to the total 
@@ -128,10 +128,10 @@ var _optimize = function(){
 			}
 			// make the total delay the duration
 			c[i].args[3]=nextdt;
-		}else if(c[i+1] 
-			&& c[i].name=="doh.robot.mouseWheel" 
-			&& c[i+1].name==c[i].name 
-			&& c[i+1].args[1]<MOUSEMOVE_MAXIMUM_DELAY){
+		}else if(c[i+1] &&
+			c[i].name=="doh.robot.mouseWheel" &&
+			c[i+1].name==c[i].name &&
+			c[i+1].args[1]<MOUSEMOVE_MAXIMUM_DELAY){
 			// mouse wheel optimization
 			// if the movement is temporally close, collapse it
 			// example: mouseWheel(1,delay 5)+mouseWheel(-2,delay 10) becomes mouseWheel(-1,delay 5, speed 10)
@@ -148,9 +148,9 @@ var _optimize = function(){
 
 			next = c[i+1];
 			nextdt = 0;
-			while(next 
-				&& next.name==c[i].name 
-				&& next.args[1]<MOUSEMOVE_MAXIMUM_DELAY){
+			while(next &&
+				next.name==c[i].name &&
+				next.args[1]<MOUSEMOVE_MAXIMUM_DELAY){
 				// cut out next
 				c.splice(i + 1, 1);
 				// add next's delay to the total 
@@ -161,9 +161,9 @@ var _optimize = function(){
 			}
 			// make the total delay the duration
 			c[i].args[2]=nextdt;
-		}else if(c[i + 2] 
-			&& c[i].name=="doh.robot.mouseMoveAt" 
-			&& c[i+2].name=="doh.robot.scrollIntoView"){
+		}else if(c[i + 2] &&
+			c[i].name=="doh.robot.mouseMoveAt" &&
+			c[i+2].name=="doh.robot.scrollIntoView"){
 			// swap scrollIntoView of widget and mouseMoveAt
 			// the recorder traps scrollIntoView after the mouse click registers, but in playback, it is better to go the other way
 			// expected pattern:
@@ -176,10 +176,10 @@ var _optimize = function(){
 			//	c[i+2]: mousePress
 			var temp = c.splice(i+2,1)[0];
 			c.splice(i,0,temp);
-		}else if(c[i + 1] 
-			&& c[i].name=="doh.robot.mousePress" 
-			&& c[i+1].name=="doh.robot.mouseRelease" 
-			&& c[i].args[0]==c[i+1].args[0]){
+		}else if(c[i + 1] &&
+			c[i].name=="doh.robot.mousePress" &&
+			c[i+1].name=="doh.robot.mouseRelease" &&
+			c[i].args[0]==c[i+1].args[0]){
 			// convert mousePress+mouseRelease to mouseClick
 			// expected pattern:
 			//	c[i]: mousePress
@@ -192,11 +192,11 @@ var _optimize = function(){
 			if(c[i+1] && c[i+1].name == "doh.robot.mouseClick" && c[i].args[0] == c[i+1].args[0]){
 				c.splice(i + 1, 1);
 			}
-		}else if(c[i + 1] 
-			&& c[i - 1]
-			&& c[i - 1].name=="doh.robot.mouseMoveAt"
-			&& c[i].name=="doh.robot.mousePress" 
-			&& c[i+1].name=="doh.robot.mouseMove"){
+		}else if(c[i + 1] &&
+			c[i - 1] &&
+			c[i - 1].name=="doh.robot.mouseMoveAt" &&
+			c[i].name=="doh.robot.mousePress" &&
+			c[i+1].name=="doh.robot.mouseMove"){
 			// convert mouseMoveAt+mousePress+mouseMove to mouseMoveAt+mousePress+mouseMoveAt+mouseMove
 			// this is to kick off dojo.dnd by moving the mouse 1 px
 			// expected pattern:
@@ -207,17 +207,17 @@ var _optimize = function(){
 			// insert new mouseMoveAt, 1px to the right
 			var cmd={name:"doh.robot.mouseMoveAt",args:[c[i-1].args[0], 1, 100, c[i-1].args[3]+1,c[i-1].args[4]]};
 			c.splice(i+1,0,cmd);
-		}else if(c[i + 1]
-			&& ((c[i].name=="doh.robot.keyPress"
-				&& typeof c[i].args[0] =="string") 
-				|| c[i].name=="doh.robot.typeKeys") 
-			&& c[i+1].name=="doh.robot.keyPress" 
-			&& typeof c[i+1].args[0] =="string"
-			&& c[i+1].args[1]<=KEYPRESS_MAXIMUM_DELAY 
-			&& !eval("("+c[i].args[2]+")").ctrl 
-			&& !eval("("+c[i].args[2]+")").alt 
-			&& !eval("("+c[i+1].args[2]+")").ctrl 
-			&& !eval("("+c[i+1].args[2]+")").alt){
+		}else if(c[i + 1] &&
+			((c[i].name=="doh.robot.keyPress" &&
+				typeof c[i].args[0] =="string") ||
+				c[i].name=="doh.robot.typeKeys") &&
+			c[i+1].name=="doh.robot.keyPress" &&
+			typeof c[i+1].args[0] =="string" &&
+			c[i+1].args[1]<=KEYPRESS_MAXIMUM_DELAY &&
+			!eval("("+c[i].args[2]+")").ctrl &&
+			!eval("("+c[i].args[2]+")").alt &&
+			!eval("("+c[i+1].args[2]+")").ctrl && 
+			!eval("("+c[i+1].args[2]+")").alt){
 			// convert keyPress+keyPress+... to typeKeys
 			// expected pattern:
 			// 	c[i]: keyPress(a)
@@ -233,12 +233,12 @@ var _optimize = function(){
 			c[i].args.splice(3,1);
 			next = c[i+1];
 			var typeTime = 0;
-			while(next 
-				&& next.name == "doh.robot.keyPress" 
-				&& typeof next.args[0] =="string"
-				&& next.args[1]<=KEYPRESS_MAXIMUM_DELAY 
-				&& !eval("("+next.args[2]+")").ctrl 
-				&& !eval("("+next.args[2]+")").alt){
+			while(next &&
+				next.name == "doh.robot.keyPress" &&
+				typeof next.args[0] =="string" &&
+				next.args[1]<=KEYPRESS_MAXIMUM_DELAY &&
+				!eval("("+next.args[2]+")").ctrl &&
+				!eval("("+next.args[2]+")").alt){
 				c.splice(i + 1,1);
 				c[i].args[0] += next.args[0];
 				typeTime += next.args[1];
@@ -256,7 +256,7 @@ var _optimize = function(){
 				// one keypress of a character by itself should be wrapped in quotes
 				c[i].args[0] = "'"+c[i].args[0]+"'";
 			}else{
-				if(c[i].args[0]==0){
+				if(!c[i].args[0]){
 					// toss null keypresses
 					c.splice(i,1);
 				}else{
@@ -271,16 +271,7 @@ var _optimize = function(){
 			}
 		}
 	}
-}
-
-var toggle = function(){
-	// summary:
-	//	Toggles recording the user's input.
-	//	Hotkey: CTRL- ALT-ENTER
-	//
-	if(!startTime){ start(); }
-	else{ stop(); }
-}
+};
 
 var stop = function(){
 	// summary:
@@ -318,36 +309,45 @@ var stop = function(){
 		s += "          return d;\n";
 		s += "     }\n";
 		s += "});\n";
-		var div = document.createElement('div');
+		var div = dojo.doc.createElement('div');
 		div.id="dojox.robot.recorder";
 		div.style.backgroundColor = "white";
 		div.style.position = "absolute";
-		var scroll = {y: (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0),
-		x: (window.pageXOffset || (window["dojo"]?dojo._fixIeBiDiScrollLeft(document.documentElement.scrollLeft):undefined) || document.body.scrollLeft || 0)};
+		var scroll = {y: (window.pageYOffset || dojo.doc.documentElement.scrollTop || dojo.body().scrollTop || 0),
+		x: (window.pageXOffset || (window.dojo?dojo._fixIeBiDiScrollLeft(dojo.doc.documentElement.scrollLeft):undefined) || dojo.body().scrollLeft || 0)};
 		div.style.left = scroll.x+"px";
 		div.style.top = scroll.y+"px";
-		var h1 = document.createElement('h1');
+		var h1 = dojo.doc.createElement('h1');
 		h1.innerHTML = "Your code:";
 		div.appendChild(h1);
-		var pre = document.createElement('pre');
+		var pre = dojo.doc.createElement('pre');
 		if(pre.innerText !== undefined){
 			pre.innerText = s;
 		}else{
 			pre.textContent = s;
 		}
 		div.appendChild(pre);
-		var button = document.createElement('button');
+		var button = dojo.doc.createElement('button');
 		button.innerHTML = "Close";
 		var connect = dojo.connect(button,'onmouseup',function(e){
 			dojo.stopEvent(e);
-			document.body.removeChild(div);
+			dojo.body().removeChild(div);
 			dojo.disconnect(connect);
 		});
 		div.appendChild(button);
-		document.body.appendChild(div);
+		dojo.body().appendChild(div);
 		commands = [];
 	}
-}
+};
+
+var toggle = function(){
+	// summary:
+	//	Toggles recording the user's input.
+	//	Hotkey: CTRL- ALT-ENTER
+	//
+	if(!startTime){ start(); }
+	else{ stop(); }
+};
 
 var getSelector = function(node){
 	// Selenium/Windmill recorders have a concept of a "selector."
@@ -364,7 +364,8 @@ var getSelector = function(node){
 	}else{
 		// TODO: need a generic selector, like CSS3/dojo.query, for the default return value
 		// for now, just do getElementsByTagName
-		var nodes = document.getElementsByTagName(node.nodeName);
+
+		var nodes = dojo.doc.getElementsByTagName(node.nodeName);
 		var i;
 		for(i = 0; i<nodes.length; i++){
 			if(nodes[i] == node){
@@ -372,24 +373,27 @@ var getSelector = function(node){
 			}
 		}
 		// wrap in a function to defer evaluation
-		return "function(){ return document.getElementsByTagName('" + node.nodeName+"')["+i+"]; }";
+		return "function(){ return dojo.doc.getElementsByTagName('" + node.nodeName+"')["+i+"]; }";
 	}
-}
+};
 
 var getMouseButtonObject = function(b){
 	// convert native event to doh.robot API
-	return "{left:" + (b==0)+", middle:" + (b==1)+", right:" + (b==2)+"}";
-}
+
+	// NOTE: Duplication (logic belongs in events module)
+
+	return "{left:" + (b===0)+", middle:" + (b==1)+", right:" + (b==2)+"}";
+};
 
 
 var getModifierObject = function(e){
 	// convert native event to doh.robot API
 	return "{'shift':" + (e.shiftKey)+", 'ctrl':" + (e.ctrlKey)+", 'alt':" + (e.altKey)+"}";
-}
+};
 
 // dojo.connects
 
-dojo.connect(document,"onkeydown",function(e){
+dojo.connect(dojo.doc,"onkeydown",function(e){
 	// the CTRL- ALT-ENTER hotkey to activate the recorder
 	//console.log(e.keyCode + ", " + e.ctrlKey+", " + e.altKey);
 	if((e.keyCode == dojo.keys.ENTER || e.keyCode==77) && e.ctrlKey && e.altKey){
@@ -407,25 +411,33 @@ var onmousedown = function(e){
 	var selector = getSelector(e.target);
 	var coords = dojo.coords(e.target);
 	addCommand("doh.robot.mouseMoveAt",[selector, 0, 100, e.clientX - coords.x, e.clientY-coords.y]);
-	addCommand("doh.robot.mousePress",[getMouseButtonObject(e.button-(dojo.isIE?1:0)), 0]);
+	addCommand("doh.robot.mousePress",[getMouseButtonObject(e.button), 0]);
 };
 
 var onclick = function(e){
 	// handler for mouse up
 	if(!e || lastEvent.type==e.type && lastEvent.button==e.button){ return; }
 	lastEvent={type:e.type, button:e.button};
-	var selector = getSelector(e.target);
-	var coords = dojo.coords(e.target);
-	addCommand("doh.robot.mouseClick",[getMouseButtonObject(e.button-(dojo.isIE?1:0)), 0]);
+
+	// NOTE: Unused
+
+	//var selector = getSelector(e.target)
+	//var coords = dojo.coords(e.target);
+
+	addCommand("doh.robot.mouseClick",[getMouseButtonObject(e.button), 0]);
 };
 
 var onmouseup = function(e){
 	// handler for mouse up
 	if(!e || lastEvent.type==e.type && lastEvent.button==e.button){ return; }
 	lastEvent={type:e.type, button:e.button};
-	var selector = getSelector(e.target);
-	var coords = dojo.coords(e.target);
-	addCommand("doh.robot.mouseRelease",[getMouseButtonObject(e.button-(dojo.isIE?1:0)), 0]);
+
+	// NOTE: Unused
+
+	//var selector = getSelector(e.target);
+	//var coords = dojo.coords(e.target);
+
+	addCommand("doh.robot.mouseRelease",[getMouseButtonObject(e.button), 0]);
 };
 
 var onmousemove = function(e){
@@ -452,22 +464,23 @@ var onkeypress = function(e){
 var onkeyup = function(e){
 	if(!e || lastEvent.type==e.type && (lastEvent.charCode == e.charCode && lastEvent.keyCode == e.keyCode)){ return; }
 	lastEvent={type:e.type, charCode:e.charCode, keyCode:e.keyCode};
-}
+};
 
 // trap all native elements' events
-dojo.connect(document,"onmousedown",onmousedown);
-dojo.connect(document,"onmouseup",onmouseup);
-dojo.connect(document,"onclick",onclick);
-dojo.connect(document,"onkeypress",onkeypress);
-dojo.connect(document,"onkeyup",onkeyup);
-dojo.connect(document,"onmousemove",onmousemove);
-dojo.connect(document,!dojo.isMozilla ? "onmousewheel" : 'DOMMouseScroll',onmousewheel);
+dojo.connect(dojo.doc,"onmousedown",onmousedown);
+dojo.connect(dojo.doc,"onmouseup",onmouseup);
+dojo.connect(dojo.doc,"onclick",onclick);
+dojo.connect(dojo.doc,"onkeypress",onkeypress);
+dojo.connect(dojo.doc,"onkeyup",onkeyup);
+dojo.connect(dojo.doc,"onmousemove",onmousemove);
+dojo.connect(dojo.doc,"onmousewheel",onmousewheel);
+dojo.connect(dojo.doc,'DOMMouseScroll',onmousewheel);
 
 dojo.addOnLoad(function(){
 	// get scrollIntoView for good measure
 	// catch: dijit might not be loaded (yet?) so addonload
-	if(window["dijit"] && dijit.scrollIntoView){
-		dojo.connect(dijit,"scrollIntoView",function(node){
+	if(window.dijit && window.dijit.scrollIntoView){
+		dojo.connect(window.dijit,"scrollIntoView",function(node){
 			addCommand("doh.robot.scrollIntoView",[getSelector(node)]);
 		});
 	}
@@ -478,11 +491,14 @@ dojo.connect(dojo, "connect",
 	function(/*Widget*/ widget, /*String*/ event, /*Function*/ f){
 		// kill recursion
 		// check for private variable _mine to make sure this isn't a recursive loop
+
+		// NOTE: Need mousewheel logic in events module
+
 		if(widget && (!f || !f._mine)){
 			var hitchedf = null;
 			if(event.toLowerCase() == "onmousedown"){
 				hitchedf = dojo.hitch(this,onmousedown);
-			}else if(event.toLowerCase() == (!dojo.isMozilla ? "onmousewheel" : 'dommousescroll')){
+			}else if(event.toLowerCase() == "onmousewheel" || event.toLowerCase() == 'dommousescroll'){
 				hitchedf = dojo.hitch(this,onmousewheel);
 			}else if(event.toLowerCase() == "onclick"){
 				hitchedf = dojo.hitch(this,onclick);
@@ -493,7 +509,7 @@ dojo.connect(dojo, "connect",
 			}else if(event.toLowerCase() == "onkeyup"){
 				hitchedf = dojo.hitch(this,onkeyup);
 			}
-			if(hitchedf == null){ return; }
+			if(hitchedf === null){ return; }
 			hitchedf._mine = true;
 			dojo.connect(widget,event,hitchedf);
 		}
