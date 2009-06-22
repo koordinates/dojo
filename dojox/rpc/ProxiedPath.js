2c984940d84ee91b1aafb09fc316fc95d85ac72e
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-22 03:23:23 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18275 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/rpc/ProxiedPath.js
+++ dojox/rpc/ProxiedPath.js
@@ -2,17 +2,19 @@ dojo.provide("dojox.rpc.ProxiedPath");
 dojo.require("dojox.rpc.Service");
 
 dojox.rpc.envelopeRegistry.register(
-	"PROXIED-PATH",function(str){return str == "PROXIED-PATH"},{
+	"PROXIED-PATH",function(str){return str == "PROXIED-PATH";},{
 		serialize:function(smd, method, data){
 			var i;
 			var target = dojox.rpc.getTarget(smd, method);
 			if(dojo.isArray(data)){
 				for(i = 0; i < data.length;i++){
-					target += '/' + (data[i] == null ? "" : data[i]);
+					target += '/' + ((data[i] === null || data[i] === undefined) ? "" : data[i]);
 				}
 			}else{
 				for(i in data){
-					target += '/' + i + '/' + data[i];
+					if (dojo.isOwnProperty(data, i)) {
+						target += '/' + i + '/' + data[i];
+					}
 				}
 			}
 			return {
