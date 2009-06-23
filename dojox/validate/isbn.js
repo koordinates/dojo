3e7d65d974076abade30e4db21a26aa9b82d35d4
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-06-23 02:21:15 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18338 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/validate/isbn.js
+++ dojox/validate/isbn.js
@@ -6,33 +6,31 @@ dojox.validate.isValidIsbn = function(/* String */value) {
 	// value: String
 	//		An ISBN to validate
 	// returns: Boolean
-	var len, sum = 0, weight;
+	var i, len, sum = 0, weight;
 	if(!dojo.isString(value)){
 		value = String(value);
 	}
-	value = value.replace(/[- ]/g,''); //ignore dashes and whitespaces
+	value = value.replace(/[\- ]/g,''); //ignore dashes and whitespaces
 	len = value.length;
 
 	switch(len){
 		case 10: 
 			weight = len;
 			// ISBN-10 validation algorithm
-			for(var i = 0; i < 9; i++){
-				sum += parseInt(value.charAt(i)) * weight;
+			for(i = 0; i < 9; i++){
+				sum += parseInt(value.charAt(i), 10) * weight;
 				weight--;
 			}
 			var t = value.charAt(9).toUpperCase();
-			sum += t == 'X' ? 10 : parseInt(t);
-			return sum % 11 == 0; // Boolean			
-			break;
+			sum += t == 'X' ? 10 : parseInt(t, 10);
+			return !(sum % 11); // Boolean
 		case 13:
 			weight = -1;
-			for(var i = 0; i< len; i++){
-				sum += parseInt(value.charAt(i)) * (2 + weight);
+			for(i = 0; i< len; i++){
+				sum += parseInt(value.charAt(i), 10) * (2 + weight);
 				weight *= -1;
 			}
-			return sum % 10 == 0; // Boolean		
-			break;			
+			return !(sum % 10); // Boolean
 	}
 	return false;
-}
+};
