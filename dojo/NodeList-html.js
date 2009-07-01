3a745ac4497512159edbd525f7766ff83b53fb74
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-01 03:39:20 +0000

    Comments
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18527 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/NodeList-html.js
+++ dojo/NodeList-html.js
@@ -1,6 +1,8 @@
+// Package ensures consistent order of execution across all implementations
+
 dojo.provide("dojo.NodeList-html");
 
-// Must have written or evaluated the HTML and NodeList module before
+// Must have written or evaluated the HTML and NodeList modules before
 // showing its face.
 
 dojo.require("dojo.html");
