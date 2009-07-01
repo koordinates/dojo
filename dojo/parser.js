0da6f061befd14ab31009617208862b8c4764c8b
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-01 22:06:31 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18540 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/parser.js
+++ dojo/parser.js
@@ -1,4 +1,6 @@
 dojo.provide("dojo.parser");
+dojo.require("dojo.date");
+dojo.require("dojo.date.stamp");
 dojo.require("dojo._base.json");
 dojo.require("dojo._base.query");
 dojo.require("dojo.parser-face");
