c140c4f531ab4330f40d2df4fb8dea4b2c9aa094
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-02 03:23:34 +0000

    Requirement bundle
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18593 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dijit/PopupMenuBarItem-face.js
@@ -0,0 +1,8 @@
+dojo.provide("dijit.PopupMenuBarItem-face");
+dojo.required("dijit.PopupMenuBarItem");
+
+dojo.declare("dijit.PopupMenuBarItem", [dijit.PopupMenuItem, dijit._MenuBarItemMixin], {
+	// summary:
+	//		Item in a MenuBar like "File" or "Edit", that spawns a submenu when pressed (or hovered)
+});
+
