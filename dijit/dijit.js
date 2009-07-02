edcb0935ab29ee10ed618d51c5c072f28e9fbf4b
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-02 03:07:29 +0000

    Redundant requirement
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18584 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/dijit.js
+++ dijit/dijit.js
@@ -20,7 +20,6 @@ dojo.require("dijit._base");
 
 // And some other stuff that we tend to pull in all the time anyway
 dojo.require("dojo.parser");
-dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dijit._Container");
 dojo.require("dijit.layout._LayoutWidget");
