442234e4839518b5f2af8b2edc60da4de09505d2
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-04 05:49:55 +0000

    Cleanup and requirement bundle
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18693 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dijit/_editor/plugins/ToggleDir-face.js
@@ -0,0 +1,54 @@
+dojo.provide("dijit._editor.plugins.ToggleDir-face");
+dojo.required("dijit._editor.plugins.ToggleDir");
+
+dojo.declare("dijit._editor.plugins.ToggleDir",
+	dijit._editor._Plugin,
+	{
+		//summary:
+		//		This plugin is used to toggle direction of the edited document only,
+		//		no matter what direction the whole page is.
+
+		// TODO: like TabIndent plugin, this should probably be using ToggleButton
+
+		// Override _Plugin.useDefaultCommand: processing is done in this plugin rather than by sending command to
+		// the Editor
+
+		useDefaultCommand: false,
+
+		// TODO: unclear if this is needed.
+
+		command: "toggleDir",
+
+		_initButton: function(){
+
+			// Override _Plugin._initButton() to setup handler for button click events.
+
+			this.inherited("_initButton", arguments);
+			this.connect(this.button, "onClick", this._toggleDir);		
+		},
+
+		updateState: function(){
+			// Override _Plugin.updateState() to do nothing, since we don't need to react to changes in the
+			// editor like arrow keys etc.
+		},
+
+		_toggleDir: function(){
+
+			// summary:
+			//		Handler for button click events, to switch the text direction of the editor
+
+			var editDoc = this.editor.editorObject.contentWindow.document.documentElement;
+			var isLtr = dojo.getComputedStyle(editDoc).direction == "ltr";
+			editDoc.dir/*html node*/ = isLtr ? "rtl" : "ltr";
+		}
+	}
+);
+
+// Register this plugin.
+
+dojo.subscribe(dijit._scopeName + ".Editor.getPlugin",null,function(o){
+	if(o.plugin){ return; }
+	if (o.args.name == "toggleDir"){
+		o.plugin = new dijit._editor.plugins.ToggleDir({command: o.args.name});
+	}
+});
