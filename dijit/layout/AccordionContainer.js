9edd7afa6bcf39bd59100d8a25505e7a84dcd9f3
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-19 01:35:44 +0000

    Fixed requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18943 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/layout/AccordionContainer.js
+++ dijit/layout/AccordionContainer.js
@@ -1,6 +1,8 @@
 dojo.provide("dijit.layout.AccordionContainer");
 dojo.require("dojo.fx");
+dojo.require("dijit._base");
 dojo.require("dijit._Container");
+dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dijit.layout.StackContainer");
 dojo.require("dijit.layout.ContentPane");
