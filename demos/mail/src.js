274874defa7a69b78aca4025b9536a41d25ffc6f
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-19 22:00:05 +0000

    !strict
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18983 560b804f-0ae3-0310-86f3-f6aa0a117693

--- demos/mail/src.js
+++ demos/mail/src.js
@@ -1,8 +1,9 @@
 dojo.require("dojo.parser");
+dojo.require("dojo.data.ItemFileReadStore");
 dojo.require("dojo.data.ItemFileWriteStore");
+dojo.require("dojo.date");
 dojo.require("dojo.date.locale");
 dojo.require("dojo.date.stamp");
-
 dojo.require("dijit.dijit");
 dojo.require("dijit.Declaration");
 dojo.require("dijit.form.Button");
@@ -17,7 +18,6 @@ dojo.require("dijit.Editor");
 dojo.require("dijit._editor.plugins.LinkDialog");
 dojo.require("dijit._editor.plugins.FontChoice");
 dojo.require("dijit.ProgressBar");
-
 dojo.require("dijit.form.ComboBox");
 dojo.require("dijit.form.CheckBox");
 dojo.require("dijit.form.Textarea");
@@ -26,12 +26,11 @@ dojo.require("dijit.form.ComboBox");
 dojo.require("dijit.form.FilteringSelect");
 dojo.require("dijit.form.Textarea");
 dojo.require("dijit.form.Form")
-
 dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("dijit.layout.AccordionPane");
 dojo.require("dijit.layout.AccordionContainer");
 dojo.require("dijit.layout.TabContainer");
-dojo.require("dijit.layout.ContentPane");
-
 dojo.require("dojox.grid.DataGrid");
 dojo.require("dojox.widget.FisheyeLite");
 dojo.require("dojox.analytics.Urchin");
