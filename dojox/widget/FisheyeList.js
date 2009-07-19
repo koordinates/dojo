a9b2b720add4dd508f4b95b5248c3a4c553817c8
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-19 02:20:21 +0000

    Fixed requirements
    !strict
    
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@18946 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/FisheyeList.js
+++ dojox/widget/FisheyeList.js
@@ -1,5 +1,5 @@
 dojo.provide("dojox.widget.FisheyeList");
-dojo.require("dijit.dijit");
+dojo.require("dijit._base");
 dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dijit._Container");
