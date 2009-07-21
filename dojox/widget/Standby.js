7a58d50c00cb9210e1c93f04b4df5e77725ab0d9
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-21 05:31:14 +0000

    Mistake in previous
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19048 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/Standby.js
+++ dojox/widget/Standby.js
@@ -1,5 +1,6 @@
 dojo.provide("dojox.widget.Standby");
 dojo.experimental("dojox.widget.Standby");
+dojo.require("dijit._base");
 dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dojo.fx");
