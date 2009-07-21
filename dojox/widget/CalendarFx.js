9c587a5f01dc548af62fc0dfa6f691fb32bf54a2
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-21 04:00:47 +0000

    Cleanup
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19036 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/CalendarFx.js
+++ dojox/widget/CalendarFx.js
@@ -1,4 +1,5 @@
 dojo.provide("dojox.widget.CalendarFx");
+dojo.required("dojox.widget.Calendar");
 dojo.require("dojox.widget.FisheyeLite");
 
 dojo.declare("dojox.widget._FisheyeFX",null, {
@@ -8,11 +9,11 @@ dojo.declare("dojox.widget._FisheyeFX",null, {
 		//Use the query and base node passed from the calendar view mixin
 		//to select the nodes to attach the event to.
 		dojo.query(query, fromNode).forEach(function(node){
-			new dojox.widget.FisheyeLite({
+			(new dojox.widget.FisheyeLite({
 				properties: {
 					fontSize: 1.1
 				}
-			}, node);
+			}, node));
 		});
 	}
 });
