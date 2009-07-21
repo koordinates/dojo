03398f25b8462424c4486688f239363690c76850
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-21 08:05:34 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19063 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/MenuSeparator.js
+++ dijit/MenuSeparator.js
@@ -1,8 +1,6 @@
 dojo.provide("dijit.MenuSeparator");
-
-dojo.require("dijit._Widget");
-dojo.require("dijit._Templated");
-dojo.require("dijit._Contained");
+dojo.required("dijit._Templated");
+dojo.required("dijit._Contained");
 
 dojo.declare("dijit.MenuSeparator",
 		[dijit._Widget, dijit._Templated, dijit._Contained],
