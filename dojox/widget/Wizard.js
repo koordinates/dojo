562c56bb4bac78b5b64be075a2265392577d65fe
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-21 04:28:12 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19041 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/Wizard.js
+++ dojox/widget/Wizard.js
@@ -1,6 +1,8 @@
 dojo.provide("dojox.widget.Wizard");
 dojo.require("dojo.i18n");
 dojo.require("dijit._base");
+dojo.require("dijit._Widget");
+dojo.require("dijit.layout.AccordionPane");
 dojo.require("dijit.layout.StackContainer");
 dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.form.Button");
