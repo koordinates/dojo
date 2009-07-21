83791a9c42384cadc6f69bd3f7909460605926bc
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-21 08:06:48 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19064 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/PlaceholderMenuItem.js
+++ dojox/widget/PlaceholderMenuItem.js
@@ -1,6 +1,3 @@
 dojo.provide("dojox.widget.PlaceholderMenuItem");
-dojo.require("dijit._base");
-dojo.require("dijit._Widget");
-dojo.require("dijit._Templated");
-dojo.require("dijit.Menu");
+dojo.required("dijit.Menu");
 dojo.require("dojox.widget.PlaceholderMenuItem-face");
