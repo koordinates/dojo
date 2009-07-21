38310ecce08324aeb785a92926e35eb6f6a3fcb8
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-21 06:24:09 +0000

    Mistake in previous
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19051 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/Iterator.js
+++ dojox/widget/Iterator.js
@@ -1,4 +1,4 @@
 dojo.provide("dojox.widget.Iterator");
 dojo.experimental("dojox.widget.Iterator"); // level: prototype, designed for dijit.chat.demo
 dojo.require("dijit.Declaration");
-dojo.require("dojox.widget.Iterator");
+dojo.require("dojox.widget.Iterator-face");
