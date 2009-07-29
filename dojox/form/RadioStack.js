d67de3c1fcaa141d991eca1b193ab2facb43fefe
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-29 19:48:45 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19136 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/form/RadioStack.js
+++ dojox/form/RadioStack.js
@@ -1,7 +1,7 @@
 dojo.provide("dojox.form.RadioStack");
 
-dojo.require("dojox.form.CheckedMultiSelect");
-dojo.require("dojox.form._SelectStackMixin");
+dojo.required("dojox.form.CheckedMultiSelect");
+dojo.required("dojox.form._SelectStackMixin");
 
 dojo.declare("dojox.form.RadioStack",
 	[ dojox.form.CheckedMultiSelect, dojox.form._SelectStackMixin ], {
