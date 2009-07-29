44eac8e70ed1d52f8ed4cbe06826822205e074a0
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-29 22:09:52 +0000

    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19164 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/widget/DocTester.js
+++ dojox/widget/DocTester.js
@@ -1,8 +1,13 @@
 dojo.provide("dojox.widget.DocTester");
+
+// Next one not required, but eliminates synchronous request
+
+dojo.require("dojo.i18n");
 dojo.require("dojo.string");
 dojo.require("dijit._base");
 dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
+dojo.require("dijit.form.button");
 dojo.require("dojox.form.BusyButton");
 dojo.require("dojox.testing.DocTest");
 dojo.require("dojox.widget.DocTester-face");
