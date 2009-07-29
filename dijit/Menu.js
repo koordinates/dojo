9ede1d75797f98e0bcc8122e52b81c92b27011ea
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-29 20:01:02 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19139 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/Menu.js
+++ dijit/Menu.js
@@ -1,5 +1,6 @@
 dojo.provide("dijit.Menu");
 dojo.require("dijit._KeyNavContainer");
+dojo.require("dijit._Contained");
 dojo.require("dijit.Menu-face");
 dojo.require("dijit.MenuItem");
 dojo.require("dijit.PopupMenuItem");
