537d842b768dfa7182a4fe952eeb909d60050d36
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-30 21:17:27 +0000

    Added warning to addClass for missing node
    !strict
    
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19310 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/_base/html.js
+++ dojo/_base/html.js
@@ -1983,6 +1983,12 @@ if (dojo.isHostObjectProperty(dojo.doc, 'all')) {
 		if (typeof node == 'string') {
 			node = byId(node);
 		}
+
+		if (!node) {
+			console.warn('Cannot add class to missing node: ' + node);
+			return;
+		}
+
 		if (!node.className) {
 			node.className = className;
 		}
