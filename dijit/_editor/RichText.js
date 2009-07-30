da49be5d239ec23d617bef43acc00840c6e62fa9
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-30 21:27:41 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19316 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/_editor/RichText.js
+++ dijit/_editor/RichText.js
@@ -3,5 +3,6 @@ dojo.require("dijit._Widget");
 dojo.require("dijit._editor.selection");
 dojo.require("dijit._editor.range");
 dojo.require("dijit._editor.html");
+dojo.require("dijit._editor.RichText-face");
 dojo.require("dojo.i18n");
 dojo.requireLocalization("dijit.form", "Textarea");
