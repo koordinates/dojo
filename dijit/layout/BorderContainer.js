de3ef66e37481bb3d72401a007a5f06e2e6acd52
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-07-30 09:36:41 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19281 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/layout/BorderContainer.js
+++ dijit/layout/BorderContainer.js
@@ -1,4 +1,5 @@
 dojo.provide("dijit.layout.BorderContainer");
+dojo.required("dijit._Templated");
 dojo.require("dijit.layout._LayoutWidget");
 dojo.require("dojo.cookie");
 dojo.require("dijit.layout.BorderContainer-face");
