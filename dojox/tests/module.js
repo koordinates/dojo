38bd1c11f36cd164d1d68914c38c79c4269ad9c2
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-01 21:00:20 +0000

    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19432 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/tests/module.js
+++ dojox/tests/module.js
@@ -1,7 +1,9 @@
 dojo.provide("dojox.tests.module");
 
 try{
+	dojo.require("dojo.date");
 	dojo.require("dojox.collections.tests.collections");
+	dojo.require("dojox.color.tests.color");
 	dojo.require("dojox.date.tests.module");
 	dojo.require("dojox.gfx.tests.module");
 }catch(e){
