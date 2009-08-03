2411bc540220165adb5f803296dd3e83b59cc161
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-03 05:08:11 +0000

    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19489 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/tests/module.js
+++ dojox/tests/module.js
@@ -1,14 +1,14 @@
 dojo.provide("dojox.tests.module");
 
 try{
-	dojo.require("dojo.date");
-	//dojo.require("dojox.atom.tests.io.module");
+
+	dojo.require("dojox.atom.tests.io.module");
 	dojo.require("dojox.collections.tests.collections");
 	dojo.require("dojox.color.tests.color");
-	//dojo.require("dojox.cometd.tests.all");
-	//dojo.require("dojox.data.tests.module");
-	//dojo.require("dojox.date.tests.module");
-	//dojo.require("dojox.dtl.tests.module");
+	dojo.require("dojox.cometd.tests.all");
+	dojo.require("dojox.data.tests.module");
+	dojo.require("dojox.date.tests.module");
+	dojo.require("dojox.dtl.tests.module");
 	dojo.require("dojox.encoding.tests.encoding");
 	dojo.require("dojox.fx.tests._base");
 	dojo.require("dojox.gfx.tests.module");
