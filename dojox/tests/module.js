ffdb61853e73d3292e3eced2c5a72f5679ed5c44
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-03 04:14:12 +0000

    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19486 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/tests/module.js
+++ dojox/tests/module.js
@@ -2,13 +2,13 @@ dojo.provide("dojox.tests.module");
 
 try{
 	dojo.require("dojo.date");
-	dojo.require("dojox.atom.tests.io.module");
+	//dojo.require("dojox.atom.tests.io.module");
 	dojo.require("dojox.collections.tests.collections");
 	dojo.require("dojox.color.tests.color");
-	dojo.require("dojox.cometd.tests.all");
-	dojo.require("dojox.data.tests.module");
-	dojo.require("dojox.date.tests.module");
-	dojo.require("dojox.dtl.tests.module");
+	//dojo.require("dojox.cometd.tests.all");
+	//dojo.require("dojox.data.tests.module");
+	//dojo.require("dojox.date.tests.module");
+	//dojo.require("dojox.dtl.tests.module");
 	dojo.require("dojox.encoding.tests.encoding");
 	dojo.require("dojox.fx.tests._base");
 	dojo.require("dojox.gfx.tests.module");
@@ -25,6 +25,7 @@ try{
 	dojo.require("dojox.uuid.tests.uuid");
 	dojo.require("dojox.wire.tests.module");
 	dojo.require("dojox.xml.tests.module");
+
 }catch(e){
 	doh.debug(e);
 }
