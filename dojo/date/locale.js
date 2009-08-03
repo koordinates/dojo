f80fe0f515df413f4855d892b494cf50dbc6a08a
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-03 07:49:49 +0000

    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19498 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/date/locale.js
+++ dojo/date/locale.js
@@ -3,4 +3,5 @@ dojo.required("dojo.date");
 dojo.require("dojo.cldr.supplemental");
 dojo.require("dojo.regexp");
 dojo.require("dojo.string");
+dojo.require("dojo.i18n");
 dojo.require("dojo.date.locale-face");
