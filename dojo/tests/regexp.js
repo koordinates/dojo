46434c102aa5c2da5aa750abe52e92065b378de5
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-08 23:43:50 +0000

    Joined two test files.  Didn't need splits in first place.
    !strict
    
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19568 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/tests/regexp.js
+++ dojo/tests/regexp.js
@@ -1,3 +1,11 @@
 dojo.provide("tests.regexp");
 dojo.require("dojo.regexp");
-dojo.require("tests.regexp-face");
+
+tests.register("tests.regexp", 
+	[
+		function test_regexp_escape(t){
+			t.assertTrue(new RegExp(dojo.regexp.escapeString("\f\b\n\t\r+.$?*|{}()[]\\/^")).test("TEST\f\b\n\t\r+.$?*|{}()[]\\/^TEST"));
+			t.assertTrue(new RegExp(dojo.regexp.escapeString("\f\b\n\t\r+.$?*|{}()[]\\/^", ".")).test("TEST\f\b\n\t\r+X$?*|{}()[]\\/^TEST"));
+		}
+	]
+);
