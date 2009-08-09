5aab5fd406c0492f58bf46b39f303843488f5c8e
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-09 22:08:35 +0000

    Removed redundant requirement
    !strict
    
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19621 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/tests/data/ItemFileWriteStore.js
+++ dojo/tests/data/ItemFileWriteStore.js
@@ -1,7 +1,6 @@
 dojo.provide("tests.data.ItemFileWriteStore");
-dojo.require("tests.data.readOnlyItemFileTestTemplates");
 
-dojo.require("dojo.data.ItemFileReadStore");
+dojo.require("tests.data.readOnlyItemFileTestTemplates");
 dojo.require("dojo.data.ItemFileWriteStore");
 dojo.require("dojo.data.api.Read");
 dojo.require("dojo.data.api.Identity");
