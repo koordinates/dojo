1756b85de873e781ca63456cf8ff56b910278e13
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-09 06:02:04 +0000

    Notes
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19602 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojo/tests.js
+++ dojo/tests.js
@@ -1,6 +1,8 @@
 //This file is the command-line entry point for running the tests in
 //Rhino and Spidermonkey.
 
+// NOTE: Move this to tests/console.js?
+
 /*=====
 dojo.tests = {
 	// summary: DOH Test files for Dojo unit testing.
@@ -8,5 +10,8 @@ dojo.tests = {
 =====*/
 
 load("dojo.js");
+
+// NOTE: Where is this file?
+
 load("tests/runner.js");
 tests.run();
