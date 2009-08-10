64de2f9def8ae5848ece81f2075e180f78b86c7e
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-10 09:50:02 +0000

    Requirements and joins.
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19668 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/form/VerticalRuleLabels.js
+++ dijit/form/VerticalRuleLabels.js
@@ -1,3 +1,26 @@
 dojo.provide("dijit.form.VerticalRuleLabels");
+
 dojo.require("dijit.form.HorizontalRuleLabels");
-dojo.require("dijit.form.VerticalRuleLabels-face");
+
+dojo.required("dijit.form.HorizontalRuleLabels", function() {
+
+dojo.declare("dijit.form.VerticalRuleLabels", dijit.form.HorizontalRuleLabels,
+{
+	// summary:
+	//		Labels for the `dijit.form.VerticalSlider`
+
+	templateString: '<div class="dijitRuleContainer dijitRuleContainerV dijitRuleLabelsContainer dijitRuleLabelsContainerV"></div>',
+
+	_positionPrefix: '<div class="dijitRuleLabelContainer dijitRuleLabelContainerV" style="top:',
+	_labelPrefix: '"><span class="dijitRuleLabel dijitRuleLabelV">',
+
+	_calcPosition: function(pos){
+		// Overrides HorizontalRuleLabel._calcPosition()
+		return 100-pos;
+	},
+
+	// TODO: remove this.   Apparently it's not used.
+	_isHorizontal: false
+});
+
+});
