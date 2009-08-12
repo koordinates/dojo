3a35b1f86166a4d9855960452853275337eedf8d
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-12 10:25:48 +0000

    Requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19778 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/storage/_common.js
+++ dojox/storage/_common.js
@@ -1,4 +1,12 @@
 dojo.provide("dojox.storage._common");
+
 dojo.require("dojox.storage.Provider");
 dojo.require("dojox.storage.manager");
-dojo.require("dojox.storage._common-face");
+
+dojo.required(["dojox.storage.Provider", "dojox.storage.manager"], function() {
+
+	// now that we are loaded and registered tell the storage manager to
+	// initialize itself
+
+	dojox.storage.manager.initialize();
+});
