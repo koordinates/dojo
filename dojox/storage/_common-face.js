71d764df2bf5b59f2f4fbb698911db3658f86d03
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-12 11:07:33 +0000

    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19779 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dojox/storage/_common-face.js
+++
@@ -1,6 +0,0 @@
-dojo.provide("dojox.storage._common-face");
-dojo.required("dojox.storage._common");
-
-// now that we are loaded and registered tell the storage manager to
-// initialize itself
-dojox.storage.manager.initialize();
