2a3b79c2972f1eb25a54112e62e250d8208f7d43
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-12 09:25:44 +0000

    Removed redundant requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19764 560b804f-0ae3-0310-86f3-f6aa0a117693

--- dijit/Dialog.js
+++ dijit/Dialog.js
@@ -2,8 +2,6 @@ dojo.provide("dijit.Dialog");
 dojo.require("dojo.i18n");
 dojo.require("dojo.dnd.TimedMoveable");
 dojo.require("dojo.fx");
-dojo.require("dijit._base");
-dojo.require("dijit._Widget");
 dojo.require("dijit._Templated");
 dojo.require("dijit._DialogMixin");
 dojo.require("dijit.form._FormMixin");
