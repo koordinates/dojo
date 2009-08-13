d07e1f5e800c92917939a723f92248a93c335c7e
davidmark <davidmark@560b804f-0ae3-0310-86f3-f6aa0a117693>
2009-08-13 06:46:39 +0000

    Removed redundant requirements
    git-svn-id: http://svn.dojotoolkit.org/src/branches/featureDetection@19829 560b804f-0ae3-0310-86f3-f6aa0a117693

--- demos/form/src.js
+++ demos/form/src.js
@@ -1,29 +1,20 @@
-dojo.require("dojo.parser");
-dojo.require("dojo.i18n");
 dojo.require("dojo.data.ItemFileReadStore");
-
-dojo.require("dojo.dnd.common");
-dojo.require("dojo.dnd.Mover");
 dojo.require("dojo.dnd.Moveable");
-dojo.require("dijit._base");
-dojo.require("dijit._Widget");
 dojo.require("dijit.form.Form");
 dojo.require("dijit.form.ValidationTextBox");
-dojo.require("dijit.form.ComboBox");
 dojo.require("dijit.form.FilteringSelect");
 dojo.require("dijit.form.CheckBox");
 dojo.require("dijit.form.DateTextBox");
 dojo.require("dijit.form.CurrencyTextBox");
 dojo.require("dijit.form.NumberSpinner");
-
 dojo.require("dijit.form.HorizontalSlider");
-dojo.require("dijit.form.HorizontalRule");
 dojo.require("dijit.form.HorizontalRuleLabels");
-
 dojo.require("dijit.form.Textarea");
 dojo.require("dijit.Editor");
-dojo.require("dijit._editor.plugins.FontChoice");
+//dojo.require("dijit._editor.plugins.FontChoice");
 dojo.require("dijit.form.Button");
 
 // make dojo.toJson() print dates correctly (this feels a bit dirty)
-Date.prototype.json = function(){ return dojo.date.stamp.toISOString(this, {selector: 'date'});};
+Date.prototype.json = function() {
+	return dojo.date.stamp.toISOString(this, {selector: 'date'});
+};
