afae1b4f13a9fe715aecf3c6e98a1d92556bc7f7
jburke <jburke@560b804f-0ae3-0310-86f3-f6aa0a117693>
2010-03-11 18:04:25 +0000

    Setting 0.4 version
    git-svn-id: http://svn.dojotoolkit.org/src/branches/0.4@21543 560b804f-0ae3-0310-86f3-f6aa0a117693

--- src/bootstrap1.js
+++ src/bootstrap1.js
@@ -87,7 +87,7 @@ dojo.locale  = djConfig.locale;
 //TODOC:  HOW TO DOC THIS?
 dojo.version = {
 	// summary: version number of this instance of dojo.
-	major: 0, minor: 4, patch: 3, flag: "+",
+	major: 0, minor: 4, patch: 4, flag: "",
 	revision: Number("$Rev$".match(/[0-9]+/)[0]),
 	toString: function(){
 		with(dojo.version){
