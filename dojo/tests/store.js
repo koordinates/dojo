0162f4b289fbb125b8599e8a93f426fd7dcdc45e
dante <dante@560b804f-0ae3-0310-86f3-f6aa0a117693>
2011-02-21 14:32:52 +0000

    Using r23848 to create a tag for the 1.6.0rc1 release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.6.0rc1@23850 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dojo/tests/store.js
@@ -0,0 +1,4 @@
+define("dojo/tests/store", ["dojo", "dojo/tests/store/Memory", "dojo/tests/store/JsonRest", "dojo/tests/store/DataStore", "dojo/tests/store/Observable", "dojo/tests/store/Cache"], function(dojo) {
+});
+
+
