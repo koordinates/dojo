2b01c8bce83d1e6ed55eacf93d8fd0322f1e4485
dante <dante@560b804f-0ae3-0310-86f3-f6aa0a117693>
2011-07-04 12:56:33 +0000

    Using r25707 to create a tag for the 1.7.0b4 release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.7.0b4@25711 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dojox/mobile/compat.js
@@ -0,0 +1,7 @@
+define(["dojo/_base/kernel", "dojo/_base/sniff"], function(dojo, has){
+	dojo.getObject("mobile.compat", true, dojox);
+	if(!dojo.isWebKit){
+		require(["dojox/mobile/_compat"]);
+	}
+	return dojox.mobile.compat;
+});
