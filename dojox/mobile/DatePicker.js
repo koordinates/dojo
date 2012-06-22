c33a73c4003c6e7274750559ee560fee13ca9ac1
csnover <csnover@560b804f-0ae3-0310-86f3-f6aa0a117693>
2012-06-22 22:23:08 +0000

    Using r28990 to create a tag for the 1.8.0b1 release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.8.0b1@28994 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dojox/mobile/DatePicker.js
@@ -0,0 +1,22 @@
+define([
+	"dojo/_base/lang",
+	"./_PickerChooser!DatePicker"
+], function(lang, DatePicker){
+
+	// module:
+	//		dojox/mobile/DatePicker
+	// summary:
+	//		A wrapper widget around SpinWheelDatePicker or ValuePickerDatePicker.
+	//		Returns ValuePickerDatePicker when the current theme is "android".
+	//		Returns SpinWheelDatePicker otherwise.
+
+	/*=====
+    return {
+		// summary:
+		//		A wrapper widget around SpinWheelDatePicker or ValuePickerDatePicker.
+		//		Returns ValuePickerDatePicker when the current theme is "android".
+		//		Returns SpinWheelDatePicker otherwise.
+    };
+    =====*/
+	return lang.setObject("dojox.mobile.DatePicker", DatePicker);
+});
---
+++ dojox/widget/CalendarViews.js
@@ -0,0 +1,6 @@
+define([
+	"dojo/_base/kernel",
+	"dojox/widget/Calendar"
+], function(kernel, Calendar){
+	kernel.experimental("dojox/widget/CalendarViews");
+});
