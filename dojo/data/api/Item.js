fd7e7b0cb74ea4e3f6ffb30f6b1f8dde180a1d94
csnover <csnover@560b804f-0ae3-0310-86f3-f6aa0a117693>
2012-06-22 22:23:07 +0000

    Using r28990 to create a tag for the 1.8.0b1 release.
    git-svn-id: http://svn.dojotoolkit.org/src/tags/release-1.8.0b1@28992 560b804f-0ae3-0310-86f3-f6aa0a117693

---
+++ dojo/data/api/Item.js
@@ -0,0 +1,12 @@
+define(["../../_base/declare"], function(declare){
+
+	// module:
+	//		dojo/data/api/Item
+
+	return declare(null, {
+		// summary:
+		//		An item in a dojo/data store
+		//		Class for documentation purposes only.
+	});
+
+});
